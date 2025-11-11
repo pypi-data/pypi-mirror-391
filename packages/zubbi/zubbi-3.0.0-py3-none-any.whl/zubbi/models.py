d33f798dff614e05372ce6df8719e81b9d53c546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 15:04:37 +0000

    Add package zubbi/3.0.0/zubbi-3.0.0-py3-none-any.whl

---
+++ packages/zubbi/zubbi-3.0.0-py3-none-any.whl/zubbi/models.py
@@ -0,0 +1,395 @@
+# Copyright 2018 BMW Car IT GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import collections
+import logging
+import os
+import ssl
+from urllib.parse import quote_plus
+
+import markupsafe
+from elasticsearch.dsl import (
+    Boolean,
+    Completion,
+    Date,
+    Document,
+    Integer,
+    Keyword,
+    Q,
+    Search,
+    Text,
+    connections,
+)
+from elasticsearch.exceptions import ApiError
+from elasticsearch.helpers import bulk
+
+LOGGER = logging.getLogger(__name__)
+
+ZUBBI_INDEX_PREFIX_ENV = "ZUBBI_INDEX_PREFIX"
+
+DEFAULT_SUGGEST_SIZE = 5
+
+DEFAULT_TLS_CONFIG = {
+    "enabled": False,
+    "check_hostname": True,
+    "verify_mode": "CERT_REQUIRED",
+}
+
+# We want to boost following fields
+SEARCH_BOOST_FIELDS = {
+    "role_name": "role_name^1.5",
+    "job_name": "job_name^1.5",
+    "description": "description^1.2",
+}
+
+RESERVED_CHARACTERS = [
+    "+",
+    "-",
+    "=",
+    "!",
+    "(",
+    ")",
+    "{",
+    "}",
+    "[",
+    "]",
+    "^",
+    '"',
+    "~",
+    "*",
+    "?",
+    ":",
+    "\\",
+    "/",
+    # Elasticsearch also uses the "&&" and "||" as reserved characters,
+    # but a translation table can only contain single characters. The
+    # simplest thing we can do here is to escape them as single
+    # characters. This should be fine since we haven't used them so far.
+    # In case this becomes a problem, we might want to escape them in
+    # a second replacement step without using a translation table.
+    "&",
+    "|",
+]
+
+TRANSLATION_TABLE = str.maketrans(
+    {
+        # Build a translation table from the list of reserved characters.
+        # This will prefix each character in the list with a '\'.
+        **{c: f"\\{c}" for c in RESERVED_CHARACTERS},
+        # ">" and "<" are also reserved characters, but according to the
+        # Elastichsearch documentation they can't be escaped at all. So
+        # we simply remove them. This shouldn't harm as range queries
+        # don't make any sense for us at the moment.
+        ">": "",
+        "<": "",
+    }
+)
+
+
+class ZubbiDoc(Document):
+    """All documents which are scraped by Zubbi and stored in Elasticsearch."""
+
+    scrape_time = Date(default_timezone="UTC")
+
+    @classmethod
+    def outdated_query(cls, timestamp, extra_filter=None):
+        extra_filter = extra_filter or []
+
+        return (
+            cls.search()
+            .query("bool", filter=extra_filter)
+            .filter("range", scrape_time={"lt": timestamp})
+        )
+
+    # Helper method as mentioned in
+    # https://github.com/elastic/elasticsearch-dsl-py/issues/403
+    @classmethod
+    def bulk_save(cls, docs):
+        try:
+            objects = (d.to_dict(include_meta=True) for d in docs)
+            client = connections.get_connection()
+            return bulk(client, objects)
+        except ApiError:
+            LOGGER.exception("Writing data to Elasticsearch failed")
+
+    @classmethod
+    def prefix_name(cls, name):
+        prefix = os.environ.get(ZUBBI_INDEX_PREFIX_ENV)
+        if not prefix:
+            return name
+        # If the user set a '-' at the end of the prefix, strip it
+        # to not mess up the index names.
+        prefix = prefix.rstrip("-")
+        return "{}-{}".format(prefix, name)
+
+
+class ZuulTenant(ZubbiDoc):
+    tenant_name = Text()
+
+    class Index:
+        name = ZubbiDoc.prefix_name("zuul-tenants")
+
+
+class GitRepo(ZubbiDoc):
+    # As the repo name contains a slash we must at least provide a Keyword()
+    # field to allow exact matches e.g. via term query.
+    repo_name = Text(fields={"keyword": Keyword()})
+    provider = Text()
+
+    class Index:
+        name = ZubbiDoc.prefix_name("git-repos")
+
+
+class Block(ZubbiDoc):
+    name_suggest = Completion(
+        contexts=[
+            {"name": "private", "type": "category", "path": "private"},
+            {"name": "tenants", "type": "category", "path": "tenants"},
+        ]
+    )
+    repo = Text(analyzer="whitespace")
+    tenants = Text(multi=True, analyzer="whitespace")
+    # NOTE (fschmidt): Elasticsearch does not support context suggestion for
+    # Boplean fields. As we are using the private flag to filter the auto-
+    # completion results, this must be Text.
+    private = Text()
+    url = Text()
+    description = Text(analyzer="whitespace")
+    description_html = Text()
+    platforms = Text(multi=True, analyzer="whitespace")
+    last_updated = Date(default_timezone="UTC")
+    reusable = Boolean()
+
+    @staticmethod
+    def suggest_field():
+        return "name_suggest"
+
+    @property
+    def has_html_description(self):
+        return bool(self.description_html)
+
+    @property
+    def has_html_changelog(self):
+        return bool(self.changelog_html)
+
+    @property
+    def description_rendered(self):
+        return self._renderable_field(self.description_html, self.description)
+
+    def _renderable_field(self, html, raw):
+        if html:
+            return markupsafe.Markup(html)
+        elif raw:
+            return markupsafe.Markup("<pre>{}</pre>".format(markupsafe.escape(raw)))
+
+
+class AnsibleRole(Block):
+    # NOTE (fschmidt): We have to store the name as 'role_name' in the result,
+    # so we can use it for aggregation in Elasticsearch later on.
+    role_name = Text(analyzer="whitespace")
+    changelog = Text(analyzer="whitespace")
+    changelog_html = Text()
+
+    class Index:
+        name = ZubbiDoc.prefix_name("ansible-roles")
+
+    @property
+    def name(self):
+        return self.role_name
+
+    @property
+    def changelog_rendered(self):
+        return self._renderable_field(self.changelog_html, self.changelog)
+
+    @classmethod
+    def bulk_save(cls, docs):
+        for doc in docs:
+            doc.name_suggest = doc.role_name
+        return super().bulk_save(docs)
+
+
+class ZuulJob(Block):
+    # NOTE (fschmidt): We have to store the name as 'job_name' in the result,
+    # so we can use it for aggregation in Elasticsearch later on.
+    job_name = Text(analyzer="whitespace")
+    parent = Text(analyzer="whitespace")
+    line_start = Integer()
+    line_end = Integer()
+
+    class Index:
+        name = ZubbiDoc.prefix_name("zuul-jobs")
+
+    @property
+    def name(self):
+        return self.job_name
+
+    def save(self, **kwargs):
+        # Make sure that the name suggestion contains the name
+        self.name_suggest = self.job_name
+        return super().save(**kwargs)
+
+
+class BlockSearch(Search):
+    def __init__(self, index=None, doc_type=None, block_class=None, **kwargs):
+        super().__init__(
+            index=self._select_index(index, block_class),
+            doc_type=self._select_doc_type(doc_type, block_class),
+            **kwargs,
+        )
+
+    @staticmethod
+    def _select_index(index, block_class):
+        if index:
+            return index
+        if block_class is not None:
+            return block_class._default_index()
+        return [AnsibleRole._default_index(), ZuulJob._default_index()]
+
+    @staticmethod
+    def _select_doc_type(doc_type, block_class):
+        if doc_type:
+            return doc_type
+        if block_class is not None:
+            return [block_class]
+        return [AnsibleRole, ZuulJob]
+
+    def search_query(self, query, fields_set, exact=False, extra_filter=None):
+        # Elasticsearch cannot work with a set, only with list
+        fields = [SEARCH_BOOST_FIELDS.get(field, field) for field in fields_set]
+        if exact:
+            search_query = Q("multi_match", query=query, fields=fields)
+        else:
+            query_string = "*{}*".format(query.translate(TRANSLATION_TABLE))
+            search_query = Q("query_string", query=query_string, fields=fields)
+
+        extra_filter = extra_filter or []
+
+        boost_reusable_query = Q("term", reusable={"value": True, "boost": 2})
+
+        return self.query(
+            "bool", filter=extra_filter, must=search_query, should=boost_reusable_query
+        )
+
+    def detail_query(self, block_name, repo, extra_filter=None):
+        extra_filter = extra_filter or []
+        query_string = block_name.translate(TRANSLATION_TABLE)
+        detail_query = [
+            Q("query_string", query=query_string, fields=["job_name", "role_name"]),
+            Q("match", repo=repo),
+        ]
+        return self.query("bool", filter=extra_filter, must=detail_query)
+
+    def suggest_query(
+        self, text, name="suggester", size=DEFAULT_SUGGEST_SIZE, contexts=None
+    ):
+        contexts = contexts or {}
+        return self.suggest(
+            name,
+            text,
+            completion={
+                "field": Block.suggest_field(),
+                "size": size,
+                "contexts": contexts,
+            },
+        )
+
+
+def role_type(item):
+    return isinstance(item, AnsibleRole)
+
+
+def job_type(item):
+    return isinstance(item, ZuulJob)
+
+
+def block_type(item):
+    if role_type(item):
+        return "role"
+    elif job_type(item):
+        return "job"
+    raise ValueError("Unsupported type: {}".format(type(item)))
+
+
+def class_from_block_type(block_type):
+    if block_type == "role":
+        return AnsibleRole
+    elif block_type == "job":
+        return ZuulJob
+    raise ValueError("No class for block type: {}".format(block_type))
+
+
+def get_elasticsearch_parameters_from_config(config):
+    es_config = config.get("ELASTICSEARCH")
+    if es_config is None:
+        es_config = {
+            "host": config["ES_HOST"],
+            "user": config.get("ES_USER"),
+            "password": config.get("ES_PASSWORD"),
+            "port": config.get("ES_PORT"),
+            "index_prefix": config.get("ES_INDEX_PREFIX"),
+            "tls": config.get("ES_TLS"),
+        }
+    return es_config
+
+
+def init_elasticsearch(app):
+    es_config = get_elasticsearch_parameters_from_config(app.config)
+    init_elasticsearch_con(**es_config)
+    init_elasticsearch_documents()
+
+    app.add_template_test(role_type)
+    app.add_template_test(job_type)
+    app.add_template_filter(block_type)
+    # Jinja2 doesn't provide it's own filter equivalent for
+    # urllib.parse.quote_plus, so we add it by ourselves.
+    app.add_template_filter(quote_plus)
+
+
+def init_elasticsearch_con(
+    host,
+    user=None,
+    password=None,
+    connection_params=None,
+    tls=None,
+):
+    if connection_params is None:
+        connection_params = {}
+
+    connection_params["hosts"] = host
+    # Set authentication parameters if available
+    if user and password:
+        connection_params["basic_auth"] = (user, password)
+
+    # Create ssl context if enabled
+    ssl_context = None
+    tls = collections.ChainMap(tls or {}, DEFAULT_TLS_CONFIG)
+    if tls["enabled"]:
+        ssl_context = ssl.create_default_context()
+        ssl_context.check_hostname = tls["check_hostname"]
+        ssl_context.verify_mode = getattr(ssl, tls["verify_mode"])
+
+    connections.create_connection(
+        **connection_params,
+        ssl_context=ssl_context,
+        verify_certs=tls.get("check_hostname"),
+    )
+
+
+def init_elasticsearch_documents(using=None):
+    # The parameter "using" is only used in the tests to directly
+    # provide a fake elasticsearch client directly to the init calls.
+    ZuulJob.init(using=using)
+    AnsibleRole.init(using=using)
+    ZuulTenant.init(using=using)
+    GitRepo.init(using=using)
---
+++ packages/zubbi/zubbi-3.0.0-py3-none-any.whl/zubbi/scraper/scraper.py
@@ -0,0 +1,224 @@
+# Copyright 2018 BMW Car IT GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+from pathlib import Path
+
+from zubbi.scraper.exceptions import CheckoutError
+
+LOGGER = logging.getLogger(__name__)
+
+ZUUL_DIRECTORIES = ["zuul.d", ".zuul.d"]
+
+ZUUL_FILES = ["zuul.yaml", ".zuul.yaml"]
+
+README_FILES = ["README.rst", "README.md", "README.txt", "README"]
+
+CHANGELOG_FILES = ["CHANGELOG.rst", "CHANGELOG.md", "CHANGELOG.txt", "CHANGELOG"]
+
+ROLES_DIRECTORY = "roles"
+
+# Role needs to contains at least one of those directories to be considered as role
+# https://docs.ansible.com/ansible/latest/playbook_guide/playbooks_reuse_roles.html#role-directory-structure
+ROLE_MANDATORY_DIRS = [
+    "tasks",
+    "handlers",
+    "library",
+    "defaults",
+    "vars",
+    "files",
+    "templates",
+    "meta",
+]
+
+REPO_ROOT = "/"
+
+
+class Scraper:
+    def __init__(self, repo, extra_config_paths=None):
+        self.repo = repo
+        self.extra_config_paths = (
+            list(extra_config_paths.keys()) if extra_config_paths else []
+        )
+
+    def scrape(self):
+        LOGGER.info("Scraping '%s'", self.repo.name)
+
+        # TODO (felix): Currently we are scraping all files of a single
+        # repository before we start parsing them. This might load a lot
+        # of files into memory. Could we change the workflow and fetch +
+        # parse each file directly. I'm not sure if we do some
+        # cross-references between files. I have to check this. At least
+        # for roles this shouldn't be a problem, as a single role
+        # directory is self-contained.
+        job_files = self.scrape_job_files()
+        role_files = self.scrape_role_files()
+
+        return job_files, role_files
+
+    def scrape_job_files(self):
+        job_files = self.iterate_directory(
+            REPO_ROOT,
+            whitelist=ZUUL_DIRECTORIES + ZUUL_FILES + self.extra_config_paths,
+            # NOTE (felix): As we provide this directly to the
+            # str.endswith() method, the argument must be a str or a
+            # tuple of strings, otherwise the following exception is
+            # raised:
+            #
+            # TypeError: endswith first arg must be str or a tuple of
+            # str, not list
+            file_extensions=(".yaml"),
+        )
+        return job_files
+
+    def iterate_directory(
+        self, path, file_infos=None, whitelist=None, file_extensions=None
+    ):
+        if file_extensions is None:
+            # By default, we don't want to filter any files, so we use
+            # an empty string as default "extension".
+            file_extensions = ""
+
+        if file_infos is None:
+            file_infos = {}
+
+        try:
+            remote_files = self.repo.directory_contents(path)
+        except CheckoutError:
+            LOGGER.exception(
+                "Unable to check out repository root. The repository might be empty."
+            )
+            # As this is the initial directory, it doesn't make much sense
+            # to go any further.
+            return file_infos
+
+        for file_name, remote_file in remote_files.items():
+            # Skip files/directories that do not match the whitelist.
+            # NOTE (felix): The whitelist is not forwarded and only
+            # evaluated on top level.
+            if whitelist and file_name not in whitelist:
+                continue
+
+            if remote_file.type == "dir":
+                try:
+                    self.iterate_directory(
+                        remote_file.path, file_infos, file_extensions=file_extensions
+                    )
+                except CheckoutError as e:
+                    LOGGER.exception(
+                        "Unable to get check out directory '%s': %s",
+                        remote_file.path,
+                        e,
+                    )
+            elif remote_file.type == "file":
+                # Skip files that don't match the required extension
+                if not file_name.endswith(file_extensions):
+                    LOGGER.debug(
+                        "Skipping file '%s' as file matchers %s don't apply",
+                        file_name,
+                        file_extensions,
+                    )
+                    continue
+                file_info = self.get_file_info(remote_file.path)
+                if file_info:
+                    file_infos[remote_file.path] = file_info
+            else:
+                # There are other file types like symlink or submodule,
+                # but we ignore them for now.
+                LOGGER.debug(
+                    "Ignoring file type '%s' in path '%s'",
+                    remote_file.type,
+                    remote_file.path,
+                )
+        return file_infos
+
+    def get_file_info(self, path):
+        file_info = {}
+        try:
+            file_info = {
+                "last_changed": self.repo.last_changed(path),
+                "blame": self.repo.blame(path),
+                "content": self.repo.file_contents(path),
+            }
+        except CheckoutError as e:
+            LOGGER.debug("Unable to get file info for '%s': %s", path, e)
+
+        return file_info
+
+    def scrape_role_files(self):
+        role_files = {}
+        # Roles might be grouped in some parent directory, so we need to
+        # recursively check nested directories.
+
+        # Listing repository files is based on the example in pyGithub doc:
+        # https://pygithub.readthedocs.io/en/v2.1.1/examples/Repository.html#get-all-of-the-contents-of-the-repository-recursively
+        try:
+            # Ansible requires roles to be defined in a certain directory structure.
+            # Thus, we are only interested in directories
+            dirs_to_search = [
+                item
+                for item in self.repo.directory_contents(ROLES_DIRECTORY).values()
+                if item.type == "dir"
+            ]
+            while dirs_to_search:
+                try:
+                    dir = dirs_to_search.pop(0)
+                    dir_items = self.repo.directory_contents(dir.path)
+
+                    subdirs = [
+                        item for item in dir_items.values() if item.type == "dir"
+                    ]
+                    # When directory does not contain one of role mandatory directories
+                    # it is not a role and its subdirectories (if any) should be further
+                    # scanned.
+                    # This is done by appending those subdirectories to 'dirs_to_search'
+                    # search list (this implements the recursive search)
+                    if set([d.name for d in subdirs]).isdisjoint(ROLE_MANDATORY_DIRS):
+                        dirs_to_search.extend(subdirs)
+                        continue
+
+                    # Once the role is found, we are only interested in the timestamp of
+                    # the latest update (the last git change), README and CHANGELOG files
+                    # Those files should be on the top-level per role.
+                    last_changed = self.repo.last_changed(dir.path)
+                    readme_file = self.find_matching_file(README_FILES, dir_items)
+                    changelog_file = self.find_matching_file(CHANGELOG_FILES, dir_items)
+                    # role name is the directory path relative to ROLES_DIRECTORY
+                    role_files[str(Path(dir.path).relative_to(ROLES_DIRECTORY))] = {
+                        "last_changed": last_changed,
+                        "readme_file": readme_file,
+                        "changelog_file": changelog_file,
+                    }
+                except CheckoutError as e:
+                    LOGGER.exception(e)
+        except CheckoutError as e:
+            LOGGER.debug(e)
+
+        # sort keys (role names) alphabetically
+        return {key: value for key, value in sorted(role_files.items())}
+
+    def find_matching_file(self, file_filter, existing_files):
+        for filename, file_content in existing_files.items():
+            if filename not in file_filter:
+                continue
+            try:
+                rel_path = file_content.path
+                # Return the first matching file
+                match = {
+                    "path": rel_path,
+                    "content": self.repo.file_contents(rel_path),
+                }
+                return match
+            except CheckoutError as e:
+                LOGGER.exception(e)
