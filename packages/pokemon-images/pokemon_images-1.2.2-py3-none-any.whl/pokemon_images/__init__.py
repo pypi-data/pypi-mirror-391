c27d2b88247768800b07d0b6111d8c2ede162d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 15:04:12 +0000

    Add package pokemon-images/1.2.2/pokemon_images-1.2.2-py3-none-any.whl

---
+++ packages/pokemon-images/pokemon_images-1.2.2-py3-none-any.whl/pokemon_images/__init__.py
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: 2024 Tomodachi94 and contributors
+#
+# SPDX-License-Identifier: MIT
+
+import random
+
+POKEMON_TOTAL: int = 1025 # Increment this as more Pokemon are added
+
+def get_url_from_number(image_number: int | str):
+    """Gets a Pokemon image URL from an image number."""
+    padded: str
+
+    image_number = str(image_number)
+
+    if len(image_number) == 1:
+        padded = "00" + image_number
+    elif len(image_number) == 2:
+        padded = "0" + image_number
+    elif len(image_number) == 3:
+        padded = image_number
+    elif len(image_number) == 4:
+        padded = image_number
+    else:
+        raise ValueError
+
+    return f"https://assets.pokemon.com/assets/cms2/img/pokedex/full/{padded}.png"
+
+
+def get_random_url():                           
+    """Gets a random Pokemon image's URL."""
+    image_number = random.randint(1, POKEMON_TOTAL)
+
+    return get_url_from_number(image_number)
+
