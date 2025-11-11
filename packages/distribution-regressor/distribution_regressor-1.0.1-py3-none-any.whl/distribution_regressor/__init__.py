080b4072426516931a2a6903f4b6f4a05fb12a6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 16:04:48 +0000

    Add package distribution-regressor/1.0.1/distribution_regressor-1.0.1-py3-none-any.whl

---
+++ packages/distribution-regressor/distribution_regressor-1.0.1-py3-none-any.whl/distribution_regressor/__init__.py
@@ -0,0 +1,9 @@
+"""
+DistributionRegressor: Nonparametric distributional regression using LightGBM.
+"""
+
+from .regressor import DistributionRegressor
+
+__version__ = "1.0.1"
+__all__ = ["DistributionRegressor"]
+
