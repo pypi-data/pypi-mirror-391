9688c6e48af38fa54a252d731c06901833d831f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 15:04:51 +0000

    Add package qoery/0.5.1/qoery-0.5.1.tar.gz

---
+++ packages/qoery/qoery-0.5.1.tar.gz/qoery-0.5.1/qoery/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from qoery.api.datasets_api import DatasetsApi
+from qoery.api.health_api import HealthApi
+from qoery.api.plans_api import PlansApi
+from qoery.api.usage_api import UsageApi
+
