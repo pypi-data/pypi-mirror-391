7fe45fb32db2b79abc3e9597da214232a1b6e16c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 16:04:36 +0000

    Add package homa/0.3.28/homa-0.3.28-py3-none-any.whl

---
+++ packages/homa/homa-0.3.28-py3-none-any.whl/homa/rl/SoftActorCritic.py
@@ -0,0 +1,70 @@
+from .sac import SoftActor, SoftCritic
+from .buffers import SoftActorCriticBuffer
+from ..core.concerns import TracksTime
+
+
+class SoftActorCritic(TracksTime):
+    def __init__(
+        self,
+        state_dimension: int,
+        action_dimension: int,
+        hidden_dimension: int = 256,
+        buffer_capacity: int = 1_000_000,
+        batch_size: int = 256,
+        actor_lr: float = 0.0002,
+        critic_lr: float = 0.0003,
+        actor_decay: float = 0.0,
+        critic_decay: float = 0.0,
+        tau: float = 0.005,
+        alpha: float = 0.2,
+        gamma: float = 0.99,
+        min_std: float = -20.0,
+        max_std: float = 2.0,
+        warmup: int = 10_000,
+    ):
+        super().__init__()
+
+        self.batch_size: int = batch_size
+        self.tau: float = tau
+        self.warmup: int = warmup
+
+        self.actor = SoftActor(
+            state_dimension=state_dimension,
+            action_dimension=action_dimension,
+            hidden_dimension=hidden_dimension,
+            lr=actor_lr,
+            weight_decay=actor_decay,
+            alpha=alpha,
+            min_std=min_std,
+            max_std=max_std,
+        )
+        self.critic = SoftCritic(
+            state_dimension=state_dimension,
+            action_dimension=action_dimension,
+            hidden_dimension=hidden_dimension,
+            lr=critic_lr,
+            weight_decay=critic_decay,
+            gamma=gamma,
+            alpha=alpha,
+        )
+        self.buffer = SoftActorCriticBuffer(capacity=buffer_capacity)
+
+    def is_warmup(self):
+        return self.t < self.warmup
+
+    def train(self):
+        # don't train before warmup
+        if self.is_warmup():
+            return
+
+        data = self.buffer.sample_torch(self.batch_size)
+        self.critic.train(
+            states=data.states,
+            actions=data.actions,
+            rewards=data.rewards,
+            terminations=data.terminations,
+            next_states=data.next_states,
+            actor=self.actor,
+        )
+        self.critic.update(tau=self.tau)
+        self.actor.train(states=data.states, critic=self.critic)
---
+++ packages/homa/homa-0.3.28-py3-none-any.whl/homa/rl/sac/modules/SoftActorModule.py
@@ -0,0 +1,40 @@
+import torch
+
+
+class SoftActorModule(torch.nn.Module):
+    def __init__(
+        self,
+        state_dimension: int,
+        hidden_dimension: int,
+        action_dimension: int,
+        min_std: float,
+        max_std: float,
+    ):
+        super().__init__()
+
+        self.state_dimension: int = state_dimension
+        self.hidden_dimension: int = hidden_dimension
+        self.action_dimension: int = action_dimension
+        self.min_std: float = float(min_std)
+        self.max_std: float = float(max_std)
+
+        self.phi = torch.nn.Sequential(
+            torch.nn.Linear(self.state_dimension, self.hidden_dimension),
+            torch.nn.ReLU(),
+            torch.nn.Linear(self.hidden_dimension, self.hidden_dimension),
+            torch.nn.ReLU(),
+        )
+
+        self.mu = torch.nn.Linear(self.hidden_dimension, self.action_dimension)
+        torch.nn.init.uniform_(self.mu.weight, -3e-3, 3e-3)
+        torch.nn.init.constant_(self.mu.bias, 0.0)
+
+        self.xi = torch.nn.Linear(self.hidden_dimension, self.action_dimension)
+        torch.nn.init.constant_(self.xi.weight, 0.0)
+        torch.nn.init.constant_(self.xi.bias, -0.5)
+
+    def forward(self, state: torch.Tensor):
+        features = self.phi(state)
+        mean = self.mu(features)
+        std = self.xi(features).clamp(self.min_std, self.max_std)
+        return mean, std
