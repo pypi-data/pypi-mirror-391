aac9793e698e956d4f3ba9e8e10098931bc80fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 14:04:28 +0000

    Add package python-roborock/3.7.2/python_roborock-3.7.2-py3-none-any.whl

---
+++ packages/python-roborock/python_roborock-3.7.2-py3-none-any.whl/roborock/devices/traits/v1/network_info.py
@@ -0,0 +1,57 @@
+"""Trait for device network information."""
+
+from __future__ import annotations
+
+import logging
+from typing import Self
+
+from roborock.data import NetworkInfo
+from roborock.devices.cache import Cache
+from roborock.devices.traits.v1 import common
+from roborock.roborock_typing import RoborockCommand
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class NetworkInfoTrait(NetworkInfo, common.V1TraitMixin):
+    """Trait for device network information.
+
+    This trait will always prefer reading from the cache if available. This
+    information is usually already fetched when creating the device local
+    connection, so reading from the cache avoids an unnecessary RPC call.
+    However, we have the fallback to reading from the device if the cache is
+    not populated for some reason.
+    """
+
+    command = RoborockCommand.GET_NETWORK_INFO
+
+    def __init__(self, device_uid: str, cache: Cache) -> None:
+        """Initialize the trait."""
+        self._device_uid = device_uid
+        self._cache = cache
+        self.ip = ""
+
+    async def refresh(self) -> Self:
+        """Refresh the network info from the cache."""
+
+        cache_data = await self._cache.get()
+        if cache_data.network_info and (network_info := cache_data.network_info.get(self._device_uid)):
+            _LOGGER.debug("Using cached network info for device %s", self._device_uid)
+            self._update_trait_values(network_info)
+            return self
+
+        # Load from device if not in cache
+        _LOGGER.debug("No cached network info for device %s, fetching from device", self._device_uid)
+        await super().refresh()
+
+        # Update the cache with the new network info
+        cache_data.network_info[self._device_uid] = self
+        await self._cache.set(cache_data)
+
+        return self
+
+    def _parse_response(self, response: common.V1ResponseData) -> NetworkInfo:
+        """Parse the response from the device into a NetworkInfo."""
+        if not isinstance(response, dict):
+            raise ValueError(f"Unexpected NetworkInfoTrait response format: {response!r}")
+        return NetworkInfo.from_dict(response)
