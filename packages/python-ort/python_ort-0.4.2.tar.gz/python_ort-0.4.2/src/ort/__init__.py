5d186f571bed39e919f7837105f30f5c31cc9902
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 15:04:12 +0000

    Add package python-ort/0.4.2/python_ort-0.4.2.tar.gz

---
+++ packages/python-ort/python_ort-0.4.2.tar.gz/python_ort-0.4.2/src/ort/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2025 Helio Chissini de Castro <heliocastro@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+from ort.models.repository_configuration import OrtRepositoryConfiguration
+
+__all__ = [
+    "OrtRepositoryConfiguration",
+]
