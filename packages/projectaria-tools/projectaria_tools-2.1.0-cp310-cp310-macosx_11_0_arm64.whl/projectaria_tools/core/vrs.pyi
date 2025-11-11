21e9ec671d57a228f092d403b9f45e5f67ef3a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 14:04:45 +0000

    Add package projectaria-tools/2.1.0/projectaria_tools-2.1.0-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/projectaria-tools/projectaria_tools-2.1.0-cp310-cp310-macosx_11_0_arm64.whl/projectaria_tools/core/vrs.pyi
@@ -0,0 +1,6 @@
+from __future__ import annotations
+__all__ = ['extract_audio_track']
+def extract_audio_track(arg0: str, arg1: str) -> str:
+    """
+    Extract the audio stream of a VRS file into a WAV file
+    """
