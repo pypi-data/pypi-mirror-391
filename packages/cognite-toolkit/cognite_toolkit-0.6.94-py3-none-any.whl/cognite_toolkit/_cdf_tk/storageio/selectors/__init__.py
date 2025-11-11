95ffe58bc6333a578f00f0c30b6492a221ea0b61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 14:05:19 +0000

    Add package cognite-toolkit/0.6.94/cognite_toolkit-0.6.94-py3-none-any.whl

---
+++ packages/cognite-toolkit/cognite_toolkit-0.6.94-py3-none-any.whl/cognite_toolkit/_cdf_tk/storageio/selectors/__init__.py
@@ -0,0 +1,53 @@
+from typing import Annotated
+
+from pydantic import Field, TypeAdapter
+
+from ._asset_centric import AssetCentricFileSelector, AssetCentricSelector, AssetSubtreeSelector, DataSetSelector
+from ._base import DataSelector
+from ._canvas import CanvasSelector
+from ._charts import AllChartsSelector, ChartOwnerSelector, ChartSelector
+from ._instances import (
+    InstanceFileSelector,
+    InstanceSelector,
+    InstanceSpaceSelector,
+    InstanceViewSelector,
+    SelectedView,
+)
+from ._raw import RawTableSelector, SelectedTable
+
+Selector = Annotated[
+    RawTableSelector
+    | InstanceViewSelector
+    | InstanceFileSelector
+    | InstanceSpaceSelector
+    | AllChartsSelector
+    | ChartOwnerSelector
+    | AssetSubtreeSelector
+    | AssetCentricFileSelector
+    | DataSetSelector,
+    Field(discriminator="type"),
+]
+
+SelectorAdapter: TypeAdapter[Selector] = TypeAdapter(Selector)
+
+
+__all__ = [
+    "AllChartsSelector",
+    "AssetCentricFileSelector",
+    "AssetCentricSelector",
+    "AssetSubtreeSelector",
+    "CanvasSelector",
+    "ChartOwnerSelector",
+    "ChartSelector",
+    "DataSelector",
+    "DataSetSelector",
+    "InstanceFileSelector",
+    "InstanceSelector",
+    "InstanceSpaceSelector",
+    "InstanceViewSelector",
+    "RawTableSelector",
+    "SelectedTable",
+    "SelectedView",
+    "Selector",
+    "SelectorAdapter",
+]
