79421f0b0077bdea5fbbe2ce513385d2681fd86c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 14:04:26 +0000

    Add package matmech/1.0.6/matmech-1.0.6.tar.gz

---
+++ packages/matmech/matmech-1.0.6.tar.gz/matmech-1.0.6/matmech/__init__.py
@@ -0,0 +1,19 @@
+"""
+The `matmech` package provides tools for processing and visualizing
+mechanical test data.
+"""
+
+__version__ = "1.0.6"
+
+"""
+It includes modules for:
+
+It includes modules for:
+- `workflow`: Orchestrates the entire data analysis process.
+- `common_utils`: General utility functions like data loading and splitting.
+- `axial_analysis`: Functions for calculating axial material properties.
+- `torsional_analysis`: Functions for calculating torsional material properties.
+- `plotting_tools`: Functions for generating static and animated plots.
+- `config_defaults`: Default configurations and registries for software profiles,
+  data columns, and plot settings.
+"""
