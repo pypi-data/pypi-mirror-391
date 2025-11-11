f0452cc67ce3eda28997dc0c458c356d5e5b2405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 14:04:32 +0000

    Add package lucidaflow/1.2.2/lucidaflow-1.2.2.tar.gz

---
+++ packages/lucidaflow/lucidaflow-1.2.2.tar.gz/lucidaflow-1.2.2/src/lucidaflow/lib/math.py
@@ -0,0 +1,54 @@
+# lib/math.py
+from lucidaflow.lucida_symbols import VarSymbol, BuiltInFunctionSymbol, ScopedSymbolTable, BuiltInTypeSymbol
+import math
+
+# --- PARTE 1: LÓGICA DE RUNTIME ---
+
+def lf_soma(args):
+    if len(args) != 2: raise TypeError("soma() espera 2 argumentos.")
+    return float(args[0]) + float(args[1])
+
+def lf_sub(args):
+    if len(args) != 2: raise TypeError("sub() espera 2 argumentos.")
+    return float(args[0]) - float(args[1])
+
+def lf_mult(args):
+    if len(args) != 2: raise TypeError("mult() espera 2 argumentos.")
+    return float(args[0]) * float(args[1])
+
+def lf_div(args):
+    if len(args) != 2: raise TypeError("div() espera 2 argumentos.")
+    if float(args[1]) == 0: raise ZeroDivisionError("Divisão por zero.")
+    return float(args[0]) / float(args[1])
+
+def lf_pot(args):
+    if len(args) != 2: raise TypeError("pot() espera 2 argumentos.")
+    return float(args[0]) ** float(args[1])
+
+
+# O dicionário que o Interpreter irá usar
+NATIVE_MATH_MODULE = {
+    "soma": lf_soma,
+    "sub": lf_sub,
+    "mult": lf_mult,
+    "div": lf_div,
+    "pot": lf_pot,
+    "pi": math.pi,
+}
+
+# --- PARTE 2: DESCRIÇÃO SEMÂNTICA ---
+
+def register_semantics():
+    float_type = BuiltInTypeSymbol('float')
+    any_type = BuiltInTypeSymbol('any')
+    
+    module_scope = ScopedSymbolTable(scope_name='math', scope_level=2)
+    
+    module_scope.define(VarSymbol('pi', float_type))
+    module_scope.define(BuiltInFunctionSymbol('soma', params=[VarSymbol('a', any_type), VarSymbol('b', any_type)], return_type=float_type))
+    module_scope.define(BuiltInFunctionSymbol('sub', params=[VarSymbol('a', any_type), VarSymbol('b', any_type)], return_type=float_type))
+    module_scope.define(BuiltInFunctionSymbol('mult', params=[VarSymbol('a', any_type), VarSymbol('b', any_type)], return_type=float_type))
+    module_scope.define(BuiltInFunctionSymbol('div', params=[VarSymbol('a', any_type), VarSymbol('b', any_type)], return_type=float_type))
+    module_scope.define(BuiltInFunctionSymbol('pot', params=[VarSymbol('a', any_type), VarSymbol('b', any_type)], return_type=float_type))
+    
+    return module_scope
---
+++ packages/lucidaflow/lucidaflow-1.2.2.tar.gz/lucidaflow-1.2.2/src/lucidaflow/lib/web.py
@@ -0,0 +1,36 @@
+# lib/web.py
+import requests
+from lucidaflow.lucida_symbols import VarSymbol, BuiltInFunctionSymbol, ScopedSymbolTable, BuiltInTypeSymbol
+
+# --- PARTE 1: Lógica de Runtime ---
+
+def lf_web_get(args):
+    if len(args) != 1:
+        raise TypeError("A função get() espera 1 argumento (a URL)")
+    url = str(args[0])
+    try:
+        response = requests.get(url)
+        response.raise_for_status() # Lança um erro para status HTTP ruins (4xx ou 5xx)
+        return response.text
+    except requests.exceptions.RequestException as e:
+        raise Exception(f"Erro de rede ao aceder a '{url}': {e}")
+
+NATIVE_WEB_MODULE = {
+    "get": lf_web_get,
+}
+
+# --- PARTE 2: Descrição Semântica ---
+
+def register_semantics():
+    string_type = BuiltInTypeSymbol('string')
+    any_type = BuiltInTypeSymbol('any')
+    
+    module_scope = ScopedSymbolTable(scope_name='web', scope_level=2)
+    module_scope.define(
+        BuiltInFunctionSymbol(
+            name='get',
+            params=[VarSymbol('url', string_type)],
+            return_type=string_type
+        )
+    )
+    return module_scope
