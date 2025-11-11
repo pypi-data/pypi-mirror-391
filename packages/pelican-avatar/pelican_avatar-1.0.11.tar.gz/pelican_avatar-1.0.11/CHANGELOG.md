0ee1a6eda6a87a880fef8933005760317f02d67b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 15:04:30 +0000

    Add package pelican-avatar/1.0.11/pelican_avatar-1.0.11.tar.gz

---
+++ packages/pelican-avatar/pelican_avatar-1.0.11.tar.gz/pelican_avatar-1.0.11/CHANGELOG.md
@@ -0,0 +1,73 @@
+CHANGELOG
+=========
+
+1.0.11 - 2025-11-11
+-------------------
+
+This is a maintenance release. The wording in the `README.md` documentation file has been improved for clarity.
+
+1.0.10 - 2024-09-23
+-------------------
+
+This is a maintenance release. No user-visible changes have been made. This
+release tests the new system to keep alignment with plugin template via
+Cruft (thanks to Justin Meyer for the changes).
+
+1.0.9 - 2024-04-14
+------------------
+
+Ensure plugin is included in sdist build
+
+1.0.8 - 2024-04-11
+------------------
+
+Maintainance release (no user visible changes)
+
+* Add acknowledgments to Lucas Cimon and Christian Clauss
+* Update list of Copyright years
+
+1.0.7 - 2024-04-11
+------------------
+
+Code improvement (no user visible changes)
+
+1.0.6 - 2024-04-07
+------------------
+
+Maintainance release (no user visible changes)
+
+* Add Python 3.12 to CI test matrix
+* Switch build system from Hatchling to PDM
+
+1.0.5 - 2023-10-30
+------------------
+
+Update list of years in Copyright notices
+
+1.0.4 - 2023-10-30
+------------------
+
+- Improve docstrings
+- Migrate to the new standard tooling
+
+1.0.3 - 2022-10-01
+------------------
+
+Update list of years in Copyright notices
+
+1.0.2 - 2022-07-24
+------------------
+
+Get the correct URL of Gravatar images (this fixes issue #11)
+
+1.0.1 - 2021-11-04
+------------------
+
+Maintenance release:
+- Add acknowledgment to Justin Mayer
+- Use full URL for the figure in the documentation
+
+1.0.0 - 2021-04-05
+------------------
+
+Initial release as namespace plugin
---
+++ packages/pelican-avatar/pelican_avatar-1.0.11.tar.gz/pelican_avatar-1.0.11/pelican/plugins/avatar/test_avatar.py
@@ -0,0 +1,160 @@
+"""Unit testing suite for the Avatar Plugin."""
+
+# Copyright (C) 2015, 2021-2023  Rafael Laboissiere <rafael@laboissiere.net>
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Affero Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/.
+
+import os
+from shutil import rmtree
+from tempfile import mkdtemp
+import unittest
+
+from libgravatar import Gravatar
+from libravatar import libravatar_url
+
+from pelican import Pelican
+from pelican.settings import read_settings
+
+from . import avatar
+
+GLOBAL_AUTHOR_EMAIL = "homer.simpson@example.com"
+ARTICLE_AUTHOR_EMAIL = "bart.simpson@example.com"
+GLOBAL_GRAVATAR_URL = Gravatar(GLOBAL_AUTHOR_EMAIL).get_image()
+GLOBAL_LIBRVATAR_URL = libravatar_url(GLOBAL_AUTHOR_EMAIL)
+ARTICLE_GRAVATAR_URL = Gravatar(ARTICLE_AUTHOR_EMAIL).get_image()
+ARTICLE_LIBRAVATAR_URL = libravatar_url(ARTICLE_AUTHOR_EMAIL)
+
+
+class TestAvatarURL(unittest.TestCase):
+    """Class for testing the URL output of the Avatar plugin."""
+
+    def setUp(self, override=None):
+        """Set up the test environment."""
+        self.output_path = mkdtemp(prefix="pelicantests.")
+        self.content_path = mkdtemp(prefix="pelicantests.")
+        with open(
+            os.path.join(self.content_path, "article_infos.html"), "w"
+        ) as article_infos_file:
+            article_infos_file.write(
+                """
+<footer class="post-info">
+        <div align="center">
+                <img src="{{ article.author_avatar }}">
+        </div>
+</footer>
+"""
+            )
+
+        with open(
+            os.path.join(self.content_path, "global_info.html"), "w"
+        ) as global_infos_file:
+            global_infos_file.write(
+                """
+<footer class="post-info">
+        <div align="center">
+                <img src="{{ author_avatar }}">
+        </div>
+</footer>
+"""
+            )
+
+        settings = {
+            "PATH": self.content_path,
+            "THEME_TEMPLATES_OVERRIDES": [self.content_path],
+            "OUTPUT_PATH": self.output_path,
+            "PLUGINS": [avatar],
+            "CACHE_CONTENT": False,
+            "AVATAR_AUTHOR_EMAIL": GLOBAL_AUTHOR_EMAIL,
+        }
+        if override:
+            settings.update(override)
+
+        with open(
+            os.path.join(self.content_path, "global_test.md"), "w"
+        ) as test_md_file:
+            test_md_file.write("Title: Global Test\nDate: 2019-09-05\n\n")
+
+        with open(os.path.join(self.content_path, "test.md"), "w") as test_md_file:
+            test_md_file.write(
+                "Title: Test\nDate: 2019-09-05\nEmail: " + ARTICLE_AUTHOR_EMAIL + "\n\n"
+            )
+
+        self.settings = read_settings(override=settings)
+        pelican = Pelican(settings=self.settings)
+        pelican.run()
+
+    def tearDown(self):
+        """Tidy up the test environment."""
+        rmtree(self.output_path)
+        rmtree(self.content_path)
+
+    def _assert_url_in_file(self, filename, url, options):
+        with open(os.path.join(self.output_path, filename)) as test_html_file:
+            found = False
+            search_url = url + options
+            for line in test_html_file:
+                if search_url in line:
+                    found = True
+                    break
+            assert found
+
+    def test_url(self, options=""):
+        """Test whether the Avatar URL appears in the generated HTML file."""
+        if self.settings["AVATAR_USE_GRAVATAR"]:
+            global_base_url = GLOBAL_GRAVATAR_URL
+            article_base_url = ARTICLE_GRAVATAR_URL
+        else:
+            global_base_url = GLOBAL_LIBRVATAR_URL
+            article_base_url = ARTICLE_LIBRAVATAR_URL
+
+        self._assert_url_in_file("test.html", article_base_url, options)
+        self._assert_url_in_file("global-test.html", global_base_url, options)
+
+
+class TestAvatarMissing(TestAvatarURL):
+    """Class for testing the "missing picture" option."""
+
+    def setUp(self, override=None):
+        """Set up the test environment."""
+        self.library = "wavatar"
+        TestAvatarURL.setUp(self, override={"AVATAR_MISSING": self.library})
+
+    def test_url(self):
+        """Test whether the 'd' option appears in the Avatar URL."""
+        TestAvatarURL.test_url(self, r"?d=" + self.library)
+
+
+class TestAvatarSize(TestAvatarURL):
+    """Class for testing the size option."""
+
+    def setUp(self, override=None):
+        """Set up the test environment."""
+        self.size = 100
+        TestAvatarURL.setUp(self, override={"AVATAR_SIZE": self.size})
+
+    def test_url(self):
+        """Test whether the 's' option appears in the Avatar URL."""
+        TestAvatarURL.test_url(self, r"?s=" + str(self.size))
+
+
+class TestAvatarUseGravatar(TestAvatarURL):
+    """Class for testing the 'use Gravatar' option."""
+
+    def setUp(self, override=None):
+        """Set up the test environment."""
+        TestAvatarURL.setUp(self, override={"AVATAR_USE_GRAVATAR": True})
+
+    def test_url(self):
+        """Test whether Gravatar is used."""
+        TestAvatarURL.test_url(self)
