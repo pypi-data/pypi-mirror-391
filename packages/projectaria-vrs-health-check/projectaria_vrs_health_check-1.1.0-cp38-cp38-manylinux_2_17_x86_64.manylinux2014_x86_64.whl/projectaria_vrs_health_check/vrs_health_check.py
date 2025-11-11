b1aeed2aadf5723b885ea8602ae032b9863ccb3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 14:05:19 +0000

    Add package projectaria-vrs-health-check/1.1.0/projectaria_vrs_health_check-1.1.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/projectaria-vrs-health-check/projectaria_vrs_health_check-1.1.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/projectaria_vrs_health_check/vrs_health_check.py
@@ -0,0 +1,8 @@
+# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+
+"""
+A pybind11 binding for projectaria_vrs_health_check submodule
+"""
+
+from _vhc_pybinds.vrs_health_check import *  # noqa
