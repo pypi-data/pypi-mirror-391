d9db7327d6c2e810b46d614bb16de92717e4c837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 16:04:53 +0000

    Add package OpenGeode-Stochastic/1.14.0/opengeode_stochastic-1.14.0-cp311-cp311-manylinux_2_28_x86_64.whl

---
+++ packages/OpenGeode-Stochastic/opengeode_stochastic-1.14.0-cp311-cp311-manylinux_2_28_x86_64.whl/opengeode_stochastic/stochastic.pyi
@@ -0,0 +1,5 @@
+from __future__ import annotations
+import opengeode as opengeode
+from opengeode_stochastic.lib64.opengeode_stochastic_py_stochastic import StochasticLibrary
+from opengeode_stochastic.lib64.opengeode_stochastic_py_stochastic import hello_world
+__all__: list[str] = ['StochasticLibrary', 'hello_world', 'opengeode']
