445ac7f4dfe2fff5aa7891a18a6c2177667d8934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 15:04:12 +0000

    Add package pokemon-images/1.2.2/pokemon_images-1.2.2.tar.gz

---
+++ packages/pokemon-images/pokemon_images-1.2.2.tar.gz/pokemon_images-1.2.2/pokemon_images/__main__.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2024 Tomodachi94 and contributors
+#
+# SPDX-License-Identifier: MIT
+
+import pokemon_images.__init__ as pokemon_images
+
+def main():
+    print(pokemon_images.get_random_url())
+
+if __name__ == "__main__":
+    main()
