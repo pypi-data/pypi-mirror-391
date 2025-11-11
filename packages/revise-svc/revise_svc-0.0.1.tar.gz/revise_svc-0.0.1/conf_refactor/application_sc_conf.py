a88f1ef9ff7e74e7cce031d636ca8f9a3835aedb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 16:04:34 +0000

    Add package revise-svc/0.0.1/revise_svc-0.0.1.tar.gz

---
+++ packages/revise-svc/revise_svc-0.0.1.tar.gz/revise_svc-0.0.1/conf_refactor/application_sc_conf.py
@@ -0,0 +1,46 @@
+import os
+from dataclasses import dataclass
+
+from conf_refactor.base_conf import BaseConf
+
+
+@dataclass
+class ApplicationScConf(BaseConf):
+    st_file: str
+    sc_ref_file: str
+
+    # annotate parameters
+    annotate_pot_reg: float = 0.1
+    annotate_pot_reg_m: float = 0.0
+    annotate_pot_reg_type: str = "entropy"
+
+    # preprocess parameters
+    prep_st_min_counts: int = 60
+    prep_st_min_cells: int = 100
+    prep_sc_min_counts: int = 0
+    prep_sc_min_cells: int = 100
+
+    # reconstruct parameters
+    rec_graph_n_neighbors: int = 10
+    rec_graph_exp_neighbor_num: int = 15
+    rec_graph_spatial_neighbor_num: int = 6
+    rec_graph_method: str = "joint"
+    rec_graph_alpha: float = 0.2
+
+    # reconstruct ot
+    rec_pot_reg: float = 0.05
+    rec_pot_reg_m: float = 1.0
+    rec_pot_reg_type: str = "kl"
+    rec_alpha = 0.5
+
+    @property
+    def result_dir(self):
+        return os.path.join(self.result_root_path, self.sub_file_path)
+
+    @property
+    def st_file_path(self):
+        return os.path.join(self.data_root_path, f"{self.sub_file_path}_{self.st_file}")
+
+    @property
+    def sc_ref_file_path(self):
+        return os.path.join(self.data_root_path, self.sc_ref_file)
