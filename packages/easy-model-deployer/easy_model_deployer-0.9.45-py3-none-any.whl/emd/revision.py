5f52405ce1df954dcc5c8ac12bdeea9c874f8dac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 15:04:50 +0000

    Add package easy-model-deployer/0.9.45/easy_model_deployer-0.9.45-py3-none-any.whl

---
+++ packages/easy-model-deployer/easy_model_deployer-0.9.45-py3-none-any.whl/emd/revision.py
@@ -0,0 +1,9 @@
+# import importlib.metadata
+VERSION = "0.9.45"
+COMMIT_HASH = "1ed68783"
+
+def convert_version_name_to_stack_name(version_name:str):
+    return version_name.replace(".","-")
+
+def convert_stack_name_to_version_name(stack_name:str):
+    return stack_name.replace("-",".")
