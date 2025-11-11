5afc6b079a35459477f348569064a164380d488c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 14:04:42 +0000

    Add package fluxloop-cli/0.2.17/fluxloop_cli-0.2.17.tar.gz

---
+++ packages/fluxloop-cli/fluxloop_cli-0.2.17.tar.gz/fluxloop_cli-0.2.17/fluxloop_cli/commands/__init__.py
@@ -0,0 +1,5 @@
+"""CLI commands."""
+
+from . import config, generate, init, parse, run, status, record, doctor
+
+__all__ = ["config", "generate", "init", "parse", "run", "status", "record", "doctor"]
