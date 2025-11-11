2c293fd3fcf0b8cb006e1b3e8b352d72174171ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 14:04:33 +0000

    Add package serverless-devs/1.0.4/serverless_devs-1.0.4-py3-none-any.whl

---
+++ packages/serverless-devs/serverless_devs-1.0.4-py3-none-any.whl/serverless_devs/__init__.py
@@ -0,0 +1,12 @@
+"""
+Serverless Devs - Python Package
+Install Serverless Devs via pip (automatically installs the latest version)
+"""
+
+__version__ = "1.0.4"
+__author__ = "Serverless Devs"
+__url__ = "https://www.serverless-devs.com/"
+
+from .installer import install_serverless_devs
+
+__all__ = ['install_serverless_devs']
