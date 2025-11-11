b7cbc9b22e77a405de2e13566b91f76a0b05b3c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 15:04:10 +0000

    Add package dodrio/0.3.8/dodrio-0.3.8-py3-none-any.whl

---
+++ packages/dodrio/dodrio-0.3.8-py3-none-any.whl/dodrio/genlist/gendatalist.py
@@ -0,0 +1,315 @@
+'''
+FilePath: /python-Dodrio/dodrio/genlist/gendatalist.py
+Descripttion: 
+Author: Yixiang Chen
+version: 
+Date: 2025-02-08 17:25:21
+LastEditors: Yixiang Chen
+LastEditTime: 2025-08-18 16:12:27
+'''
+
+import os
+import json
+
+LANGUAGE_NONE = 'LangNone'
+
+def checkutt(utt, pack_id, start, end, spk, text, language, ptype='pack'):
+    threshold_audio = 10 # 48000 sample rate 
+    if ptype == 'pack':
+        if (int(end) - int(start)) > threshold_audio * 2:
+            return True
+    else:
+        return False
+
+def SetProcess_pack(uttinfo_text_file, package_info_file, package_dir):
+    with open(uttinfo_text_file, 'r') as otif:
+        ti_lines = otif.readlines()
+    info_dict = {}
+    index_list = ti_lines[0].strip().split('|')
+    spkid, textid, langid = -1, -1, -1 
+    for idx in range(len(index_list)):
+        if index_list[idx] == 'speaker':
+            spkid = idx
+        elif index_list[idx] == 'text':
+            textid = idx
+        if 'language' in index_list:
+            if index_list[idx] == 'language': 
+                langid = idx
+    for idx in range(1, len(ti_lines)):
+        spl = ti_lines[idx].strip().split('|')
+        utt = spl[0]
+        spk = spl[spkid]
+        text = spl[textid]
+        language = spl[langid] if langid != -1 else LANGUAGE_NONE
+        info_dict[utt] = [spk, text, language]
+    del ti_lines
+    with open(package_info_file, 'r') as opif:
+        p_lines = opif.readlines()
+    outinfo_list = []
+    for idx in range(len(p_lines)):
+        spl = p_lines[idx].strip().split('|')
+        utt, pack_id, start, end = spl[0], spl[1], spl[2], spl[3]
+        # utt_id|save_type|pack_path|start_pointer|end_pointer|spk|language|text
+        spk, text, language = info_dict[utt]
+        if checkutt(utt, pack_id, start, end, spk, text, language, ptype='pack'):
+            outline = [utt, 'pack', os.path.join(package_dir, pack_id), start, end, spk, language, text]
+            outinfo_list.append(outline)
+    return outinfo_list
+
+def SetProcess_dir(package_dir, info_dir):
+    uttinfo_text_file = os.path.join(info_dir, 'uttinfo_text.list')
+    package_info_file = os.path.join(package_dir, 'uttinfo.list')
+    use_info_list = SetProcess_pack(uttinfo_text_file, package_info_file, package_dir)
+    return use_info_list
+
+def genListDir(supdir_list, outdir, prefix='test', subnum=50000):
+
+    os.makedirs(outdir, exist_ok=True)
+    all_list_file = os.path.join(outdir, 'all_usage_utt.list')
+    sub_table_dir = os.path.join(outdir, 'subtable')
+    os.makedirs(sub_table_dir, exist_ok=True)
+    spk_id_file = os.path.join(outdir, 'spk_name.dict') 
+    list_list_file = os.path.join(outdir, 'list_subtable.list')
+
+    all_info_list = []
+    for supdir in supdir_list:
+        package_dir = os.path.join(supdir, 'pack_dir') 
+        info_dir = os.path.join(supdir, 'info_dir')
+        use_info_list = SetProcess_dir(package_dir, info_dir)
+        all_info_list.extend(use_info_list)
+    
+    spkdict = {}
+    spkid = 0
+    save_all = True
+    olistsub = open(list_list_file, 'w')
+    if save_all:
+        oallt = open(all_list_file, 'w')
+    for idx in range(0, len(all_info_list), subnum):
+        use_id = (idx+1) // subnum 
+        subtable_file = os.path.join(sub_table_dir, prefix+'_sub_'+str(use_id).rjust(6,'0')+ '.table') 
+        olistsub.write(subtable_file+'\n')
+        osubt = open(subtable_file, 'w')
+        for ik in range(idx, min(len(all_info_list), idx+subnum)):
+            info_line = all_info_list[ik]
+            spk = info_line[5]
+            if spk not in spkdict.keys():
+                spkdict[spk] = spkid
+                spkid += 1
+            outline = '|'.join(info_line) + '\n'
+            osubt.write(outline)
+            if save_all:
+                oallt.write(outline)
+        osubt.close()
+    if save_all:
+        oallt.close()
+    olistsub.close()
+    with open(spk_id_file, 'w', encoding="utf-8") as opf:
+        json.dump(spkdict, opf, indent=2, ensure_ascii=False)
+    
+########################  new version  ###################################
+
+def pack_dict_load(package_dir):
+    package_info_file = os.path.join(package_dir, 'uttinfo.list')
+    with open(package_info_file, 'r') as opif:
+        p_lines = opif.readlines()
+    outinfo_dict = {}
+    for idx in range(len(p_lines)):
+        spl = p_lines[idx].strip().split('|')
+        utt, pack_id, start, end = spl[0], spl[1], spl[2], spl[3]
+        packpath = os.path.join(package_dir, pack_id)
+        outinfo_dict[utt] = [packpath, start, end]
+    keylist = ['packpath', 'start', 'end']
+    return outinfo_dict, keylist 
+
+def info_dict_load(info_dir):
+    uttinfo_text_file = os.path.join(info_dir, 'uttinfo_text.list')
+    with open(uttinfo_text_file, 'r') as otif:
+        ti_lines = otif.readlines()
+    info_dict = {}
+    index_list = ti_lines[0].strip().split('|')
+    spkid, textid, langid = -1, -1, -1 
+    for idx in range(len(index_list)):
+        if index_list[idx] == 'speaker':
+            spkid = idx
+        elif index_list[idx] == 'text':
+            textid = idx
+        if 'language' in index_list:
+            if index_list[idx] == 'language': 
+                langid = idx
+    for idx in range(1, len(ti_lines)):
+        spl = ti_lines[idx].strip().split('|')
+        utt = spl[0]
+        spk = spl[spkid]
+        text = spl[textid]
+        language = spl[langid] if langid != -1 else LANGUAGE_NONE
+        info_dict[utt] = [spk, text, language]
+    keylist = ['speaker', 'text', 'language']
+    del ti_lines 
+    return info_dict, keylist
+
+def feat_dict_load(feat_dir, featname):
+    featinfo_file = os.path.join(feat_dir, 'feat_info_'+ featname +'.list')
+    with open(featinfo_file, 'r') as ofif:
+        info_lines = ofif.readlines()
+    info_dict = {}
+    for line in info_lines:
+        spl = line.strip().split('|')
+        utt, packid, start, end, shape = spl[0], spl[1], spl[2], spl[3], spl[4]
+        packpath = os.path.join(feat_dir, packid)
+        info_dict[utt] = [packpath, start, end, shape]
+    keylist = ['packpath', 'start', 'end', 'shape' ]
+    return info_dict, keylist 
+
+def align_dict_load(align_dir, featname='align'):
+    featinfo_file = os.path.join(align_dir, 'feat_info_'+ featname +'.list')
+    with open(featinfo_file, 'r') as ofif:
+        info_lines = ofif.readlines()
+    info_dict = {}
+    for line in info_lines:
+        spl = line.strip().split('|')
+        utt, packid, phone_list, phone_dur = spl[0], spl[1], spl[2], spl[3]
+        info_dict[utt] = [phone_list, phone_dur]
+    keylist = ['phones', 'duration']
+    return info_dict, keylist 
+
+def datadirProcess(datadir, featlist, check_func):
+    all_keylist = []
+    package_dir = os.path.join(datadir, 'pack_dir')
+    wav_info_dict, kl1 = pack_dict_load(package_dir)
+    all_keylist.extend(kl1)
+    info_dir = os.path.join(datadir, 'info_dir')
+    info_dict, kl2 = info_dict_load(info_dir)
+    all_keylist.extend(kl2)
+    supfeat_dict = {}
+    kl_dict = {}
+    for featname in featlist:
+        feat_dir = os.path.join(datadir, featname+'_dir')
+        supfeat_dict[featname], kl_dict[featname] =  feat_dict_load(feat_dir, featname)
+        all_keylist.append('featname_'+featname)
+        all_keylist.extend(kl_dict[featname])
+    
+    out_dict = {}
+    intersection = wav_info_dict.keys() & info_dict.keys() 
+    for featname in featlist:
+        intersection = intersection & supfeat_dict[featname].keys() 
+    
+    for utt in intersection:
+        outinfo_list = wav_info_dict[utt]
+        outinfo_list.extend(info_dict[utt])
+        for featname in featlist:
+            outinfo_list.append(featname)
+            outinfo_list.extend(supfeat_dict[featname][utt])
+        if check_func(outinfo_list):
+            out_dict[utt] = outinfo_list
+    return out_dict, all_keylist
+
+def datadirProcess_align(datadir, featlist, check_func):
+    all_keylist = []
+    package_dir = os.path.join(datadir, 'pack_dir')
+    wav_info_dict, kl1 = pack_dict_load(package_dir)
+    all_keylist.extend(kl1)
+
+    tmp_featlist = featlist.copy()
+
+    info_dir = os.path.join(datadir, 'info_dir')
+    info_dict, kl2 = info_dict_load(info_dir)
+    all_keylist.extend(kl2)
+    supfeat_dict = {}
+    kl_dict = {}
+
+    align_flag = False
+    if 'align' in tmp_featlist:
+        align_flag = True
+        tmp_featlist.remove('align')
+        align_dir = os.path.join(datadir, 'align_dir')
+        align_dict, kl3 = align_dict_load(align_dir)
+        all_keylist.extend(kl3)
+
+    for featname in tmp_featlist:
+        feat_dir = os.path.join(datadir, featname+'_dir')
+        supfeat_dict[featname], kl_dict[featname] =  feat_dict_load(feat_dir, featname)
+        all_keylist.append('featname_'+featname)
+        all_keylist.extend(kl_dict[featname])
+    
+    out_dict = {}
+    intersection = wav_info_dict.keys() & info_dict.keys() 
+    if align_flag:
+        intersection = intersection & align_dict.keys() 
+    for featname in tmp_featlist:
+        intersection = intersection & supfeat_dict[featname].keys() 
+    
+    for utt in intersection:
+        outinfo_list = wav_info_dict[utt]
+        outinfo_list.extend(info_dict[utt])
+        if align_flag:
+            outinfo_list.extend(align_dict[utt])
+        for featname in tmp_featlist:
+            outinfo_list.append(featname)
+            outinfo_list.extend(supfeat_dict[featname][utt])
+        if check_func(outinfo_list):
+            out_dict[utt] = outinfo_list
+    return out_dict, all_keylist
+
+
+def check_func(outinfo_list):
+    return True
+
+def gen_datalist(supdir_list, outdir, featlist, check_func, prefix, subnum=50000):
+    os.makedirs(outdir, exist_ok=True)
+    allsave_flag = True
+    if allsave_flag:
+        all_list_file = os.path.join(outdir, 'all_usage_utt.list')
+    sub_table_dir = os.path.join(outdir, 'subtable')
+    os.makedirs(sub_table_dir, exist_ok=True)
+    spk_id_file = os.path.join(outdir, 'spk_name.dict') 
+    list_list_file = os.path.join(outdir, 'list_subtable.list')
+    keys_file = os.path.join(outdir, 'keys_name') 
+
+    spkdict = {}
+    spkid = 0
+    subtabel_id = 0
+    tmp_idx = 0
+    olistsub = open(list_list_file, 'w')
+    if allsave_flag:
+        oallt = open(all_list_file, 'w')
+    for datadir in supdir_list:
+        #out_dict, all_keylist = datadirProcess(datadir, featlist, check_func)
+        out_dict, all_keylist = datadirProcess_align(datadir, featlist, check_func)
+        for utt in out_dict.keys():
+            if tmp_idx >=subnum:
+                tmp_idx = 0
+                subtabel_id += 1 
+                opsubt.close()
+            if tmp_idx == 0:
+                subtable_file = os.path.join(sub_table_dir, prefix+'_sub_'+str(subtabel_id).rjust(6,'0')+ '.table')  
+                opsubt = open(subtable_file, 'w')
+                olistsub.write(subtable_file+'\n')
+            
+            outlist = [utt]
+            outlist.extend(out_dict[utt])
+            spk = outlist[4]
+            if spk not in spkdict.keys():
+                spkdict[spk] = spkid
+                spkid += 1
+            outlist[4] = str(spkid)
+            
+            outline = '|'.join(outlist) + '\n'
+            opsubt.write(outline)
+            if allsave_flag:
+                oallt.write(outline)
+
+            tmp_idx += 1
+    with open(keys_file, 'w') as okf:
+        save_keys = ['uttid']
+        save_keys.extend(all_keylist)
+        oline = '|'.join(save_keys) + '\n'
+        okf.write(oline)
+
+    if allsave_flag:
+        oallt.close()
+    opsubt.close()
+    olistsub.close()
+    with open(spk_id_file, 'w', encoding="utf-8") as opf:
+        json.dump(spkdict, opf, indent=2, ensure_ascii=False)
+    
+        
---
+++ packages/dodrio/dodrio-0.3.8-py3-none-any.whl/dodrio/tools/load_data.py
@@ -0,0 +1,218 @@
+'''
+FilePath: /python-Dodrio/dodrio/tools/load_data.py
+Descripttion: 
+Author: Yixiang Chen
+version: 
+Date: 2025-03-26 19:23:27
+LastEditors: Yixiang Chen
+LastEditTime: 2025-09-26 11:46:14
+'''
+
+
+import os
+from io import BytesIO
+import librosa
+from tqdm import tqdm
+import numpy as np
+import pyarrow.parquet as pq
+
+
+####################### Load data block ###################### 
+
+def get_file_list(inp_dir, suffix='.wav'):
+    itm = []
+    for home, dirs, files in os.walk(inp_dir):
+        pppp = list( map(lambda fname: home + '/' + fname,
+            list( filter( lambda filename: os.path.splitext(filename)[1] == suffix,
+            files) ) ) )
+        itm.extend(pppp)
+    file_list = itm
+    return file_list
+
+def load_datalist_dict(input_dir, from_type='parquet'):
+    if from_type == 'parquet':
+        load_file = os.path.join(input_dir, 'utt2parquet.list')
+    else:
+        load_file = os.path.join(input_dir, 'uttinfo.list')
+    data_dict = {}
+    with open(load_file, 'r') as lf:
+        lines = lf.readlines()
+    for line in lines:
+        spl = line.strip().split('|')
+        name= spl[0]
+        packname = spl[1].split('.')[0]
+        data_dict.setdefault(packname, []).append(name)
+    return data_dict
+
+def load_data_dict(input_dir, from_type='parquet'):
+    if from_type=='parquet':
+        suffix = '.parquet'
+        infolistf = ''
+    else:
+        suffix = '.pack'
+        infolistf = os.path.join(input_dir, 'uttinfo.list')
+    packlist = get_file_list(input_dir, suffix)
+    packlist.sort()
+    data_dict = {}
+    for idx in range(len(packlist)):
+        packfile = packlist[idx]
+        basename = os.path.split(packfile)[-1].split(suffix)[0]
+        uttdict = load_pack_audio_data(packfile, infolistf, return_sr = False)
+        data_dict[basename] = uttdict
+    return data_dict
+
+
+####################### Load audio ######################  
+
+def audiotran(data, atype, dtype):
+    if atype == 'wav':
+        if dtype == 'int16':
+            dnum = 32768
+        elif dtype == 'int32':
+            dnum = 2147483648
+        else:
+            print(f"{dtype} is not normal data type")
+            return 
+        return data / dnum
+    elif atype == 'mp3':
+        wav, sr = librosa.load(BytesIO(data))
+        return wav
+
+def load_pack_audio_data(packp, infolistf='', return_sr = False):
+    ftype = os.path.split(packp)[-1].split('.')[-1]
+    assert ftype in ['parquet', 'pack']
+    audio_type = os.path.split(packp)[-1].split('_')[0]
+    assert audio_type in ['wav', 'mp3']
+    outdict = {}
+    if ftype == 'parquet':
+        df = pq.read_table(packp).to_pandas()
+        for idx in tqdm(range(len(df)), desc=f'Loading'):
+            utt = df.iloc[idx]['utt']
+            sr = df.iloc[idx]['sample_rate']
+            dtype = df.iloc[idx]['dtype']
+            audio = df.iloc[idx]['audio_data']
+            wav = audiotran(audio, audio_type, dtype)
+            if return_sr:
+                outdict[utt] = [wav,sr]
+            else:
+                outdict[utt] = wav
+    else:
+        info_file = os.path.join(infolistf)
+        with open(info_file, 'r') as iff:
+            info_list = iff.readlines()
+        packname = os.path.split(packp)[-1] 
+        #for info in tqdm(info_list):
+        for info in info_list:
+            utt, pf, start, end = info.split('|')
+            start = int(start)
+            end = int(end)
+            if pf != packname:
+                continue
+            with open(packp, 'rb') as opf :
+                opf.seek(start)
+                data = opf.read(end-start)
+            wav = np.frombuffer(data, dtype=np.int16)
+            wav = wav / 32768
+            if return_sr:
+                outdict[utt] = [wav,48000]
+            else:
+                outdict[utt] = wav
+    return outdict
+
+######################## Load Text #########################
+def load_textinfo_data(infolist):
+    with open(infolist, 'r') as oifl:
+        lines = oifl.readlines()
+    keys = lines[0].strip().split('|')
+    id_idx = 0
+    for idx in range(len(keys)):
+        if keys[idx] == 'id':
+            id_idx = idx
+            break
+    info_dict = {}
+    for idx in range(1, len(lines)):
+        line = lines[idx]
+        spl = line.strip().split('|')
+        info_dict[spl[id_idx]] = {}
+        for idx in range(len(keys)):
+            kk = keys[idx]
+            info_dict[spl[id_idx]][kk] = spl[idx]
+    return info_dict
+
+####################### Load single ######################
+
+def load_audio_single(packp, start, end):
+    with open(packp, 'rb') as opf :
+        opf.seek(start)
+        data = opf.read(end-start)
+    wav = np.frombuffer(data, dtype=np.int16)
+    wav = wav / 32768
+    return wav
+
+def load_feat_single(featpack, start, end, shape):
+    with open(featpack, 'rb') as opf :
+        opf.seek(start)
+        data = opf.read(end-start)
+    feat = np.frombuffer(data, dtype=np.float32)
+    feat = np.reshape(feat, shape)
+    return feat
+
+def load_data_from_line(infoline):
+    spl = infoline.strip().split('|')
+    data_dict = {}
+    uttid = spl[0]
+    wavp, wstart, wend = spl[1], int(spl[2]), int(spl[3])
+    audio = load_audio_single(wavp, wstart, wend) 
+    spkid, text, language = spl[4], spl[5], spl[6]
+
+    data_dict['uttid'] = uttid
+    data_dict['audio'] = audio
+    data_dict['spkid'] = spkid
+    data_dict['text'] = text
+    data_dict['language'] = language
+
+    feat_type_num = (len(spl)- 7) // 5
+    for idx in range(feat_type_num):
+        beginid = 7 + idx*5 
+        featname = spl[beginid]
+        featpack = spl[beginid+1]
+        fstart, fend, fshape = int(spl[beginid+2]), int(spl[beginid+3]), spl[beginid+4]
+        fshape = [int(xx) for xx in fshape.split(',')]
+        feat_data = load_feat_single(featpack, fstart, fend, fshape)
+        data_dict[featname] = feat_data
+
+    return data_dict 
+        
+def load_data_from_line_align(infoline):
+    spl = infoline.strip().split('|')
+    data_dict = {}
+    uttid = spl[0]
+    wavp, wstart, wend = spl[1], int(spl[2]), int(spl[3])
+    audio = load_audio_single(wavp, wstart, wend) 
+    spkid, text, language = spl[4], spl[5], spl[6]
+
+    phones, duration = spl[7], spl[8]
+    phones = phones.split()
+    duration = [int(xx) for xx in duration.split()]
+
+    data_dict['uttid'] = uttid
+    data_dict['audio'] = audio
+    data_dict['spkid'] = spkid
+    data_dict['text'] = text
+    data_dict['language'] = language
+    data_dict['phones'] = phones
+    data_dict['duration'] = duration
+
+    feat_type_num = (len(spl)- 9) // 5
+    for idx in range(feat_type_num):
+        beginid = 9 + idx*5 
+        featname = spl[beginid]
+        featpack = spl[beginid+1]
+        fstart, fend, fshape = int(spl[beginid+2]), int(spl[beginid+3]), spl[beginid+4]
+        fshape = [int(xx) for xx in fshape.split(',')]
+        feat_data = load_feat_single(featpack, fstart, fend, fshape)
+        data_dict[featname] = feat_data
+
+    return data_dict 
+
+
