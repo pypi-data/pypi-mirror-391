153790ec1fbd38828498ce1531138cc385e72090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 16:05:22 +0000

    Add package pyodim/0.5.6/pyodim-0.5.6-py3-none-any.whl

---
+++ packages/pyodim/pyodim-0.5.6-py3-none-any.whl/pyodim/__init__.py
@@ -0,0 +1,7 @@
+# primary read routines
+from .pyodim import read_odim
+from .pyodim import read_write_odim
+
+# helper routines
+from .pyodim import copy_h5_data
+from .pyodim import write_odim_str_attrib
