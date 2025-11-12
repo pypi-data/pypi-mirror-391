015f90ce6a8aaa86192aa5ffca39e5a25f60b513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 08:05:17 +0000

    Add package astrocytes/0.1.1a1/astrocytes-0.1.1a1.tar.gz

---
+++ packages/astrocytes/astrocytes-0.1.1a1.tar.gz/astrocytes-0.1.1a1/tests/test_datasets.py
@@ -0,0 +1,14 @@
+"""Tests for OpenAstrocyte datasets"""
+
+##
+# Imports
+
+import pytest
+
+
+##
+# Test units
+
+def test_placeholder():
+    """TODO A placeholder test to get the CI workflow up"""
+    assert True == True
