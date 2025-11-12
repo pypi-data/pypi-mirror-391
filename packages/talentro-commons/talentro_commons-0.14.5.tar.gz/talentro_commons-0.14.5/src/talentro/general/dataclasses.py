36fd96390b2f0232e7c1e2cbaa09f8501a5cc23b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 05:04:48 +0000

    Add package talentro-commons/0.14.5/talentro_commons-0.14.5.tar.gz

---
+++ packages/talentro-commons/talentro_commons-0.14.5.tar.gz/talentro_commons-0.14.5/src/talentro/general/dataclasses.py
@@ -0,0 +1,43 @@
+from abc import abstractmethod, ABC
+from typing import Literal
+from uuid import UUID
+
+from pydantic import BaseModel
+from pydantic.dataclasses import dataclass
+
+
+class ResolvableModel(BaseModel, ABC):
+
+    @staticmethod
+    @abstractmethod
+    async def resolve_object(object_id: UUID) -> "ResolvableModel":
+        pass
+
+
+class ResolvableCompanyModel(BaseModel, ABC):
+
+    @staticmethod
+    @abstractmethod
+    async def resolve_object(object_id: UUID, organization_id: str) -> "ResolvableCompanyModel":
+        pass
+
+
+@dataclass
+class DropdownOption:
+    value: str
+    label: str
+    detail: str | None = None
+
+
+@dataclass
+class VacancySelectionRule:
+    field: str
+    operator: str
+    value: str
+
+
+@dataclass
+class VacancySelectionCriteria:
+    rules: list[VacancySelectionRule]
+    selected_vacancies: list[UUID]
+    selection_method: Literal['list', 'all', 'rules']
