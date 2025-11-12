f113549870774189b6c5f19a39143dcf40f7843d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 04:06:38 +0000

    Add package tombi/0.6.44/tombi-0.6.44.tar.gz

---
+++ packages/tombi/tombi-0.6.44.tar.gz/tombi-0.6.44/crates/tombi-formatter/src/format/key.rs
@@ -0,0 +1,34 @@
+use std::fmt::Write;
+
+use itertools::Itertools;
+use tombi_ast::AstNode;
+
+use crate::Format;
+
+impl Format for tombi_ast::Keys {
+    fn format(&self, f: &mut crate::Formatter) -> Result<(), std::fmt::Error> {
+        let keys = self
+            .keys()
+            .map(|key| key.syntax().text().to_string())
+            .collect_vec()
+            .join(".");
+
+        write!(f, "{keys}")
+    }
+}
+
+impl Format for tombi_ast::BareKey {
+    fn format(&self, f: &mut crate::Formatter) -> Result<(), std::fmt::Error> {
+        write!(f, "{}", self.syntax().text())
+    }
+}
+
+impl Format for tombi_ast::Key {
+    fn format(&self, f: &mut crate::Formatter) -> Result<(), std::fmt::Error> {
+        match self {
+            Self::BareKey(it) => it.format(f),
+            Self::BasicString(it) => it.format(f),
+            Self::LiteralString(it) => it.format(f),
+        }
+    }
+}
