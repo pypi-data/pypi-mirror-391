4378a74bbb2f560c02ca6f07eaeaa940a57f5fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 05:05:04 +0000

    Add package kdb-python/1.0.8/kdb_python-1.0.8.tar.gz

---
+++ packages/kdb-python/kdb_python-1.0.8.tar.gz/kdb_python-1.0.8/README.md
@@ -0,0 +1,2 @@
+# KDB framework
+KDB automation testing framework for web (desktop &amp; mobile) and mobile app (android &amp; iOS)
---
+++ packages/kdb-python/kdb_python-1.0.8.tar.gz/kdb_python-1.0.8/src/kdb_python/exceptions.py
@@ -0,0 +1,26 @@
+class DriverNotFoundError(Exception):
+    """
+    Exception raised when a driver is not found.
+
+    Example:
+
+        >>> b = Browser('unknown driver') # raises DriverNotFoundError
+    """
+
+    pass
+
+
+class ElementDoesNotExist(Exception):
+    """
+    Exception raised when an element is not found in the page.
+
+    The exception is raised only when someone tries to access the element,
+    not when the driver is finding it.
+
+    Example:
+
+        >>> elements = browser.find_by_id('unknown-id') # returns an empty list
+        >>> elements[0] # raises ElementDoesNotExist
+    """
+
+    pass
