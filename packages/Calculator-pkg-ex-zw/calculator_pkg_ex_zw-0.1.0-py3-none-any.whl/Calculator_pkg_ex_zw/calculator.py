2e29ed58ad37acf7cca49d80dfa8727517835602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 07:04:28 +0000

    Add package Calculator-pkg-ex-zw/0.1.0/calculator_pkg_ex_zw-0.1.0-py3-none-any.whl

---
+++ packages/Calculator-pkg-ex-zw/calculator_pkg_ex_zw-0.1.0-py3-none-any.whl/Calculator_pkg_ex_zw/calculator.py
@@ -0,0 +1,11 @@
+
+class Calculator:
+    def add(self, a, b):
+        print("I'm a calculator")
+        return a+b
+    def subtract(self, a, b):
+        return a-b
+    def multiply(self, a, b):
+        return a*b
+    def divide(self, a, b):
+        return a/b
