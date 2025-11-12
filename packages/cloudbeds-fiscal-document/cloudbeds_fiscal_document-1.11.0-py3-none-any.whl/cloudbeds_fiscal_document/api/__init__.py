8d2aed2e7c562af504239381b488f3197dbe2f5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 03:24:19 +0000

    Add package cloudbeds-fiscal-document/1.11.0/cloudbeds_fiscal_document-1.11.0-py3-none-any.whl

---
+++ packages/cloudbeds-fiscal-document/cloudbeds_fiscal_document-1.11.0-py3-none-any.whl/cloudbeds_fiscal_document/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from cloudbeds_fiscal_document.api.configs_api import ConfigsApi
+from cloudbeds_fiscal_document.api.features_api import FeaturesApi
+from cloudbeds_fiscal_document.api.fiscal_documents_api import FiscalDocumentsApi
+
