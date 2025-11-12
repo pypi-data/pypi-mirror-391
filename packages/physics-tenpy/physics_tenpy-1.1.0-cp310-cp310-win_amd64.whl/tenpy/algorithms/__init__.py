312bb7cb1051db2f6bbb93067abab09a41a4e77d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 14:06:06 +0000

    Add package physics-tenpy/1.1.0/physics_tenpy-1.1.0-cp310-cp310-win_amd64.whl

---
+++ packages/physics-tenpy/physics_tenpy-1.1.0-cp310-cp310-win_amd64.whl/tenpy/algorithms/__init__.py
@@ -0,0 +1,71 @@
+"""A collection of algorithms such as TEBD and DMRG.
+
+.. rubric:: Submodules
+
+.. autosummary::
+    :toctree: .
+
+    algorithm
+    tebd
+    mps_common
+    dmrg
+    dmrg_parallel
+    tdvp
+    purification
+    mpo_evolution
+    vumps
+    plane_wave_excitation
+    network_contractor
+    exact_diag
+    disentangler
+"""
+# Copyright (C) TeNPy Developers, Apache license
+
+from . import (
+    algorithm,
+    disentangler,
+    dmrg,
+    dmrg_parallel,
+    exact_diag,
+    mpo_evolution,
+    mps_common,
+    network_contractor,
+    plane_wave_excitation,
+    purification,
+    tdvp,
+    tebd,
+    vumps,
+)
+from .algorithm import *
+from .disentangler import *
+from .dmrg import *
+from .dmrg_parallel import *
+from .exact_diag import *
+from .mpo_evolution import *
+from .mps_common import *
+from .network_contractor import *
+from .plane_wave_excitation import *
+from .purification import *
+from .tdvp import *
+from .tebd import *
+from .vumps import *
+
+__all__ = [
+    *algorithm.__all__,
+    *dmrg.__all__,
+    *dmrg_parallel.__all__,
+    *disentangler.__all__,
+    *mps_common.__all__,
+    *tebd.__all__,
+    *tdvp.__all__,
+    *exact_diag.__all__,
+    *purification.__all__,
+    *network_contractor.__all__,
+    *mpo_evolution.__all__,
+    *vumps.__all__,
+    *plane_wave_excitation.__all__,
+]
+
+__skip_import__ = [
+    'truncation',  # deprecated, moved to tenpy.linalg
+]
---
+++ packages/physics-tenpy/physics_tenpy-1.1.0-cp310-cp310-win_amd64.whl/tenpy/algorithms/truncation.py
@@ -0,0 +1,14 @@
+"""Dummy module for backwards compatibility.
+
+The `tenpy.algorithms.truncation` module was moved to :mod:`tenpy.linalg.truncation`.
+Here we just import all the names again for backwards compatibility,
+to support loading pickle and HDF5 data files with `TruncationError` instances in them.
+"""
+# Copyright (C) TeNPy Developers, Apache license
+
+# just provide namespace from ..linalg.truncation
+
+from ..linalg import truncation
+from ..linalg.truncation import TruncationError, decompose_theta_qr_based, svd_theta, truncate  # noqa: F401
+
+__all__ = truncation.__all__
