3089a2e216ec712b80a6c3d26d565190ed2fd336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 15:05:12 +0000

    Add package quillsql/2.2.6/quillsql-2.2.6.tar.gz

---
+++ packages/quillsql/quillsql-2.2.6.tar.gz/quillsql-2.2.6/quillsql/utils/__init__.py
@@ -0,0 +1,4 @@
+# __init__.py
+
+from .run_query_processes import remove_fields, array_to_map
+from .filters import Filter, FilterType, FieldType, StringOperator, NumberOperator, NullOperator, DateOperator, convert_custom_filter
