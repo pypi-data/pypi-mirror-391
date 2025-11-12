b1f383fac7b5e04dd8b116b62721497385114ec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 08:04:29 +0000

    Add package sf-quant/0.1.15/sf_quant-0.1.15-py3-none-any.whl

---
+++ packages/sf-quant/sf_quant-0.1.15-py3-none-any.whl/sf_quant/data/__init__.py
@@ -0,0 +1,35 @@
+"""
+The ``data`` module provides access to assets and related datasets used
+throughout the package. It exposes a clean public API for loading asset data
+and retrieving available columns, while keeping internal implementation
+details hidden.
+"""
+
+from .assets import load_assets, load_assets_by_date, get_assets_columns
+from .crsp_daily import load_crsp_daily, get_crsp_daily_columns
+from .crsp_monthly import load_crsp_monthly, get_crsp_monthly_columns
+from .exposures import load_exposures, load_exposures_by_date, get_exposures_columns
+from .covariances import load_covariances_by_date, get_covariances_columns
+from .covariance_matrix import construct_covariance_matrix
+from .benchmark import load_benchmark
+from .factors import load_factors, get_factors_columns, get_factor_names
+
+__all__ = [
+    "load_assets",
+    "load_assets_by_date",
+    "get_assets_columns",
+    "load_crsp_daily",
+    "get_crsp_daily_columns",
+    "load_crsp_monthly",
+    "get_crsp_monthly_columns",
+    "load_exposures",
+    "load_exposures_by_date",
+    "get_exposures_columns",
+    "load_covariances_by_date",
+    "get_covariances_columns",
+    "construct_covariance_matrix",
+    "load_benchmark",
+    "load_factors",
+    "get_factors_columns",
+    "get_factor_names"
+]
