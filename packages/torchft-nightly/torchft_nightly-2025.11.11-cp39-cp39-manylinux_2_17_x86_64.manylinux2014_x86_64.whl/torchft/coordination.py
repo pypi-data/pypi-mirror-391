fa8e227b479f790f53c3927f3e006ccf1a2f0080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 03:23:51 +0000

    Add package torchft-nightly/2025.11.11/torchft_nightly-2025.11.11-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/torchft-nightly/torchft_nightly-2025.11.11-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/torchft/coordination.py
@@ -0,0 +1,33 @@
+"""
+Coordination (Low Level API)
+============================
+
+.. warning::
+    As torchft is still in development, the APIs in this module are subject to change.
+
+This module exposes low level coordination APIs to allow you to build your own
+custom fault tolerance algorithms on top of torchft.
+
+If you're looking for a more complete solution, please use the other modules in
+torchft.
+
+This provides direct access to the Lighthouse and Manager servers and clients.
+"""
+
+from torchft._torchft import (
+    LighthouseClient,
+    LighthouseServer,
+    ManagerClient,
+    ManagerServer,
+    Quorum,
+    QuorumMember,
+)
+
+__all__ = [
+    "LighthouseClient",
+    "LighthouseServer",
+    "ManagerServer",
+    "ManagerClient",
+    "Quorum",
+    "QuorumMember",
+]
