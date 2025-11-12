3c40772f6a35ea9ffbe59b94a498907f3ca85a85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 08:05:06 +0000

    Add package ORCA-API/0.0.2/orca_api-0.0.2.tar.gz

---
+++ packages/ORCA-API/orca_api-0.0.2.tar.gz/orca_api-0.0.2/src/pyorca/__init__.py
@@ -0,0 +1,4 @@
+"""pyORCA: Python bindings for the [ORCA](https://www.faccts.de/orca/) quantum chemistry package."""
+
+from pyorca import helper, input, output, settings
+from pyorca.run import run
