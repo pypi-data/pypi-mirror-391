6908d68f61c268ccc6554a65302a05dc5e20447b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 06:06:00 +0000

    Add package pyrelukko/0.9.0/pyrelukko-0.9.0.tar.gz

---
+++ packages/pyrelukko/pyrelukko-0.9.0.tar.gz/pyrelukko-0.9.0/cicd/tox_docs_pages.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+# REPO_DIR="$(readlink -f "${SCRIPT_DIR}/..")"
+# PUBLIC_DIR="${REPO_DIR}/public"
+
+# Main script execution
+main() {
+    # mkdir -p "${PUBLIC_DIR}"
+    # libdoc --pythonpath ./src  Relukko  "${PUBLIC_DIR}/index.html"
+    echo "Not Implemented yet"
+}
+
+# Execute the main function
+main
+
+# vim:set softtabstop=4 shiftwidth=4 tabstop=4 expandtab:
---
+++ packages/pyrelukko/pyrelukko-0.9.0.tar.gz/pyrelukko-0.9.0/src/pyrelukko/version.py
@@ -0,0 +1,2 @@
+# pylint: disable=all
+__version__ = "0.9.0"
