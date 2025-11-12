fca7f0aa1c47042871a776456f0b45c787469948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 17:06:56 +0000

    Add package aws-cost-calculator-cli/2.8.8/aws_cost_calculator_cli-2.8.8.tar.gz

---
+++ packages/aws-cost-calculator-cli/aws_cost_calculator_cli-2.8.8.tar.gz/aws_cost_calculator_cli-2.8.8/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='aws-cost-calculator-cli',
+    version='2.8.8',
+    packages=find_packages(exclude=['backend', 'backend.*', 'infrastructure', 'infrastructure.*', 'tests', 'tests.*']),
+    install_requires=[
+        'click>=8.0.0',
+        'boto3>=1.26.0',
+        'requests>=2.28.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'cc=cost_calculator.cli:cli',
+        ],
+    },
+    author='Cost Optimization Team',
+    author_email='',
+    description='AWS Cost Calculator CLI - Calculate daily and annual AWS costs across multiple accounts',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/trilogy-group/aws-cost-calculator',
+    project_urls={
+        'Documentation': 'https://github.com/trilogy-group/aws-cost-calculator/blob/main/README.md',
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Topic :: System :: Monitoring',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    python_requires='>=3.8',
+    keywords='aws cost calculator billing optimization cloud',
+)
