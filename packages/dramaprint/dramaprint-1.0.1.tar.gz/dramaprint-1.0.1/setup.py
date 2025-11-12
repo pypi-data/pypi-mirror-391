4d2cfe642cac61c528831c3557829f9bbbe69eee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 14:06:15 +0000

    Add package dramaprint/1.0.1/dramaprint-1.0.1.tar.gz

---
+++ packages/dramaprint/dramaprint-1.0.1.tar.gz/dramaprint-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name='dramaprint',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Jayden Liu',
+    description='Printing module with pausing and sound effects',
+    url='https://github.com/Dji-Mini-4/dramaprint'
+)
