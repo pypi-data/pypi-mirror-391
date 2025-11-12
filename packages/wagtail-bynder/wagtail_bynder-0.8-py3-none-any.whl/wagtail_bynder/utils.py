1fb0320a828283915c9d728dfdda4aa100aac1b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 17:06:45 +0000

    Add package wagtail-bynder/0.8/wagtail_bynder-0.8-py3-none-any.whl

---
+++ packages/wagtail-bynder/wagtail_bynder-0.8-py3-none-any.whl/wagtail_bynder/utils.py
@@ -0,0 +1,131 @@
+import mimetypes
+import os
+
+from http import HTTPStatus
+from io import BytesIO
+
+import requests
+
+from asgiref.local import Local
+from bynder_sdk import BynderClient
+from django.conf import settings
+from django.core.files import File
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.template.defaultfilters import filesizeformat
+from wagtail.models import Collection
+from willow import Image
+
+from .exceptions import BynderAssetDownloadError, BynderAssetFileTooLarge
+
+
+_DEFAULT_COLLECTION = Local()
+
+
+def download_file(
+    url: str, max_filesize: int, max_filesize_setting_name: str
+) -> InMemoryUploadedFile:
+    name = os.path.basename(url)
+    response = requests.get(url, timeout=20, stream=True)
+
+    # Make sure we don't store error responses instead of the file requested
+    if response.status_code != HTTPStatus.OK:
+        raise BynderAssetDownloadError(
+            f"Server error downloading '{name}' from Bynder. "
+        )
+
+    file = BytesIO()
+    # Stream the file to memory. We use iter_content() instead of the default iterator for requests.Response,
+    # as the latter uses iter_lines() which isn't suitable for streaming binary data.
+    for chunk in response.iter_content():
+        if not chunk:
+            continue
+        file.write(chunk)
+        if file.tell() > max_filesize:
+            file.truncate(0)
+            raise BynderAssetFileTooLarge(
+                f"File '{name}' exceeded the size limit enforced by the {max_filesize_setting_name} setting, which is currently set to {filesizeformat(max_filesize)}."
+            )
+
+    size = file.tell()
+    file.seek(0)
+
+    content_type, charset = mimetypes.guess_type(name)
+    return InMemoryUploadedFile(
+        file,
+        field_name="file",
+        name=name,
+        content_type=content_type,
+        size=size,
+        charset=charset,
+    )
+
+
+def download_document(url: str) -> InMemoryUploadedFile:
+    max_filesize_setting_name = "BYNDER_MAX_DOCUMENT_FILE_SIZE"
+    max_filesize = getattr(settings, max_filesize_setting_name, 5242880)
+    return download_file(url, max_filesize, max_filesize_setting_name)
+
+
+def download_image(url: str) -> InMemoryUploadedFile:
+    max_filesize_setting_name = "BYNDER_MAX_IMAGE_FILE_SIZE"
+    max_filesize = getattr(settings, max_filesize_setting_name, 5242880)
+    return download_file(url, max_filesize, max_filesize_setting_name)
+
+
+def get_image_info(file: File) -> tuple[int, int, str, bool]:
+    willow_image = Image.open(file)
+    width, height = willow_image.get_size()
+    return (width, height, willow_image.format_name, willow_image.has_animation())
+
+
+def get_output_image_format(original_format: str, *, is_animated: bool = False) -> str:
+    conversions = {
+        "avif": "png",
+        "bmp": "png",
+        "webp": "png",
+    }
+    if is_animated:
+        # Convert non-animated GIFs to PNG as well
+        conversions["gif"] = "png"
+
+    # Allow the user to override the conversions
+    custom_conversions = getattr(settings, "WAGTAILIMAGES_FORMAT_CONVERSIONS", {})
+    conversions.update(custom_conversions)
+
+    return conversions.get(original_format, original_format)
+
+
+def filename_from_url(url: str) -> str:
+    return os.path.basename(url)
+
+
+def get_bynder_client() -> BynderClient:
+    return BynderClient(
+        domain=getattr(settings, "BYNDER_DOMAIN", ""),
+        permanent_token=getattr(settings, "BYNDER_API_TOKEN", ""),
+    )
+
+
+def get_default_collection() -> Collection:
+    """
+    Return a Collection object that should be used as the default for images and
+    documents created to represent Bynder assets.
+
+    The result is cached for the current thread / asyncio task.
+    """
+    try:
+        return _DEFAULT_COLLECTION.value
+    except AttributeError:
+        pass
+
+    name = "Imported from Bynder"
+    try:
+        # Use existing collection
+        collection = Collection.objects.get(name=name)
+    except Collection.DoesNotExist:
+        # Create a new one
+        collection = Collection.get_first_root_node().add_child(name=name)
+
+    # Cache result for the current thread / asyncio task
+    _DEFAULT_COLLECTION.value = collection
+    return collection
---
+++ packages/wagtail-bynder/wagtail_bynder-0.8-py3-none-any.whl/wagtail_bynder/views/image.py
@@ -0,0 +1,99 @@
+from typing import TYPE_CHECKING
+
+from django.conf import settings
+from django.utils.html import format_html
+from django.views.generic import UpdateView
+from wagtail import VERSION as WAGTAIL_VERSION
+from wagtail.admin.modal_workflow import render_modal_workflow
+from wagtail.images import get_image_model
+from wagtail.images.views import chooser as chooser_views
+
+
+if WAGTAIL_VERSION < (6, 3):
+    from wagtail.images.views.images import DeleteView
+    from wagtail.images.views.images import edit as image_edit
+else:
+    from wagtail.images.views.images import DeleteView, EditView
+
+from wagtail_bynder.exceptions import BynderAssetDownloadError
+
+from .mixins import BynderAssetCopyMixin, RedirectToBynderMixin
+
+
+if TYPE_CHECKING:
+    from django.http import HttpRequest, JsonResponse
+
+
+if WAGTAIL_VERSION < (6, 3):
+
+    class ClassBasedWagtailImageEditView(UpdateView):
+        """
+        A class-based view that mimics the behaviour of wagtail's function-based
+        image edit view, and can be extended with view mixins.
+        """
+
+        # TODO: Use class from Wagtail once the image app views are refactored
+        model = get_image_model()
+        pk_url_kwarg = "image_id"
+
+        def get(self, request, *args, **kwargs):
+            return image_edit(request, *args, **kwargs)
+
+        def post(self, request, *args, **kwargs):
+            return image_edit(request, *args, **kwargs)
+
+    class ImageEditView(RedirectToBynderMixin, ClassBasedWagtailImageEditView):
+        pass
+else:
+
+    class ImageEditView(RedirectToBynderMixin, EditView):
+        pass
+
+
+class ImageDeleteView(RedirectToBynderMixin, DeleteView):
+    pass
+
+
+class ImageChooseView(chooser_views.ImageChooseView):
+    choose_one_text = chooser_views.ImageChooserViewSet.choose_one_text
+    choose_another_text = chooser_views.ImageChooserViewSet.choose_another_text
+    page_title = choose_one_text
+    permission_policy = chooser_views.ImageChooserViewSet.permission_policy
+    icon = chooser_views.ImageChooserViewSet.icon
+    template_name = "wagtailadmin/chooser/chooser-bynder.html"
+
+    def __init__(self, *args, **kwargs):
+        self.create_url_name = chooser_views.viewset.get_url_name("create")
+        self.results_url_name = chooser_views.viewset.get_url_name("choose_results")
+        self.collections = []
+        super().__init__(*args, **kwargs)
+
+
+class ImageChosenView(BynderAssetCopyMixin, chooser_views.ImageChosenView):
+    model = get_image_model()
+
+    def get(self, request: "HttpRequest", pk: str) -> "JsonResponse":
+        try:
+            try:
+                obj = self.model.objects.get(bynder_id=pk)
+            except self.model.DoesNotExist:
+                obj = self.create_object(pk)
+            else:
+                if getattr(settings, "BYNDER_SYNC_EXISTING_IMAGES_ON_CHOOSE", False):
+                    self.update_object(pk, obj)
+        except BynderAssetDownloadError as e:
+            # Return error step to display message in the chooser modal
+            return render_modal_workflow(
+                request,
+                None,
+                None,
+                None,
+                json_data={
+                    "step": "error",
+                    "error_message": format_html(
+                        "<strong>Failed to download image from Bynder:</strong> {error} Please try again later.",
+                        error=e,
+                    ),
+                },
+            )
+        return self.get_chosen_response(obj)
