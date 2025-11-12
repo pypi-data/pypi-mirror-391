40060e21146f88aef8110caba8920d4083830270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 14:05:31 +0000

    Add package dapr-agents/0.10.1/dapr_agents-0.10.1.tar.gz

---
+++ packages/dapr-agents/dapr_agents-0.10.1.tar.gz/dapr_agents-0.10.1/dapr_agents/workflow/__init__.py
@@ -0,0 +1,8 @@
+from .decorators import message_router, http_router, llm_activity, agent_activity
+
+__all__ = [
+    "message_router",
+    "http_router",
+    "llm_activity",
+    "agent_activity",
+]
