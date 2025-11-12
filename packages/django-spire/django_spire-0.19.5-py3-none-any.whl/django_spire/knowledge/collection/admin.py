56b113c8f6074b1172ff09bee6fce95f0a54898c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 17:07:15 +0000

    Add package django-spire/0.19.5/django_spire-0.19.5-py3-none-any.whl

---
+++ packages/django-spire/django_spire-0.19.5-py3-none-any.whl/django_spire/knowledge/collection/admin.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from django.contrib import admin
+
+from .models import Collection, CollectionGroup
+
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'parent', 'is_deleted']
+    list_select_related = ['parent']
+    list_filter = ['is_deleted', 'is_active']
+    search_fields = ['id', 'name', 'description', 'parent__name']
+    ordering = ['name']
+    autocomplete_fields = ['parent']
+
+
+@admin.register(CollectionGroup)
+class CollectionGroupAdmin(admin.ModelAdmin):
+    list_display = ['id', 'collection', 'auth_group']
+    list_select_related = ['collection', 'auth_group']
+    search_fields = ['id', 'collection__name', 'auth_group__name']
