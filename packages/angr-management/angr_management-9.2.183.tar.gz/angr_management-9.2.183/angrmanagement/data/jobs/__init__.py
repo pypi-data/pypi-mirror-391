d31b1657610cb33f6478bca1091962aa1d69db50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 06:08:35 +0000

    Add package angr-management/9.2.183/angr_management-9.2.183.tar.gz

---
+++ packages/angr-management/angr_management-9.2.183.tar.gz/angr_management-9.2.183/angrmanagement/data/jobs/__init__.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from .calling_convention_recovery import CallingConventionRecoveryJob
+from .cfg_generation import CFGGenerationJob
+from .code_tagging import CodeTaggingJob
+from .ddg_generation import DDGGenerationJob
+from .decompile_function import DecompileFunctionJob
+from .deobfuscation import APIDeobfuscationJob
+from .dependency_analysis import DependencyAnalysisJob
+from .flirt_signature_recognition import FlirtSignatureRecognitionJob
+from .job import Job
+from .prototype_finding import PrototypeFindingJob
+from .simgr_explore import SimgrExploreJob
+from .simgr_step import SimgrStepJob
+from .variable_recovery import VariableRecoveryJob
+from .vfg_generation import VFGGenerationJob
+
+__all__ = [
+    "APIDeobfuscationJob",
+    "CallingConventionRecoveryJob",
+    "CFGGenerationJob",
+    "CodeTaggingJob",
+    "DDGGenerationJob",
+    "DecompileFunctionJob",
+    "DependencyAnalysisJob",
+    "FlirtSignatureRecognitionJob",
+    "Job",
+    "PrototypeFindingJob",
+    "SimgrExploreJob",
+    "SimgrStepJob",
+    "VariableRecoveryJob",
+    "VFGGenerationJob",
+]
---
+++ packages/angr-management/angr_management-9.2.183.tar.gz/angr_management-9.2.183/angrmanagement/data/jobs/calling_convention_recovery.py
@@ -0,0 +1,95 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from angrmanagement.logic.threads import gui_thread_schedule_async
+
+from .job import InstanceJob
+
+if TYPE_CHECKING:
+    from angrmanagement.data.instance import Instance
+    from angrmanagement.logic.jobmanager import JobContext
+
+
+class CallingConventionRecoveryJob(InstanceJob):
+    """
+    Recover calling conventions for functions without recovering variables.
+    """
+
+    def __init__(
+        self,
+        instance: Instance,
+        on_finish=None,
+        on_cc_recovered=None,
+        workers: int | None = None,
+        func_addr: int | None = None,
+        auto_start: bool = False,
+        **kwargs,
+    ) -> None:
+        super().__init__("Calling Convention Recovery", instance, on_finish=on_finish)
+
+        self.cc_recovery_args = kwargs
+        self.on_cc_recovered = on_cc_recovered
+        self.workers = workers
+        self.ccc = None
+        self.started = False
+        self.auto_start = auto_start
+        self.func_addr = func_addr
+        self.func_addrs_to_prioritize = set() if func_addr is None else {func_addr}
+
+    def prioritize_function(self, func_addr: int) -> None:
+        """
+        Prioritize the specified function and its callee functions.
+
+        :param func_addr:   Address of the function to prioritize.
+        """
+        if not self.started:
+            # hasn't started - cache all requests
+            self.func_addrs_to_prioritize.add(func_addr)
+            return
+
+        if self.instance is None:
+            return
+        if self.ccc is None:
+            return
+
+        # find its callee functions
+        callees = set(self.instance.kb.functions.callgraph.successors(func_addr))
+        self.ccc.prioritize_functions({func_addr} | callees)
+
+    def run(self, ctx: JobContext) -> None:
+        self.started = True
+
+        cc_callback = self._cc_callback if self.on_cc_recovered is not None else None
+
+        # update addrs to prioritize with their callees
+        func_addrs_to_prioritize = set()
+        if self.func_addrs_to_prioritize:
+            for func_addr in self.func_addrs_to_prioritize:
+                if func_addr in self.instance.kb.functions:
+                    callees = set(self.instance.kb.functions.callgraph.successors(func_addr))
+                    func_addrs_to_prioritize |= {func_addr} | callees
+
+        self.ccc = self.instance.project.analyses.CompleteCallingConventions(
+            recover_variables=False,
+            low_priority=True,
+            cfg=self.instance.cfg.am_obj,
+            progress_callback=ctx.set_progress,
+            cc_callback=cc_callback,
+            max_function_blocks=2000,
+            max_function_size=16384,
+            workers=0 if self.workers is None else self.workers,
+            prioritize_func_addrs=func_addrs_to_prioritize,
+            skip_other_funcs=self.func_addr is not None,
+            auto_start=self.auto_start,
+            **self.cc_recovery_args,
+        )
+        self.ccc.work()
+
+        self.ccc = None
+
+    def _cc_callback(self, func_addr: int) -> None:
+        gui_thread_schedule_async(self.on_cc_recovered, args=(func_addr,))
+
+    def __repr__(self) -> str:
+        return "<Calling Convention Recovery Job>"
