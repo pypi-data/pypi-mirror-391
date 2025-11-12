38cb78c4cabd5612f1f12d9b7aa53a379b9aae73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 03:23:28 +0000

    Add package galileo-core/3.70.0/galileo_core-3.70.0-py3-none-any.whl

---
+++ packages/galileo-core/galileo_core-3.70.0-py3-none-any.whl/galileo_core/exceptions/execution.py
@@ -0,0 +1,7 @@
+from galileo_core.exceptions.base import BaseGalileoException
+
+
+class ExecutionError(BaseGalileoException):
+    """Raised when there is an issue with the execution of a task or process."""
+
+    ...
