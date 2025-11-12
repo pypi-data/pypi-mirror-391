1d6f5442ca303ca94877b78d00b4908bb514855b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 03:24:01 +0000

    Add package horizon-hwm-store/1.1.4/horizon_hwm_store-1.1.4.tar.gz

---
+++ packages/horizon-hwm-store/horizon_hwm_store-1.1.4.tar.gz/horizon_hwm_store-1.1.4/README.rst
@@ -0,0 +1,59 @@
+.. title
+
+Horizon HWM Store
+=================
+
+|Repo Status| |PyPI Latest Release| |PyPI License| |PyPI Python Version| |PyPI Downloads|
+|Documentation| |CI Status| |Test Coverage| |pre-commit.ci Status|
+
+.. |Repo Status| image:: https://www.repostatus.org/badges/latest/active.svg
+    :alt: Repo status - Active
+    :target: https://github.com/MobileTeleSystems/horizon-hwm-store
+.. |PyPI Latest Release| image:: https://img.shields.io/pypi/v/horizon-hwm-store
+    :alt: PyPI - Latest Release
+    :target: https://pypi.org/project/horizon-hwm-store/
+.. |PyPI License| image:: https://img.shields.io/pypi/l/horizon-hwm-store.svg
+    :alt: PyPI - License
+    :target: https://github.com/MobileTeleSystems/horizon-hwm-store/blob/develop/LICENSE.txt
+.. |PyPI Python Version| image:: https://img.shields.io/pypi/pyversions/horizon-hwm-store.svg
+    :alt: PyPI - Python Version
+    :target: https://pypi.org/project/horizon-hwm-store/
+.. |PyPI Downloads| image:: https://img.shields.io/pypi/dm/horizon-hwm-store
+    :alt: PyPI - Downloads
+    :target: https://pypi.org/project/horizon-hwm-store/
+.. |Documentation| image:: https://readthedocs.org/projects/horizon-hwm-store/badge/?version=stable
+    :alt: Documentation - ReadTheDocs
+    :target: https://horizon-hwm-store.readthedocs.io/
+.. |CI Status| image:: https://github.com/MobileTeleSystems/horizon-hwm-store/workflows/Tests/badge.svg
+    :alt: Github Actions - latest CI build status
+    :target: https://github.com/MobileTeleSystems/horizon-hwm-store/actions
+.. |Test Coverage| image:: https://img.shields.io/endpoint?url=https://gist.githubusercontent.com/
+    MTSOnGithub/03e73a82ecc4709934540ce8201cc3b4/raw/horizon-hwm-store_badge.json
+    :target: https://github.com/MobileTeleSystems/horizon-hwm-store/actions
+.. |pre-commit.ci Status| image:: https://results.pre-commit.ci/badge/github/MobileTeleSystems/horizon-hwm-store/develop.svg
+    :alt: pre-commit.ci - status
+    :target: https://results.pre-commit.ci/latest/github/MobileTeleSystems/horizon-hwm-store/develop
+
+``horizon-hwm-store`` is a Python library which implements `etl-entities' BaseHWMStore <https://etl-entities.readthedocs.io/en/stable/hwm_store/base_hwm_store.html>`_
+interface for `Horizon <https://data-horizon.readthedocs.io/>`_.
+
+Can be used by `onETL <https://onetl.readthedocs.io/en/stable/hwm_store/index.html>`_ for storing HWM values.
+
+Requirements
+------------
+
+**Python 3.7+**
+
+Documentation
+-------------
+
+See https://horizon-hwm-store.readthedocs.io/
+
+.. install
+
+Installation
+---------------
+
+.. code:: bash
+
+    pip install horizon-hwm-store
---
+++ packages/horizon-hwm-store/horizon_hwm_store-1.1.4.tar.gz/horizon_hwm_store-1.1.4/setup.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+
+import os
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+here = Path(__file__).parent.resolve()
+
+
+def get_version():
+    if os.getenv("GITHUB_REF_TYPE", "branch") == "tag":
+        return os.environ["GITHUB_REF_NAME"]
+
+    version_file = here / "horizon_hwm_store" / "VERSION"
+    version = version_file.read_text().strip()  # noqa: WPS410
+
+    build_num = os.environ.get("GITHUB_RUN_ID", "0")
+    branch_name = os.environ.get("GITHUB_REF_NAME", "")
+
+    if not branch_name:
+        return version
+
+    return f"{version}.dev{build_num}"
+
+
+def parse_requirements(file: Path) -> list[str]:
+    lines = file.read_text().splitlines()
+    return [line.rstrip() for line in lines if line and not line.startswith("#")]
+
+
+requirements = parse_requirements(here / "requirements.txt")
+long_description = (here / "README.rst").read_text()
+
+setup(
+    name="horizon-hwm-store",
+    version=get_version(),
+    author="DataOps.ETL Team",
+    author_email="onetools@mts.ru",
+    description="onETL Plugin for Horizon store",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    license="Apache-2.0",
+    license_files=("LICENSE.txt",),
+    url="https://github.com/MobileTeleSystems/horizon-hwm-store",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Topic :: Software Development :: Libraries",
+        "Typing :: Typed",
+    ],
+    project_urls={
+        "Documentation": "https://horizon-hwm-store.readthedocs.io/",
+        "Source": "https://github.com/MobileTeleSystems/horizon-hwm-store",
+        "CI/CD": "https://github.com/MobileTeleSystems/horizon-hwm-store/actions",
+        "Tracker": "https://github.com/MobileTeleSystems/horizon-hwm-store/issues",
+    },
+    keywords=["ETL", "Horizon", "HWM"],
+    entry_points={
+        "etl_entities.plugins": ["horizon-hwm-store=horizon_hwm_store"],
+        "tricoder_package_spy.register": ["horizon-hwm-store=horizon_hwm_store"],
+    },
+    packages=find_packages(exclude=["docs", "docs.*", "tests", "tests.*"]),
+    python_requires=">=3.7",
+    install_requires=requirements,
+    include_package_data=True,
+    zip_safe=False,
+)
