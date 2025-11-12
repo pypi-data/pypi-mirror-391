fbb184b26c96b00352f5a4aa1e43f7f99d05a549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 16:05:42 +0000

    Add package nedo-vision-annotator/1.0.0/nedo_vision_annotator-1.0.0.tar.gz

---
+++ packages/nedo-vision-annotator/nedo_vision_annotator-1.0.0.tar.gz/nedo_vision_annotator-1.0.0/nedo_vision_annotator/services/__init__.py
@@ -0,0 +1,15 @@
+"""
+Service modules for gRPC and RabbitMQ communication
+"""
+from .GrpcClientBase import GrpcClientBase, set_auth_failure_callback, get_auth_failure_callback
+from .AnnotatorGrpcClient import AnnotatorGrpcClient
+from .RabbitMQConsumer import RabbitMQConsumer
+
+__all__ = [
+    'GrpcClientBase',
+    'set_auth_failure_callback',
+    'get_auth_failure_callback',
+    'AnnotatorGrpcClient',
+    'RabbitMQConsumer'
+]
+
