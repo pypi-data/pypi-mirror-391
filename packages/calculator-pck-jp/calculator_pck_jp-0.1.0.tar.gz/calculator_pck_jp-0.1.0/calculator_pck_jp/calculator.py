63f21871cd3fc399543a80b5095d6c0fdfe122cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 07:04:44 +0000

    Add package calculator-pck-jp/0.1.0/calculator_pck_jp-0.1.0.tar.gz

---
+++ packages/calculator-pck-jp/calculator_pck_jp-0.1.0.tar.gz/calculator_pck_jp-0.1.0/calculator_pck_jp/calculator.py
@@ -0,0 +1,11 @@
+class Calculator:
+    def __init__(self):
+        print("hello")
+    def add(self, a ,b):
+        return a + b
+    def substract(self,a,b):
+        return a - b
+    def multiple(self, a ,b):
+        return a * b
+    def divide(self, a, b):
+        return a / b
