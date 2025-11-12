efb99a3c7954b178a1bc707735ac70bbca4d044c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 14:05:55 +0000

    Add package zapish-logger/1.1.3/zapish_logger-1.1.3-py3-none-any.whl

---
+++ packages/zapish-logger/zapish_logger-1.1.3-py3-none-any.whl/zapish_logger/__init__.py
@@ -0,0 +1,12 @@
+"""
+init for zapish_logger
+"""
+
+from zapish_logger.logger import (
+    file_logger,
+    console_logger,
+    read_log_file,
+    file_and_console_logger,
+    custom_logger,
+    LoggingConfig,
+)
