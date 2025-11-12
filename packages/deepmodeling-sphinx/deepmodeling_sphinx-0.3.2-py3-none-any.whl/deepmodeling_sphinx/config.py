1bc7f26e0b36083928dc680955584c2ccdc19755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 18:05:11 +0000

    Add package deepmodeling-sphinx/0.3.2/deepmodeling_sphinx-0.3.2-py3-none-any.whl

---
+++ packages/deepmodeling-sphinx/deepmodeling_sphinx-0.3.2-py3-none-any.whl/deepmodeling_sphinx/config.py
@@ -0,0 +1,33 @@
+sitemap = [
+    {"title": "Home", "url": "https://deepmodeling.com"},
+    {"title": "Blog", "url": "https://blogs.deepmodeling.com"},
+    {"title": "Tutorials", "url": "https://tutorials.deepmodeling.com"},
+    {
+        "title": "Docs",
+        "url": "https://docs.deepmodeling.com/",
+        "subitem": [
+            {
+                "title": "DeePMD-kit",
+                "url": "https://docs.deepmodeling.com/projects/deepmd/",
+            },
+            {"title": "DP-GEN", "url": "https://docs.deepmodeling.com/projects/dpgen/"},
+            {
+                "title": "dpdata",
+                "url": "https://docs.deepmodeling.com/projects/dpdata/",
+            },
+            {
+                "title": "DPDispatcher",
+                "url": "https://docs.deepmodeling.com/projects/dpdispatcher/",
+            },
+            {"title": "ABACUS", "url": "https://abacus.deepmodeling.com/"},
+            {"title": "DeepFlame", "url": "https://deepflame.deepmodeling.com/"},
+            {"title": "DPTI", "url": "https://docs.deepmodeling.com/projects/dpti/"},
+        ],
+    },
+    {"title": "Publications", "url": "https://blogs.deepmodeling.com/papers/"},
+    {"title": "GitHub", "url": "https://github.com/deepmodeling"},
+]
+
+active_class = "active docs-active"
+
+icp_no = "京ICP备20010051号-8"
