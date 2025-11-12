4245dc4a48402cb48bece577c41ec5e2896e3afd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 04:06:36 +0000

    Add package codexprime-aditya/0.0.1/codexprime_aditya-0.0.1.tar.gz

---
+++ packages/codexprime-aditya/codexprime_aditya-0.0.1.tar.gz/codexprime-aditya-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="codexprime-aditya",  # !! UNIQUE NAAM CHUNEIN !!
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "requests",  # Isse 'requests' library ki zaroorat hai
+    ],
+)
