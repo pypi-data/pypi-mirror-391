7bda9e7d3032ee4e3085e6c8e4707284191bb4a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 08:04:57 +0000

    Add package goldborn/0.1.0/goldborn-0.1.0-py3-none-any.whl

---
+++ packages/goldborn/goldborn-0.1.0-py3-none-any.whl/goldborn/__init__.py
@@ -0,0 +1,10 @@
+from .core import (
+    golden_fig,
+    square_fig,
+    Gax,
+    prepare_golden_fig,
+    prepare_square_fig,
+    plot_scatter,
+    plot_normality,
+    plot_univariate_distribution
+)
