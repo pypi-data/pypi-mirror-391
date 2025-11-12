1fa97b782ef744742a73ece837f972931897e687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 08:04:27 +0000

    Add package pyfelicita/0.1.14/pyfelicita-0.1.14.tar.gz

---
+++ packages/pyfelicita/pyfelicita-0.1.14.tar.gz/pyfelicita-0.1.14/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyfelicita",
+    version="0.1.14",
+    description="A Bluetooth scale integration for coffee machines",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Kevin Guldager Stampe",
+    author_email="kevinstampe@gmail.com",
+    url="https://github.com/kevinstampe/pyfelicita",  # Link to your GitHub repo
+    license="MIT",
+    packages=find_packages(),  # Automatically find submodules
+    install_requires=[
+        "bleak"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",  # Adjust as needed
+)
