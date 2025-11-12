fe9bccb0d0c64f72c493c4eff3ff91eaa77aef66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 15:05:20 +0000

    Add package dircomply/0.9.0/dircomply-0.9.0.tar.gz

---
+++ packages/dircomply/dircomply-0.9.0.tar.gz/dircomply-0.9.0/dircomply/version.py
@@ -0,0 +1,13 @@
+"""
+version.py
+
+Author: Benevant Mathew
+Date: 2025-11-12
+"""
+__version__ = "0.9.0"
+__author__ = "Benevant Mathew"
+__email__ = "benevantmathewv@gmail.com"
+__release_date__="12-11-2025"
+
+if __name__ == "__main__":
+    print(__version__)
