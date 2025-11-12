20422f5cb57645acb3244301c865248f450006f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 16:05:15 +0000

    Add package telnyx/3.12.1/telnyx-3.12.1.tar.gz

---
+++ packages/telnyx/telnyx-3.12.1.tar.gz/telnyx-3.12.1/src/telnyx/types/porting_order_phone_number_configuration_param.py
@@ -0,0 +1,25 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing_extensions import TypedDict
+
+from .._types import SequenceNotStr
+
+__all__ = ["PortingOrderPhoneNumberConfigurationParam"]
+
+
+class PortingOrderPhoneNumberConfigurationParam(TypedDict, total=False):
+    billing_group_id: str
+    """identifies the billing group to set on the numbers when ported"""
+
+    connection_id: str
+    """identifies the connection to set on the numbers when ported"""
+
+    emergency_address_id: str
+    """identifies the emergency address to set on the numbers when ported"""
+
+    messaging_profile_id: str
+    """identifies the messaging profile to set on the numbers when ported"""
+
+    tags: SequenceNotStr[str]
