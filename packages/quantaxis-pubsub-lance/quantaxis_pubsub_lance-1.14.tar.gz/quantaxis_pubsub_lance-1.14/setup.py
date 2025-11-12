8273dcdfba3a8ee0d8261522441ec0e529fb79dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 17:07:11 +0000

    Add package quantaxis-pubsub-lance/1.14/quantaxis_pubsub_lance-1.14.tar.gz

---
+++ packages/quantaxis-pubsub-lance/quantaxis_pubsub_lance-1.14.tar.gz/quantaxis_pubsub_lance-1.14/setup.py
@@ -0,0 +1,59 @@
+import codecs
+import io
+import os
+import re
+import sys
+import webbrowser
+import platform
+
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+
+NAME = "quantaxis_pubsub_lance"
+"""
+名字，一般放你包的名字即可
+"""
+PACKAGES = ["QAPUBSUB"]
+"""
+包含的包，可以多个，这是一个列表
+"""
+
+DESCRIPTION = "QUANTAXIS PUBSUB:QUANTAXIS PUB/SUB MODEL"
+KEYWORDS = ["quantaxis", "quant", "finance", "Backtest", 'Framework']
+AUTHOR_EMAIL = "yutiansut@qq.com"
+AUTHOR = 'yutiansut'
+URL = "https://github.com/yutiansut/QAPUBSUB"
+
+
+LICENSE = "MIT"
+
+setup(
+    name=NAME,
+    version='1.14',
+    description=DESCRIPTION,
+    long_description='publisher and subscriber',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=['pika==1.1.0', 'quantaxis>=1.10.18'],
+    entry_points={
+        'console_scripts': [
+            'qaps_pub = QAPUBSUB.__init__:debug_pub',
+            'qaps_sub = QAPUBSUB.__init__:debug_sub'
+        ]
+    },
+    keywords=KEYWORDS,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    license=LICENSE,
+    packages=PACKAGES,
+    include_package_data=True,
+    zip_safe=True
+)
