adcd5961ea200c402319db22df5118739ed2ac27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 03:23:48 +0000

    Add package japanese-address-parser-py/0.2.12/japanese_address_parser_py-0.2.12-cp37-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/japanese-address-parser-py/japanese_address_parser_py-0.2.12-cp37-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/japanese_address_parser_py/__init__.py
@@ -0,0 +1,5 @@
+from .japanese_address_parser_py import *
+
+__doc__ = japanese_address_parser_py.__doc__
+if hasattr(japanese_address_parser_py, "__all__"):
+    __all__ = japanese_address_parser_py.__all__
