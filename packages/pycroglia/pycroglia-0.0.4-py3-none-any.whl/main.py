19c9d11cd17dd468787aa386c1125f616a6c87ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 14:06:17 +0000

    Add package pycroglia/0.0.4/pycroglia-0.0.4-py3-none-any.whl

---
+++ packages/pycroglia/pycroglia-0.0.4-py3-none-any.whl/main.py
@@ -0,0 +1,13 @@
+from PyQt6 import QtWidgets
+
+from pycroglia.ui.widgets.wizard.config import DEFAULT_CONFIG
+from pycroglia.ui.widgets.wizard.wizard import ConfigurableMainStack
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    wizard = ConfigurableMainStack(config=DEFAULT_CONFIG)
+    wizard.setWindowTitle("Pycroglia")
+    screen = app.primaryScreen().availableGeometry()
+    wizard.resize(int(screen.width() * 0.75), int(screen.height() * 0.75))
+    wizard.show()
+    app.exec()
