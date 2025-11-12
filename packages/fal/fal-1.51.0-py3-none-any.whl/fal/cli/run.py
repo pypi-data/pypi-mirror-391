ed335a36446eb0733f2e72a886d7f52746f40427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 05:04:57 +0000

    Add package fal/1.51.0/fal-1.51.0-py3-none-any.whl

---
+++ packages/fal/fal-1.51.0-py3-none-any.whl/fal/cli/run.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+
+from ._utils import get_app_data_from_toml, is_app_name
+from .parser import FalClientParser, RefAction
+
+
+def _run(args):
+    from fal.api.client import SyncServerlessClient
+    from fal.utils import load_function_from
+
+    if is_app_name(args.func_ref):
+        app_name = args.func_ref[0]
+        app_ref, *_ = get_app_data_from_toml(app_name)
+        file_path, func_name = RefAction.split_ref(app_ref)
+    else:
+        file_path, func_name = args.func_ref
+        # Turn relative path into absolute path for files
+        file_path = str(Path(file_path).absolute())
+
+    client = SyncServerlessClient(host=args.host, team=args.team)
+    host = client._create_host(local_file_path=file_path)
+
+    loaded = load_function_from(host, file_path, func_name)
+
+    isolated_function = loaded.function
+    # let our exc handlers handle UserFunctionException
+    isolated_function.reraise = False
+    isolated_function()
+
+
+def add_parser(main_subparsers, parents):
+    run_help = "Run fal function."
+    epilog = "Examples:\n" "  fal run path/to/myfile.py::myfunc"
+    parser = main_subparsers.add_parser(
+        "run",
+        description=run_help,
+        parents=[*parents, FalClientParser(add_help=False)],
+        help=run_help,
+        epilog=epilog,
+    )
+    parser.add_argument(
+        "func_ref",
+        action=RefAction,
+        help="Function reference.",
+    )
+    parser.set_defaults(func=_run)
