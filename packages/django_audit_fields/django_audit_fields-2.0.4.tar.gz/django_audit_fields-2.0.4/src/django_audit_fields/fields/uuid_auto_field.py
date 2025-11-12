f1bd681f3bf2dcda84ba8c9d8c6706036377c382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 15:04:55 +0000

    Add package django_audit_fields/2.0.4/django_audit_fields-2.0.4.tar.gz

---
+++ packages/django_audit_fields/django_audit_fields-2.0.4.tar.gz/django_audit_fields-2.0.4/src/django_audit_fields/fields/uuid_auto_field.py
@@ -0,0 +1,17 @@
+import uuid
+
+from django.db.models.fields import UUIDField
+from django.utils.translation import gettext_lazy as _
+
+
+class UUIDAutoField(UUIDField):
+    """AutoField for Universally unique identifier."""
+
+    description = _("AutoField for universally unique identifier.")
+
+    def pre_save(self, model_instance, add):
+        value = super().pre_save(model_instance, add)
+        if (add and value is None) or not value:
+            value = uuid.uuid4()
+            setattr(model_instance, self.attname, value)
+        return value
