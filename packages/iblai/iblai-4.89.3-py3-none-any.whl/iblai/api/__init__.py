5c01a79df13c3402634968280899a4541afa1f33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 14:05:43 +0000

    Add package iblai/4.89.3/iblai-4.89.3-py3-none-any.whl

---
+++ packages/iblai/iblai-4.89.3-py3-none-any.whl/iblai/api/__init__.py
@@ -0,0 +1,29 @@
+# flake8: noqa
+
+# import apis into api package
+from iblai.api.ai_account_api import AiAccountApi
+from iblai.api.ai_analytics_api import AiAnalyticsApi
+from iblai.api.ai_bot_api import AiBotApi
+from iblai.api.ai_finetuning_api import AiFinetuningApi
+from iblai.api.ai_index_api import AiIndexApi
+from iblai.api.ai_marketing_api import AiMarketingApi
+from iblai.api.ai_media_api import AiMediaApi
+from iblai.api.ai_mentor_api import AiMentorApi
+from iblai.api.ai_prompt_api import AiPromptApi
+from iblai.api.analytics_api import AnalyticsApi
+from iblai.api.career_api import CareerApi
+from iblai.api.catalog_api import CatalogApi
+from iblai.api.commerce_api import CommerceApi
+from iblai.api.core_api import CoreApi
+from iblai.api.credentials_api import CredentialsApi
+from iblai.api.custom_domains_api import CustomDomainsApi
+from iblai.api.features_api import FeaturesApi
+from iblai.api.media_api import MediaApi
+from iblai.api.notifications_api import NotificationsApi
+from iblai.api.recommendations_api import RecommendationsApi
+from iblai.api.reports_api import ReportsApi
+from iblai.api.scim_api import ScimApi
+from iblai.api.search_api import SearchApi
+from iblai.api.skills_api import SkillsApi
+from iblai.api.v2_api import V2Api
+
