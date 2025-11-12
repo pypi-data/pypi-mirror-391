e7140c41c38d34718cd697106be1e078c65f4c1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 15:05:20 +0000

    Add package aa-fleetfinder/3.0.1/aa_fleetfinder-3.0.1.tar.gz

---
+++ packages/aa-fleetfinder/aa_fleetfinder-3.0.1.tar.gz/aa_fleetfinder-3.0.1/fleetfinder/static/fleetfinder/js/fleetfinder.js
@@ -0,0 +1,23 @@
+/* global bootstrap */
+
+/* jshint -W097 */
+'use strict';
+
+/**
+ * Bootstrap tooltip
+ *
+ * @param {string} [selector=body] Selector for the tooltip elements, defaults to 'body'
+ *                                 to apply to all elements with the data-bs-tooltip attribute.
+ *                                 Example: 'body', '.my-tooltip-class', '#my-tooltip-id'
+ *                                 If you want to apply it to a specific element, use that element's selector.
+ *                                 If you want to apply it to all elements with the data-bs-tooltip attribute,
+ *                                 use 'body' or leave it empty.
+ * @param {string} [namespace=aa-fleetfinderl] Namespace for the tooltip
+ * @returns {void}
+ */
+const fleetfinderBootstrapTooltip = ({selector = 'body', namespace = 'aa-fleetfinder'}) => { // eslint-disable-line no-unused-vars
+    document.querySelectorAll(`${selector} [data-bs-tooltip="${namespace}"]`)
+        .forEach((tooltipTriggerEl) => {
+            return new bootstrap.Tooltip(tooltipTriggerEl);
+        });
+};
