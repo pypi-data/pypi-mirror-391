1460f7dbca5466d5d69b653dddf806212d619ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 16:05:11 +0000

    Add package serverz/0.1.17/serverz-0.1.17-py3-none-any.whl

---
+++ packages/serverz/serverz-0.1.17-py3-none-any.whl/serverz/server/routers/__init__.py
@@ -0,0 +1,2 @@
+from .chat import router as chat_router
+from .reader import router as read_router
---
+++ packages/serverz/serverz-0.1.17-py3-none-any.whl/serverz/server/routers/task.py
@@ -0,0 +1,7 @@
+# import os
+
+# from daipilot.server.router.tasks import create_router
+# from serverz import logger
+
+
+# # router = create_router(database_url = os.getenv("database_url"),logger=logger)
