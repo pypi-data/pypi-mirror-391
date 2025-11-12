eee4c270791a6c1e04e84422063e2a9779c10fa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 15:05:01 +0000

    Add package types-aiobotocore-mediaconnect/2.25.2/types_aiobotocore_mediaconnect-2.25.2.tar.gz

---
+++ packages/types-aiobotocore-mediaconnect/types_aiobotocore_mediaconnect-2.25.2.tar.gz/types_aiobotocore_mediaconnect-2.25.2/types_aiobotocore_mediaconnect/__init__.pyi
@@ -0,0 +1,74 @@
+"""
+Main interface for mediaconnect service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediaconnect/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_mediaconnect import (
+        Client,
+        FlowActiveWaiter,
+        FlowDeletedWaiter,
+        FlowStandbyWaiter,
+        ListBridgesPaginator,
+        ListEntitlementsPaginator,
+        ListFlowsPaginator,
+        ListGatewayInstancesPaginator,
+        ListGatewaysPaginator,
+        ListOfferingsPaginator,
+        ListReservationsPaginator,
+        MediaConnectClient,
+    )
+
+    session = get_session()
+    async with session.create_client("mediaconnect") as client:
+        client: MediaConnectClient
+        ...
+
+
+    flow_active_waiter: FlowActiveWaiter = client.get_waiter("flow_active")
+    flow_deleted_waiter: FlowDeletedWaiter = client.get_waiter("flow_deleted")
+    flow_standby_waiter: FlowStandbyWaiter = client.get_waiter("flow_standby")
+
+    list_bridges_paginator: ListBridgesPaginator = client.get_paginator("list_bridges")
+    list_entitlements_paginator: ListEntitlementsPaginator = client.get_paginator("list_entitlements")
+    list_flows_paginator: ListFlowsPaginator = client.get_paginator("list_flows")
+    list_gateway_instances_paginator: ListGatewayInstancesPaginator = client.get_paginator("list_gateway_instances")
+    list_gateways_paginator: ListGatewaysPaginator = client.get_paginator("list_gateways")
+    list_offerings_paginator: ListOfferingsPaginator = client.get_paginator("list_offerings")
+    list_reservations_paginator: ListReservationsPaginator = client.get_paginator("list_reservations")
+    ```
+"""
+
+from .client import MediaConnectClient
+from .paginator import (
+    ListBridgesPaginator,
+    ListEntitlementsPaginator,
+    ListFlowsPaginator,
+    ListGatewayInstancesPaginator,
+    ListGatewaysPaginator,
+    ListOfferingsPaginator,
+    ListReservationsPaginator,
+)
+from .waiter import FlowActiveWaiter, FlowDeletedWaiter, FlowStandbyWaiter
+
+Client = MediaConnectClient
+
+__all__ = (
+    "Client",
+    "FlowActiveWaiter",
+    "FlowDeletedWaiter",
+    "FlowStandbyWaiter",
+    "ListBridgesPaginator",
+    "ListEntitlementsPaginator",
+    "ListFlowsPaginator",
+    "ListGatewayInstancesPaginator",
+    "ListGatewaysPaginator",
+    "ListOfferingsPaginator",
+    "ListReservationsPaginator",
+    "MediaConnectClient",
+)
---
+++ packages/types-aiobotocore-mediaconnect/types_aiobotocore_mediaconnect-2.25.2.tar.gz/types_aiobotocore_mediaconnect-2.25.2/types_aiobotocore_mediaconnect/waiter.pyi
@@ -0,0 +1,86 @@
+"""
+Type annotations for mediaconnect service client waiters.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediaconnect/waiters/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_mediaconnect.client import MediaConnectClient
+    from types_aiobotocore_mediaconnect.waiter import (
+        FlowActiveWaiter,
+        FlowDeletedWaiter,
+        FlowStandbyWaiter,
+    )
+
+    session = get_session()
+    async with session.create_client("mediaconnect") as client:
+        client: MediaConnectClient
+
+        flow_active_waiter: FlowActiveWaiter = client.get_waiter("flow_active")
+        flow_deleted_waiter: FlowDeletedWaiter = client.get_waiter("flow_deleted")
+        flow_standby_waiter: FlowStandbyWaiter = client.get_waiter("flow_standby")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+
+from aiobotocore.waiter import AIOWaiter
+
+from .type_defs import (
+    DescribeFlowRequestWaitExtraExtraTypeDef,
+    DescribeFlowRequestWaitExtraTypeDef,
+    DescribeFlowRequestWaitTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+__all__ = ("FlowActiveWaiter", "FlowDeletedWaiter", "FlowStandbyWaiter")
+
+class FlowActiveWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediaconnect/waiter/FlowActive.html#MediaConnect.Waiter.FlowActive)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediaconnect/waiters/#flowactivewaiter)
+    """
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeFlowRequestWaitTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediaconnect/waiter/FlowActive.html#MediaConnect.Waiter.FlowActive.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediaconnect/waiters/#flowactivewaiter)
+        """
+
+class FlowDeletedWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediaconnect/waiter/FlowDeleted.html#MediaConnect.Waiter.FlowDeleted)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediaconnect/waiters/#flowdeletedwaiter)
+    """
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeFlowRequestWaitExtraTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediaconnect/waiter/FlowDeleted.html#MediaConnect.Waiter.FlowDeleted.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediaconnect/waiters/#flowdeletedwaiter)
+        """
+
+class FlowStandbyWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediaconnect/waiter/FlowStandby.html#MediaConnect.Waiter.FlowStandby)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediaconnect/waiters/#flowstandbywaiter)
+    """
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeFlowRequestWaitExtraExtraTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediaconnect/waiter/FlowStandby.html#MediaConnect.Waiter.FlowStandby.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediaconnect/waiters/#flowstandbywaiter)
+        """
