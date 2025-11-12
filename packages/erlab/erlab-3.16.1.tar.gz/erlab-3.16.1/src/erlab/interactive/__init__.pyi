b8a64a8473d1d8c5cf651691cc2f4006eed7faf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 03:24:22 +0000

    Add package erlab/3.16.1/erlab-3.16.1.tar.gz

---
+++ packages/erlab/erlab-3.16.1.tar.gz/erlab-3.16.1/src/erlab/interactive/__init__.pyi
@@ -0,0 +1,36 @@
+__all__ = [
+    "_dask",
+    "_options",
+    "colors",
+    "data_explorer",
+    "derivative",
+    "dtool",
+    "explorer",
+    "fermiedge",
+    "goldtool",
+    "imagetool",
+    "itool",
+    "kspace",
+    "ktool",
+    "options",
+    "restool",
+    "utils",
+]
+
+from . import (
+    _dask,
+    _options,
+    colors,
+    derivative,
+    explorer,
+    fermiedge,
+    imagetool,
+    kspace,
+    utils,
+)
+from ._options import options
+from .derivative import dtool
+from .explorer import data_explorer
+from .fermiedge import goldtool, restool
+from .imagetool import itool
+from .kspace import ktool
