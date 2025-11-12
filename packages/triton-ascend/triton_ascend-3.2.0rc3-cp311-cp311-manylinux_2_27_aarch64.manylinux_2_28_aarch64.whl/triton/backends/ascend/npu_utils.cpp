4be4ce70e31fb4d630f8a87711c6d2e979017ad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 17:06:48 +0000

    Add package triton-ascend/3.2.0rc3/triton_ascend-3.2.0rc3-cp311-cp311-manylinux_2_27_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/triton-ascend/triton_ascend-3.2.0rc3-cp311-cp311-manylinux_2_27_aarch64.manylinux_2_28_aarch64.whl/triton/backends/ascend/npu_utils.cpp
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+#include <memory>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+
+#include "experiment/runtime/runtime/rt.h"
+
+// Use map to differentiate same name functions from different binary
+static std::unordered_map<std::string, size_t> registered_names;
+static std::unordered_map<std::string, std::unique_ptr<size_t>> func_stubs;
+
+static std::tuple<void *, void *>
+registerKernel(const char *name, const void *data, size_t data_size, int shared,
+               int device, const char *kernel_mode_str) {
+  rtError_t rtRet;
+
+  rtDevBinary_t devbin;
+  devbin.data = data;
+  devbin.length = data_size;
+  const std::string kernel_mode{kernel_mode_str};
+  if (kernel_mode == "aiv")
+    devbin.magic = RT_DEV_BINARY_MAGIC_ELF_AIVEC;
+  else
+    devbin.magic = RT_DEV_BINARY_MAGIC_ELF;
+  devbin.version = 0;
+
+  rtRet = rtSetDevice(device);
+  if (rtRet != RT_ERROR_NONE) {
+    printf("rtSetDevice failed, 0x%x\n", rtRet);
+    return {NULL, NULL};
+  }
+
+  void *devbinHandle = NULL;
+  rtRet = rtDevBinaryRegister(&devbin, &devbinHandle);
+  if (rtRet != RT_ERROR_NONE) {
+    printf("rtDevBinaryRegister failed, 0x%x\n", rtRet);
+    return {NULL, NULL};
+  }
+
+  std::string stubName = name;
+  stubName += "_" + std::to_string(registered_names[name]);
+  registered_names[name]++;
+  auto registered = func_stubs.emplace(stubName, std::make_unique<size_t>(0));
+  void *func_stub_handle = registered.first->second.get();
+  rtRet = rtFunctionRegister(devbinHandle, func_stub_handle, stubName.c_str(),
+                             (void *)name, 0);
+  if (rtRet != RT_ERROR_NONE) {
+    printf("rtFunctionRegister failed(stubName = %s), 0x%x\n", stubName.c_str(),
+           rtRet);
+    return {NULL, NULL};
+  }
+
+  return std::make_tuple(devbinHandle, func_stub_handle);
+}
+
+static PyObject *loadKernelBinary(PyObject *self, PyObject *args) {
+  const char *name;        // kernel name
+  const char *data;        // binary pointer
+  Py_ssize_t data_size;    // binary size
+  int shared;              // shared_memory(meaningless now)
+  int device;              // device ID
+  const char *kernel_mode; // kernel mode
+
+  if (!PyArg_ParseTuple(args, "ss#iis", &name, &data, &data_size, &shared,
+                        &device, &kernel_mode)) {
+    return NULL;
+  }
+
+  auto [module_handle, func_handle] =
+      registerKernel(name, data, data_size, shared, device, kernel_mode);
+
+  uint64_t mod = reinterpret_cast<uint64_t>(module_handle);
+  uint64_t func = reinterpret_cast<uint64_t>(func_handle);
+  if (PyErr_Occurred()) {
+    return NULL;
+  }
+
+  return Py_BuildValue("(KKii)", mod, func, 0, 0);
+}
+
+static PyObject *getArch(PyObject *self, PyObject *args) {
+  char name[64] = {'\0'};
+
+  rtError_t rtRet = rtGetSocVersion(name, 64);
+
+  if (rtRet != RT_ERROR_NONE) {
+    printf("rtGetSocVersion failed, 0x%x", rtRet);
+    return NULL;
+  }
+  if (PyErr_Occurred()) {
+    return NULL;
+  }
+  return Py_BuildValue("s", name);
+}
+
+static PyObject *getAiCoreNum(PyObject *self, PyObject *args) {
+  uint32_t aiCoreCnt;
+
+  rtError_t rtRet = rtGetAiCoreCount(&aiCoreCnt);
+
+  if (rtRet != RT_ERROR_NONE) {
+    printf("rtGetAiCoreCount failed, 0x%x", rtRet);
+    return NULL;
+  }
+  if (PyErr_Occurred()) {
+    return NULL;
+  }
+  return Py_BuildValue("I", aiCoreCnt);
+}
+
+static PyMethodDef NpuUtilsMethods[] = {
+    {"load_kernel_binary", loadKernelBinary, METH_VARARGS,
+     "Load NPU kernel binary into NPU driver"},
+    {"get_arch", getArch, METH_VARARGS, "Get soc version of NPU"},
+    // sentinel
+    {"get_aicore_num", getAiCoreNum, METH_VARARGS, "Get the number of AI core"},
+    {NULL, NULL, 0, NULL}};
+
+static PyModuleDef ModuleDef = {
+    PyModuleDef_HEAD_INIT, "npu_utils",
+    "Utilities for fetching NPU device info and preparing kernel binary", -1,
+    NpuUtilsMethods};
+
+PyMODINIT_FUNC PyInit_npu_utils(void) {
+  PyObject *m = PyModule_Create(&ModuleDef);
+  if (m == NULL) {
+    return NULL;
+  }
+
+  PyModule_AddFunctions(m, NpuUtilsMethods);
+  return m;
+}
