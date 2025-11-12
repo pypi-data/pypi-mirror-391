7f16521ea8b8e3c918555a0418ac1704bc4f0637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 15:05:12 +0000

    Add package quillsql/2.2.6/quillsql-2.2.6-py3-none-any.whl

---
+++ packages/quillsql/quillsql-2.2.6-py3-none-any.whl/quillsql/__init__.py
@@ -0,0 +1,5 @@
+# __init__.py
+
+from .core import Quill
+from .error import PgQueryError
+from .utils import Filter, FilterType, FieldType, StringOperator, DateOperator, NumberOperator, NullOperator
