4a038eccd8a7611d1c2cca77f73ab0264fe6e426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 16:05:25 +0000

    Add package geondptinf/0.0.0.0.0/geondptinf-0.0.0.0.0-py3-none-any.whl

---
+++ packages/geondptinf/geondptinf-0.0.0.0.0-py3-none-any.whl/geondptinf/geondpt.py
@@ -0,0 +1,2 @@
+print("Thank you for your interest, however, geondptinf is a licensed library available at https://geond.tech, this is a placegolder. You can use the freely available geondptfree instead, if you want to try out its funcionality.")
+quit()
