fd66305c1dfa545382c158af21993b07116c266f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 17:07:23 +0000

    Add package fractal-server/2.17.1a1/fractal_server-2.17.1a1.tar.gz

---
+++ packages/fractal-server/fractal_server-2.17.1a1.tar.gz/fractal_server-2.17.1a1/fractal_server/app/routes/admin/v2/__init__.py
@@ -0,0 +1,28 @@
+"""
+`admin/v2` module
+"""
+from fastapi import APIRouter
+
+from .accounting import router as accounting_router
+from .impersonate import router as impersonate_router
+from .job import router as job_router
+from .profile import router as profile_router
+from .project import router as project_router
+from .resource import router as resource_router
+from .task import router as task_router
+from .task_group import router as task_group_router
+from .task_group_lifecycle import router as task_group_lifecycle_router
+
+router_admin_v2 = APIRouter()
+
+router_admin_v2.include_router(accounting_router, prefix="/accounting")
+router_admin_v2.include_router(job_router, prefix="/job")
+router_admin_v2.include_router(project_router, prefix="/project")
+router_admin_v2.include_router(task_router, prefix="/task")
+router_admin_v2.include_router(task_group_router, prefix="/task-group")
+router_admin_v2.include_router(
+    task_group_lifecycle_router, prefix="/task-group"
+)
+router_admin_v2.include_router(impersonate_router, prefix="/impersonate")
+router_admin_v2.include_router(resource_router, prefix="/resource")
+router_admin_v2.include_router(profile_router, prefix="/profile")
