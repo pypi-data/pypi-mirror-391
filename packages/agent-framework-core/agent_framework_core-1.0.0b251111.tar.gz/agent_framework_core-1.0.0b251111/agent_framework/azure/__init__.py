9b7148894164a708fc8a45a19134ebca36b988f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 14:06:26 +0000

    Add package agent-framework-core/1.0.0b251111/agent_framework_core-1.0.0b251111.tar.gz

---
+++ packages/agent-framework-core/agent_framework_core-1.0.0b251111.tar.gz/agent_framework_core-1.0.0b251111/agent_framework/azure/__init__.py
@@ -0,0 +1,32 @@
+# Copyright (c) Microsoft. All rights reserved.
+
+
+import importlib
+from typing import Any
+
+_IMPORTS: dict[str, tuple[str, str]] = {
+    "AzureAIAgentClient": ("agent_framework_azure_ai", "azure-ai"),
+    "AzureOpenAIAssistantsClient": ("agent_framework.azure._assistants_client", "core"),
+    "AzureOpenAIChatClient": ("agent_framework.azure._chat_client", "core"),
+    "AzureAISettings": ("agent_framework_azure_ai", "azure-ai"),
+    "AzureOpenAISettings": ("agent_framework.azure._shared", "core"),
+    "AzureOpenAIResponsesClient": ("agent_framework.azure._responses_client", "core"),
+    "get_entra_auth_token": ("agent_framework.azure._entra_id_authentication", "core"),
+}
+
+
+def __getattr__(name: str) -> Any:
+    if name in _IMPORTS:
+        package_name, package_extra = _IMPORTS[name]
+        try:
+            return getattr(importlib.import_module(package_name), name)
+        except ModuleNotFoundError as exc:
+            raise ModuleNotFoundError(
+                f"please use `pip install agent-framework-{package_extra}`, "
+                "or update your requirements.txt or pyproject.toml file."
+            ) from exc
+    raise AttributeError(f"Module `azure` has no attribute {name}.")
+
+
+def __dir__() -> list[str]:
+    return list(_IMPORTS.keys())
