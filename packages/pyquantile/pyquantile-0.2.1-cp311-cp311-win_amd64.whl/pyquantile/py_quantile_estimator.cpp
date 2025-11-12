094b4f1e9ae140214f87a0200875cf526d54ecf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 17:06:38 +0000

    Add package pyquantile/0.2.1/pyquantile-0.2.1-cp311-cp311-win_amd64.whl

---
+++ packages/pyquantile/pyquantile-0.2.1-cp311-cp311-win_amd64.whl/pyquantile/py_quantile_estimator.cpp
@@ -0,0 +1,27 @@
+//// UNCOMMENT FOR BUILDING AND DISTRIBUTING
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>  
+#include "quantile-estimator.h" 
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(_pyquantile, m) { 
+    m.doc() = "A fast C++ P-Square quantile estimator for streaming data.";
+
+
+    py::class_<QuantileEstimator>(m, "QuantileEstimator")
+
+        .def(py::init<double>(), py::arg("p"),
+            "Initializes the estimator for quantile 'p' (0 to 1).")
+
+
+        .def("add", &QuantileEstimator::Add, py::arg("value"),
+            "Adds a single data point to the stream.")
+
+        .def("quantile", &QuantileEstimator::GetQuantile,
+            "Returns the current estimated quantile for 'p'.")
+
+
+        .def("get_markers", &QuantileEstimator::GetCurrentMarkers,
+            "Returns the current markers (Q0, Q1, Q2, Q3, Q4) as a tuple.");
+}
