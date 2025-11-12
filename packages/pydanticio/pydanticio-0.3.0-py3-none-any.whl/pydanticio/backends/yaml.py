731299746e79157f65228c2eb12ef129cdeb8a3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 16:05:30 +0000

    Add package pydanticio/0.3.0/pydanticio-0.3.0-py3-none-any.whl

---
+++ packages/pydanticio/pydanticio-0.3.0-py3-none-any.whl/pydanticio/backends/yaml.py
@@ -0,0 +1,13 @@
+from typing import TextIO
+
+import yaml
+from pydantic import BaseModel
+
+
+def read_record[T: BaseModel](reader: TextIO, model: type[T]) -> T:
+    data = yaml.safe_load(reader)
+    return model.model_validate(data)
+
+
+def write_record(writer: TextIO, record: BaseModel) -> None:
+    yaml.safe_dump(record.model_dump(mode="json"), writer)
