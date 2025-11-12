fdd4fa4d67986022e772d4caa3b4713573bbbd91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 14:05:15 +0000

    Add package crystalwindow/3.0/crystalwindow-3.0.tar.gz

---
+++ packages/crystalwindow/crystalwindow-3.0.tar.gz/crystalwindow-3.0/crystalwindow/gametests/__init__.py
@@ -0,0 +1,14 @@
+"""
+CrystalWindow Example Demos
+---------------------------
+Run demos like this:
+
+    python -m CrystalWindow.examples.<demo_name>
+
+Available demos:
+    guitesting     → GUI widgets & layout demo
+    gravitytest    → Physics & gravity simulation
+    windowtesting  → Basic window + drawing
+    sandbox        → Open testbed for experiments
+"""
+__all__ = ["guitesting", "gravitytest", "windowtesting", "sandbox"]
---
+++ packages/crystalwindow/crystalwindow-3.0.tar.gz/crystalwindow-3.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="crystalwindow",        # this is the name ppl will pip install
+    version="3.0",                # update when u change stuff
+    packages=find_packages(include=["crystalwindow", "crystalwindow.*"]),
+    include_package_data=True,
+    install_requires=["pygame>=2.3.0"],
+    author="CrystalBallyHereXD",
+    author_email="mavilla.519@gmail.com",
+    description="Easier Pygame!, Made by Crystal (mee)!!",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    python_requires='>=3.1',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
