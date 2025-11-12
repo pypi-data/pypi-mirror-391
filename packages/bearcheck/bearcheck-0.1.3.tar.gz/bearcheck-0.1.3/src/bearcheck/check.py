93f5e8766ded5ce2aa62d6478b58c6c67721fe90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 17:07:19 +0000

    Add package bearcheck/0.1.3/bearcheck-0.1.3.tar.gz

---
+++ packages/bearcheck/bearcheck-0.1.3.tar.gz/bearcheck-0.1.3/src/bearcheck/check.py
@@ -0,0 +1,27 @@
+from typing import Any, cast
+from beartype.door import die_if_unbearable, is_bearable
+
+
+class Check[A]:  # type: ignore
+    pass
+
+
+def bearcheck[T](a: Any, check: type[Check[T]]) -> T:
+    args = getattr(check, "__args__", None)
+    if args is None:
+        if not isinstance(check, Check):
+            raise ValueError(f"Not a valid Check class: {check!r}")
+        raise ValueError("Check class must be templated e.g. Check[int | str]")
+    die_if_unbearable(a, args[0])
+    return cast(T, a)
+
+
+def beartest[T](a: Any, check: type[Check[T]]) -> T | None:
+    args = getattr(check, "__args__", None)
+    if args is None:
+        if not isinstance(check, Check):
+            raise ValueError(f"Not a valid Check class: {check!r}")
+        raise ValueError("Check class must be templated e.g. Check[int | str]")
+    if is_bearable(a, args[0]):
+        return cast(T, a)
+    return None
