bb38c5ae1be34a37265279ac89826f577598cd30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 15:04:56 +0000

    Add package types-aiobotocore-artifact/2.25.2/types_aiobotocore_artifact-2.25.2.tar.gz

---
+++ packages/types-aiobotocore-artifact/types_aiobotocore_artifact-2.25.2.tar.gz/types_aiobotocore_artifact-2.25.2/types_aiobotocore_artifact/__init__.py
@@ -0,0 +1,36 @@
+"""
+Main interface for artifact service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_artifact/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_artifact import (
+        ArtifactClient,
+        Client,
+        ListCustomerAgreementsPaginator,
+        ListReportsPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("artifact") as client:
+        client: ArtifactClient
+        ...
+
+
+    list_customer_agreements_paginator: ListCustomerAgreementsPaginator = client.get_paginator("list_customer_agreements")
+    list_reports_paginator: ListReportsPaginator = client.get_paginator("list_reports")
+    ```
+"""
+
+from .client import ArtifactClient
+from .paginator import ListCustomerAgreementsPaginator, ListReportsPaginator
+
+Client = ArtifactClient
+
+
+__all__ = ("ArtifactClient", "Client", "ListCustomerAgreementsPaginator", "ListReportsPaginator")
