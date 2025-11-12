77f823e5ce89adec9c21505c9e6e4fe54f9f91be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 14:05:52 +0000

    Add package PyPermission/0.2.1/pypermission-0.2.1-py3-none-any.whl

---
+++ packages/PyPermission/pypermission-0.2.1-py3-none-any.whl/pypermission/__init__.py
@@ -0,0 +1,48 @@
+"""
+`PyPermission` - The python RBAC library for projects where `sqlalchemy` is a valid option.
+
+Author: Jendrik A. Potyka, Fabian A. Preiss
+"""
+
+__version__ = "0.2.1"
+__author__ = "Jendrik A. Potyka, Fabian A. Preiss"
+
+from typing import Final
+
+from pypermission.service.role import RoleService
+from pypermission.service.subject import SubjectService
+from pypermission.models import (
+    Policy,
+    Permission,
+    FrozenClass,
+    create_rbac_database_table,
+    set_sqlite_pragma,
+)
+from pypermission.exc import PyPermissionError, PermissionNotGrantedError
+
+
+class RBAC(metaclass=FrozenClass):
+    """
+    Namespace for the Role and Subject services.
+
+    Attributes
+    ----------
+    role : RoleService
+        Shorthand for all RoleService functions.
+    subject : SubjectService
+        Shorthand for all SubjectService functions.
+    """
+
+    role: Final = RoleService
+    subject: Final = SubjectService
+
+
+__all__ = [
+    "RBAC",
+    "Policy",
+    "Permission",
+    "create_rbac_database_table",
+    "set_sqlite_pragma",
+    "PyPermissionError",
+    "PermissionNotGrantedError",
+]
