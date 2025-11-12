4403e04fe725bdd2a686783b56153fa3d37c853e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 16:05:27 +0000

    Add package geondtfinf/0.0.0.0.0/geondtfinf-0.0.0.0.0-py3-none-any.whl

---
+++ packages/geondtfinf/geondtfinf-0.0.0.0.0-py3-none-any.whl/geondtfinf/geondtf.py
@@ -0,0 +1,2 @@
+print("Thank you for your interest, however, geondtfinf is a licensed library available at https://geond.tech, this is a placegolder. You can use the freely available geondtffree instead, if you want to try out its funcionality.")
+quit()
