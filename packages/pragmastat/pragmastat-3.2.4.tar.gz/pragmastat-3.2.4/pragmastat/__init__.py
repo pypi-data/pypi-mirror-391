5b2a85677105dd0fd06a9b359b1d606c95e31f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 03:24:05 +0000

    Add package pragmastat/3.2.4/pragmastat-3.2.4.tar.gz

---
+++ packages/pragmastat/pragmastat-3.2.4.tar.gz/pragmastat-3.2.4/pragmastat/__init__.py
@@ -0,0 +1,21 @@
+from .estimators import (
+    center,
+    spread,
+    rel_spread,
+    shift,
+    ratio,
+    avg_spread,
+    disparity,
+)
+
+__all__ = [
+    "center",
+    "spread",
+    "rel_spread",
+    "shift",
+    "ratio",
+    "avg_spread",
+    "disparity",
+]
+
+__version__ = "3.2.4"
