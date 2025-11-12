49608956478dc204c912a1da9105f72fda037325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 08:04:37 +0000

    Add package wnstatmech/1.0.4/wnstatmech-1.0.4.tar.gz

---
+++ packages/wnstatmech/wnstatmech-1.0.4.tar.gz/wnstatmech-1.0.4/wnstatmech/__about__.py
@@ -0,0 +1,18 @@
+"""
+A package of python routines to handle thermodynamics of
+fermions and bosons.
+"""
+
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__version__",
+    "__author__",
+    "__copyright__",
+]
+
+__title__ = "wnstatmech"
+__summary__ = "Python project to handle thermodynamics of fermions and bosons."
+__version__ = "1.0.4"
+__author__ = "Clemson University"
+__copyright__ = "Clemson University, 2025"
