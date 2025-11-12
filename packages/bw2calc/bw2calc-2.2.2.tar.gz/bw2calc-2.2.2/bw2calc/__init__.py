f749afd9233c52bcf54ae84e0ed0c807c9ee01d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 08:05:10 +0000

    Add package bw2calc/2.2.2/bw2calc-2.2.2.tar.gz

---
+++ packages/bw2calc/bw2calc-2.2.2.tar.gz/bw2calc-2.2.2/bw2calc/__init__.py
@@ -0,0 +1,75 @@
+# flake8: noqa
+__all__ = [
+    "CachingLCA",
+    "DenseLCA",
+    "LCA",
+    "LeastSquaresLCA",
+    "IterativeLCA",
+    "MethodConfig",
+    "MultiLCA",
+    "FastScoresOnlyMultiLCA",
+]
+
+__version__ = "2.2.2"
+
+
+import platform
+import warnings
+
+from packaging.version import Version
+
+ARM = {"arm", "arm64", "aarch64_be", "aarch64", "armv8b", "armv8l"}
+AMD_INTEL = {"ia64", "i386", "i686", "x86_64"}
+UMFPACK_WARNING = """
+It seems like you have an ARM architecture, but haven't installed scikit-umfpack:
+
+    https://pypi.org/project/scikit-umfpack/
+
+Installing it could give you much faster calculations.
+"""
+PYPARDISO_WARNING = """
+It seems like you have an AMD/INTEL x64 architecture, but haven't installed pypardiso:
+
+    https://pypi.org/project/pypardiso/
+
+Installing it could give you much faster calculations.
+"""
+
+PYPARDISO, UMFPACK = False, False
+
+try:
+    from pypardiso import factorized, spsolve
+
+    PYPARDISO = True
+except ImportError:
+    pltf = platform.machine().lower()
+
+    try:
+        import scikits.umfpack
+
+        UMFPACK = True
+    except ModuleNotFoundError:
+        if pltf in ARM:
+            warnings.warn(UMFPACK_WARNING)
+        elif pltf in AMD_INTEL:
+            warnings.warn(PYPARDISO_WARNING)
+        else:
+            warnings.warn("No fast sparse solver found")
+    except ImportError as e:
+        warnings.warn(f"scikit-umfpack found but couldn't be imported. Error: {e}")
+
+    from scipy.sparse.linalg import factorized, spsolve
+try:
+    from presamples import PackagesDataLoader
+except ImportError:
+    PackagesDataLoader = None
+
+
+from .caching_lca import CachingLCA
+from .dense_lca import DenseLCA
+from .fast_scores import FastScoresOnlyMultiLCA
+from .iterative_lca import IterativeLCA
+from .lca import LCA
+from .least_squares import LeastSquaresLCA
+from .method_config import MethodConfig
+from .multi_lca import MultiLCA
---
+++ packages/bw2calc/bw2calc-2.2.2.tar.gz/bw2calc-2.2.2/tests/fixtures/single-matrix/generate_fixture.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+import itertools
+import json
+import os
+import random
+import tarfile
+import tempfile
+from pathlib import Path
+
+import numpy as np
+
+
+def generate_fixture():
+    dtype = [
+        ("input", np.uint32),
+        ("output", np.uint32),
+        ("row", np.uint32),
+        ("col", np.uint32),
+        ("type", np.uint8),
+        ("uncertainty_type", np.uint8),
+        ("amount", np.float32),
+        ("loc", np.float32),
+        ("scale", np.float32),
+        ("shape", np.float32),
+        ("minimum", np.float32),
+        ("maximum", np.float32),
+        ("negative", bool),
+    ]
+
+    # Exchange types
+    # "generic production": 11,
+    # "generic consumption": 12,
+
+    MAX_INT_32 = 4294967295
+    LETTERS = "abcdefgh"
+    NUMBERS = "1234"
+    GREEK = "αβγδεζηθ"
+    mapping = {k: i for i, k in enumerate(LETTERS + NUMBERS + GREEK)}
+
+    a = [(a, b, random.random(), 12) for a, b in itertools.combinations(LETTERS, 2)]
+    b = [(x, x, 1, 11) for x in LETTERS]
+    c = [(random.choice(NUMBERS), random.choice(LETTERS), random.random(), 11) for _ in range(10)]
+    d = [(x, x, 1, 11) for x in NUMBERS]
+    e = [(x, y, random.random(), 11) for x, y in zip(GREEK, NUMBERS)]
+    f = [(x, x, 1, 11) for x in GREEK]
+
+    data = a + b + c + d + e + f
+    array = np.zeros(len(data), dtype=dtype)
+
+    for i, (a, b, c, d) in enumerate(data):
+        array[i] = (
+            mapping[a],
+            mapping[b],
+            MAX_INT_32,
+            MAX_INT_32,
+            d,
+            0,
+            c,
+            np.NaN,
+            np.NaN,
+            np.NaN,
+            np.NaN,
+            np.NaN,
+            False,
+        )
+
+    with tempfile.TemporaryDirectory() as t:
+        with tarfile.open(Path(t) / "sm-fixture.tar.bz2", "w:bz2") as f:
+            path = os.path.join(t, "array.npy")
+            np.save(path, array, allow_pickle=False)
+            f.add(path, "array.npy")
+
+            path = os.path.join(t, "row.mapping")
+            with open(path, "w", encoding="utf-8") as j:
+                json.dump(mapping, j, ensure_ascii=False)
+            f.add(path, "row.mapping")
+            f.add(path, "col.mapping")
+
+            path = os.path.join(t, "categories.mapping")
+            with open(path, "w", encoding="utf-8") as j:
+                json.dump({"foo": {g: mapping[g] for g in GREEK[:5]}}, j, ensure_ascii=False)
+            f.add(path, "categories.mapping")
+
+
+generate_fixture()
