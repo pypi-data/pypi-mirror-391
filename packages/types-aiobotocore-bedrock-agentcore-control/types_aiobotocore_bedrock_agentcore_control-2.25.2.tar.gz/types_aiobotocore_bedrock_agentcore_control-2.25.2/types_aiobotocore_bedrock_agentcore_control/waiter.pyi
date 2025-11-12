830de0c13340d1e3d0010256a18f7cedb36691c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 15:04:58 +0000

    Add package types-aiobotocore-bedrock-agentcore-control/2.25.2/types_aiobotocore_bedrock_agentcore_control-2.25.2.tar.gz

---
+++ packages/types-aiobotocore-bedrock-agentcore-control/types_aiobotocore_bedrock_agentcore_control-2.25.2.tar.gz/types_aiobotocore_bedrock_agentcore_control-2.25.2/types_aiobotocore_bedrock_agentcore_control/waiter.pyi
@@ -0,0 +1,52 @@
+"""
+Type annotations for bedrock-agentcore-control service client waiters.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agentcore_control/waiters/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_bedrock_agentcore_control.client import BedrockAgentCoreControlClient
+    from types_aiobotocore_bedrock_agentcore_control.waiter import (
+        MemoryCreatedWaiter,
+    )
+
+    session = get_session()
+    async with session.create_client("bedrock-agentcore-control") as client:
+        client: BedrockAgentCoreControlClient
+
+        memory_created_waiter: MemoryCreatedWaiter = client.get_waiter("memory_created")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+
+from aiobotocore.waiter import AIOWaiter
+
+from .type_defs import GetMemoryInputWaitTypeDef
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+__all__ = ("MemoryCreatedWaiter",)
+
+class MemoryCreatedWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agentcore-control/waiter/MemoryCreated.html#BedrockAgentCoreControl.Waiter.MemoryCreated)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agentcore_control/waiters/#memorycreatedwaiter)
+    """
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[GetMemoryInputWaitTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agentcore-control/waiter/MemoryCreated.html#BedrockAgentCoreControl.Waiter.MemoryCreated.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agentcore_control/waiters/#memorycreatedwaiter)
+        """
