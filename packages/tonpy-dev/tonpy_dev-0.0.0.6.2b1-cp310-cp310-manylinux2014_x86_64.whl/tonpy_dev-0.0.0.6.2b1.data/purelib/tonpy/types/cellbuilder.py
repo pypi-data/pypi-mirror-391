4c454d59c2ea8a733a9fa37e900e47438650398f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 04:06:44 +0000

    Add package tonpy-dev/0.0.0.6.2b1/tonpy_dev-0.0.0.6.2b1-cp310-cp310-manylinux2014_x86_64.whl

---
+++ packages/tonpy-dev/tonpy_dev-0.0.0.6.2b1-cp310-cp310-manylinux2014_x86_64.whl/tonpy_dev-0.0.0.6.2b1.data/purelib/tonpy/types/cellbuilder.py
@@ -0,0 +1,424 @@
+# Copyright (c) 2023 Disintar LLP Licensed under the Apache License Version 2.0
+
+from typing import Union, TYPE_CHECKING
+
+from tonpy.libs.python_ton import PyCellBuilder
+from tonpy.types.cell import Cell
+from tonpy.types.cellslice import CellSlice
+from bitstring import BitArray
+
+from tonpy.types.address import Address
+from tonpy.utils.bit_int import test_value_len
+
+if TYPE_CHECKING:
+    from tonpy.types.tlb import TLB
+
+
+class CellBuilder:
+    def __init__(self, builder=None):
+        """CellBuilder class allow you to create cells"""
+        if builder is None:
+            self.builder: PyCellBuilder = PyCellBuilder()
+        elif isinstance(builder, str):
+            self.builder: PyCellBuilder = PyCellBuilder()
+            self.store_slice(CellSlice(builder))
+        elif isinstance(builder, PyCellBuilder):
+            self.builder = builder
+        else:
+            raise ValueError(f"{type(builder)} for CellBuilder is not supported")
+
+    def store_libref(self, hash_: "str | int"):
+        """Create and store special library cell with hash of library cell"""
+
+        if isinstance(hash_, str):
+            hash_ = int(hash_, 16)
+
+        c = CellBuilder() \
+            .store_uint(CellSlice.SpecialType.Library.value, 8) \
+            .store_uint(hash_, 256) \
+            .end_cell(special=True)
+
+        self.builder.store_ref(c.cell)
+        return self
+
+    @property
+    def bits(self) -> int:
+        """Bits num that been used in cell"""
+
+        return self.builder.bits
+
+    @property
+    def refs(self) -> int:
+        """Refs num that been used in cell"""
+
+        return self.builder.refs
+
+    @property
+    def remaining_bits(self) -> int:
+        """Remaining bits in cell"""
+
+        return self.builder.remaining_bits
+
+    @property
+    def remaining_refs(self) -> int:
+        """Refs num that been used in cell"""
+
+        return self.builder.remaining_refs
+
+    def store_ref(self, cell: Cell) -> "CellBuilder":
+        """
+        Take cell and store it to next reference in current builder  |br|
+
+        :param cell: Cell to be stored as reference to current builder
+        :return: Updated (self) builder with new reference to cell
+        """
+        self.builder.store_ref(cell.cell)
+        return self
+
+    def store_ref_or_tlb(self, cell: Union[Cell, "TLB.Record"]):
+        if not isinstance(cell, Cell):
+            cell = cell.cell_pack()
+
+        return self.store_ref(cell)
+
+    def store_builder(self, b: "CellBuilder") -> "CellBuilder":
+        """
+        Append CellBuilder (bits & refs) ``b`` to current builder  |br|
+
+        :param b: CellBuilder that will be appended to this builder
+        :return: Current CellBuilder
+        """
+        self.builder.store_builder(b.builder)
+        return self
+
+    def store_uint(self, uint_: int, uint_bits: int) -> "CellBuilder":
+        """
+        Stores ``uint_`` unsigned integer with ``uint_bits`` size (up to 256) to current cell  |br|
+
+        :param uint_: Unsigned integer to be stored into cell
+        :param uint_bits: Bits num that will be used to store integer to cell
+        :return: Current CellBuilder
+        """
+
+        if uint_ < 0:
+            raise ValueError("Only unsigned integers supported")
+
+        if uint_bits > 256:
+            raise ValueError("Max value for int_bits is 256")
+
+        test_value_len(uint_, uint_bits)
+
+        # Large ints need to be converted to string to be parsed in C++
+        self.builder.store_uint_str(str(int(uint_)), uint_bits)
+        return self
+
+    def store_uint_big(self, a, b):
+        self.builder.store_256uint_str(str(a), b)
+        return self
+
+    def begin_parse(self) -> CellSlice:
+        """Convert CellBuilder to CellSlice"""
+
+        return self.end_cell().begin_parse()
+
+    def store_int(self, int_: int, int_bits: int) -> "CellBuilder":
+        """
+        Stores ``int_`` integer with ``int_bits`` size (up to 257) to current cell  |br|
+
+        :param int_: Integer to be stored into cell
+        :param int_bits: Bits num that will be used to store integer to cell
+        :return: Current CellBuilder
+        """
+
+        if int_bits > 257:
+            raise ValueError("Max value for int_bits is 257")
+
+        test_value_len(int_, int_bits)
+
+        self.builder.store_int_str(str(int_), int_bits)
+        return self
+
+    def store_slice(self, cs: CellSlice) -> "CellBuilder":
+        """
+        Append CellSlice ``b`` (bits & refs) to current CellBuilder  |br|
+
+        :param cs: CellSlice that will be appended to this builder
+        :return: Current CellBuilder
+        """
+
+        assert isinstance(cs, CellSlice), "Only CellSlice supported"
+
+        self.builder.store_slice(cs.cell_slice)
+        return self
+
+    def store_slice_or_tlb(self, cs: Union[CellSlice, "TLB.Record"]):
+        """Allow to append cell slice or TLB field"""
+
+        if not isinstance(cs, CellSlice):
+            cs = cs.cell_pack().begin_parse()
+
+        return self.store_slice(cs)
+
+    def store_slice_or_tlb_ext(self, cs: Union[CellSlice, "TLB.Record"], ext):
+        if not isinstance(cs, CellSlice):
+            cs = cs.cell_pack().begin_parse()
+
+        assert cs.size_ext() == ext, f"Expected: {hex(ext)}, got: {cs.size_ext()}"
+        return self.store_slice_or_tlb(cs)
+
+    def store_zeroes(self, n: int) -> "CellBuilder":
+        """
+        Store ``n`` 0 bits to current CellBuilder  |br|
+
+        :param n: Num of 0 bits to be stored
+        :return: Current CellBuilder
+        """
+
+        if n < 0:
+            raise ValueError("Only unsigned integer supported")
+
+        self.builder.store_zeroes(n)
+
+        return self
+
+    def store_ones(self, n: int) -> "CellBuilder":
+        """
+        Store ``n`` 1 bits to current CellBuilder  |br|
+
+        :param n: Num of 1 bits to be stored
+        :return: Current CellBuilder
+        """
+
+        if n < 0:
+            raise ValueError("Only unsigned integer supported")
+
+        self.builder.store_ones(n)
+
+        return self
+
+    def store_var_uint(self, uint_: int, bits: int) -> "CellBuilder":
+        """
+        Store ``VarUInteger bits`` to CellSlice  |br|
+
+        The main idea that we store size of our integer in first up to ``bits`` in Cell  |br|
+        So we can use less space when storing large integers  |br|
+
+        TLB scheme:  |br|
+
+        .. code-block::
+
+            var_uint$_ {n:#} len:(#< n) value:(uint (len * 8))
+              = VarUInteger n;
+
+
+        :param uint_: Unsigned integer to be stored
+        :param bits: Num of bits for VarUInteger
+        :return: Current CellBuilder
+        """
+
+        if uint_ < 0:
+            raise ValueError("Only unsigned integers supported")
+
+        self.builder.store_var_integer(str(uint_), bits, False)
+        return self
+
+    def store_var_int(self, int_: int, bits: int) -> "CellBuilder":
+        """
+        Same as ``store_var_uint`` but work with integers  |br|
+
+        TLB Scheme:  |br|
+
+        .. code-block::
+
+            var_int$_ {n:#} len:(#< n) value:(int (len * 8))
+              = VarInteger n;
+
+
+        :param int_: Signed integer to be stored
+        :param bits: Num of bits for VarInteger
+        :return: Current CellBuilder
+        """
+
+        self.builder.store_var_integer(str(int_), bits, True)
+        return self
+
+    def store_uint_less(self, upper_bound: int, value: int) -> "CellBuilder":
+        """
+        Store ``value`` less than ``upper_bound`` unsigned integer stored as ``bitCount(upper_bound - 1)`` bits  |br|
+
+        :param upper_bound: Max unsigned integer that can be stored
+        :param value: Value to store
+        :return: Current CellBuilder
+        """
+
+        if value > upper_bound - 1:
+            raise ValueError("Value can't be upper than upper_bound - 1")
+
+        if upper_bound < 0:
+            raise ValueError("Upper bound can't be less then zero in unsigned int")
+
+        if value < 0:
+            raise ValueError("Only unsigned integer supported")
+
+        self.builder.store_uint_less(upper_bound, str(value))
+        return self
+
+    def store_uint_leq(self, upper_bound: int, value: int) -> "CellBuilder":
+        """
+        Store ``value`` less or equal than ``upper_bound`` unsigned integer stored as ``bitCount(upper_bound)`` bits  |br|
+
+        :param upper_bound: Max unsigned integer that can be stored
+        :param value: Value to store
+        :return: Current CellBuilder
+        """
+
+        if value > upper_bound:
+            raise ValueError("Value can't be upper than upper_bound")
+
+        if upper_bound < 0:
+            raise ValueError("Upper bound can't be less then zero in unsigned int")
+
+        if value < 0:
+            raise ValueError("Only unsigned integer supported")
+
+        self.builder.store_uint_leq(upper_bound, str(value))
+        return self
+
+    def store_bitstring(self, bitstring: Union[str, BitArray]) -> "CellBuilder":
+        """
+        Store bits from ``bitstring`` string  |br|
+
+        :param bitstring: Bits to store to cell (ex. '11001')
+        :return: Current CellBuilder
+        """
+
+        if isinstance(bitstring, BitArray):
+            bitstring = bitstring.bin
+
+        self.builder.store_bitstring(bitstring)
+        return self
+
+    def store_bitstring_chk(self, bitstring: Union[str, BitArray], size):
+        assert len(bitstring) <= size
+        if isinstance(bitstring, BitArray):
+            bitstring = bitstring.bin
+
+        if len(bitstring) < size:
+            bitstring = bitstring.zfill(size)
+
+        return self.store_bitstring(bitstring)
+
+    def end_cell(self, special=False) -> Cell:
+        """Convert CellBuilder to Cell, if special is True - convert cell to special"""
+
+        return Cell(self.builder.get_cell(special))
+
+    def to_boc(self) -> str:
+        """Convert CellBuilder to BOC string"""
+
+        return self.builder.to_boc()
+
+    def dump(self) -> str:
+        """Recursively dump all cells as hex"""
+
+        return self.builder.dump()
+
+    def dump_as_tlb(self, tlb: str) -> str:
+        """Dump as C++ PrettyPrint parsed by TLB type"""
+
+        return self.builder.dump_as_tlb(tlb)
+
+    def get_hash(self) -> str:
+        """Get hash of cell"""
+
+        return self.builder.get_hash()
+
+    def store_grams(self, grams: int) -> "CellBuilder":
+        """
+        Same as store ``.store_var_uint(grams, 16)``, grams is nanoTON value  |br|
+
+        :param grams: nano TON value to store
+        :return: Current CellBuilder
+        """
+
+        self.builder.store_grams_str(str(grams))
+        return self
+
+    def store_address(self, address: Union[str, "Address"]) -> "CellBuilder":
+        """
+        Parse smart-contract address from string and store as ``MsgAddress`` TLB structure  |br|
+
+        :param address: Smart-contract address in any format
+        :return: Current CellBuilder
+        """
+
+        if isinstance(address, str):
+            address = Address(address)
+        address.append_to_builder(self)
+
+        return self
+
+    def _store_string_rec(self, binstrint_to_store: str) -> Cell:
+        """Store bitsring as chain of cells"""
+
+        bits_in_bitstring = len(binstrint_to_store)
+        cb = CellBuilder()
+        reamining = cb.remaining_bits
+
+        if reamining > bits_in_bitstring:
+            cb.store_bitstring(binstrint_to_store)
+            return cb.end_cell()
+        else:
+            body = binstrint_to_store[:reamining]
+            tail = binstrint_to_store[reamining:]
+            cb.store_bitstring(body)
+            return cb.store_ref(self._store_string_rec(tail)).end_cell()
+
+    def store_string(self, string_to_store: str) -> "CellBuilder":
+        """
+        Convert string ``string_to_store`` to bitstring and store it in chain of cells  |br|
+
+        :param string_to_store: string to be stored
+        :return: current CellBuilder
+        """
+        string_as_bitstring = ''.join(format(byte, '08b') for byte in string_to_store.encode('utf-8'))
+
+        reamining = self.builder.remaining_bits
+
+        if reamining > len(string_as_bitstring):
+            self.builder.store_bitstring(string_as_bitstring)
+        else:
+            body = string_as_bitstring[:reamining]
+            tail = string_as_bitstring[reamining:]
+
+            self.builder.store_bitstring(body)
+            tail_cell = self._store_string_rec(tail)
+            self.store_ref(tail_cell)
+
+        return self
+
+    def store_bool(self, bool_: bool) -> "CellBuilder":
+        """Store bool ``bool_`` as 1 bit to current CellBuilder and return modified self"""
+        if bool(bool_) is True:
+            self.store_uint(1, 1)
+        else:
+            self.store_uint(0, 1)
+
+        return self
+
+    def __getstate__(self):
+        return self.to_boc()
+
+    def __setstate__(self, boc):
+        self.builder: PyCellBuilder = PyCellBuilder()
+        self.store_slice(Cell(boc).begin_parse())
+
+    def __repr__(self):
+        b = self.bits
+        r = self.refs
+
+        return f"<CellBuilder [{b}] bits, [{r}] refs, [{self.get_hash()}] hash>"
+
+
+def begin_cell():
+    return CellBuilder()
---
+++ packages/tonpy-dev/tonpy_dev-0.0.0.6.2b1-cp310-cp310-manylinux2014_x86_64.whl/tonpy_dev-0.0.0.6.2b1.data/purelib/tonpy/types/tlb.py
@@ -0,0 +1,328 @@
+# Copyright (c) 2023 Disintar LLP Licensed under the Apache License Version 2.0
+
+from collections import defaultdict
+from enum import Enum
+from typing import Optional, List, Union, Callable
+
+from tonpy.types import CellSlice, CellBuilder, Cell
+
+
+def rec_dump(item, dump_bin_as_hex, with_types: bool = False):
+    output = {}
+
+    if with_types:
+        output['type'] = str(type(item)).replace("<class 'tonpy.tlb_gen.py.Asset.Record_", "")[:-2]
+
+    for name in item.field_names:
+        value = getattr(item, name)
+        if issubclass(type(value), RecordBase):
+            output[name] = rec_dump(value, dump_bin_as_hex=dump_bin_as_hex, with_types=with_types)
+        else:
+            if dump_bin_as_hex and isinstance(value, str) and len(value) % 8 == 0:
+                try:
+                    output[name] = hex(int(value, 2)).upper()[2:]
+                    continue
+                except Exception as e:
+                    pass
+
+            output[name] = value
+    return output
+
+
+class RecordBase:
+    """
+    Each TLB type have ``Record`` subclass means instance of TLB type |br|
+    If you have multiple constructors name of ``Record`` class will be change to ``Record_{{constructor_name}}`` |br|
+    Each record have ``__init__`` function witch contains all fields of TLB type |br|
+    Check out ``test_tlb.py`` ``test_records`` function
+    """
+
+    def __init__(self):
+        self.field_names = []  # names of all fetched fields
+        self.conditional_fields = []  # names of all fields that are conditional
+        self.negate_params = []  # params that determinate on serialization process
+
+    def get_tag_enum(self):
+        """Get current TLB.Record constructor tag in ``Enum`` type of ``TLB.Tag``"""
+        raise NotImplementedError
+
+    def get_tag(self):
+        """Get current TLB.Record constructor tag"""
+        raise NotImplementedError
+
+    def get_tag_len(self):
+        """Get length of bits for current TLB.Record constructor tag"""
+        raise NotImplementedError
+
+    def get_type_class(self):
+        """Get TLB type of current record"""
+        raise NotImplementedError
+
+    def unpack(self, cs: CellSlice, rec_unpack: bool = False,
+               strict: bool = True) -> bool:
+        """
+        Unpack current CellSlice as TLB.Record to fields, if success return True |br|
+        All field values store in class object. If ``rec_unpack`` is True - unpack all types with recursion |br|
+        Note: simple types that contains only from tags don't need ``rec_unpack``, they will be fetched immediately |br|
+
+        :param cs: CellSlice to be fetched as TLB.Record
+        :param rec_unpack: Need to unpack all types with recursion
+        :param strict: If False some failed to parse subtypes can be None
+        :return: Is unpack was success
+        """
+        raise NotImplementedError
+
+    def cell_unpack(self, cell_ref: Cell, rec_unpack: bool = False,
+                    strict: bool = True) -> bool:
+        """
+        Unpack current Cell as TLB.Record to fields, if success return True |br|
+        All field values store in class object. If ``rec_unpack`` is True - unpack all types with recursion |br|
+        Note: simple types that contains only from tags don't need ``rec_unpack``, they will be fetched immediately |br|
+        If after unpack Cell contains some data - return False |br|
+
+        :param cell_ref: Cell to be fetched as TLB.Record
+        :param rec_unpack: Need to unpack all types with recursion
+        :param strict: If False some failed to parse subtypes can be None
+        :return: Is unpack was success
+        """
+        raise NotImplementedError
+
+    def pack(self, cb: CellBuilder) -> None:
+        raise NotImplementedError
+
+    def cell_pack(self) -> Cell:
+        raise NotImplementedError
+
+    def add_r1(self, val: str, y: int, z: int) -> bool:
+        if y > z:
+            return False
+
+        x = z - y
+        setattr(self, val, x)
+        return x >= 0
+
+    def mul_r1(self, val: str, y: int, z: int) -> bool:
+        if not y or z % y != 0:
+            return False
+
+        x = z / y
+        setattr(self, val, x)
+        return x >= 0
+
+    def store_from(self, cb, value):
+        """Recursively pack TLB type to CellBuilder"""
+        cb.store_slice_or_tlb(value)
+
+    def dump(self, dump_bin_as_hex=True, with_types=False):
+        """Recursively convert TLB to dict"""
+        return rec_dump(self, dump_bin_as_hex=dump_bin_as_hex, with_types=with_types)
+
+    def to_dict(self, rec_unpack=False, convert_cells_to_bocs=False):
+        answer = {}
+
+        for field in self.field_names:
+            value = getattr(self, field)
+
+            if rec_unpack and issubclass(type(value), RecordBase):
+                answer[field] = value.to_dict(rec_unpack=rec_unpack, convert_cells_to_bocs=convert_cells_to_bocs)
+            else:
+                if convert_cells_to_bocs and isinstance(value, (Cell, CellSlice, CellBuilder)):
+                    value = value.to_boc()
+
+                answer[field] = value
+        return answer
+
+
+class TLB(object):
+    class Tag(Enum):
+        """
+        Contractor tags enums stored as lexicographic order |br|
+
+        .. code-block::
+
+            a$0 = A;
+            b$10 = A;
+            c$1 = A;
+
+        Means: |br|
+
+        .. code-block:: Python
+
+            class Tag(Enum):
+               a = 0
+               b = 1
+               c = 2
+
+            cons_len = [1, 2, 1]
+            cons_tag = [0, 2, 1]
+        """
+
+        # raise NotImplemented
+        pass
+
+    class Record(RecordBase):
+        pass
+
+    cons_len: Union[List[int], int] = None
+    cons_tag: List[int] = None
+    tag_to_class = {}
+    original_cell: Optional[Cell] = None
+    original_cell_slice: Optional[CellSlice] = None
+    params_attrs = []  # all params defined for TLB type from __init__
+    has_params = False  # is type is parametrized
+
+    def get_tag(self, cs: CellSlice) -> Optional["TLB.Tag"]:
+        """
+        Fetch tag from CellSlice ``cs`` and return ``TLB.Tag`` enum |br|
+        :param cs: CellSlice to fetch tag from
+        :return: ``TLB.Tag`` enum
+        """
+        raise NotImplemented
+
+    def fetch_enum(self, cs: CellSlice) -> int:
+        """
+        Fetch enum tag value from ``CellSlice`` of type ``TLB`` |br|
+
+        :param cs: CellSlice to fetch enum tag value from
+        :return: Enum tag value of type ``TLB`` store in ``cs: CellSlice``
+        """
+
+        raise NotImplementedError
+
+    def store_enum_from(self, cb: CellBuilder, value: int = None) -> bool:
+        """
+        Store enum ``value`` from ``self.cons_tag`` to `cb: CellBuilder` |br|
+        If ``self.const_tag`` is exact (tags are matched positions in lexicographic order) then will store ``value`` |br|
+        If ``value is None`` and ``TLB.Tag is constant`` will store constant ``TLB.Tag`` else will raise an error |br|
+
+        :param cb: CellBuilder to store enum to
+        :param value: Value or enum position to store enum from
+        :return: True
+        """
+        raise NotImplementedError
+
+    def store_from(self, cb, value):
+        cb.store_slice_or_tlb(value)
+
+    def always_special(self) -> bool:
+        """Is current type marked as special cell or not"""
+        return False
+
+    def unpack(self, cs: CellSlice, rec_unpack: bool = False,
+               strict: bool = True) -> Optional[RecordBase]:
+        """
+        Unpack current TLB and return TLB.Record if success, else return None |br|
+
+        By default, rec_unpack is False because for large TLB structures it can be slow. |br|
+        More simple way is to skip needed structures one by one and load to python objects only needed ones. |br|
+
+
+        :param cs: CellSlice to unpack TLB from
+        :param rec_unpack: pass to RecordBase ``rec_unpack``
+        :param strict: pass to RecordBase ``strict``
+        :return: TLB.Record instance or None
+        """
+
+        self.original_cell_slice = cs
+
+        try:
+            t = self.tag_to_class[self.get_tag(cs)]()
+
+            if not t.unpack(cs, rec_unpack, strict=strict):
+                return None
+
+            return t
+        except (TabError):
+            return None
+
+    def cell_unpack(self, cell_ref: Cell, rec_unpack: bool = False,
+                    strict: bool = True) -> Optional[RecordBase]:
+        """
+        Same as ``unpack`` but
+
+        :param cell_ref:
+        :param rec_unpack: pass to RecordBase ``rec_unpack``
+        :param strict: pass to RecordBase ``strict``
+        :return:
+        """
+        if cell_ref.is_null():
+            return None
+
+        # save original cell
+        self.original_cell = cell_ref
+
+        cs = cell_ref.begin_parse()
+
+        t = self.unpack(cs, rec_unpack, strict=strict)
+
+        if t is not None and not cs.empty_ext():
+            return None
+
+        return t
+
+    def fetch(self, cell_or_slice: Union[Cell, CellSlice], rec_unpack: bool = False,
+              strict: bool = True, **kwargs) -> "Optional[Union[Union[Union[TLB.Record, Cell], CellSlice], None]]":
+        """
+        :param cell_or_slice:
+        :param rec_unpack: pass to RecordBase ``rec_unpack``
+        :param strict: pass to RecordBase ``strict``
+        :return: Will return cell/cellslice if cell is special, else will return record on success, else None
+        """
+
+        if self.always_special():
+            return cell_or_slice
+
+        if isinstance(cell_or_slice, Cell):
+            return self.cell_unpack(cell_or_slice, rec_unpack, strict=strict)
+        elif isinstance(cell_or_slice, CellSlice):
+            return self.unpack(cell_or_slice, rec_unpack, strict=strict)
+        else:
+            raise ValueError(f"Type {type(cell_or_slice)} is not supported")
+
+    def fetch_to(self, to_obj: object,
+                 cell_or_slice: Union[Cell, CellSlice],
+                 unpack_names: List[str],
+                 rec_unpack: bool = False,
+                 strict: bool = True):
+        """
+        Same as fetch, but copy negate params to ``to_obj`` with names from ``unpack_names``
+
+        :param to_obj:
+        :param cell_or_slice:
+        :param unpack_names:
+        :param rec_unpack:
+        :param strict:
+        :return:
+        """
+
+        rec = self.fetch(cell_or_slice=cell_or_slice, rec_unpack=rec_unpack, strict=strict)
+
+        if rec is None:
+            return rec
+
+        if not (len(rec.negate_params) == len(unpack_names)):
+            return None
+
+        for param, name in zip(rec.negate_params, unpack_names):
+            setattr(to_obj, name, getattr(rec, param))
+
+        return rec
+
+    def get_param_record(self, item: str) -> Callable:
+        """Copy params from TLB to Record"""
+        TMPClass = type("TMPClass", (getattr(self, item),), {})
+        TMPClass.name = item
+
+        if self.has_params:
+            # copy all params
+            for i in set(self.params_attrs):
+                if hasattr(self, i):
+                    setattr(TMPClass, i, getattr(self, i))
+
+        return TMPClass
+
+    def nat_abs(self, x: int):
+        return (x > 1) * 2 + (x & 1)
+
+    def store_ref_or_tlb(self, cb: CellBuilder, value):
+        cb.store_ref_or_tlb(value)
