147cd7dda556eeac10ebff13f4367ece7c54054c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 15:05:06 +0000

    Add package wink-sdk-affiliate-winklinks/0.0.56/wink_sdk_affiliate_winklinks-0.0.56-py3-none-any.whl

---
+++ packages/wink-sdk-affiliate-winklinks/wink_sdk_affiliate_winklinks-0.0.56-py3-none-any.whl/wink_sdk_affiliate_winklinks/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from wink_sdk_affiliate_winklinks.api.syndicated_item_api import SyndicatedItemApi
+from wink_sdk_affiliate_winklinks.api.syndication_category_api import SyndicationCategoryApi
+from wink_sdk_affiliate_winklinks.api.syndication_settings_api import SyndicationSettingsApi
+
