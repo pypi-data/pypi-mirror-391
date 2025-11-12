8baa9454e2f1586668f01d508f989f8ba1367d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 14:05:02 +0000

    Add package genki-robotics/0.0.1/genki_robotics-0.0.1.tar.gz

---
+++ packages/genki-robotics/genki_robotics-0.0.1.tar.gz/genki_robotics-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='genki-robotics',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://genki.com',
+    author='ロボ@genki.com',
+    author_email='ロボ@genki.com',
+    packages=[],
+    classifiers=[''],
+)
