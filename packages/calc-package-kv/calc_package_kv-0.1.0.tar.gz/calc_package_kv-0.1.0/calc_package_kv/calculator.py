8cd444c14dcf092bb558b668415b478d3f69d811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 07:04:53 +0000

    Add package calc-package-kv/0.1.0/calc_package_kv-0.1.0.tar.gz

---
+++ packages/calc-package-kv/calc_package_kv-0.1.0.tar.gz/calc_package_kv-0.1.0/calc_package_kv/calculator.py
@@ -0,0 +1,12 @@
+class Calculator:
+    def __init__(self):
+        print("hi")
+
+    def add(self,a,b):
+        return a+b
+    def subtract(self,a,b):
+        return a-b
+    def multiple(self,a,b):
+        return a*b
+    def divide(self,a,b):
+        return a/b
