66552650637bcc60d7dd4ec561ce62f250ee5ebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 05:04:54 +0000

    Add package file-tree-display/1.2.0/file_tree_display-1.2.0-py3-none-any.whl

---
+++ packages/file-tree-display/file_tree_display-1.2.0-py3-none-any.whl/file_tree_display/__init__.py
@@ -0,0 +1,13 @@
+"""file-tree-display - A utility for generating a visually structured directory tree.
+Copyright (c) 2025 Yaron Dayan
+"""
+
+from pathlib import Path
+from importlib.metadata import version
+
+from .ftd import FileTreeDisplay
+from ._constants import PKG_NAME
+
+filetree_display = FileTreeDisplay(root_dir=str(Path.cwd()))
+
+__version__ = version(PKG_NAME)
