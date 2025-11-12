0d4e0413e207027d6420a68634f603cb55d6ca53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 06:07:58 +0000

    Add package Niranjan/1.0.1/niranjan-1.0.1.tar.gz

---
+++ packages/Niranjan/niranjan-1.0.1.tar.gz/niranjan-1.0.1/Niranjan.py
@@ -0,0 +1,4 @@
+import os
+def server():
+    print("Server is Runing Under Niranjan Domain :\nTo Closing 'ctrl+c'")
+    os.system("python -m http.server 1514")
---
+++ packages/Niranjan/niranjan-1.0.1.tar.gz/niranjan-1.0.1/setup.py
@@ -0,0 +1,2 @@
+from setuptools import setup
+setup(name="Niranjan",version="1.0.1",py_module=["Niranjan"],author="K. Niranjan KUmar",python_requires=">=3.8")
