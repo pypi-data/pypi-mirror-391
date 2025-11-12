42e07d2fc5ed264d7dbebbfe426b51db595fc58e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 06:05:35 +0000

    Add package test_locatr/0.56.2/test_locatr-0.56.2-py3-none-any.whl

---
+++ packages/test_locatr/test_locatr-0.56.2-py3-none-any.whl/locatr/__init__.py
@@ -0,0 +1,13 @@
+# ruff: noqa
+from locatr._locatr import Locatr
+from locatr.exceptions import *
+from locatr.schema import (
+    LlmProvider,
+    LlmSettings,
+    LocatrAppiumSettings,
+    LocatrCdpSettings,
+    LocatrSeleniumSettings,
+    PluginType,
+    LocatrOutput,
+    LogLevel,
+)
