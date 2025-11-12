20fa92a4cb7d6592889e30bb56af51b56702c075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 06:08:47 +0000

    Add package calculator-danfos/0.1.0/calculator_danfos-0.1.0.tar.gz

---
+++ packages/calculator-danfos/calculator_danfos-0.1.0.tar.gz/calculator_danfos-0.1.0/calculator_danfos/calculator.py
@@ -0,0 +1,11 @@
+class Calculator:
+    def __init__(self):
+        print("im in calculator")
+    def add(self, a, b):
+        return a + b
+    def subtract(self, a,b):
+        return a - b
+    def multiply(self, a,b):
+        return a * b
+    def divide(self, a,b):
+        return a / b
