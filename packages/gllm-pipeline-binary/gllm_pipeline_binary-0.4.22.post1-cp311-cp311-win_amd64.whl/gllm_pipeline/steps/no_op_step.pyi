8d6a0bb93318a3b7074ddf1ed082c0e1db9cf7fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 16:05:11 +0000

    Add package gllm-pipeline-binary/0.4.22.post1/gllm_pipeline_binary-0.4.22.post1-cp311-cp311-win_amd64.whl

---
+++ packages/gllm-pipeline-binary/gllm_pipeline_binary-0.4.22.post1-cp311-cp311-win_amd64.whl/gllm_pipeline/steps/no_op_step.pyi
@@ -0,0 +1,40 @@
+from gllm_pipeline.steps.pipeline_step import BasePipelineStep as BasePipelineStep
+from langgraph.runtime import Runtime as Runtime
+from pydantic import BaseModel as BaseModel
+from typing import Any
+
+class NoOpStep(BasePipelineStep):
+    '''A step that does nothing.
+
+    This step is useful when you want to add a step that does not perform any processing.
+    For example, you can use this step to implement a toggle pattern for a certain component.
+
+    Example:
+    ```python
+    pipeline = (
+        step_a
+        | ConditionalStep(
+            name="branch",
+            branches={
+                "execute": step_b,
+                "continue": NoOpStep("no_op")
+            },
+            condition=lambda x: "execute" if x["should_execute"] else "continue"
+        )
+        | step_c
+    )
+    ```
+
+    Attributes:
+        name (str): A unique identifier for this pipeline step.
+    '''
+    async def execute(self, state: dict[str, Any], runtime: Runtime[dict[str, Any] | BaseModel]) -> None:
+        """Executes this step, which does nothing.
+
+        Args:
+            state (dict[str, Any]): The current state of the pipeline.
+            runtime (Runtime[dict[str, Any] | BaseModel]): Runtime information for this step's execution.
+
+        Returns:
+            None: This step does not modify the pipeline state.
+        """
