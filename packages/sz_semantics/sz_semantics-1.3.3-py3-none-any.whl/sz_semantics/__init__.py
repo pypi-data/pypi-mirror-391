99ec7315c50dd33d04c7fd9c055af97c8ced1bbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 06:05:14 +0000

    Add package sz_semantics/1.3.3/sz_semantics-1.3.3-py3-none-any.whl

---
+++ packages/sz_semantics/sz_semantics-1.3.3-py3-none-any.whl/sz_semantics/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Package definitions.
+
+see copyright/license https://github.com/senzing-garage/sz-semantics/README.md
+"""
+
+from .mask import Mask
+
+from .sem import Thesaurus
+
+from .sz import SzClient
+
+from .util import KeyValueStore
