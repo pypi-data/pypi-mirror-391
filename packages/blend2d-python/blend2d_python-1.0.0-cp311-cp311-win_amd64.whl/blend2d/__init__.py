42915293a6fe665afde56ae52afb9af9e2f435fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 14:05:12 +0000

    Add package blend2d-python/1.0.0/blend2d_python-1.0.0-cp311-cp311-win_amd64.whl

---
+++ packages/blend2d-python/blend2d_python-1.0.0-cp311-cp311-win_amd64.whl/blend2d/__init__.py
@@ -0,0 +1,42 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2019 John Wiggins
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+# start delvewheel patch
+def _delvewheel_patch_1_11_2():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'blend2d_python.libs'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_11_2()
+del _delvewheel_patch_1_11_2
+# end delvewheel patch
+
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version("blend2d")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.1"  # Default version if package is not installed
+
+from ._capi import *
