4235bf03d0a9ba75959c0fd0822f437cffc92090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 14:05:24 +0000

    Add package matplobbot-shared/0.1.56/matplobbot_shared-0.1.56.tar.gz

---
+++ packages/matplobbot-shared/matplobbot_shared-0.1.56.tar.gz/matplobbot_shared-0.1.56/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="matplobbot-shared",
+    version="0.1.56", # Let's use the version from your requirements.txt
+    packages=find_packages(include=['shared_lib', 'shared_lib.*']),
+    description="Shared library for the Matplobbot ecosystem (database, services, i18n).",
+    author="Ackrome",
+    author_email="ivansergeyevich@gmail.com",
+    # Declare dependencies for this library
+    install_requires=[
+        "asyncpg",
+        "aiohttp", # Specify versions as needed
+        "certifi",
+        "redis",
+        "cachetools"
+    ],
+    # This tells setuptools that the package data (like .json files) should be included
+    package_data={
+        'shared_lib.locales': ['*.json'],
+    },
+    include_package_data=True,
+    python_requires='>=3.11',
+)
