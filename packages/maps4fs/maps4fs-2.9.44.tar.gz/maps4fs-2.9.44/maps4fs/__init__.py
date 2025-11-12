b0cb45c910842c10a6377565e05b3e7ea46a8ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 03:23:29 +0000

    Add package maps4fs/2.9.44/maps4fs-2.9.44.tar.gz

---
+++ packages/maps4fs/maps4fs-2.9.44.tar.gz/maps4fs-2.9.44/maps4fs/__init__.py
@@ -0,0 +1,10 @@
+# pylint: disable=missing-module-docstring
+import pydtmdl.providers as dtm
+from pydtmdl import DTMProvider
+
+import maps4fs.generator.component as component
+import maps4fs.generator.settings as settings
+from maps4fs.generator.game import Game
+from maps4fs.generator.map import Map
+from maps4fs.generator.monitor import Logger
+from maps4fs.generator.settings import GenerationSettings, MainSettings
