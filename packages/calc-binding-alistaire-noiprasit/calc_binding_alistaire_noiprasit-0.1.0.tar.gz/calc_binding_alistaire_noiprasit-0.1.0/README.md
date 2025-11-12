278b3a6ca875edca600a822d36a6177856afd764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 07:05:36 +0000

    Add package calc-binding-alistaire-noiprasit/0.1.0/calc_binding_alistaire_noiprasit-0.1.0.tar.gz

---
+++ packages/calc-binding-alistaire-noiprasit/calc_binding_alistaire_noiprasit-0.1.0.tar.gz/calc_binding_alistaire_noiprasit-0.1.0/README.md
@@ -0,0 +1,2 @@
+# Please refer to license
+This was adapted from cse3150 class materials by Justin Furuness
---
+++ packages/calc-binding-alistaire-noiprasit/calc_binding_alistaire_noiprasit-0.1.0.tar.gz/calc_binding_alistaire_noiprasit-0.1.0/calc_binding_alistaire_noiprasit/calculator.py
@@ -0,0 +1,14 @@
+class Calculator:
+    def add(self, a, b):
+        """Adds two numbers together"""
+        return a + b
+    def divide(self, a, b):
+        """Divides two numbers together"""
+        return a / b;
+    def subtract(self, a, b):
+        """Subtracts two numbers together"""
+        return a - b;
+
+    def multiply(self, a, b):
+        """Multiplies two numbers together"""
+        return a*b;
