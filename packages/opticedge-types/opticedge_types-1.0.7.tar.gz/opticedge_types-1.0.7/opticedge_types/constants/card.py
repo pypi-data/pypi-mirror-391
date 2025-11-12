baea11d0a4f55546d11d4be9440acdb87f8c12ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 05:05:01 +0000

    Add package opticedge-types/1.0.7/opticedge_types-1.0.7.tar.gz

---
+++ packages/opticedge-types/opticedge_types-1.0.7.tar.gz/opticedge_types-1.0.7/opticedge_types/constants/card.py
@@ -0,0 +1,13 @@
+from opticedge_types.enums.card import CardType
+
+
+CARD_COLLECTION_MAP: dict[CardType, str] = {
+    CardType.POKEMON: "pokemon_cards",
+    CardType.LORCANA: "lorcana_cards",
+    CardType.MAGIC: "magic_cards",
+    CardType.YUGIOH: "yugioh_cards",
+    CardType.VIDEO_GAMES: "video_games",
+    CardType.DIGIMON: "digimon_cards",
+    CardType.DRAGON_BALL: "dragon_ball_cards",
+    CardType.ONE_PIECE: "one_piece_cards"
+}
