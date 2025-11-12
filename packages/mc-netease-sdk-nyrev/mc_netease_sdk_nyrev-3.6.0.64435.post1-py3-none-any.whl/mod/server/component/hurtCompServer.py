8e47c8ede05b3630acf742b4d0914f923b79b10a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 03:24:24 +0000

    Add package mc-netease-sdk-nyrev/3.6.0.64435.post1/mc_netease_sdk_nyrev-3.6.0.64435.post1-py3-none-any.whl

---
+++ packages/mc-netease-sdk-nyrev/mc_netease_sdk_nyrev-3.6.0.64435.post1-py3-none-any.whl/mod/server/component/hurtCompServer.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+from typing import Optional
+from mod.common.component.baseComponent import BaseComponent
+
+
+class HurtCompServer(BaseComponent):
+    def ImmuneDamage(self, immune):
+        # type: (bool) -> bool
+        """
+        设置实体是否免疫伤害（该属性存档）
+        """
+        pass
+
+    def Hurt(self, damage, cause, attackerId=None, childAttackerId=None, knocked=True, customTag=None):
+        # type: (float, str, Optional[str], Optional[str], bool, Optional[str]) -> bool
+        """
+        设置实体伤害
+        """
+        pass
+
