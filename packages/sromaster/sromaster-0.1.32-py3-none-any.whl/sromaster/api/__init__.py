e2d8fb06362ae1710981be9782c156cc4945c910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 03:23:41 +0000

    Add package sromaster/0.1.32/sromaster-0.1.32-py3-none-any.whl

---
+++ packages/sromaster/sromaster-0.1.32-py3-none-any.whl/sromaster/api/__init__.py
@@ -0,0 +1,16 @@
+# flake8: noqa
+
+# import apis into api package
+from sromaster.api.customers_api import CustomersApi
+from sromaster.api.im_insurers_api import ImInsurersApi
+from sromaster.api.im_mailing_api import ImMailingApi
+from sromaster.api.im_mailing_invoices_api import ImMailingInvoicesApi
+from sromaster.api.im_sros_api import ImSrosApi
+from sromaster.api.insurances_api import InsurancesApi
+from sromaster.api.organization_emails_api import OrganizationEmailsApi
+from sromaster.api.organizations_api import OrganizationsApi
+from sromaster.api.parser_api import ParserApi
+from sromaster.api.parser_write_api import ParserWriteApi
+from sromaster.api.pixel_api import PixelApi
+from sromaster.api.users_api import UsersApi
+
