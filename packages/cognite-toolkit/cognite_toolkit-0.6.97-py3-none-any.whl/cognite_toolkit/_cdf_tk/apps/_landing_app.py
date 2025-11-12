40c1a78134076d5ddc6f16c18bb4780fc5cbab32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-12 17:06:57 +0000

    Add package cognite-toolkit/0.6.97/cognite_toolkit-0.6.97-py3-none-any.whl

---
+++ packages/cognite-toolkit/cognite_toolkit-0.6.97-py3-none-any.whl/cognite_toolkit/_cdf_tk/apps/_landing_app.py
@@ -0,0 +1,14 @@
+import typer
+
+from cognite_toolkit._cdf_tk.commands import InitCommand
+
+
+class LandingApp(typer.Typer):
+    def __init__(self, *args, **kwargs) -> None:  # type: ignore
+        super().__init__(*args, **kwargs)
+        self.command()(self.main_init)
+
+    def main_init(self) -> None:
+        """Guidance on how to get started"""
+        cmd = InitCommand()
+        cmd.run(cmd.execute)
