faa4b44d40e6b0a97f4feb2fd20dddddab9d2569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 04:09:33 +0000

    Add package gamengine3d/1.5.0/gamengine3d-1.5.0.tar.gz

---
+++ packages/gamengine3d/gamengine3d-1.5.0.tar.gz/gamengine3d-1.5.0/gamengine3d/__init__.py
@@ -0,0 +1,14 @@
+from .context import Context
+from .engine import Engine
+from .game_objects import (
+    GameObject,
+    Cuboid,
+    Cylinder,
+)
+
+from .helper import (
+    Color,
+    vector3d,
+    Light,
+    Camera
+)
