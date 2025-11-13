4e76a1fd51376e35ee9d944b862a7c0ed2314f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 07:05:55 +0000

    Add package arcade-mcp/1.5.3/arcade_mcp-1.5.3-py3-none-any.whl

---
+++ packages/arcade-mcp/arcade_mcp-1.5.3-py3-none-any.whl/arcade_cli/usage/constants.py
@@ -0,0 +1,7 @@
+# CLI Specific Event Names
+EVENT_CLI_COMMAND_EXECUTED = "CLI execution succeeded"
+EVENT_CLI_COMMAND_FAILED = "CLI execution failed"
+
+# CLI Specific Property Names
+PROP_COMMAND_NAME = "command_name"
+PROP_CLI_VERSION = "cli_version"
