8113857222e83cf2c348d0e2a602ef4d498102f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 16:05:57 +0000

    Add package Apolien/0.0.6/apolien-0.0.6.tar.gz

---
+++ packages/Apolien/apolien-0.0.6.tar.gz/apolien-0.0.6/src/apolien/core/evaluator.py
@@ -0,0 +1,52 @@
+import ollama
+from . import constants
+from . import testsettings
+from . import customlogger as cl
+import logging
+
+class evaluator():
+    """Main class for Apolien, holds model information and used to call other testing procedures"""
+    
+    def __init__(self, 
+                 model: str, 
+                 modelConfig: dict | None = None, 
+                 statsConfig: list[str] |  None = None,
+                 fileLogging: bool = False,
+                 fileName: str = testsettings.outputFile):
+        
+        ollama.chat(model, options=modelConfig) # Validating the model name and configs
+        self.modelName = model
+        self.modelConfig = modelConfig
+        self.statsConfig = statsConfig
+        self.logger = cl.setupLogger(toFile=fileLogging, filename=fileName)
+        self.outfile = fileName
+        self.testsConfig = {
+            "cot_lookback" : None
+        }
+        print(f"Apolien Initialized: {self.modelName}")
+    
+    def evaluate(self, 
+                 userTests: list[str],
+                 testsConfig: dict = {},
+                 fileName: str | None = None,
+                 testLogFiles: bool = False,
+                 datasets: list = ['math_debug_five']):
+        try:
+            if not fileName:
+                fileName = self.outfile
+            
+            self.testsConfig.update(testsConfig)
+            
+            if testLogFiles:
+                self.logger.setLevel(logging.DEBUG)
+            else:
+                self.logger.setLevel(logging.INFO)
+
+            for test in userTests:
+                print("Starting",test,"tests")
+                
+                constants.testMapping[test](self.logger, self.modelName, self.modelConfig, self.testsConfig, self.outfile, datasets)
+                
+                print("Finished",test,"tests")
+        except Exception as err:
+            raise err
