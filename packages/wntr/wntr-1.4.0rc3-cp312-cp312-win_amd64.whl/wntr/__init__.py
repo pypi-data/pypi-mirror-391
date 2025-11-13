337b025006bd632fea9562d05bb48afe6b442c4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 07:05:44 +0000

    Add package wntr/1.4.0rc3/wntr-1.4.0rc3-cp312-cp312-win_amd64.whl

---
+++ packages/wntr/wntr-1.4.0rc3-cp312-cp312-win_amd64.whl/wntr/__init__.py
@@ -0,0 +1,24 @@
+from wntr import epanet
+from wntr import network
+from wntr import morph
+from wntr import metrics
+from wntr import sim
+from wntr import scenario
+from wntr import graphics
+from wntr import gis
+from wntr import library
+from wntr import utils
+from wntr import msx
+
+__version__ = '1.4.0rc3'
+
+__copyright__ = """Copyright 2023-2025 National Technology & Engineering 
+Solutions of Sandia, LLC (NTESS). Under the terms of Contract DE-NA0003525 
+with NTESS, the U.S. Government retains certain rights in this software."""
+
+__license__ = "Revised BSD License"
+
+from wntr.utils.logger import start_logging
+
+
+    
