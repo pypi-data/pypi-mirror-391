de89fd8033321a7b2e272486ebf1621e44ec2f54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 16:06:31 +0000

    Add package spaceone-core/2.0.142/spaceone_core-2.0.142-py3-none-any.whl

---
+++ packages/spaceone-core/spaceone_core-2.0.142-py3-none-any.whl/spaceone/core/pygrpc/__init__.py
@@ -0,0 +1,5 @@
+from spaceone.core.pygrpc.server import serve
+from spaceone.core.pygrpc.client import client
+from spaceone.core.pygrpc.api import BaseAPI
+
+__all__ = ['serve', 'BaseAPI', 'client']
