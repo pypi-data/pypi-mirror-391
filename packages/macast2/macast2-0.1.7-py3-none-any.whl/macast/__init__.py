ff25e3abd767fdc6fe14f96c23ec76995a879e67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 16:06:22 +0000

    Add package macast2/0.1.7/macast2-0.1.7-py3-none-any.whl

---
+++ packages/macast2/macast2-0.1.7-py3-none-any.whl/macast/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025 by isdkz. All Rights Reserved.
+
+
+from .macast import Service, Macast, gui, cli
+from .utils import SETTING_DIR, Setting, SettingProperty
+from .renderer import Renderer, RendererSetting
+from .plugin import RendererPlugin
+from .gui import App, MenuItem, Platform
