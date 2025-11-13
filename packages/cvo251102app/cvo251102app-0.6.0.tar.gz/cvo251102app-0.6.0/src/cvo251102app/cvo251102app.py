40ede45e52ddc26b5cdf73312dda52179d86ddf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 03:26:25 +0000

    Add package cvo251102app/0.6.0/cvo251102app-0.6.0.tar.gz

---
+++ packages/cvo251102app/cvo251102app-0.6.0.tar.gz/cvo251102app-0.6.0/src/cvo251102app/cvo251102app.py
@@ -0,0 +1,19 @@
+import sys
+
+from cvo251102lib import hello
+
+
+def main() -> None:
+    if len(sys.argv) > 1:
+        print(hello(someone=sys.argv[1]))
+    else:
+        print(hello())
+
+
+def lambda_handler(event: dict, context: object) -> dict:
+    if "queryStringParameters" in event and "someone" in event["queryStringParameters"]:
+        print(hello(someone=event["queryStringParameters"]["someone"]))
+    else:
+        print(hello())
+
+    return {"message": "Success!"}
