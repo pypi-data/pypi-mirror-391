505cd6dae1d4a29d3a821097974d1ede93879917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 04:09:32 +0000

    Add package radt/0.2.21/radt-0.2.21.tar.gz

---
+++ packages/radt/radt-0.2.21.tar.gz/radt-0.2.21/radt/radt.py
@@ -0,0 +1,195 @@
+import argparse
+import sys
+from pathlib import Path
+
+from . import constants
+from .run import start_run
+from .schedule import start_schedule
+
+
+def schedule_split_arguments():
+    """Split arguments for `radt` into parsed arguments and passthrough arguments
+
+    Returns:
+        list, Path, list: Split arguments
+    """
+    sysargs = sys.argv[1:]
+
+    for i, arg in enumerate(sysargs):
+        if arg.strip()[-3:] == ".py" or arg.strip()[-4:] == ".csv":
+            return sysargs[:i], Path(arg), sysargs[i + 1 :]
+    else:
+        print("Please supply a python or csv file.")
+        exit()
+
+
+def schedule_parse_arguments(args: list):
+    """Argparse for `radt`
+
+    Args:
+        args (list): List of raw arguments
+
+    Returns:
+        argparse.Namespace: Parsed arguments
+    """
+    parser = argparse.ArgumentParser(
+        description="RADt Automatic Tracking and Benchmarking"
+    )
+    parser.add_argument(
+        "-e",
+        "--experiment",
+        type=int,
+        dest="experiment",
+        default=0,
+        help="Experiment ID",
+    )
+    parser.add_argument(
+        "-w", "--workload", type=int, dest="workload", default=0, help="Workload ID"
+    )
+    parser.add_argument(
+        "-d",
+        "--devices",
+        type=str,
+        dest="devices",
+        default="0",
+        help="Devices to run on separated by +, e.g. 0, 1+2",
+    )
+    parser.add_argument(
+        "-n",
+        "--name",
+        type=str,
+        dest="name",
+        default="0",
+        help="Name of the run",
+    )
+    parser.add_argument(
+        "-c",
+        "--collocation",
+        type=str,
+        dest="collocation",
+        default="-",
+        help="Method of collocation, either empty, mps, or a MIG profile string",
+    )
+    parser.add_argument(
+        "-l",
+        "--listeners",
+        type=str,
+        dest="listeners",
+        default="smi+top+dcgmi+iostat+free",
+        help=f"Metric collectors separated by +, available: {' '.join(constants.RUN_LISTENERS + list(constants.WORKLOAD_LISTENERS.keys()))}",
+    )
+    parser.add_argument(
+        "-r",
+        "--rerun",
+        action="store_true",
+        dest="rerun",
+        default=False,
+        help="Whether to force rerun runs that have previously failed",
+    )
+    parser.add_argument(
+        "-i",
+        "--epoch",
+        type=int,
+        dest="max_epoch",
+        default=5,
+        help="Maximum number of epochs to train for",
+    )
+    parser.add_argument(
+        "-t",
+        "--time",
+        type=int,
+        dest="max_time",
+        default=2 * 24 * 60,
+        help="Maximum amount of time to train for (minutes)",
+    )
+    parser.add_argument(
+        "--conda",
+        action="store_true",
+        dest="useconda",
+        default=True,
+        help="Use conda.yaml to create a conda environment",
+    )
+    parser.add_argument(
+        "--local",
+        action="store_false",
+        dest="useconda",
+        default=True,
+        help="Use the current active environment",
+    )
+    parser.add_argument(
+        "--manual",
+        action="store_true",
+        dest="manual",
+        default=False,
+        help="Only start tracking run when context is initialised",
+    )
+
+    return parser.parse_args(args)
+
+
+def run_parse_arguments(args: list):
+    """Argparse for `radt run`
+
+    Args:
+        args (list): List of raw arguments
+
+    Returns:
+        argparse.Namespace: Parsed arguments
+    """
+    parser = argparse.ArgumentParser(description="RADt runner")
+
+    parser.add_argument(
+        "-l",
+        "--listeners",
+        metavar="LISTENERS",
+        required=True,
+        help=f"listeners, available: {' '.join(constants.RUN_LISTENERS)}",
+    )
+    parser.add_argument(
+        "-c",
+        "--command",
+        type=str,
+        metavar="COMMAND",
+        required=True,
+    )
+    parser.add_argument("-p", "--params", type=str, metavar="PARAMS")
+
+    return parser.parse_args(args)
+
+
+def check_run_listeners(l):
+    """Check whether all run listeners are registered
+
+    Args:
+        l (list): Listeners
+
+    Raises:
+        Exception: Listener unavailable
+    """
+    if len(l) == 1 and l[0] == "none":
+        return
+    for entry in l:
+        if entry not in constants.RUN_LISTENERS:
+            raise Exception(f"Unavailable listener: {entry}")
+
+
+def cli_schedule():
+    args, file, args_passthrough = schedule_split_arguments()
+    parsed_args = schedule_parse_arguments(args)
+
+    start_schedule(parsed_args, file, args_passthrough)
+
+
+def cli_run():
+    args = run_parse_arguments(sys.argv[2:])
+    listeners = args.listeners.lower().split("+")
+    check_run_listeners(listeners)
+    start_run(args, listeners)
+
+
+def cli():
+    """Entrypoint for `radt` and `radt run`"""
+    if sys.argv[1].strip() == "run":
+        cli_run()
+    else:
+        cli_schedule()
---
+++ packages/radt/radt-0.2.21.tar.gz/radt-0.2.21/radt/run/benchmark.py
@@ -0,0 +1,167 @@
+import os
+import sys
+import types
+from time import time
+from subprocess import PIPE, Popen
+import mlflow
+
+from .listeners import (
+    dcgmi_listener,
+    ps_listener,
+    smi_listener,
+    top_listener,
+    iostat_listener,
+    free_listener,
+)
+
+
+def dummy(*args, **kwargs):
+    return
+
+
+def execute_command(cmd: str):
+    """Execute a command
+
+    Args:
+        cmd (str or list): Command to run
+
+    Returns:
+        str: stdout output of the command
+    """
+
+    if isinstance(cmd, str):
+        cmd = cmd.split()
+
+    env = os.environ.copy()
+
+    result = []
+    with Popen(cmd, stdout=PIPE, bufsize=1, universal_newlines=True, env=env) as p:
+        result.extend(p.stdout)
+
+        if p.returncode != 0:
+            pass
+
+    return result
+
+
+class RADTBenchmark:
+    def __init__(self):
+        """
+        Context manager for a run.
+        Will track ML operations while active.
+        """
+        if "RADT_MAX_EPOCH" not in os.environ:
+            return
+
+        try:
+            run = mlflow.start_run(run_id=os.getenv("RADT_RUN_ID"))
+        except Exception as e:
+            run = mlflow.active_run()
+        self.run_id = run.info.run_id
+
+        # Capture (package) versions for pip, conda, smi
+        try:
+            self.log_text("".join(execute_command("pip freeze")), "pip.txt")
+        except FileNotFoundError as e:
+            pass
+
+        try:
+            self.log_text("".join(execute_command("conda list")), "conda.txt")
+        except FileNotFoundError as e:
+            pass
+
+        try:
+            self.log_text("".join(execute_command("nvidia-smi")), "smi.txt")
+        except FileNotFoundError as e:
+            pass
+
+    def __dir__(self):
+        return dir(super()) + dir(mlflow)
+
+    def __getattribute__(self, name):
+        """Get attribute, overwrites methods and functions
+        if RADT has not been loaded"""
+        try:
+            att = super().__getattribute__(name)
+        except AttributeError:
+            att = getattr(mlflow, name)
+
+        if "RADT_MAX_EPOCH" not in os.environ:
+            if isinstance(att, types.MethodType) or isinstance(att, types.FunctionType):
+                return dummy
+        return att
+
+    def __enter__(self):
+        if "RADT_MAX_EPOCH" not in os.environ:
+            return self
+
+        self.threads = []
+        self.max_epoch = int(os.getenv("RADT_MAX_EPOCH"))
+        self.max_time = time() + int(os.getenv("RADT_MAX_TIME"))
+
+        # Spawn threads for listeners
+        if os.getenv("RADT_LISTENER_PS") == "True":
+            os.environ["RADT_LISTENER_PS"] = "False"
+            self.threads.append(ps_listener.PSThread(self.run_id))
+        if os.getenv("RADT_LISTENER_SMI") == "True":
+            os.environ["RADT_LISTENER_SMI"] = "False"
+            self.threads.append(smi_listener.SMIThread(self.run_id))
+        if os.getenv("RADT_LISTENER_DCGMI") == "True":
+            os.environ["RADT_LISTENER_DCGMI"] = "False"
+            self.threads.append(dcgmi_listener.DCGMIThread(self.run_id))
+        if os.getenv("RADT_LISTENER_TOP") == "True":
+            os.environ["RADT_LISTENER_TOP"] = "False"
+            self.threads.append(top_listener.TOPThread(self.run_id))
+        if os.getenv("RADT_LISTENER_IOSTAT") == "True":
+            os.environ["RADT_LISTENER_IOSTAT"] = "False"
+            self.threads.append(iostat_listener.IOstatThread(self.run_id))
+        if os.getenv("RADT_LISTENER_FREE") == "True":
+            os.environ["RADT_LISTENER_FREE"] = "False"
+            self.threads.append(free_listener.FreeThread(self.run_id))
+
+        for thread in self.threads:
+            thread.start()
+
+        return self
+
+    def __exit__(self, type, value, traceback):
+        # Terminate listeners and run
+        if "RADT_MAX_EPOCH" not in os.environ:
+            return
+        for thread in self.threads:
+            thread.terminate()
+        mlflow.end_run()
+
+    def log_metric(self, name, value, epoch=0):
+        """
+        Log a metric. Terminates the run if epoch/time limit has been reached.
+
+        :param name: Metric name (string). This string may only contain alphanumerics, underscores
+                    (_), dashes (-), periods (.), spaces ( ), and slashes (/).
+                    All backend stores will support keys up to length 250, but some may
+                    support larger keys.
+        :param value: Metric value (float).
+        :param epoch: Integer training step (epoch) at which was the metric calculated.
+                     Defaults to 0.
+        """
+        if "RADT_MAX_EPOCH" not in os.environ:
+            return
+        mlflow.log_metric(name, value, epoch)
+        if epoch >= self.max_epoch or time() > self.max_time:
+            print("Maximum epoch reached")
+            sys.exit()
+
+    def log_metrics(self, metrics, epoch=0):
+        """
+        Log multiple metrics. Terminates the run if epoch/time limit has been reached.
+
+        :param name: Dict of metrics (string: float). Key-value pairs of metrics to be logged.
+        :param epoch: Integer training step (epoch) at which was the metric calculated.
+                     Defaults to 0.
+        """
+        if "RADT_MAX_EPOCH" not in os.environ:
+            return
+        mlflow.log_metrics(metrics, epoch)
+        if epoch >= self.max_epoch or time() > self.max_time:
+            print("Maximum epoch reached")
+            sys.exit()
