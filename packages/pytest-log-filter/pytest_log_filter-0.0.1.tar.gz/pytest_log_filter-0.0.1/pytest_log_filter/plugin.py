8334e9971fdbf20f4212e389aa78e5c90f14795f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 16:06:29 +0000

    Add package pytest-log-filter/0.0.1/pytest_log_filter-0.0.1.tar.gz

---
+++ packages/pytest-log-filter/pytest_log_filter-0.0.1.tar.gz/pytest_log_filter-0.0.1/pytest_log_filter/plugin.py
@@ -0,0 +1,20 @@
+import logging
+
+
+def pytest_addoption(parser):
+    parser.addoption('--log-ignore', action='append', help="Ignore some loggers' log")
+    parser.addini('log_ignore', help="Ignore some loggers' log")
+
+
+def pytest_configure(config):
+    logger_names = config.getoption('--log-ignore') or []
+    if config.getini('log_ignore'):
+        logger_names += config.getini('log_ignore').split('\n')
+    logger_names = list(set([item.strip() for item in logger_names]))
+    print("ignore-loggers", logger_names)
+
+    for logger_name in logger_names:
+        logger = logging.getLogger(logger_name)
+        logger.setLevel(logging.CRITICAL + 1)
+        logger.propagate = False
+        logger.addHandler(logging.NullHandler())
---
+++ packages/pytest-log-filter/pytest_log_filter-0.0.1.tar.gz/pytest_log_filter-0.0.1/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+
+setup_requirements = ['pytest-runner', ]
+
+version = '0.0.1'
+
+def read_file(filename):
+    with open(os.path.join(this_directory, filename), encoding='utf-8') as f:
+        long_description = f.read()
+    return long_description
+
+
+setup(
+    author="Han Zhichao",
+    author_email='superhin@126.com',
+    description="Ignore some loggers' log for pytest",
+    long_description=read_file('README.md'),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Framework :: Pytest',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python :: 3.7',
+    ],
+    license="MIT license",
+    include_package_data=True,
+    keywords=[
+        'pytest', 'py.test', 'pytest ignore logs', 'pytest log filter'
+    ],
+    name='pytest-log-filter',
+    packages=find_packages(include=['pytest_log_filter']),
+    setup_requires=setup_requirements,
+    url='https://github.com/hanzhichao/pytest-log-filter',
+    version=version,
+    zip_safe=True,
+    install_requires=[
+        'pytest',
+        'pytest-runner',
+    ],
+    entry_points={
+        'pytest11': [
+            'pytest-relative-path = pytest_relative_path.plugin',
+        ]
+    }
+)
