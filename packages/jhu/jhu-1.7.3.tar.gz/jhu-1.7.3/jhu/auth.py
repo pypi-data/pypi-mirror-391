cbb2494bae8d1fe35ba1f8d854892111c273fa63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 16:06:43 +0000

    Add package jhu/1.7.3/jhu-1.7.3.tar.gz

---
+++ packages/jhu/jhu-1.7.3.tar.gz/jhu-1.7.3/jhu/auth.py
@@ -0,0 +1,198 @@
+"""
+整合钉钉,飞书第三方网页登录
+"""
+from enum import IntEnum
+from urllib.parse import quote_plus
+from requests import post, get
+
+
+class AuthType(IntEnum):
+    # 钉钉
+    DINGTALK = 0
+    # 飞书
+    FEISHU = 1
+
+
+def get_dingtalk_user_info(ak: str, sk: str, auth_code: str) -> dict:
+    """获取钉钉用户信息
+
+    Args:
+        ak: 钉钉应用的AppKey, 如:ding1cincjju0rvrl6c5
+        sk: 钉钉应用的SecurityKey,如:w4dkdOyB3iy9lMv7VstUqnVHgj3f-WmbUGiHj2X-HsE4Yp_Fn0pHrQ_deI1oy1Nl
+        auth_code: 钉钉扫码得到的auth_code, 如:e8ce805bbd5434168292f75c11832eac
+    """
+    # 1.获取access_token
+    ACCESS_TOKEN_URL = "https://api.dingtalk.com/v1.0/oauth2/userAccessToken"
+    data = {"clientId": ak, "clientSecret": sk,
+            "code": auth_code, "grantType": "authorization_code"}
+
+    with post(ACCESS_TOKEN_URL, json=data) as rsp:
+        access_token = rsp.json().get("accessToken", "")
+
+    # 2.获取用户信息
+    USER_INFO_URL = "https://api.dingtalk.com/v1.0/contact/users/me"
+    headers = {"x-acs-dingtalk-access-token": f"{access_token}"}
+
+    with get(USER_INFO_URL, headers=headers) as rsp:
+        result = rsp.json()
+
+    return result
+
+
+def get_feishu_user_info(ak: str, sk: str, auth_code: str) -> dict:
+    """获取飞书的用户信息
+
+    Args:
+        ak: 飞书应用的AppKey,如:cli_3a2c007d00da5bdc
+        sk: 飞书应用的SecurityKey,如:BZUG0yREh8XJfl14teRZ6rr0HeFhr2I5
+        auth_code: 飞书扫码得到的auth_code , 如:5c0t8bf1efe14119a00c5555ee066053
+    """
+    # 1.获取app_access_token
+    app_access_token_data = {"app_id": ak, "app_secret": sk}
+    APP_ACCESS_TOKEN_URL = "https://open.feishu.cn/open-apis/auth/v3/app_access_token/internal"
+    with post(APP_ACCESS_TOKEN_URL, json=app_access_token_data) as rsp:
+        app_access_token = rsp.json().get("app_access_token", "")
+
+    # 2.获取access_token
+    ACCESS_TOKEN_URL = "https://open.feishu.cn/open-apis/authen/v1/oidc/access_token"
+    access_token_headers = {"Authorization": f"Bearer {app_access_token}"}
+    access_token_data = {"grant_type": "authorization_code", "code": auth_code}
+    with post(ACCESS_TOKEN_URL, headers=access_token_headers, json=access_token_data) as rsp:
+        user_access_token = rsp.json()["data"]["access_token"]
+
+    # 3.获取用户信息
+    USER_INFO_URL = "https://open.feishu.cn/open-apis/authen/v1/user_info"
+    user_access_token_headers = {
+        "Authorization": f"Bearer {user_access_token}"}
+    with get(USER_INFO_URL, headers=user_access_token_headers) as rsp:
+        result = rsp.json()
+    return result
+
+
+class ThridAuthBase:
+    def __init__(self, ak: str, sk: str, auth_type: AuthType) -> None:
+        """三方扫码登录初始化
+
+        Args:
+            ak:str,应用的appkey
+            sk:str,应用的secretkey
+            auth_type:AuthType,应用类型,当前支持 钉钉:AuthType.DINGTALK,飞书:AuthType.FEISHU
+        """
+        self._auth_type = auth_type
+        self._ak = ak
+        self._sk = sk
+
+        # 回调函数注册
+        match(auth_type):
+            case AuthType.DINGTALK:
+                # 钉钉三方登录网页地址拼接
+                self._prefix_url = "https://login.dingtalk.com/oauth2/challenge.htm"
+                self.get_user_info_fn = get_dingtalk_user_info
+            case AuthType.FEISHU:
+                # 飞书三方登录网页地址拼接
+                self._prefix_url = "https://open.feishu.cn/open-apis/authen/v1/authorize"
+                self.get_user_info_fn = get_feishu_user_info
+            case _:
+                raise TypeError("认证类型类型错误")
+
+    @property
+    def ak(self) -> str:
+        return self._ak
+
+    @property
+    def sk(self) -> str:
+        return self._sk
+
+    @property
+    def auth_type(self) -> AuthType:
+        return self._auth_type
+
+    @property
+    def prefix_url(self) -> str:
+        return self._prefix_url
+
+    def create_login_url(self, redirect_uri: str, state: str | None = None) -> str:
+        """生成三方扫码登录的url地址
+
+        Args:
+            redirect_uri: 扫码通过后,回调的地址
+            state: 生成地址用的state
+
+        Returns:
+            三方扫码登录的url地址
+        """
+
+        match(self.auth_type):
+            case AuthType.DINGTALK:
+                state = state or "JHU_DINGTALK"
+                data = {
+                    "redirect_uri": quote_plus(redirect_uri),
+                    "client_id": self.ak,
+                    "response_type": "code",
+                    "scope": "openid",
+                    "state": state,
+                    "prompt": "consent"
+                }
+            case AuthType.FEISHU:
+                state = state or "JHU_FEISHU"
+                data = {
+                    "redirect_uri": quote_plus(redirect_uri),
+                    "app_id": self.ak,
+                    "scope": "contact:user.phone:readonly",
+                    "state": state
+                }
+            case _:
+                raise TypeError("认证类型类型错误")
+
+        query_params = '&'.join([f"{k}={data[k]}" for k in data])
+        url = self.prefix_url+'?'+query_params
+        return url
+
+    def get_user_info(self, auth_code: str) -> dict:
+        """获取三方扫码得到的用户信息
+
+        Args:
+            auth_code:str,三方登录连接返回的auth_code
+
+        Returns:
+            用户的json信息
+        """
+        return self.get_user_info_fn(self.ak, self.sk, auth_code)
+
+
+class DingTalk(ThridAuthBase):
+    def __init__(self, ak: str, sk: str) -> None:
+        """钉钉三方登录
+
+        Args:
+            ak: 钉钉应用的AppKey, 如:ding1cincjju0rvrl6c5
+            sk: 钉钉应用的SecurityKey,如:w4dkdOyB3iy9lMv7VstUqnVHgj3f-WmbUGiHj2X-HsE4Yp_Fn0pHrQ_deI1oy1Nl
+        """
+        super().__init__(ak, sk, AuthType.DINGTALK)
+
+
+class FeiShu(ThridAuthBase):
+    def __init__(self, ak: str, sk: str) -> None:
+        """飞书三方登录
+
+        Args:
+            ak: 飞书应用的AppKey,如:cli_3a2c007d00da5bdc
+            sk: 飞书应用的SecurityKey,如:BZUG0yREh8XJfl14teRZ6rr0HeFhr2I5
+        """
+        super().__init__(ak, sk, AuthType.FEISHU)
+
+
+if __name__ == "__main__":
+    ding_ak = "ding_ak"
+    ding_sk = "ding_sk"
+    d = DingTalk(ding_ak, ding_sk)
+    d.create_login_url("http://localhost:9000/")
+    # 这里的code从redirect_url返回的code中获取
+    d.get_user_info("968999a21b9b38c794d230c5d80742bc")
+
+    feishu_ak = "feishu_ak"
+    feishu_sk = "feishu_sk"
+    f = FeiShu(feishu_ak, feishu_sk)
+    f.create_login_url("http://localhost:9000/")
+    # 这里的code从redirect_url返回的code中获取
+    f.get_user_info("b8ag458bd0f54712be8c6877273ce415")
---
+++ packages/jhu/jhu-1.7.3.tar.gz/jhu-1.7.3/jhu/webhook.py
@@ -0,0 +1,146 @@
+"""
+日期:2023-12-01
+内容:
+- 集成钉钉和企业微信的webhook能力
+参考文档:
+- https://open.dingtalk.com/document/robots/custom-robot-access
+- https://developer.work.weixin.qq.com/document/path/91770
+"""
+from typing import Any
+from base64 import b64encode
+from enum import IntEnum
+from hashlib import sha256
+from hmac import new as hmac_new
+from requests import session
+from time import time
+from urllib.parse import quote_plus
+
+
+class WebHookType(IntEnum):
+    DINGTALK = 0
+    WECOM = 1
+
+
+class WebHook:
+    def __init__(self, webhook: WebHookType, url: str, sk: str | None = None) -> None:
+        """Webhook对象
+
+        Args:
+            webhook:WebHookType,webhook的类型
+            url:str,webhook的url
+            sk:str,webhook的密钥,由webhook提供
+        """
+        self.__webhook = webhook
+        self.__url = url
+        self.__sk = sk
+
+    @property
+    def webhook(self):
+        return self.__webhook
+
+    @property
+    def url(self):
+        return self.__url
+
+    @property
+    def sk(self):
+        return self.__sk
+
+    def text(self, content: str, phone: list[str] | None = None, id: list[str] | None = None, all: bool = False) -> dict:
+        """text类型,支持钉钉和企微
+
+        Args:
+            content:str,发送内容
+            phone:list[str],需要@的用户手机号,比如['151****1234', '151****5678']
+            id:list[str],需要@的用户ID,比如['id001', 'id002']
+            all:bool,是否@ALL, 仅钉钉有效,企微请在id或phone里用'@all'来表示
+        """
+        result: dict[str, Any] = {"msgtype": "text"}
+        text: dict[str, Any] = {"content": content}
+
+        match(self.webhook):
+            case WebHookType.DINGTALK:
+                at = {"atMobiles": phone, "isAtAll": all, "atUserIds": id}
+                result["at"] = at
+            case WebHookType.WECOM:
+                text["mentioned_list"] = id
+                text["mentioned_mobile_list"] = phone
+        result["text"] = text
+        return result
+
+    def link(self, title: str, text: str, url: str, pic: str = "") -> dict:
+        """link类型,仅支持钉钉
+
+        Args:
+            title:str,消息标题
+            text:str,消息内容.如果太长只会展示部分
+            url:str,点击消息跳转URL
+            pic:str,图片URL
+        """
+        result: dict[str, str | dict] = {"msgtype": "link"}
+        link = {"title": title, "text": text, "messageUrl": url, "picUrl": pic}
+        result["link"] = link
+        return result
+
+    def markdown(self, text: str, title: str = "") -> dict:
+        """markdown类型,支持钉钉和企微
+
+        Args:
+            text:str,markdown内容
+            title:str,markdown标题,仅钉钉需要填写
+        """
+        result: dict[str, str | dict] = {"msgtype": "markdown"}
+
+        match(self.webhook):
+            case WebHookType.DINGTALK:
+                markdown = {"text": text, "title": title}
+            case WebHookType.WECOM:
+                markdown = {"content": text}
+
+        result["markdown"] = markdown
+        return result
+
+    def send(self, data: dict) -> None:
+        """发送webhook消息
+
+        Args:
+            data:dict,Webhook消息,可用类的text,markdown等方法构造
+        """
+        url = self.url
+        sk = self.sk
+
+        match(self.webhook):
+            case WebHookType.DINGTALK:
+                # 钉钉的处理
+                if sk:
+                    sk_enc = sk.encode()
+                    timestamp = str(round(time()*1000))
+                    sign_str = f"{timestamp}\n{sk}"
+                    sign_str_enc = sign_str.encode()
+                    hmac_code = hmac_new(sk_enc, sign_str_enc,
+                                         digestmod=sha256).digest()
+                    sign = quote_plus(b64encode(hmac_code))
+                    url = f"{url}&timestamp={timestamp}&sign={sign}"
+
+        # 消息发送
+        with session() as s:
+            result = s.post(url, json=data)
+
+        rsp = result.json()
+        if rsp["errcode"] != 0:
+            raise Exception(rsp["errmsg"])
+
+
+if __name__ == "__main__":
+    dt = WebHook(WebHookType.DINGTALK, "url", "sk")
+
+    data = dt.text("钉钉测试内容", phone=["151****9832"])
+    data = dt.link(title="百度", text="点我可以打开baidu的地址",
+                   url="https://www.baidu.com")
+    data = dt.markdown("# 这是markdown的内容", title="markdown文本")
+    dt.send(data)
+
+    wb = WebHook(WebHookType.WECOM, "url")
+    data = wb.text("微信测试内容", phone=["@all"])
+    data = wb.markdown("# 这是markdown的内容")
+    wb.send(data)
