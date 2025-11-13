038f746407e46ae58baede71d5df3ca425b1b9f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 15:06:22 +0000

    Add package plain.api/0.17.0/plain_api-0.17.0.tar.gz

---
+++ packages/plain.api/plain_api-0.17.0.tar.gz/plain_api-0.17.0/plain/api/models.py
@@ -0,0 +1,38 @@
+import binascii
+import os
+import uuid
+
+from plain import models
+
+
+def generate_token() -> str:
+    return binascii.hexlify(os.urandom(20)).decode()
+
+
+@models.register_model
+class APIKey(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    expires_at = models.DateTimeField(required=False, allow_null=True)
+    last_used_at = models.DateTimeField(required=False, allow_null=True)
+
+    name = models.CharField(max_length=255, required=False)
+
+    token = models.CharField(max_length=40, default=generate_token)
+
+    api_version = models.CharField(max_length=255, required=False)
+
+    model_options = models.Options(
+        constraints=[
+            models.UniqueConstraint(
+                fields=["uuid"], name="plainapi_apikey_unique_uuid"
+            ),
+            models.UniqueConstraint(
+                fields=["token"], name="plainapi_apikey_unique_token"
+            ),
+        ],
+    )
+
+    def __str__(self) -> str:
+        return self.name or str(self.uuid)
