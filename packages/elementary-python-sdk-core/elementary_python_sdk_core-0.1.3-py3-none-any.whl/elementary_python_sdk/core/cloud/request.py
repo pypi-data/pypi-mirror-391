89c8e8332c8d88f1cd7cd643adb601d35b7cdf5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 04:09:04 +0000

    Add package elementary-python-sdk-core/0.1.3/elementary_python_sdk_core-0.1.3-py3-none-any.whl

---
+++ packages/elementary-python-sdk-core/elementary_python_sdk_core-0.1.3-py3-none-any.whl/elementary_python_sdk/core/cloud/request.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from typing import Annotated, Union
+
+from elementary_python_sdk.core.types.asset import TableAsset
+from elementary_python_sdk.core.types.test import Test, TestExecution
+from pydantic import BaseModel, Field
+
+ElementaryObject = Annotated[
+    Union[TableAsset, Test, TestExecution],
+    Field(discriminator="kind"),
+]
+
+
+class ElementaryCloudIngestRequest(BaseModel):
+    project: str | None = None
+    objects: list[ElementaryObject]
+
+
+class ElementaryCloudIngestError(BaseModel):
+    status_code: int
+    error: str
