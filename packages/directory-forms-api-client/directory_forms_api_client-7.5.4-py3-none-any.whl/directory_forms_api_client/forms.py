9ba97160bc44ae7ef0f202f4e0b02d2a969a69a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 08:06:27 +0000

    Add package directory-forms-api-client/7.5.4/directory_forms_api_client-7.5.4-py3-none-any.whl

---
+++ packages/directory-forms-api-client/directory_forms_api_client-7.5.4-py3-none-any.whl/directory_forms_api_client/forms.py
@@ -0,0 +1,105 @@
+from django.forms import Form
+
+from directory_forms_api_client import actions
+
+
+class AbstractActionMixin:
+
+    @property
+    def action_class(self):
+        raise NotImplementedError
+
+    def save(self, *args, **kwargs):
+        action = self.action_class(*args, **kwargs)
+        return action.save(self.serialized_data)
+
+    @property
+    def serialized_data(self):
+        return self.cleaned_data
+
+
+class SaveOnlyInDatabaseActionMixin(AbstractActionMixin):
+    action_class = actions.SaveOnlyInDatabaseAction
+
+
+class HCSatActionMixin(AbstractActionMixin):
+    action_class = actions.HCSatAction
+
+
+class UnverifiedReminderActionMixin(AbstractActionMixin):
+    action_class = actions.UnverifiedReminderAction
+
+
+class EmailActionMixin(AbstractActionMixin):
+    action_class = actions.EmailAction
+
+    @property
+    def serialized_data(self):
+        return {
+            'text_body': self.text_body,
+            'html_body': self.html_body,
+        }
+
+    @property
+    def text_body(self):
+        raise NotImplementedError()
+
+    @property
+    def html_body(self):
+        raise NotImplementedError()
+
+
+class ZendeskActionMixin(AbstractActionMixin):
+    action_class = actions.ZendeskAction
+
+
+class GovNotifyEmailActionMixin(AbstractActionMixin):
+    action_class = actions.GovNotifyEmailAction
+
+
+class GovNotifyBulkEmailActionMixin(AbstractActionMixin):
+    action_class = actions.GovNotifyBulkEmailAction
+
+
+class GovNotifyLetterActionMixin(AbstractActionMixin):
+    action_class = actions.GovNotifyLetterAction
+
+
+class PardotActionMixin(AbstractActionMixin):
+    action_class = actions.PardotAction
+
+
+class SaveOnlyInDatabaseAPIForm(SaveOnlyInDatabaseActionMixin, Form):
+    pass
+
+
+class HCSatAPIForm(HCSatActionMixin, Form):
+    pass
+
+
+class UnverifiedRemindersAPIForm(UnverifiedReminderActionMixin, Form):
+    pass
+
+
+class EmailAPIForm(EmailActionMixin, Form):
+    pass
+
+
+class ZendeskAPIForm(ZendeskActionMixin, Form):
+    pass
+
+
+class GovNotifyEmailAPIForm(GovNotifyEmailActionMixin, Form):
+    pass
+
+
+class GovNotifyBulkEmailAPIForm(GovNotifyBulkEmailActionMixin, Form):
+    pass
+
+
+class GovNotifyLetterAPIForm(GovNotifyLetterActionMixin, Form):
+    pass
+
+
+class PardotAPIForm(PardotActionMixin, Form):
+    pass
