c3e5e87c697128af05b969409fe2ec6c0867fc59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 05:06:11 +0000

    Add package django-bulk-hooks/0.2.117/django_bulk_hooks-0.2.117.tar.gz

---
+++ packages/django-bulk-hooks/django_bulk_hooks-0.2.117.tar.gz/django_bulk_hooks-0.2.117/django_bulk_hooks/operations/__init__.py
@@ -0,0 +1,18 @@
+"""
+Operations module for django-bulk-hooks.
+
+This module contains all services for bulk operations following
+a clean, service-based architecture.
+"""
+
+from django_bulk_hooks.operations.analyzer import ModelAnalyzer
+from django_bulk_hooks.operations.bulk_executor import BulkExecutor
+from django_bulk_hooks.operations.coordinator import BulkOperationCoordinator
+from django_bulk_hooks.operations.mti_handler import MTIHandler
+
+__all__ = [
+    "BulkExecutor",
+    "BulkOperationCoordinator",
+    "MTIHandler",
+    "ModelAnalyzer",
+]
