1cc2a88a73568db4658705a877106f4653fe5536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 06:06:31 +0000

    Add package mindgard/0.95.0/mindgard-0.95.0-py3-none-any.whl

---
+++ packages/mindgard/mindgard-0.95.0-py3-none-any.whl/mindgard/api_service.py
@@ -0,0 +1,42 @@
+# Standard library imports
+from typing import Any, Dict
+
+# Third party imports
+import requests
+from tenacity import retry, stop_after_attempt, wait_fixed
+
+from .constants import (
+    API_RETRY_ATTEMPTS,
+    API_RETRY_WAIT_BETWEEN_ATTEMPTS_SECONDS,
+    VERSION,
+)
+
+
+def _standard_headers(access_token: str) -> Dict[str, str]:
+    return {
+        "Authorization": f"Bearer {access_token}",
+        "User-Agent": f"mindgard-cli/{VERSION}",
+        "X-User-Agent": f"mindgard-cli/{VERSION}",
+    }
+
+
+@retry(
+    stop=stop_after_attempt(API_RETRY_ATTEMPTS),
+    wait=wait_fixed(API_RETRY_WAIT_BETWEEN_ATTEMPTS_SECONDS),
+    reraise=True,
+)
+def api_post(url: str, access_token: str, payload: Dict[str, Any]) -> requests.Response:
+    response = requests.post(url=url, json=payload, headers=_standard_headers(access_token))
+    response.raise_for_status()
+    return response
+
+
+@retry(
+    stop=stop_after_attempt(API_RETRY_ATTEMPTS),
+    wait=wait_fixed(API_RETRY_WAIT_BETWEEN_ATTEMPTS_SECONDS),
+    reraise=True,
+)
+def api_get(url: str, access_token: str) -> requests.Response:
+    response = requests.get(url=url, headers=_standard_headers(access_token))
+    response.raise_for_status()
+    return response
---
+++ packages/mindgard/mindgard-0.95.0-py3-none-any.whl/mindgard/types.py
@@ -0,0 +1,79 @@
+# Standard library imports
+from dataclasses import dataclass
+from typing import Any, Callable, Dict, Literal, Optional, Tuple, get_args
+
+# Third party imports
+import requests
+from rich.progress import Progress, TaskID
+from rich.table import Table
+
+
+@dataclass
+class ExceptionCountTuple:
+    task_id: TaskID
+    count: int
+
+
+# Web pub sub message types
+type_wps_message_type = Literal["StartTest", "StartedTest", "Response", "Request"]
+
+# UI task map stores a name for a task along with its taskid to be updated in progress object
+type_ui_task_map = Dict[str, TaskID]
+# Stores an exception message with the task to update, and the number of times the exception has been thrown
+type_ui_exception_map = Dict[str, ExceptionCountTuple]
+
+# interfaces that submit, polling and output functions MUST MEET to be usable in run_poll_display.
+type_submit_func = Callable[[str, type_ui_exception_map, Progress], Any]
+type_polling_func = Callable[
+    [str, Any, type_ui_task_map, Progress],
+    Optional[Any],
+]
+type_output_func = Callable[[Any, bool], Optional[Table]]
+
+# Orchestrator submit/request enums
+type_orchestrator_attack_pack = Literal["sandbox", "threat_intel"]
+type_orchestrator_source = Literal["threat_intel", "user", "mindgard"]
+type_model_presets = Literal[
+    "huggingface-openai",
+    "openai-compatible",
+    "huggingface",
+    "openai",
+    "azure-openai",
+    "azure-aistudio",
+    "anthropic",
+    "tester",
+    "local",
+]
+type_model_presets_list: Tuple[type_model_presets, ...] = get_args(type_model_presets)
+
+# Types for dependency injecting get/post request functions into over api_post and api_get in orchestrator
+type_post_request_function = Callable[[str, str, Dict[str, Any]], requests.Response]
+type_get_request_function = Callable[[str, str], requests.Response]
+
+# Different log levels
+log_levels = [
+    "critical",
+    "fatal",
+    "error",
+    "warn",
+    "warning",
+    "info",
+    "debug",
+    "notset",
+]  # [n.lower() for n in logging.getLevelNamesMapping().keys()]
+
+valid_llm_datasets = {
+    "customerservice": "BadCustomer",
+    "finance": "BadFinance",
+    "legal": "BadLegal",
+    "medical": "BadMedical",
+    "injection": "SqlInjection",
+    "rce": "Xss",
+    "xss": "Xss",
+    "bypass.deepset": "GuardrailBypassDeepset",
+    "bypass.llmguard": "GuardrailBypassLlmGuard",
+    "bypass.metapromptguardv1": "GuardrailBypassMetaPromptGuardV1",
+    "bypass.protectaiv1": "GuardrailBypassProtectAiV1",
+    "bypass.protectaiv2": "GuardrailBypassProtectAiV2",
+    "bypass.vijilpromptinjection": "GuardrailBypassVijilPromptInjection",
+}
