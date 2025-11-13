2b0b644353ff4d7111a121748a64ecbed42d889a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 06:06:05 +0000

    Add package firexapp/5.7.139/firexapp-5.7.139.tar.gz

---
+++ packages/firexapp/firexapp-5.7.139.tar.gz/firexapp-5.7.139/firexapp/events/event_aggregator.py
@@ -0,0 +1,372 @@
+"""
+Aggregates events in to the task data model.
+"""
+from datetime import datetime
+import logging
+from typing import Optional, Any, Callable
+import dataclasses
+
+from firexapp.events.model import RunStates, TaskColumn
+
+logger = logging.getLogger(__name__)
+
+@dataclasses.dataclass
+class EventAggregatorConfig:
+    copy_fields: list[str]
+    merge_fields: list[str]
+    keep_initial_fields: list[str]
+    field_to_celery_transforms: dict[
+        str,
+        Callable[[dict[str, Any]], dict[str, Any]]]
+
+    # Event data extraction/transformation without current state context.
+    def get_new_event_data(self, event: dict[str, Any]) -> dict[str, dict[str, Any]]:
+        new_task_data = {}
+        for field in self.copy_fields:
+            if field in event:
+                new_task_data[field] = event[field]
+
+        # Note if a field is both a copy field and a transform, the transform overrides if the output writes to the same key.
+        for field, transform in self.field_to_celery_transforms.items():
+            if field in event:
+                transformed_dict = transform(event)
+                new_task_data.update(transformed_dict)
+
+        return {event['uuid']: new_task_data}
+
+    def find_data_changes(
+        self,
+        existing_task: dict[str, Any],
+        new_task_data: dict[str, Any],
+    ) -> dict[str, Any]:
+        # Some fields overwrite whatever is present. Be permissive, since not all fields captured are from celery,
+        # so not all have entries in the field config.
+        no_overwrite_fields = self.keep_initial_fields + self.merge_fields + ['state']
+        override_dict = {
+            k: v for k, v in new_task_data.items()
+            if k not in no_overwrite_fields}
+
+        changed_data = {}
+        for new_data_key, new_data_val in override_dict.items():
+            if (
+                new_data_key not in existing_task
+                or existing_task[new_data_key] != new_data_val
+            ):
+                changed_data[new_data_key] = new_data_val
+
+        # Some field updates are dropped if there is already a value for that field name (keep initial).
+        for keep_init_key in self.keep_initial_fields:
+            if (
+                keep_init_key in new_task_data
+                and keep_init_key not in existing_task
+            ):
+                changed_data[keep_init_key] = new_task_data[keep_init_key]
+
+        # Some fields need to be accumulated across events, not overwritten from latest event.
+        merged_values = _deep_merge_keys(existing_task, new_task_data, self.merge_fields)
+        for merged_data_key, merged_data_val in merged_values.items():
+            if (
+                merged_data_key not in existing_task
+                or existing_task[merged_data_key] != merged_data_val
+            ):
+                changed_data[merged_data_key] = merged_data_val
+
+        if new_task_data.get('state'):
+            changed_data['state'] = RunStates.get_higher_priority_state(
+                existing_task.get('state'),
+                new_task_data.get('state'),
+            )
+
+        return changed_data
+
+
+def transform_task_state(
+    celery_event: dict[str, Any],
+) -> dict[str, Any]:
+    transformed_data = {}
+    event_type: Optional[str] = celery_event.get('type')
+    if event_type == 'task-completed':
+        transformed_data['has_completed'] = True
+    else:
+        try:
+            state = RunStates(event_type)
+        except ValueError:
+            pass
+        else:
+            transformed_data['state'] = state.to_ui_state()
+            transformed_data['states'] = [{
+                TaskColumn.STATE.value: state.to_ui_state(),
+                'timestamp': celery_event.get('local_received')
+            }]
+            if state.is_revoke():
+                # tasks can become not-revoked after being revoked, so we need to keep track of revoked state
+                # explicitly.
+                transformed_data['was_revoked'] = True
+    return transformed_data
+
+
+#
+# config field options:
+#   copy_celery - True if this field should be copied from the celery event to the task data model. If the field already
+#                   has a value on the data model, more recent celery field values will overwrite existing values by
+#                   default. If overwriting should be avoided, see 'aggregate_merge' and 'aggregate_keep_initial'
+#                   options described below.
+#
+#   transform_celery - A function to be executed on the entire event when the corresponding key is present in a celery
+#                       event. The function returns a dict that dict.update the existing data from the event, possibly
+#                       overwriting data copied from the celery event by copy_celery=True. Can be used to change
+#                       the field name on the data model from the field name from celery.
+#
+#   aggregate_merge - True if model updates should deep merge collection data types (lists, dicts, sets) instead of
+#                       overwriting.
+#
+#   aggregate_keep_initial - True if data model field updates should be ignored after an initial value has been set.
+#                               This is one way of preventing overwriting, see also 'aggregate_merge'.
+#
+#
+FIELD_CONFIG = {
+    TaskColumn.UUID.value: {'copy_celery': True},
+    TaskColumn.HOSTNAME.value: {'copy_celery': True},
+    TaskColumn.PARENT_ID.value: {'copy_celery': True},
+    'type': {
+        'copy_celery': True,
+        'transform_celery': transform_task_state,
+    },
+    TaskColumn.RETRIES.value: {'copy_celery': True},
+    TaskColumn.BOUND_ARGS.value: {'copy_celery': True},
+    TaskColumn.ACTUAL_RUNTIME.value: {'copy_celery': True},
+    TaskColumn.UTCOFFSET.value: {'copy_celery': True},
+    TaskColumn.DEFAULT_BOUND_ARGS.value: {'copy_celery': True},
+    TaskColumn.FROM_PLUGIN.value: {'copy_celery': True},
+    TaskColumn.RESULTS.value: {'copy_celery': True},
+    TaskColumn.TRACEBACK.value: {'copy_celery': True},
+    TaskColumn.EXCEPTION.value: {'copy_celery': True},
+    TaskColumn.LONG_NAME.value: {
+        'copy_celery': True,
+        'transform_celery': lambda e: {'name': e['long_name'].split('.')[-1]},
+    },
+    TaskColumn.NAME.value: {
+        # TODO: firexapp should send long_name, since it will overwrite 'name' copied from celery. Then get rid of
+        # the following config.
+        'transform_celery': lambda e: {
+            TaskColumn.NAME.value: e[TaskColumn.NAME.value].split('.')[-1],
+            TaskColumn.LONG_NAME.value: e[TaskColumn.NAME.value]},
+    },
+    TaskColumn.CHAIN_DEPTH.value: {'copy_celery': True},
+    TaskColumn.FIRST_STARTED.value: {'aggregate_keep_initial': True},
+    TaskColumn.EXCEPTION_CAUSE_UUID.value: {'copy_celery': True},
+    'states': {'aggregate_merge': True},
+    'url': {
+        # TODO: only for backwards compat. Can use log_filepath.
+        'transform_celery': lambda e: {TaskColumn.LOGS_URL.value: e['url']},
+    },
+    'log_filepath': {
+        'transform_celery': lambda e: {TaskColumn.LOGS_URL.value: e['log_filepath']},
+    },
+    'local_received': {
+        # Note first_started is never overwritten by aggregation.
+        'transform_celery': lambda e: {TaskColumn.FIRST_STARTED.value: e['local_received']},
+    },
+}
+
+
+def _get_keys_with_true(input_dict, key):
+    return [k for k, v in input_dict.items() if v.get(key, False)]
+
+
+def event_aggregator_from_field_spec(field_spec: dict[str, dict[str, Any]]):
+    return EventAggregatorConfig(
+        copy_fields=_get_keys_with_true(field_spec, 'copy_celery'),
+        merge_fields=_get_keys_with_true(field_spec, 'aggregate_merge'),
+        keep_initial_fields=_get_keys_with_true(field_spec, 'aggregate_keep_initial'),
+        field_to_celery_transforms={
+            k: v['transform_celery']
+            for k, v in field_spec.items()
+            if 'transform_celery' in v},
+    )
+
+DEFAULT_AGGREGATOR_CONFIG = event_aggregator_from_field_spec(FIELD_CONFIG)
+
+
+def _deep_merge_keys(dict1, dict2, keys):
+    dict1_to_merge = {k: v for k, v in dict1.items() if k in keys}
+    dict2_to_merge = {k: v for k, v in dict2.items() if k in keys}
+    return _deep_merge(dict1_to_merge, dict2_to_merge)
+
+
+def _both_instance(o1, o2, _type):
+    return isinstance(o1, _type) and isinstance(o2, _type)
+
+
+def _deep_merge(dict1, dict2):
+    result = dict(dict1)
+    for d2_key in dict2:
+        if d2_key in dict1:
+            v1 = dict1[d2_key]
+            v2 = dict2[d2_key]
+            if _both_instance(v1, v2, dict):
+                result[d2_key] = _deep_merge(v1, v2)
+            elif _both_instance(v1, v2, list):
+                result[d2_key] = v1 + v2
+            elif _both_instance(v1, v2, set):
+                result[d2_key] = v1.union(v2)
+            elif v1 == v2:
+                # already the same value in both dicts, take from either.
+                result[d2_key] = v1
+            else:
+                # Both d1 and d2 have entries for d2_key, both entries are not dicts or lists or sets,
+                # and the values are not the same. This is a conflict.
+                # Overwrite d1's value to simulate dict.update() behaviour.
+                result[d2_key] = v2
+        else:
+            # New key for d1, just add it.
+            result[d2_key] = dict2[d2_key]
+    return result
+
+
+class AbstractFireXEventAggregator:
+    """ Aggregates many events in to the task data model. """
+
+    def __init__(self, aggregator_config: EventAggregatorConfig):
+        self.new_task_num = 1
+        self.root_uuid = None
+        self.aggregator_config = aggregator_config
+
+    def aggregate_events(self, events):
+        new_data_by_task_uuid = {}
+        for e in events:
+            event_new_data_by_task_uuid = self._aggregate_event(e)
+            for uuid, new_data in event_new_data_by_task_uuid.items():
+                if uuid not in new_data_by_task_uuid:
+                    new_data_by_task_uuid[uuid] = {}
+                new_data_by_task_uuid[uuid].update(new_data)
+        return new_data_by_task_uuid
+
+    def generate_incomplete_events(self):
+        """
+        Unfortunately, if a run terminates ungracefully, incomplete tasks will never arrive at a
+        terminal runstate. The 'task-incomplete' runstate is a fake (non-backend) terminal runstate
+        that is generated here so that the UI can show a non-incomplete runstate.
+        :return:
+        """
+        new_events = []
+        now = datetime.now().timestamp()
+        for incomplete_task in self._get_incomplete_tasks():
+            new_event = {
+                'uuid': incomplete_task['uuid'],
+                'type': RunStates.get_forced_complete_celery_event_type(
+                    incomplete_task.get('state')),
+            }
+
+            if not incomplete_task.get(TaskColumn.ACTUAL_RUNTIME.value):
+                task_runtime = now - (incomplete_task.get('first_started') or now)
+                new_event[TaskColumn.ACTUAL_RUNTIME.value] = task_runtime
+            new_events.append(new_event)
+        return new_events
+
+    def is_root_complete(self) -> bool:
+        if (
+            not self.root_uuid
+            or not self._task_exists(self.root_uuid)
+        ):
+            return False  # Might not have root event yet.
+        return RunStates.is_complete_state(
+            self._get_task(self.root_uuid).get('state'))
+
+    def are_all_tasks_complete(self) -> bool:
+        if not self.is_root_complete():
+            # optimization: don't query all incomplete tasks if the root isn't done yet.
+            return False
+        return self._get_incomplete_tasks() == []
+
+    def _get_or_create_task(self, task_uuid) -> tuple[dict[str, Any], bool]:
+        if not self._task_exists(task_uuid):
+            task = self._insert_new_task(
+                {
+                    'uuid': task_uuid,
+                    'task_num': self.new_task_num,
+                }
+            )
+            self.new_task_num += 1 # only update after insert succeeds.
+            is_new = True
+        else:
+            task = self._get_task(task_uuid)
+            is_new = False
+        assert task
+        return task, is_new
+
+    def _aggregate_event(self, event: dict[str, Any]) -> dict[str, dict[str, Any]]:
+        if (
+            # The uuid can be null, it's unclear what this means but it can't be associated with a task.
+            not event.get('uuid')
+            # Revoked events can be sent before any other, and we'll never get any data (name, etc) for that task.
+            # Therefore ignore events that are for a new UUID that have revoked type.
+            or (
+                not self._task_exists(event['uuid'])
+                and event.get('type') == RunStates.REVOKED.to_celery_event_type())
+        ):
+            return {}
+
+        if event.get(TaskColumn.PARENT_ID.value, '__no_match') is None and self.root_uuid is None:
+            self.root_uuid = event['uuid']
+
+        new_data_by_task_uuid = self.aggregator_config.get_new_event_data(event)
+        changes_by_task_uuid = {}
+        for task_uuid, new_task_data in new_data_by_task_uuid.items():
+            existing_task, is_new_task = self._get_or_create_task(task_uuid)
+
+            changed_data = self.aggregator_config.find_data_changes(existing_task, new_task_data)
+            if changed_data:
+                self._update_task(task_uuid, existing_task, changed_data)
+
+            # If we just created the task, we need to send the auto-initialized fields, as well as data from the event.
+            # If this isn't a new event, we only need to send what has changed.
+            changes_by_task_uuid[task_uuid] = dict(existing_task) if is_new_task else dict(changed_data)
+
+        return changes_by_task_uuid
+
+    def _update_task(self, task_uuid: str, full_task: dict[str, Any], changed_data: dict[str, Any]) -> None:
+        full_task.update(changed_data)
+
+    def _task_exists(self, task_uuid):
+        raise NotImplementedError("This should be implemented by concrete subclasses")
+
+    def _get_task(self, task_uuid: str) -> Optional[dict[str, Any]]:
+        raise NotImplementedError("This should be implemented by concrete subclasses")
+
+    def _get_incomplete_tasks(self) -> list[dict[str, Any]]:
+        raise NotImplementedError("This should be implemented by concrete subclasses")
+
+    def _insert_new_task(self, task: dict[str, Any]) -> dict[str, Any]:
+        raise NotImplementedError("This should be implemented by concrete subclasses")
+
+
+class FireXEventAggregator(AbstractFireXEventAggregator):
+    """ Aggregates many Celery events in to the FireX tasks data model. """
+
+    def __init__(self, aggregator_config: EventAggregatorConfig = DEFAULT_AGGREGATOR_CONFIG):
+        super().__init__(aggregator_config)
+        self.tasks_by_uuid : dict[str, Any] = {}
+
+    def _task_exists(self, task_uuid: str) -> bool:
+        if not task_uuid:
+            return False
+        return task_uuid in self.tasks_by_uuid
+
+    def _get_task(self, task_uuid: str) -> Optional[dict[str, Any]]:
+        return self.tasks_by_uuid.get(task_uuid)
+
+    def _get_incomplete_tasks(self) -> list[dict[str, Any]]:
+        return [
+            task for task in self.tasks_by_uuid.values()
+            if (
+                task.get(TaskColumn.ACTUAL_RUNTIME.value) is None
+                or RunStates.is_incomplete_state(task.get('state'))
+            )
+        ]
+
+    def _insert_new_task(self, task: dict[str, Any]) -> dict[str, Any]:
+        assert 'uuid' in task, f'Cannot insert task without uuid: {task}'
+        assert not self._task_exists(task['uuid']), f'Task already exists, cannot insert: {task}'
+        self.tasks_by_uuid[task['uuid']] = task
+        return task
---
+++ packages/firexapp/firexapp-5.7.139.tar.gz/firexapp-5.7.139/firexapp/submit/uid.py
@@ -0,0 +1,186 @@
+from dataclasses import dataclass
+import os
+import datetime
+import pytz
+import tempfile
+from getpass import getuser
+import random
+import firexkit
+import shutil
+import re
+from typing import Optional
+
+from firexapp.submit.arguments import whitelist_arguments
+from firexkit.permissions import DEFAULT_CHMOD_MODE
+
+BASE_LOGGING_DIR_ENV_VAR_KEY = 'firex_base_logging_dir'
+
+FIREX_ID_DATE_FMT = "%y%m%d-%H%M%S"
+ALL_FIREX_IDS_REGEX = re.compile(r'(FireX-\w+?-\d{6}-\d{6}-\d+)')
+FIREX_ID_REGEX = re.compile(r'^FireX-(?P<user>.*?)-(?P<datetime_str>\d{6}-\d{6})-(?P<random_int>\d+)$')
+
+
+def firex_id_str(user: str, timestamp: datetime.datetime, random_int: int) -> str:
+    return f'FireX-{user}-{timestamp.strftime(FIREX_ID_DATE_FMT)}-{random_int}'
+
+
+@dataclass(frozen=True)
+class FireXIdParts:
+    user: str
+    timestamp: datetime.datetime
+    random_int: int
+
+    def firex_id(self):
+        return firex_id_str(self.user, self.timestamp, self.random_int)
+
+    @staticmethod
+    def from_str(firex_id: str) -> 'FireXIdParts':
+        maybe_parts = get_firex_id_parts(firex_id)
+        if not maybe_parts:
+            raise Exception(f'Failed to find FireX ID in: {firex_id}')
+        return maybe_parts
+
+
+def get_firex_id_parts(maybe_firex_id: str) -> Optional[FireXIdParts]:
+    m = FIREX_ID_REGEX.match(maybe_firex_id)
+    if m:
+        parts = m.groupdict()
+        try:
+             tz_unaware_datetime = datetime.datetime.strptime(
+                parts['datetime_str'],
+                FIREX_ID_DATE_FMT)
+        except ValueError:
+            pass # invalidate date format.
+        else:
+            tz_aware_datetime = pytz.utc.localize(tz_unaware_datetime)
+            return FireXIdParts(parts['user'], tz_aware_datetime, int(parts['random_int']))
+    return None
+
+
+def is_firex_id(maybe_firex_id: str) -> bool:
+    return bool(get_firex_id_parts(maybe_firex_id))
+
+
+def find_all_firex_ids_from_str(input_str) -> list[str]:
+    if not input_str:
+        return []
+    # unique, keeping order from input.
+    return list(
+        {
+        fid: None
+        for fid in ALL_FIREX_IDS_REGEX.findall(input_str)
+        }.keys()
+    )
+
+
+def find_single_firex_id_from_str(input_str) -> str:
+    firex_ids = find_all_firex_ids_from_str(input_str)
+    if len(firex_ids) != 1:
+        raise Exception(
+            f'Expected exactly one firex ID in {input_str}, found {len(firex_ids)}'
+        )
+    return firex_ids[0]
+
+
+class Uid(object):
+    debug_dirname = 'firex_internal'
+    _resources_dirname = os.path.join(debug_dirname, 'resources')
+
+    def __init__(self, identifier=None, firex_requester=None):
+        self.timestamp = datetime.datetime.now(tz=pytz.utc)
+        self.user = getuser()
+        self.firex_requester = firex_requester or self.user
+        if identifier:
+            self.identifier = identifier
+        else:
+            random.seed()
+            self.identifier = firex_id_str(self.user, self.timestamp, random.randint(1, 65536))
+        self._base_logging_dir = None
+        self._logs_dir = None
+        self._debug_dir = None
+        self._viewers = {}
+
+    @property
+    def base_logging_dir(self):
+        if not self._base_logging_dir:
+            self._base_logging_dir = os.environ.get(BASE_LOGGING_DIR_ENV_VAR_KEY, tempfile.gettempdir())
+        return self._base_logging_dir
+
+    @property
+    def logs_dir(self) -> str:
+        if not self._logs_dir:
+            self._logs_dir = self.create_logs_dir()
+            self._debug_dir = self.create_debug_dir()
+            self.copy_resources()
+        return self._logs_dir
+
+    @property
+    def debug_dir(self):
+        if not self._debug_dir:
+            self._debug_dir = self.create_debug_dir()
+        return self._debug_dir
+
+    @classmethod
+    def get_resources_path(cls, logs_dir):
+        return os.path.join(logs_dir, cls._resources_dirname)
+
+    @property
+    def resources_dir(self):
+        return self.get_resources_path(self.logs_dir)
+
+    def _create_logs_dir_from_base(self, base_logging_dir):
+        path = os.path.join(base_logging_dir, self.identifier)
+        try:
+            os.makedirs(path)
+        except FileExistsError:
+            pass
+        return path
+
+    def create_logs_dir(self):
+        return self._create_logs_dir_from_base(self.base_logging_dir)
+
+    def create_debug_dir(self):
+        path = os.path.join(self.logs_dir, self.debug_dirname)
+        try:
+            os.makedirs(path)
+        except FileExistsError:
+            # Could have been created by other dependencies (e.g. redis)
+            pass
+        return path
+
+    def __str__(self):
+        return self.identifier
+
+    def __repr__(self):
+        return self.identifier
+
+    def __eq__(self, other):
+        return str(other) == self.identifier
+
+    def copy_resources(self):
+        # pkg_resources.resource_filename('firexkit', 'resources') would have been a cleaner way, but
+        # pkg_reources is very slow to load
+        pkg_resource_dir = os.path.join(os.path.dirname(firexkit.__file__), 'resources')
+        resources_dir = self.resources_dir
+        shutil.copytree(pkg_resource_dir, resources_dir)
+        # Open permissions
+        os.chmod(self.resources_dir, DEFAULT_CHMOD_MODE)
+        for file in os.listdir(resources_dir):
+            os.chmod(os.path.join(resources_dir, file), DEFAULT_CHMOD_MODE)
+
+    def add_viewers(self, **attrs):
+        self._viewers.update(**attrs)
+
+    @property
+    def viewers(self):
+        return self._viewers
+
+    @property
+    def logs_url(self):
+        try:
+            return self.viewers['logs_url']
+        except KeyError:
+            return None
+
+
+whitelist_arguments("uid")
