8ddb6b03d7880a520ba795a52951e60092b145b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 16:06:21 +0000

    Add package erlab/3.16.2/erlab-3.16.2.tar.gz

---
+++ packages/erlab/erlab-3.16.2.tar.gz/erlab-3.16.2/src/erlab/analysis/__init__.pyi
@@ -0,0 +1,12 @@
+__all__ = [
+    "correlation",
+    "fit",
+    "gold",
+    "image",
+    "interpolate",
+    "kspace",
+    "mask",
+    "transform",
+]
+
+from . import correlation, fit, gold, image, interpolate, kspace, mask, transform
