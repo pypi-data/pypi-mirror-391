22180644cd225b414d6f532cc8060e3ef1c12db5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 04:08:59 +0000

    Add package pypnusershub/3.0.4/pypnusershub-3.0.4.tar.gz

---
+++ packages/pypnusershub/pypnusershub-3.0.4.tar.gz/pypnusershub-3.0.4/src/pypnusershub/tests/conftest.py
@@ -0,0 +1,36 @@
+import pytest
+
+from flask import Flask
+
+from utils_flask_sqla.tests.utils import JSONClient
+
+from pypnusershub.env import db, ma
+from pypnusershub.login_manager import login_manager
+from pypnusershub.auth.auth_manager import auth_manager
+
+
+@pytest.fixture(scope="session", autouse=True)
+def app():
+    app = Flask("pypnusershub")
+    from pypnusershub.routes import routes
+
+    app.testing = True
+    app.test_client_class = JSONClient
+    app.config.from_envvar("USERSHUB_AUTH_MODULE_SETTINGS")
+    app.testing = True
+    db.init_app(app)
+    ma.init_app(app)
+    auth_manager.init_app(
+        app, providers_declaration=app.config["AUTHENTICATION"]["PROVIDERS"]
+    )
+    login_manager.init_app(app)
+
+    with app.app_context():
+        transaction = db.session.begin_nested()  # execute tests in a savepoint
+        yield app
+        transaction.rollback()  # rollback all database changes
+
+
+@pytest.fixture(scope="session")
+def _session(app):
+    return db.session
