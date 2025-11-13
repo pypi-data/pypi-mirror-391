06a3cd29872a8a1caf87e381c30c7f673fb78601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 16:06:30 +0000

    Add package hestia-earth-validation/0.37.10/hestia_earth_validation-0.37.10-py3-none-any.whl

---
+++ packages/hestia-earth-validation/hestia_earth_validation-0.37.10-py3-none-any.whl/hestia_earth/validation/validators/infrastructure.py
@@ -0,0 +1,32 @@
+from hestia_earth.utils.date import diff_in_years
+
+from hestia_earth.validation.utils import _filter_list_errors
+
+
+def validate_lifespan(infrastructure: list):
+    """
+    Validate defaultLifespan
+
+    This validation will give an error if the duration between the start and the end dates is not equal to the
+    `defaultLifespan`.
+    """
+
+    def validate(values: tuple):
+        index, value = values
+        start_date = value.get("startDate")
+        end_date = value.get("endDate")
+        lifespan = value.get("defaultLifespan", -1)
+        diff = diff_in_years(start_date, end_date) if start_date and end_date else -1
+        return (
+            lifespan == -1
+            or diff == -1
+            or diff == round(lifespan, 1)
+            or {
+                "level": "error",
+                "dataPath": f".infrastructure[{index}].defaultLifespan",
+                "message": "must equal to endDate - startDate in decimal years",
+                "params": {"expected": diff},
+            }
+        )
+
+    return _filter_list_errors(map(validate, enumerate(infrastructure)))
