1a3ad87f60621329cd202578f218b505e67da776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 14:07:16 +0000

    Add package django-unfold-conf/0.71.1/django_unfold_conf-0.71.1.tar.gz

---
+++ packages/django-unfold-conf/django_unfold_conf-0.71.1.tar.gz/django_unfold_conf-0.71.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django_unfold_conf",
+    version="0.71.1",
+    packages=find_packages(),
+    install_requires=[],
+    description="This is a package forked from https://unfoldadmin.com/ that I make some adjustments, earby I fully credit the original author.",
+)
