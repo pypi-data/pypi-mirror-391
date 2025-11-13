32154ad17f36473b5f5a4e8f3086d4110233f02a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 03:26:45 +0000

    Add package mzy-py/0.2.1/mzy_py-0.2.1-py3-none-any.whl

---
+++ packages/mzy-py/mzy_py-0.2.1-py3-none-any.whl/mzy_py/__init__.py
@@ -0,0 +1,7 @@
+from .core import mzy  # 从core.py导出函数
+from .utils import print_info, plus_mzy, minus_mzy, print_my_name_mzy, liujinglun, FuJunYi, LiuziHao
+
+__version__ = "0.2.1"  # 版本号（重要，上传时不能重复）
+
+__author__ = "马子延"
+__all__ = ["mzy", "print_info", "plus_mzy", "minus_mzy", "print_my_name_mzy", "liujinglun", "FuJunYi", "LiuziHao"]
---
+++ packages/mzy-py/mzy_py-0.2.1-py3-none-any.whl/mzy_py/core.py
@@ -0,0 +1,9 @@
+from typing import List
+
+def mzy() -> str:
+    """Return a confession string."""
+    return "I LOVE WeiJinHe"
+
+
+#def print_my_name() -> str:
+#    return "My name is Mzy, I am a student in BPU."
