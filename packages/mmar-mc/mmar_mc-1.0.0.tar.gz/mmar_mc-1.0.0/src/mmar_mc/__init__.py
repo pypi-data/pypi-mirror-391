eeab378a944fd0b66fee2edf360111e5ff300e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 05:09:49 +0000

    Add package mmar-mc/1.0.0/mmar_mc-1.0.0.tar.gz

---
+++ packages/mmar-mc/mmar_mc-1.0.0.tar.gz/mmar_mc-1.0.0/src/mmar_mc/__init__.py
@@ -0,0 +1,11 @@
+from mmar_mc.maestro_client import MaestroClient
+from mmar_mc.maestro_client_dummy import MaestroClientDummy
+from mmar_mc.models import FileData, FileName, MessageData
+
+__all__ = [
+    MaestroClient,
+    MaestroClientDummy,
+    FileName,
+    FileData,
+    MessageData,
+]
