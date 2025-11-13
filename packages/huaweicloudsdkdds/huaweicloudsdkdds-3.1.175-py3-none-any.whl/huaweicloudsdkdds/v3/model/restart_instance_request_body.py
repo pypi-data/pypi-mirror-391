5fab78afbb8ed5c13f9e4c8cdaa7185a51224561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 17:07:48 +0000

    Add package huaweicloudsdkdds/3.1.175/huaweicloudsdkdds-3.1.175-py3-none-any.whl

---
+++ packages/huaweicloudsdkdds/huaweicloudsdkdds-3.1.175-py3-none-any.whl/huaweicloudsdkdds/v3/model/restart_instance_request_body.py
@@ -0,0 +1,136 @@
+# coding: utf-8
+
+from huaweicloudsdkcore.utils.http_utils import sanitize_for_serialization
+
+
+class RestartInstanceRequestBody:
+
+    """
+    Attributes:
+      openapi_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    sensitive_list = []
+
+    openapi_types = {
+        'target_type': 'str',
+        'target_id': 'str'
+    }
+
+    attribute_map = {
+        'target_type': 'target_type',
+        'target_id': 'target_id'
+    }
+
+    def __init__(self, target_type=None, target_id=None):
+        r"""RestartInstanceRequestBody
+
+        The model defined in huaweicloud sdk
+
+        :param target_type: 待重启对象的类型。 - 重启集群实例下的节点时，该参数必选。取值为“mongos”、“shard”、或“config”。 - 重启整个实例时，不传该参数。
+        :type target_type: str
+        :param target_id: 待重启对象的ID，可以调用“查询实例列表和详情”接口获取。如果未申请实例，可以调用“创建实例”接口创建。 - 重启集群实例下的节点时，对于mongos节点，取值为mongos节点ID，对于shard和config组，取值为shard和config组ID。 - 重启整个实例时，取值为实例ID。
+        :type target_id: str
+        """
+        
+        
+
+        self._target_type = None
+        self._target_id = None
+        self.discriminator = None
+
+        if target_type is not None:
+            self.target_type = target_type
+        self.target_id = target_id
+
+    @property
+    def target_type(self):
+        r"""Gets the target_type of this RestartInstanceRequestBody.
+
+        待重启对象的类型。 - 重启集群实例下的节点时，该参数必选。取值为“mongos”、“shard”、或“config”。 - 重启整个实例时，不传该参数。
+
+        :return: The target_type of this RestartInstanceRequestBody.
+        :rtype: str
+        """
+        return self._target_type
+
+    @target_type.setter
+    def target_type(self, target_type):
+        r"""Sets the target_type of this RestartInstanceRequestBody.
+
+        待重启对象的类型。 - 重启集群实例下的节点时，该参数必选。取值为“mongos”、“shard”、或“config”。 - 重启整个实例时，不传该参数。
+
+        :param target_type: The target_type of this RestartInstanceRequestBody.
+        :type target_type: str
+        """
+        self._target_type = target_type
+
+    @property
+    def target_id(self):
+        r"""Gets the target_id of this RestartInstanceRequestBody.
+
+        待重启对象的ID，可以调用“查询实例列表和详情”接口获取。如果未申请实例，可以调用“创建实例”接口创建。 - 重启集群实例下的节点时，对于mongos节点，取值为mongos节点ID，对于shard和config组，取值为shard和config组ID。 - 重启整个实例时，取值为实例ID。
+
+        :return: The target_id of this RestartInstanceRequestBody.
+        :rtype: str
+        """
+        return self._target_id
+
+    @target_id.setter
+    def target_id(self, target_id):
+        r"""Sets the target_id of this RestartInstanceRequestBody.
+
+        待重启对象的ID，可以调用“查询实例列表和详情”接口获取。如果未申请实例，可以调用“创建实例”接口创建。 - 重启集群实例下的节点时，对于mongos节点，取值为mongos节点ID，对于shard和config组，取值为shard和config组ID。 - 重启整个实例时，取值为实例ID。
+
+        :param target_id: The target_id of this RestartInstanceRequestBody.
+        :type target_id: str
+        """
+        self._target_id = target_id
+
+    def to_dict(self):
+        result = {}
+
+        for attr, _ in self.openapi_types.items():
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                if attr in self.sensitive_list:
+                    result[attr] = "****"
+                else:
+                    result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        import simplejson as json
+        return json.dumps(sanitize_for_serialization(self), ensure_ascii=False)
+
+    def __repr__(self):
+        """For `print`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, RestartInstanceRequestBody):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
