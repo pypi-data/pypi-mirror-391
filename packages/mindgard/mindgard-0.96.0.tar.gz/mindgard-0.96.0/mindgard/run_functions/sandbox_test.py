9875e3d1a86d4c682626cc99700cbd6b351af5d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 06:06:12 +0000

    Add package mindgard/0.96.0/mindgard-0.96.0.tar.gz

---
+++ packages/mindgard/mindgard-0.96.0.tar.gz/mindgard-0.96.0/mindgard/run_functions/sandbox_test.py
@@ -0,0 +1,36 @@
+# Standard library imports
+from typing import Optional
+
+# Third party imports
+from rich.progress import Progress
+
+from ..orchestrator import OrchestratorTestResponse, submit_sandbox_test
+from ..types import (
+    type_submit_func,
+    type_ui_exception_map,
+    type_ui_task_map,
+)
+from ..ui_prefabs import poll_and_display_test
+
+POLL_INTERVAL_SECONDS = 3
+
+
+def submit_sandbox_submit_factory() -> type_submit_func:
+
+    def submit_sandbox_submit(
+        access_token: str,
+        ui_exception_map: type_ui_exception_map,
+        ui_exception_progress: Progress,
+    ) -> OrchestratorTestResponse:
+        return submit_sandbox_test(access_token=access_token, target_name="sandbox-openai-gpt4o-mini")
+
+    return submit_sandbox_submit
+
+
+def submit_sandbox_polling(
+    access_token: str,
+    initial_test: OrchestratorTestResponse,
+    ui_task_map: type_ui_task_map,
+    ui_task_progress: Progress,
+) -> Optional[OrchestratorTestResponse]:
+    return poll_and_display_test(access_token, ui_task_map, ui_task_progress, initial_test)
