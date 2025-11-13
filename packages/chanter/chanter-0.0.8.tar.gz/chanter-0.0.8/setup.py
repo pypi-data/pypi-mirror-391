8f997f50790d9bf58330f13bb4cb197314a21dda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 06:06:29 +0000

    Add package chanter/0.0.8/chanter-0.0.8.tar.gz

---
+++ packages/chanter/chanter-0.0.8.tar.gz/chanter-0.0.8/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='chanter',
+
+    version='0.0.8',
+
+    description='Simple galaxy spectral modelling and fitting',
+
+    long_description=long_description,
+
+    long_description_content_type='text/markdown',
+
+    author='Struan Stevenson',
+
+    author_email='struan.stevenson@ed.ac.uk',
+
+    packages= setuptools.find_packages(),
+
+    package_data = {'': ['*.txt', '*.fits'],},  
+
+    install_requires=["numpy", "astropy", "matplotlib", "spectres", "nautilus-sampler"],
+
+)
