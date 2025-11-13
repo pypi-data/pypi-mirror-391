5c608b12b586e73f4934596f027bfe7a69b1ad86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 04:09:24 +0000

    Add package splatstream/0.0.5/splatstream-0.0.5-cp313-cp313-manylinux_2_34_x86_64.whl

---
+++ packages/splatstream/splatstream-0.0.5-cp313-cp313-manylinux_2_34_x86_64.whl/splatstream/__init__.py
@@ -0,0 +1,8 @@
+from .singleton_renderer import singleton_renderer
+from .renderer import gaussian_splats, load_from_ply, draw
+
+__all__ = [
+    "gaussian_splats",
+    "load_from_ply",
+    "draw",
+]
