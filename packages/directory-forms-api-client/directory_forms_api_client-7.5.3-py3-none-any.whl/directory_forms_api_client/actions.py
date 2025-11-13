2f1c1e42d4d8e3bc33a979db9acce176a8002548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 06:05:50 +0000

    Add package directory-forms-api-client/7.5.3/directory_forms_api_client-7.5.3-py3-none-any.whl

---
+++ packages/directory-forms-api-client/directory_forms_api_client-7.5.3-py3-none-any.whl/directory_forms_api_client/actions.py
@@ -0,0 +1,168 @@
+import abc
+
+from directory_forms_api_client.client import forms_api_client
+
+
+class AbstractAction(abc.ABC):
+
+    def __init__(self, form_url, client=forms_api_client, form_session=None, sender=None, spam_control=None):
+        self.form_url = form_url
+        self.client = client
+        self.form_session = form_session
+        self.sender = sender or {}
+        self.spam_control = spam_control or {}
+
+    @property
+    @abc.abstractmethod
+    def name(self):
+        return ''
+
+    def serialize_data(self, data):
+        return {'data': data, 'meta': self.serialize_meta()}
+
+    def serialize_meta(self):
+        meta = {
+            'action_name': self.name,
+            'form_url': self.form_url,
+            'sender': self.sender,
+            'spam_control': self.spam_control,
+            **self.meta,
+        }
+        if self.form_session:
+            meta['funnel_steps'] = self.form_session.funnel_steps
+            meta['ingress_url'] = self.form_session.ingress_url
+        return meta
+
+    def save(
+        self,
+        data,
+        form_session=None,
+    ):
+        serialized_data = self.serialize_data(data)
+        return self.client.submit_generic(serialized_data)
+
+
+class SaveOnlyInDatabaseAction(AbstractAction):
+    name = 'save-only-in-db'
+
+    def __init__(self, subject, full_name, email_address, *args, **kwargs):
+        self.meta = {
+            'full_name': full_name,
+            'email_address': email_address,
+            'subject': subject,
+            'recipient_email': email_address,
+        }
+        super().__init__(*args, **kwargs)
+
+
+class HCSatAction(AbstractAction):
+    name = 'hcsat-feedback-submission'
+
+    def __init__(self, *args, **kwargs):
+        self.meta = {}
+        super().__init__(*args, **kwargs)
+
+    def save(self, data):
+        return self.client.hcsat_feedback_submission(data)
+
+
+# To be deprecated - DO NOT USE EmailAction Use GovNotifyEmailAction
+class EmailAction(AbstractAction):
+    name = 'email'
+
+    def __init__(self, recipients, subject, reply_to, *args, **kwargs):
+        self.meta = {
+            'recipients': recipients,
+            'subject': subject,
+            'reply_to': reply_to,
+        }
+        super().__init__(*args, **kwargs)
+
+
+class ZendeskAction(AbstractAction):
+    name = 'zendesk'
+
+    def __init__(
+        self,
+        subject,
+        full_name,
+        email_address,
+        service_name,
+        subdomain=None,
+        sort_fields_alphabetically=True,
+        *args,
+        **kwargs,
+    ):
+        self.meta = {
+            'full_name': full_name,
+            'email_address': email_address,
+            'subject': subject,
+            'service_name': service_name,
+            'sort_fields_alphabetically': sort_fields_alphabetically,
+        }
+        # if empty Forms API will use the default configured zendesk subdomain
+        if subdomain:
+            self.meta['subdomain'] = subdomain
+        super().__init__(*args, **kwargs)
+
+
+class GovNotifyEmailAction(AbstractAction):
+    name = 'gov-notify-email'
+
+    def __init__(self, template_id, email_address, email_reply_to_id=None, *args, **kwargs):
+        self.meta = {
+            'template_id': template_id,
+            'email_address': email_address,
+        }
+        if email_reply_to_id:
+            self.meta['email_reply_to_id'] = email_reply_to_id
+        super().__init__(*args, **kwargs)
+
+
+class GovNotifyBulkEmailAction(AbstractAction):
+    name = 'gov-notify-bulk-email'
+
+    def __init__(self, template_id, email_reply_to_id=None, *args, **kwargs):
+        self.meta = {'template_id': template_id}
+        if email_reply_to_id:
+            self.meta['email_reply_to_id'] = email_reply_to_id
+        super().__init__(*args, **kwargs)
+
+    def save(self, data):
+        return self.client.gov_notify_bulk_email(data)
+
+
+class GovNotifyLetterAction(AbstractAction):
+    name = 'gov-notify-letter'
+
+    def __init__(self, template_id, *args, **kwargs):
+        self.meta = {
+            'template_id': template_id,
+        }
+        super().__init__(*args, **kwargs)
+
+
+class PardotAction(AbstractAction):
+    name = 'pardot'
+
+    def __init__(self, pardot_url, *args, **kwargs):
+        self.meta = {
+            'pardot_url': pardot_url,
+        }
+        super().__init__(*args, **kwargs)
+
+
+class UnverifiedReminderAction(AbstractAction):
+    name = 'verification-reminders'
+
+    def __init__(self, template_id, email_reply_to_id=None, *args, **kwargs):
+        self.meta = {
+            'template_id': template_id,
+        }
+        if email_reply_to_id:
+            self.meta['email_reply_to_id'] = email_reply_to_id
+        super().__init__(*args, **kwargs)
+
+    def save(self, data):
+        serialized_data = self.serialize_data(data)
+        return self.client.verification_reminders(serialized_data)
---
+++ packages/directory-forms-api-client/directory_forms_api_client-7.5.3-py3-none-any.whl/directory_forms_api_client/client.py
@@ -0,0 +1,55 @@
+import pkg_resources
+from directory_client_core.base import AbstractAPIClient
+from django.conf import settings
+
+
+class APIFormsClient(AbstractAPIClient):
+
+    endpoints = {
+        # API V1 endpoints
+        'ping': 'api/healthcheck/ping/',
+        'submission': 'api/submission/',
+        'delete_submissions': 'api/delete-submissions/',
+        # API V2 endpoints
+        'gov_notify_bulk_email': 'api/v2/gov-notify-bulk-email/',
+        'hcsat_feedback_submission': 'api/v2/hcsat-feedback-submission/',
+        'verification_reminders': 'api/v2/verification-reminders',
+    }
+    version = pkg_resources.get_distribution(__package__).version
+
+    # API V1
+    def ping(self, authenticator=None):
+        return self.get(url=self.endpoints['ping'], authenticator=authenticator)
+
+    def submit_generic(self, data, authenticator=None):
+        return self.post(url=self.endpoints['submission'], data=data, authenticator=authenticator)
+
+    def delete_submissions(self, email_address, authenticator=None):
+        endpoint = self.endpoints['delete_submissions'] + f'{email_address}/'
+        return self.delete(url=endpoint, authenticator=authenticator)
+
+    # API V2
+    def gov_notify_bulk_email(self, data, authenticator=None):
+        """
+        Allows an email with multiple recipients to be sent be gov.notify.
+
+        :param data: Email meta data.
+        :param authenticator: API authenticator class (default None)
+        :return: Request object
+        """
+
+        return self.post(url=self.endpoints['gov_notify_bulk_email'], data=data, authenticator=authenticator)
+
+    def hcsat_feedback_submission(self, data, authenticator=None):
+        return self.post(url=self.endpoints['hcsat_feedback_submission'], data=data, authenticator=authenticator)
+
+    def verification_reminders(self, data, authenticator=None):
+        return self.post(url=self.endpoints['verification_reminders'], data=data, authenticator=authenticator)
+
+
+forms_api_client = APIFormsClient(
+    base_url=settings.DIRECTORY_FORMS_API_BASE_URL,
+    api_key=settings.DIRECTORY_FORMS_API_API_KEY,
+    sender_id=settings.DIRECTORY_FORMS_API_SENDER_ID,
+    timeout=settings.DIRECTORY_FORMS_API_DEFAULT_TIMEOUT,
+)
