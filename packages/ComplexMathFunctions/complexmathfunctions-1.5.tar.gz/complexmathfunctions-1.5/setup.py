64af31eb7761fa6af2433f6299820137e28d49ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 08:06:28 +0000

    Add package ComplexMathFunctions/1.5/complexmathfunctions-1.5.tar.gz

---
+++ packages/ComplexMathFunctions/complexmathfunctions-1.5.tar.gz/complexmathfunctions-1.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from os import path
+
+working_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(working_directory, 'README.md'), encoding ='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name = "ComplexMathFunctions",
+    version = "1.5",
+    author = "Enes5234r",
+    description = "Complex math",
+    long_description=  long_description,
+    long_description_content_type= "text/markdown",
+    author_email = "bayintinkercadhesabi@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
