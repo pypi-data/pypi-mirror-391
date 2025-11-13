2c6638a15aa44a34d311826387083fddeebcdb32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 07:06:17 +0000

    Add package urban-mapper/0.1.5/urban_mapper-0.1.5-py3-none-any.whl

---
+++ packages/urban-mapper/urban_mapper-0.1.5-py3-none-any.whl/urban_mapper/modules/__init__.py
@@ -0,0 +1,83 @@
+from .loader import (
+    LoaderBase,
+    FileLoaderBase,
+    CSVLoader,
+    ShapefileLoader,
+    ParquetLoader,
+    DataFrameLoader,
+    HuggingFaceLoader,
+)
+from .imputer import (
+    GeoImputerBase,
+    SimpleGeoImputer,
+    AddressGeoImputer,
+)
+from .filter import (
+    GeoFilterBase,
+    BoundingBoxFilter,
+)
+from .enricher import (
+    EnricherBase,
+    BaseAggregator,
+    SimpleAggregator,
+    SingleAggregatorEnricher,
+    EnricherFactory,
+)
+from .visualiser import VisualiserBase, StaticVisualiser, InteractiveVisualiser
+
+from .urban_layer import (
+    OSMNXStreets,
+    OSMNXIntersections,
+    Tile2NetSidewalks,
+    Tile2NetCrosswalks,
+    OSMFeatures,
+    UrbanLayerFactory,
+    CustomUrbanLayer,
+    RegionCities,
+    RegionCountries,
+    RegionStates,
+    RegionNeighborhoods,
+)
+
+from .pipeline_generator import (
+    GPT4OPipelineGenerator,
+    PipelineGeneratorBase,
+    PipelineGeneratorFactory,
+)
+
+__all__ = [
+    "LoaderBase",
+    "FileLoaderBase",
+    "CSVLoader",
+    "ShapefileLoader",
+    "ParquetLoader",
+    "DataFrameLoader",
+    "HuggingFaceLoader",
+    "GeoImputerBase",
+    "SimpleGeoImputer",
+    "AddressGeoImputer",
+    "GeoFilterBase",
+    "BoundingBoxFilter",
+    "EnricherBase",
+    "BaseAggregator",
+    "SimpleAggregator",
+    "SingleAggregatorEnricher",
+    "EnricherFactory",
+    "VisualiserBase",
+    "StaticVisualiser",
+    "InteractiveVisualiser",
+    "OSMNXStreets",
+    "OSMNXIntersections",
+    "Tile2NetSidewalks",
+    "Tile2NetCrosswalks",
+    "OSMFeatures",
+    "UrbanLayerFactory",
+    "GPT4OPipelineGenerator",
+    "PipelineGeneratorBase",
+    "PipelineGeneratorFactory",
+    "CustomUrbanLayer",
+    "RegionCities",
+    "RegionCountries",
+    "RegionStates",
+    "RegionNeighborhoods",
+]
