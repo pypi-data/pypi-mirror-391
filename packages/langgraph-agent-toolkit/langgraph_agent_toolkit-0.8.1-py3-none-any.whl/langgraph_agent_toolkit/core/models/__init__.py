6cf93f0e395019f3f095bd8d31a0caa5e4b32deb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 08:05:44 +0000

    Add package langgraph-agent-toolkit/0.8.1/langgraph_agent_toolkit-0.8.1-py3-none-any.whl

---
+++ packages/langgraph-agent-toolkit/langgraph_agent_toolkit-0.8.1-py3-none-any.whl/langgraph_agent_toolkit/core/models/__init__.py
@@ -0,0 +1,5 @@
+from langgraph_agent_toolkit.core.models.chat_openai import ChatOpenAIPatched
+from langgraph_agent_toolkit.core.models.fake import FakeToolModel
+
+
+__all__ = ["ChatOpenAIPatched", "FakeToolModel"]
