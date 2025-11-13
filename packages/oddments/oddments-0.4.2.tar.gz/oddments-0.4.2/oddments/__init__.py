5d883a35d6505f94d91de08155f4cd53ca13fa79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 15:06:28 +0000

    Add package oddments/0.4.2/oddments-0.4.2.tar.gz

---
+++ packages/oddments/oddments-0.4.2.tar.gz/oddments-0.4.2/oddments/__init__.py
@@ -0,0 +1,8 @@
+from .iteration import *
+from .pandas import *
+from .sorting import *
+from .text import *
+from .validation import *
+
+__version__ = '0.4.2'
+__author__ = 'Zachary Einck <zacharyeinck@gmail.com>'
