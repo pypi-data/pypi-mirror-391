5183a8c280905ae2741a87efb05eac8f8e94b5e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 05:07:52 +0000

    Add package bluer-ugv/7.293.1/bluer_ugv-7.293.1-py3-none-any.whl

---
+++ packages/bluer-ugv/bluer_ugv-7.293.1-py3-none-any.whl/bluer_ugv/README/rangin/consts.py
@@ -0,0 +1,9 @@
+from bluer_objects.README.consts import assets_url, designs_repo
+
+
+rangin_assets2 = assets_url(
+    suffix="rangin",
+    volume=2,
+)
+
+rangin_mechanical_design = f"{designs_repo}/blob/main/rangin"
