b0e76d8f037a67acbae688edd9c61d6f77f13464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 06:05:37 +0000

    Add package chellow/1762965893.0.0/chellow-1762965893.0.0-py3-none-any.whl

---
+++ packages/chellow/chellow-1762965893.0.0-py3-none-any.whl/chellow/e/ro.py
@@ -0,0 +1,19 @@
+def hh(supply_source):
+    try:
+        ro_cache = supply_source.caches["ro"]
+    except KeyError:
+        ro_cache = supply_source.caches["ro"] = {}
+
+    for hh in supply_source.hh_data:
+        try:
+            hh["ro-rate"] = ro_rate = ro_cache[hh["start-date"]]
+        except KeyError:
+            h_start = hh["start-date"]
+            hh["ro-rate"] = ro_rate = ro_cache[h_start] = float(
+                supply_source.non_core_rate("ro", h_start)["ro_gbp_per_msp_kwh"]
+            )
+
+        ro_kwh = hh["msp-kwh"]
+        hh["ro-msp-kwh"] = ro_kwh
+        hh["ro-kwh"] = ro_kwh
+        hh["ro-gbp"] = ro_kwh * ro_rate
