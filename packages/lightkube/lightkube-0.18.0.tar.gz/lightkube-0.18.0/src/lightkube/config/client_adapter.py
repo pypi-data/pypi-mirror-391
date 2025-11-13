672b8f2984c8244a268c64ea38164e84f07829c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 03:27:26 +0000

    Add package lightkube/0.18.0/lightkube-0.18.0.tar.gz

---
+++ packages/lightkube/lightkube-0.18.0.tar.gz/lightkube-0.18.0/src/lightkube/config/client_adapter.py
@@ -0,0 +1,168 @@
+import asyncio.subprocess
+import json
+import os
+import ssl
+import subprocess
+from typing import Optional
+
+import httpx
+
+from ..core.exceptions import ConfigError
+from .kubeconfig import SingleConfig
+from .models import Cluster, FileStr, User, UserExec
+
+
+def Client(
+    config: SingleConfig,
+    timeout: httpx.Timeout,
+    trust_env: bool = True,
+    transport: httpx.BaseTransport = None,
+    proxy: Optional[str] = None,
+    http2: bool = False,
+) -> httpx.Client:
+    return httpx.Client(
+        transport=transport,
+        http2=http2,
+        **httpx_parameters(config, timeout, proxy, trust_env),
+    )
+
+
+def AsyncClient(
+    config: SingleConfig,
+    timeout: httpx.Timeout,
+    trust_env: bool = True,
+    transport: httpx.AsyncBaseTransport = None,
+    proxy: Optional[str] = None,
+    http2: bool = False,
+) -> httpx.AsyncClient:
+    return httpx.AsyncClient(
+        transport=transport,
+        http2=http2,
+        **httpx_parameters(config, timeout, proxy, trust_env),
+    )
+
+
+def httpx_parameters(config: SingleConfig, timeout: httpx.Timeout, proxy: str, trust_env: bool):
+    return dict(
+        timeout=timeout,
+        proxy=proxy,
+        base_url=config.cluster.server,
+        verify=verify_cluster(config.cluster, config.user, config.abs_file, trust_env=trust_env),
+        auth=user_auth(config.user),
+        trust_env=trust_env,
+    )
+
+
+class BearerAuth(httpx.Auth):
+    def __init__(self, token):
+        self._bearer = f"Bearer {token}"
+
+    def auth_flow(self, request: httpx.Request):
+        request.headers["Authorization"] = self._bearer
+        yield request
+
+
+async def async_check_output(command, env):
+    PIPE = asyncio.subprocess.PIPE
+    proc = await asyncio.create_subprocess_exec(*command, env=env, stdin=None, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = await proc.communicate()
+    if proc.returncode != 0:
+        raise ConfigError(f"Exec {command[0]} returned {proc.returncode}: {stderr.decode()}")
+    return stdout
+
+
+def sync_check_output(command, env):
+    proc = subprocess.Popen(command, env=env, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    if proc.returncode != 0:
+        raise ConfigError(f"Exec {command[0]} returned {proc.returncode}: {stderr.decode()}")
+    return stdout
+
+
+class ExecAuth(httpx.Auth):
+    # The param name `exec` shadows the Python builtin, but changing the kwarg name would potentially break users code
+    def __init__(self, exec: UserExec):  # noqa: A002
+        self._exec = exec
+        self._last_bearer = None
+
+    def _prepare(self):
+        _exec = self._exec
+        if _exec.apiVersion not in (
+            "client.authentication.k8s.io/v1alpha1",
+            "client.authentication.k8s.io/v1beta1",
+        ):
+            raise ConfigError(f"auth exec api version {_exec.apiVersion} not implemented")
+        cmd_env_vars = dict(os.environ)
+        if _exec.env:
+            cmd_env_vars.update((var.name, var.value) for var in _exec.env)
+        # TODO: add support for passing KUBERNETES_EXEC_INFO env var
+        # https://github.com/kubernetes/community/blob/master/contributors/design-proposals/auth/kubectl-exec-plugins.md
+        args = _exec.args if _exec.args else []
+        return [_exec.command, *args], cmd_env_vars
+
+    def sync_auth_flow(self, request: httpx.Request):
+        if self._last_bearer:
+            request.headers["Authorization"] = self._last_bearer
+            response = yield request
+            if response.status_code != 401:
+                return
+
+        command, env = self._prepare()
+        output = sync_check_output(command, env=env)
+        token = json.loads(output)["status"]["token"]
+        request.headers["Authorization"] = self._last_bearer = f"Bearer {token}"
+        yield request
+
+    async def async_auth_flow(self, request: httpx.Request):
+        if self._last_bearer:
+            request.headers["Authorization"] = self._last_bearer
+            response = yield request
+            if response.status_code != 401:
+                return
+
+        command, env = self._prepare()
+        output = await async_check_output(command, env=env)
+        token = json.loads(output)["status"]["token"]
+        request.headers["Authorization"] = self._last_bearer = f"Bearer {token}"
+        yield request
+
+
+def user_auth(user: Optional[User]):
+    if user is None:
+        return None
+
+    if user.token is not None:
+        return BearerAuth(user.token)
+
+    if user.exec:
+        return ExecAuth(user.exec)
+
+    if user.username and user.password:
+        return httpx.BasicAuth(user.username, user.password)
+
+    if user.auth_provider:
+        raise ConfigError("auth-provider not supported")
+
+
+def user_cert(user: User, abs_file):
+    """Extract user certificates"""
+    if user.client_cert or user.client_cert_data:
+        return (
+            FileStr(user.client_cert_data) or abs_file(user.client_cert),
+            FileStr(user.client_key_data) or abs_file(user.client_key),
+        )
+    return None
+
+
+def verify_cluster(cluster: Cluster, user: User, abs_file, trust_env: bool = True):
+    """setup certificate verification"""
+    if cluster.certificate_auth:
+        ctx = ssl.create_default_context(cafile=abs_file(cluster.certificate_auth))
+    elif cluster.certificate_auth_data:
+        ctx = ssl.create_default_context(cafile=FileStr(cluster.certificate_auth_data))
+    else:
+        ctx = httpx.create_ssl_context(verify=not cluster.insecure, trust_env=trust_env)
+    cert = user_cert(user, abs_file)
+    if cert:
+        ctx.load_cert_chain(*cert)
+    return ctx
---
+++ packages/lightkube/lightkube-0.18.0.tar.gz/lightkube-0.18.0/src/lightkube/operators.py
@@ -0,0 +1,51 @@
+from typing import Iterable
+
+__all__ = ["equal", "exists", "in_", "not_equal", "not_exists", "not_in"]
+
+
+class Operator:
+    def __init__(self, op_name: str, op: str, value=None):
+        self.op = op
+        self.value = value
+        self.op_name = op_name
+
+    def encode(self, key):
+        return f"{key}{self.op}{self.value}"
+
+
+class SequenceOperator(Operator):
+    def encode(self, key):
+        return f"{key} {self.op} ({','.join(self.value)})"
+
+
+class BinaryOperator(Operator):
+    pass
+
+
+class UnaryOperator(Operator):
+    def encode(self, key):
+        return f"{self.op}{key}"
+
+
+def in_(values: Iterable) -> SequenceOperator:
+    return SequenceOperator("in_", "in", sorted(values))
+
+
+def not_in(values: Iterable) -> SequenceOperator:
+    return SequenceOperator("not_in", "notin", sorted(values))
+
+
+def exists() -> UnaryOperator:
+    return UnaryOperator("exists", "")
+
+
+def not_exists() -> UnaryOperator:
+    return UnaryOperator("not_exists", "!")
+
+
+def equal(value: str) -> BinaryOperator:
+    return BinaryOperator("equal", "=", value)
+
+
+def not_equal(value: str) -> BinaryOperator:
+    return BinaryOperator("not_equal", "!=", value)
