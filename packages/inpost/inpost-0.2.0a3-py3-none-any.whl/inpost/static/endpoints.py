4bb7a61fd408661e931d11bbb6e0d0217d478e08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 08:05:57 +0000

    Add package inpost/0.2.0a3/inpost-0.2.0a3-py3-none-any.whl

---
+++ packages/inpost/inpost-0.2.0a3-py3-none-any.whl/inpost/static/endpoints.py
@@ -0,0 +1,42 @@
+# AUTH #
+login_url: str = "https://api-inmobile-pl.easypack24.net/v1/authenticate"
+send_sms_code_url: str = "https://api-inmobile-pl.easypack24.net/v1/account"  # post
+confirm_sms_code_url: str = "https://api-inmobile-pl.easypack24.net/v1/account/verification"  # post
+logout_url: str = "https://api-inmobile-pl.easypack24.net/v1/logout"  # post
+refresh_token_url: str = "https://api-inmobile-pl.easypack24.net/v1/authenticate"  # post
+
+# INCOMING PARCELS #
+tracked_url: str = "https://api-inmobile-pl.easypack24.net/v4/parcels/tracked"  # get
+multi_url: str = "https://api-inmobile-pl.easypack24.net/v4/parcels/multi"  # get
+collect_url: str = "https://api-inmobile-pl.easypack24.net/v2/collect/validate"  # post
+compartment_reopen_url: str = "https://api-inmobile-pl.easypack24.net/v1/collect/compartment/reopen"  # post
+compartment_open_url: str = "https://api-inmobile-pl.easypack24.net/v1/collect/compartment/open"  # post
+compartment_status_url: str = "https://api-inmobile-pl.easypack24.net/v1/collect/compartment/status"  # post
+terminate_collect_session_url: str = "https://api-inmobile-pl.easypack24.net/v1/collect/terminate"  # post
+shared_url: str = "https://api-inmobile-pl.easypack24.net/v4/parcels/shared"  # post
+
+# CREATING PARCEL #
+create_url: str = "https://api-inmobile-pl.easypack24.net/v1/parcels"
+points_url: str = "https://api-inmobile-pl.easypack24.net/v3/points"
+blik_status_url: str = "https://api-inmobile-pl.easypack24.net/v1/payments/blik/alias/status"
+create_blik_url: str = "https://api-inmobile-pl.easypack24.net/v1/payments/transactions/create/blik"
+
+# OUTGOING PARCELS #
+sent_url: str = "https://api-inmobile-pl.easypack24.net/v2/parcels/sent/"  # get
+parcel_points_url: str = "https://api-inmobile-pl.easypack24.net/v3/points/"  # get
+validate_sent_url: str = "https://api-inmobile-pl.easypack24.net/v1/send/validate/"  # post
+open_sent_url: str = "https://api-inmobile-pl.easypack24.net/v1/send/compartment/open"  # post
+reopen_sent_url: str = "https://api-inmobile-pl.easypack24.net/v1/send/compartment/reopen"  # post
+status_sent_url: str = "https://api-inmobile-pl.easypack24.net/v1/send/compartment/status"  # post
+confirm_sent_url: str = "https://api-inmobile-pl.easypack24.net/v1/send/confirm"  # post
+parcel_prices_url: str = "https://api-inmobile-pl.easypack24.net/v1/prices/parcels"  # get
+
+# RETURNS #
+returns_url: str = "https://api-inmobile-pl.easypack24.net/v1/returns/parcels/"  # get
+tickets_url: str = "https://api-inmobile-pl.easypack24.net/v1/returns/tickets"  # get
+parcel_notifications_url: str = "https://api-inmobile-pl.easypack24.net/v2/notifications?type=PUSH%2CNEWS%2CTILE"  # get
+
+# FRIENDS #
+friendship_url: str = "https://api-inmobile-pl.easypack24.net/v1/friends/"  # get, post, patch, delete
+validate_friendship_url: str = "https://api-inmobile-pl.easypack24.net/v1/invitations/validate"  # post
+accept_friendship_url: str = "https://api-inmobile-pl.easypack24.net/v1/invitations/accept"  # post
