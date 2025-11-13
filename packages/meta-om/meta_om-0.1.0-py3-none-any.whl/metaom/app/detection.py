a8110c9d1bbe76603308766c9e6f6fd5153bd207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 03:27:39 +0000

    Add package meta-om/0.1.0/meta_om-0.1.0-py3-none-any.whl

---
+++ packages/meta-om/meta_om-0.1.0-py3-none-any.whl/metaom/app/detection.py
@@ -0,0 +1,46 @@
+import metacv as mc
+from ..model_zoo import load_model, run, release
+
+
+class Detection(mc.Detection):
+    def __init__(self,
+                 model_path: str,
+                 input_width: int,
+                 input_height: int,
+                 use_preprocess=True,
+                 pad=None,
+                 normal=None,
+                 mean=None,
+                 std=None,
+                 swap=None,
+                 confidence_thresh=None,
+                 nms_thresh=None,
+                 class_names=None,
+                 device_id=0):
+        super().__init__(model_path,
+                         input_width,
+                         input_height,
+                         use_preprocess,
+                         pad,
+                         normal,
+                         mean,
+                         std,
+                         swap,
+                         confidence_thresh,
+                         nms_thresh,
+                         class_names)
+        self.device_id = device_id
+        self.model = None
+        self.det_output = None
+        self.initialize_model()
+
+    def initialize_model(self):
+        self.model = load_model(self.model_path, self.device_id)
+
+    def infer(self, image):
+        # 由继承类实现模型推理
+        outputs = run(image, self.model)
+        self.det_output = outputs[0]
+
+    def __del__(self):
+        release(self.model)
