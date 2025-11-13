1cc65f4e518ebb01a0f64554dc70bbbfafc87a03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 03:26:35 +0000

    Add package pytest-embedded-arduino/2.4.0/pytest_embedded_arduino-2.4.0.tar.gz

---
+++ packages/pytest-embedded-arduino/pytest_embedded_arduino-2.4.0.tar.gz/pytest_embedded_arduino-2.4.0/pytest_embedded_arduino/serial.py
@@ -0,0 +1,62 @@
+import logging
+
+import esptool
+from pytest_embedded_serial_esp.serial import EspSerial
+
+from .app import ArduinoApp
+
+
+class ArduinoSerial(EspSerial):
+    """
+    Arduino serial Dut class
+
+    Auto flash the app while starting test.
+    """
+
+    SUGGEST_FLASH_BAUDRATE = 921600
+
+    def __init__(
+        self,
+        app: ArduinoApp,
+        target: str | None = None,
+        **kwargs,
+    ) -> None:
+        self.app = app
+        super().__init__(
+            target=target or self.app.target,
+            **kwargs,
+        )
+
+    def _start(self):
+        if self.skip_autoflash:
+            logging.info('Skipping auto flash...')
+            super()._start()
+        else:
+            self.flash()
+
+    @EspSerial.use_esptool()
+    def flash(self) -> None:
+        """
+        Flash the binary files to the board.
+        """
+        flash_files = []
+        for offset, path, encrypted in self.app.flash_files:
+            if encrypted:
+                continue
+            flash_files.extend((str(offset), path))
+
+        flash_settings = []
+        for k, v in self.app.flash_settings[self.app.target].items():
+            flash_settings.append(f'--{k}')
+            flash_settings.append(v)
+
+        if self.esp_flash_force:
+            flash_settings.append('--force')
+
+        try:
+            esptool.main(
+                ['--chip', self.app.target, 'write-flash', *flash_files, *flash_settings],
+                esp=self.esp,
+            )
+        except Exception:
+            raise
