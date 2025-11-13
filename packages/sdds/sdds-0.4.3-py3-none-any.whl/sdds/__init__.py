e4ff44384af248dcb54f215b14b7c2777b2b09ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 17:07:37 +0000

    Add package sdds/0.4.3/sdds-0.4.3-py3-none-any.whl

---
+++ packages/sdds/sdds-0.4.3-py3-none-any.whl/sdds/__init__.py
@@ -0,0 +1,20 @@
+"""Exposes SddsFile, read_sdds and write_sdds directly in sdds namespace."""
+
+from sdds.classes import SddsFile
+from sdds.reader import read_sdds
+from sdds.writer import write_sdds
+
+__title__ = "sdds"
+__description__ = "SDDS file handling."
+__url__ = "https://github.com/pylhc/sdds"
+__version__ = "0.4.3"
+__author__ = "pylhc"
+__author_email__ = "pylhc@github.com"
+__license__ = "MIT"
+
+# aliases
+read = read_sdds
+write = write_sdds
+
+
+__all__ = ["read", "SddsFile", "write", "__version__"]
