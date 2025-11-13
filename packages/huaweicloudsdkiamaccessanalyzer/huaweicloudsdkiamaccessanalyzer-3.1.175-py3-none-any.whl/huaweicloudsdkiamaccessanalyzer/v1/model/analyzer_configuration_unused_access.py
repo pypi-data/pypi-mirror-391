ff4397fa7e4cc5adce93b398cbf2c643f6567cba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 17:07:52 +0000

    Add package huaweicloudsdkiamaccessanalyzer/3.1.175/huaweicloudsdkiamaccessanalyzer-3.1.175-py3-none-any.whl

---
+++ packages/huaweicloudsdkiamaccessanalyzer/huaweicloudsdkiamaccessanalyzer-3.1.175-py3-none-any.whl/huaweicloudsdkiamaccessanalyzer/v1/model/analyzer_configuration_unused_access.py
@@ -0,0 +1,108 @@
+# coding: utf-8
+
+from huaweicloudsdkcore.utils.http_utils import sanitize_for_serialization
+
+
+class AnalyzerConfigurationUnusedAccess:
+
+    """
+    Attributes:
+      openapi_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    sensitive_list = []
+
+    openapi_types = {
+        'unused_access_age': 'int'
+    }
+
+    attribute_map = {
+        'unused_access_age': 'unused_access_age'
+    }
+
+    def __init__(self, unused_access_age=None):
+        r"""AnalyzerConfigurationUnusedAccess
+
+        The model defined in huaweicloud sdk
+
+        :param unused_access_age: 生成分析结果的预设天数。
+        :type unused_access_age: int
+        """
+        
+        
+
+        self._unused_access_age = None
+        self.discriminator = None
+
+        if unused_access_age is not None:
+            self.unused_access_age = unused_access_age
+
+    @property
+    def unused_access_age(self):
+        r"""Gets the unused_access_age of this AnalyzerConfigurationUnusedAccess.
+
+        生成分析结果的预设天数。
+
+        :return: The unused_access_age of this AnalyzerConfigurationUnusedAccess.
+        :rtype: int
+        """
+        return self._unused_access_age
+
+    @unused_access_age.setter
+    def unused_access_age(self, unused_access_age):
+        r"""Sets the unused_access_age of this AnalyzerConfigurationUnusedAccess.
+
+        生成分析结果的预设天数。
+
+        :param unused_access_age: The unused_access_age of this AnalyzerConfigurationUnusedAccess.
+        :type unused_access_age: int
+        """
+        self._unused_access_age = unused_access_age
+
+    def to_dict(self):
+        result = {}
+
+        for attr, _ in self.openapi_types.items():
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                if attr in self.sensitive_list:
+                    result[attr] = "****"
+                else:
+                    result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        import simplejson as json
+        return json.dumps(sanitize_for_serialization(self), ensure_ascii=False)
+
+    def __repr__(self):
+        """For `print`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, AnalyzerConfigurationUnusedAccess):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
---
+++ packages/huaweicloudsdkiamaccessanalyzer/huaweicloudsdkiamaccessanalyzer-3.1.175-py3-none-any.whl/huaweicloudsdkiamaccessanalyzer/v1/model/finding_principal.py
@@ -0,0 +1,135 @@
+# coding: utf-8
+
+from huaweicloudsdkcore.utils.http_utils import sanitize_for_serialization
+
+
+class FindingPrincipal:
+
+    """
+    Attributes:
+      openapi_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    sensitive_list = []
+
+    openapi_types = {
+        'identifier': 'str',
+        'type': 'str'
+    }
+
+    attribute_map = {
+        'identifier': 'identifier',
+        'type': 'type'
+    }
+
+    def __init__(self, identifier=None, type=None):
+        r"""FindingPrincipal
+
+        The model defined in huaweicloud sdk
+
+        :param identifier: 外部主体身份的标识符。
+        :type identifier: str
+        :param type: 外部主体身份的类型。
+        :type type: str
+        """
+        
+        
+
+        self._identifier = None
+        self._type = None
+        self.discriminator = None
+
+        self.identifier = identifier
+        self.type = type
+
+    @property
+    def identifier(self):
+        r"""Gets the identifier of this FindingPrincipal.
+
+        外部主体身份的标识符。
+
+        :return: The identifier of this FindingPrincipal.
+        :rtype: str
+        """
+        return self._identifier
+
+    @identifier.setter
+    def identifier(self, identifier):
+        r"""Sets the identifier of this FindingPrincipal.
+
+        外部主体身份的标识符。
+
+        :param identifier: The identifier of this FindingPrincipal.
+        :type identifier: str
+        """
+        self._identifier = identifier
+
+    @property
+    def type(self):
+        r"""Gets the type of this FindingPrincipal.
+
+        外部主体身份的类型。
+
+        :return: The type of this FindingPrincipal.
+        :rtype: str
+        """
+        return self._type
+
+    @type.setter
+    def type(self, type):
+        r"""Sets the type of this FindingPrincipal.
+
+        外部主体身份的类型。
+
+        :param type: The type of this FindingPrincipal.
+        :type type: str
+        """
+        self._type = type
+
+    def to_dict(self):
+        result = {}
+
+        for attr, _ in self.openapi_types.items():
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                if attr in self.sensitive_list:
+                    result[attr] = "****"
+                else:
+                    result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        import simplejson as json
+        return json.dumps(sanitize_for_serialization(self), ensure_ascii=False)
+
+    def __repr__(self):
+        """For `print`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, FindingPrincipal):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
