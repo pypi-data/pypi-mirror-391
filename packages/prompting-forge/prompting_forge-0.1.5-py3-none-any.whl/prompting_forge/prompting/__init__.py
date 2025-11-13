ebfee2a94762d7485eb1af5ef7a9801db18380c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 06:05:37 +0000

    Add package prompting-forge/0.1.5/prompting_forge-0.1.5-py3-none-any.whl

---
+++ packages/prompting-forge/prompting_forge-0.1.5-py3-none-any.whl/prompting_forge/prompting/__init__.py
@@ -0,0 +1,5 @@
+from .prompt import ChatPrompt, BasePromptTemplate, PromptTemplate, FinalPromptTemplate
+
+__all__ = ["BasePromptTemplate", "PromptTemplate", "FinalPromptTemplate", "ChatPrompt"]
+
+
