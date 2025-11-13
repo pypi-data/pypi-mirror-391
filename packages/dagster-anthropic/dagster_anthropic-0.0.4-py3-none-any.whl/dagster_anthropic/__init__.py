98cab3632d51cedf7eab7a066298d163204cd5a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 05:08:24 +0000

    Add package dagster-anthropic/0.0.4/dagster_anthropic-0.0.4-py3-none-any.whl

---
+++ packages/dagster-anthropic/dagster_anthropic-0.0.4-py3-none-any.whl/dagster_anthropic/__init__.py
@@ -0,0 +1,9 @@
+from dagster._core.libraries import DagsterLibraryRegistry
+
+from dagster_anthropic.resource import AnthropicResource as AnthropicResource
+
+__version__ = "0.0.4"
+
+DagsterLibraryRegistry.register(
+    "dagster-anthropic", __version__, is_dagster_package=False
+)
