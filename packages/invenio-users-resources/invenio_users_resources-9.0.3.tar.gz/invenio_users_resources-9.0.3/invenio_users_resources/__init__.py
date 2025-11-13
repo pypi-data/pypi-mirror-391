e0a952de4616decfa91985b710f46d3cc74f41ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 04:09:08 +0000

    Add package invenio-users-resources/9.0.3/invenio_users_resources-9.0.3.tar.gz

---
+++ packages/invenio-users-resources/invenio_users_resources-9.0.3.tar.gz/invenio_users_resources-9.0.3/invenio_users_resources/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022-2025 CERN.
+# Copyright (C) 2022-2023 TU Wien.
+# Copyright (C) 2024-2025 Graz University of Technology.
+#
+# Invenio-Users-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Invenio module providing management APIs for users and roles/groups."""
+
+__version__ = "9.0.3"
+
+__all__ = ("__version__",)
