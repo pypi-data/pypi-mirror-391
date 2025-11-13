2ccc8df7cb3568a8a5bad4fb0d330708c69ba5d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 05:05:53 +0000

    Add package sortition-algorithms/0.10.11/sortition_algorithms-0.10.11-py3-none-any.whl

---
+++ packages/sortition-algorithms/sortition_algorithms-0.10.11-py3-none-any.whl/sortition_algorithms/errors.py
@@ -0,0 +1,57 @@
+import html
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    # this is done to avoid circular imports
+    from sortition_algorithms.features import FeatureCollection
+
+
+class SortitionBaseError(Exception):
+    """A base class that allows all errors to be caught easily."""
+
+    def to_html(self) -> str:
+        return html.escape(str(self))
+
+
+class BadDataError(SortitionBaseError):
+    """Error for when bad data is found while reading things in"""
+
+
+class SelectionError(SortitionBaseError):
+    """Generic error for things that happen in selection"""
+
+
+class SelectionMultilineError(SelectionError):
+    """Generic error for things that happen in selection - multiline"""
+
+    def __init__(self, lines: list[str]) -> None:
+        self.all_lines = lines
+
+    def __str__(self) -> str:
+        return "\n".join(self.all_lines)
+
+    def to_html(self) -> str:
+        return "<br />".join(html.escape(line) for line in self.all_lines)
+
+    def lines(self) -> list[str]:
+        return self.all_lines
+
+    def combine(self, other: "SelectionMultilineError") -> None:
+        """Add all the lines from the other error to this one."""
+        self.all_lines += other.all_lines
+
+
+class InfeasibleQuotasError(SelectionMultilineError):
+    """
+    The quotas can't be met, and a feasible relaxation was found.
+
+    The details of what relaxations are recommended are included in the error.
+    """
+
+    def __init__(self, features: "FeatureCollection", output: list[str]) -> None:
+        self.features = features
+        super().__init__(lines=["The quotas are infeasible:", *output])
+
+
+class InfeasibleQuotasCantRelaxError(SortitionBaseError):
+    """The quotas can't be met, and no feasible relaxation was found"""
