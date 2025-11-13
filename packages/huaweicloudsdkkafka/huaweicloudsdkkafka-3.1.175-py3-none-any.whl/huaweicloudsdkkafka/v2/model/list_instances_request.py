800f8e2f148ad81979dde94c6577a2e987f02b40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 17:07:55 +0000

    Add package huaweicloudsdkkafka/3.1.175/huaweicloudsdkkafka-3.1.175-py3-none-any.whl

---
+++ packages/huaweicloudsdkkafka/huaweicloudsdkkafka-3.1.175-py3-none-any.whl/huaweicloudsdkkafka/v2/model/list_instances_request.py
@@ -0,0 +1,339 @@
+# coding: utf-8
+
+from huaweicloudsdkcore.utils.http_utils import sanitize_for_serialization
+
+
+class ListInstancesRequest:
+
+    """
+    Attributes:
+      openapi_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    sensitive_list = []
+
+    openapi_types = {
+        'engine': 'str',
+        'name': 'str',
+        'instance_id': 'str',
+        'status': 'str',
+        'include_failure': 'str',
+        'exact_match_name': 'str',
+        'enterprise_project_id': 'str',
+        'offset': 'str',
+        'limit': 'str'
+    }
+
+    attribute_map = {
+        'engine': 'engine',
+        'name': 'name',
+        'instance_id': 'instance_id',
+        'status': 'status',
+        'include_failure': 'include_failure',
+        'exact_match_name': 'exact_match_name',
+        'enterprise_project_id': 'enterprise_project_id',
+        'offset': 'offset',
+        'limit': 'limit'
+    }
+
+    def __init__(self, engine=None, name=None, instance_id=None, status=None, include_failure=None, exact_match_name=None, enterprise_project_id=None, offset=None, limit=None):
+        r"""ListInstancesRequest
+
+        The model defined in huaweicloud sdk
+
+        :param engine: 消息引擎：kafka。
+        :type engine: str
+        :param name: 实例名称。
+        :type name: str
+        :param instance_id: 实例ID。
+        :type instance_id: str
+        :param status: 实例状态。 详细状态说明请参考[实例状态说明](kafka-api-180514012.xml)。
+        :type status: str
+        :param include_failure: 是否返回创建失败的实例数。  当参数值为“true”时，返回创建失败的实例数。参数值为“false”，不返回创建失败的实例数。
+        :type include_failure: str
+        :param exact_match_name: 是否按照实例名称进行精确匹配查询。  默认为“false”，表示模糊匹配实例名称查询。若参数值为“true”表示按照实例名称进行精确匹配查询。
+        :type exact_match_name: str
+        :param enterprise_project_id: 企业项目ID。
+        :type enterprise_project_id: str
+        :param offset: 偏移量，表示从此偏移量开始查询， offset大于等于0。
+        :type offset: str
+        :param limit: 当次查询返回的最大实例个数，默认值为10，取值范围为1~50。
+        :type limit: str
+        """
+        
+        
+
+        self._engine = None
+        self._name = None
+        self._instance_id = None
+        self._status = None
+        self._include_failure = None
+        self._exact_match_name = None
+        self._enterprise_project_id = None
+        self._offset = None
+        self._limit = None
+        self.discriminator = None
+
+        self.engine = engine
+        if name is not None:
+            self.name = name
+        if instance_id is not None:
+            self.instance_id = instance_id
+        if status is not None:
+            self.status = status
+        if include_failure is not None:
+            self.include_failure = include_failure
+        if exact_match_name is not None:
+            self.exact_match_name = exact_match_name
+        if enterprise_project_id is not None:
+            self.enterprise_project_id = enterprise_project_id
+        if offset is not None:
+            self.offset = offset
+        if limit is not None:
+            self.limit = limit
+
+    @property
+    def engine(self):
+        r"""Gets the engine of this ListInstancesRequest.
+
+        消息引擎：kafka。
+
+        :return: The engine of this ListInstancesRequest.
+        :rtype: str
+        """
+        return self._engine
+
+    @engine.setter
+    def engine(self, engine):
+        r"""Sets the engine of this ListInstancesRequest.
+
+        消息引擎：kafka。
+
+        :param engine: The engine of this ListInstancesRequest.
+        :type engine: str
+        """
+        self._engine = engine
+
+    @property
+    def name(self):
+        r"""Gets the name of this ListInstancesRequest.
+
+        实例名称。
+
+        :return: The name of this ListInstancesRequest.
+        :rtype: str
+        """
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        r"""Sets the name of this ListInstancesRequest.
+
+        实例名称。
+
+        :param name: The name of this ListInstancesRequest.
+        :type name: str
+        """
+        self._name = name
+
+    @property
+    def instance_id(self):
+        r"""Gets the instance_id of this ListInstancesRequest.
+
+        实例ID。
+
+        :return: The instance_id of this ListInstancesRequest.
+        :rtype: str
+        """
+        return self._instance_id
+
+    @instance_id.setter
+    def instance_id(self, instance_id):
+        r"""Sets the instance_id of this ListInstancesRequest.
+
+        实例ID。
+
+        :param instance_id: The instance_id of this ListInstancesRequest.
+        :type instance_id: str
+        """
+        self._instance_id = instance_id
+
+    @property
+    def status(self):
+        r"""Gets the status of this ListInstancesRequest.
+
+        实例状态。 详细状态说明请参考[实例状态说明](kafka-api-180514012.xml)。
+
+        :return: The status of this ListInstancesRequest.
+        :rtype: str
+        """
+        return self._status
+
+    @status.setter
+    def status(self, status):
+        r"""Sets the status of this ListInstancesRequest.
+
+        实例状态。 详细状态说明请参考[实例状态说明](kafka-api-180514012.xml)。
+
+        :param status: The status of this ListInstancesRequest.
+        :type status: str
+        """
+        self._status = status
+
+    @property
+    def include_failure(self):
+        r"""Gets the include_failure of this ListInstancesRequest.
+
+        是否返回创建失败的实例数。  当参数值为“true”时，返回创建失败的实例数。参数值为“false”，不返回创建失败的实例数。
+
+        :return: The include_failure of this ListInstancesRequest.
+        :rtype: str
+        """
+        return self._include_failure
+
+    @include_failure.setter
+    def include_failure(self, include_failure):
+        r"""Sets the include_failure of this ListInstancesRequest.
+
+        是否返回创建失败的实例数。  当参数值为“true”时，返回创建失败的实例数。参数值为“false”，不返回创建失败的实例数。
+
+        :param include_failure: The include_failure of this ListInstancesRequest.
+        :type include_failure: str
+        """
+        self._include_failure = include_failure
+
+    @property
+    def exact_match_name(self):
+        r"""Gets the exact_match_name of this ListInstancesRequest.
+
+        是否按照实例名称进行精确匹配查询。  默认为“false”，表示模糊匹配实例名称查询。若参数值为“true”表示按照实例名称进行精确匹配查询。
+
+        :return: The exact_match_name of this ListInstancesRequest.
+        :rtype: str
+        """
+        return self._exact_match_name
+
+    @exact_match_name.setter
+    def exact_match_name(self, exact_match_name):
+        r"""Sets the exact_match_name of this ListInstancesRequest.
+
+        是否按照实例名称进行精确匹配查询。  默认为“false”，表示模糊匹配实例名称查询。若参数值为“true”表示按照实例名称进行精确匹配查询。
+
+        :param exact_match_name: The exact_match_name of this ListInstancesRequest.
+        :type exact_match_name: str
+        """
+        self._exact_match_name = exact_match_name
+
+    @property
+    def enterprise_project_id(self):
+        r"""Gets the enterprise_project_id of this ListInstancesRequest.
+
+        企业项目ID。
+
+        :return: The enterprise_project_id of this ListInstancesRequest.
+        :rtype: str
+        """
+        return self._enterprise_project_id
+
+    @enterprise_project_id.setter
+    def enterprise_project_id(self, enterprise_project_id):
+        r"""Sets the enterprise_project_id of this ListInstancesRequest.
+
+        企业项目ID。
+
+        :param enterprise_project_id: The enterprise_project_id of this ListInstancesRequest.
+        :type enterprise_project_id: str
+        """
+        self._enterprise_project_id = enterprise_project_id
+
+    @property
+    def offset(self):
+        r"""Gets the offset of this ListInstancesRequest.
+
+        偏移量，表示从此偏移量开始查询， offset大于等于0。
+
+        :return: The offset of this ListInstancesRequest.
+        :rtype: str
+        """
+        return self._offset
+
+    @offset.setter
+    def offset(self, offset):
+        r"""Sets the offset of this ListInstancesRequest.
+
+        偏移量，表示从此偏移量开始查询， offset大于等于0。
+
+        :param offset: The offset of this ListInstancesRequest.
+        :type offset: str
+        """
+        self._offset = offset
+
+    @property
+    def limit(self):
+        r"""Gets the limit of this ListInstancesRequest.
+
+        当次查询返回的最大实例个数，默认值为10，取值范围为1~50。
+
+        :return: The limit of this ListInstancesRequest.
+        :rtype: str
+        """
+        return self._limit
+
+    @limit.setter
+    def limit(self, limit):
+        r"""Sets the limit of this ListInstancesRequest.
+
+        当次查询返回的最大实例个数，默认值为10，取值范围为1~50。
+
+        :param limit: The limit of this ListInstancesRequest.
+        :type limit: str
+        """
+        self._limit = limit
+
+    def to_dict(self):
+        result = {}
+
+        for attr, _ in self.openapi_types.items():
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                if attr in self.sensitive_list:
+                    result[attr] = "****"
+                else:
+                    result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        import simplejson as json
+        return json.dumps(sanitize_for_serialization(self), ensure_ascii=False)
+
+    def __repr__(self):
+        """For `print`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, ListInstancesRequest):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
---
+++ packages/huaweicloudsdkkafka/huaweicloudsdkkafka-3.1.175-py3-none-any.whl/huaweicloudsdkkafka/v2/model/show_instance_messages_request.py
@@ -0,0 +1,396 @@
+# coding: utf-8
+
+from huaweicloudsdkcore.utils.http_utils import sanitize_for_serialization
+
+
+class ShowInstanceMessagesRequest:
+
+    """
+    Attributes:
+      openapi_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    sensitive_list = []
+
+    openapi_types = {
+        'instance_id': 'str',
+        'topic': 'str',
+        'asc': 'bool',
+        'start_time': 'str',
+        'end_time': 'str',
+        'limit': 'str',
+        'offset': 'str',
+        'download': 'bool',
+        'message_offset': 'str',
+        'partition': 'str',
+        'keyword': 'str'
+    }
+
+    attribute_map = {
+        'instance_id': 'instance_id',
+        'topic': 'topic',
+        'asc': 'asc',
+        'start_time': 'start_time',
+        'end_time': 'end_time',
+        'limit': 'limit',
+        'offset': 'offset',
+        'download': 'download',
+        'message_offset': 'message_offset',
+        'partition': 'partition',
+        'keyword': 'keyword'
+    }
+
+    def __init__(self, instance_id=None, topic=None, asc=None, start_time=None, end_time=None, limit=None, offset=None, download=None, message_offset=None, partition=None, keyword=None):
+        r"""ShowInstanceMessagesRequest
+
+        The model defined in huaweicloud sdk
+
+        :param instance_id: 实例ID。
+        :type instance_id: str
+        :param topic: Topic名称。  Topic名称必须以字母开头且只支持大小写字母、中横线、下划线以及数字。
+        :type topic: str
+        :param asc: 是否按照时间排序。
+        :type asc: bool
+        :param start_time: 开始时间。  Unix毫秒时间戳。  查询消息偏移量时，为必选参数。
+        :type start_time: str
+        :param end_time: 结束时间。  Unix毫秒时间戳。  查询消息偏移量时，为必选参数。
+        :type end_time: str
+        :param limit: 每一页显示的message数量。
+        :type limit: str
+        :param offset: 页数。
+        :type offset: str
+        :param download: 是否下载。
+        :type download: bool
+        :param message_offset: 消息偏移量。  **查询消息内容时，为必选参数。**  若start_time、end_time参数不为空，该参数无效。
+        :type message_offset: str
+        :param partition: 分区。  **查询消息内容时，为必选参数。**  若start_time、end_time参数不为空，该参数无效。
+        :type partition: str
+        :param keyword: 关键词。 取值范围为0~50。
+        :type keyword: str
+        """
+        
+        
+
+        self._instance_id = None
+        self._topic = None
+        self._asc = None
+        self._start_time = None
+        self._end_time = None
+        self._limit = None
+        self._offset = None
+        self._download = None
+        self._message_offset = None
+        self._partition = None
+        self._keyword = None
+        self.discriminator = None
+
+        self.instance_id = instance_id
+        self.topic = topic
+        if asc is not None:
+            self.asc = asc
+        if start_time is not None:
+            self.start_time = start_time
+        if end_time is not None:
+            self.end_time = end_time
+        if limit is not None:
+            self.limit = limit
+        if offset is not None:
+            self.offset = offset
+        if download is not None:
+            self.download = download
+        if message_offset is not None:
+            self.message_offset = message_offset
+        if partition is not None:
+            self.partition = partition
+        if keyword is not None:
+            self.keyword = keyword
+
+    @property
+    def instance_id(self):
+        r"""Gets the instance_id of this ShowInstanceMessagesRequest.
+
+        实例ID。
+
+        :return: The instance_id of this ShowInstanceMessagesRequest.
+        :rtype: str
+        """
+        return self._instance_id
+
+    @instance_id.setter
+    def instance_id(self, instance_id):
+        r"""Sets the instance_id of this ShowInstanceMessagesRequest.
+
+        实例ID。
+
+        :param instance_id: The instance_id of this ShowInstanceMessagesRequest.
+        :type instance_id: str
+        """
+        self._instance_id = instance_id
+
+    @property
+    def topic(self):
+        r"""Gets the topic of this ShowInstanceMessagesRequest.
+
+        Topic名称。  Topic名称必须以字母开头且只支持大小写字母、中横线、下划线以及数字。
+
+        :return: The topic of this ShowInstanceMessagesRequest.
+        :rtype: str
+        """
+        return self._topic
+
+    @topic.setter
+    def topic(self, topic):
+        r"""Sets the topic of this ShowInstanceMessagesRequest.
+
+        Topic名称。  Topic名称必须以字母开头且只支持大小写字母、中横线、下划线以及数字。
+
+        :param topic: The topic of this ShowInstanceMessagesRequest.
+        :type topic: str
+        """
+        self._topic = topic
+
+    @property
+    def asc(self):
+        r"""Gets the asc of this ShowInstanceMessagesRequest.
+
+        是否按照时间排序。
+
+        :return: The asc of this ShowInstanceMessagesRequest.
+        :rtype: bool
+        """
+        return self._asc
+
+    @asc.setter
+    def asc(self, asc):
+        r"""Sets the asc of this ShowInstanceMessagesRequest.
+
+        是否按照时间排序。
+
+        :param asc: The asc of this ShowInstanceMessagesRequest.
+        :type asc: bool
+        """
+        self._asc = asc
+
+    @property
+    def start_time(self):
+        r"""Gets the start_time of this ShowInstanceMessagesRequest.
+
+        开始时间。  Unix毫秒时间戳。  查询消息偏移量时，为必选参数。
+
+        :return: The start_time of this ShowInstanceMessagesRequest.
+        :rtype: str
+        """
+        return self._start_time
+
+    @start_time.setter
+    def start_time(self, start_time):
+        r"""Sets the start_time of this ShowInstanceMessagesRequest.
+
+        开始时间。  Unix毫秒时间戳。  查询消息偏移量时，为必选参数。
+
+        :param start_time: The start_time of this ShowInstanceMessagesRequest.
+        :type start_time: str
+        """
+        self._start_time = start_time
+
+    @property
+    def end_time(self):
+        r"""Gets the end_time of this ShowInstanceMessagesRequest.
+
+        结束时间。  Unix毫秒时间戳。  查询消息偏移量时，为必选参数。
+
+        :return: The end_time of this ShowInstanceMessagesRequest.
+        :rtype: str
+        """
+        return self._end_time
+
+    @end_time.setter
+    def end_time(self, end_time):
+        r"""Sets the end_time of this ShowInstanceMessagesRequest.
+
+        结束时间。  Unix毫秒时间戳。  查询消息偏移量时，为必选参数。
+
+        :param end_time: The end_time of this ShowInstanceMessagesRequest.
+        :type end_time: str
+        """
+        self._end_time = end_time
+
+    @property
+    def limit(self):
+        r"""Gets the limit of this ShowInstanceMessagesRequest.
+
+        每一页显示的message数量。
+
+        :return: The limit of this ShowInstanceMessagesRequest.
+        :rtype: str
+        """
+        return self._limit
+
+    @limit.setter
+    def limit(self, limit):
+        r"""Sets the limit of this ShowInstanceMessagesRequest.
+
+        每一页显示的message数量。
+
+        :param limit: The limit of this ShowInstanceMessagesRequest.
+        :type limit: str
+        """
+        self._limit = limit
+
+    @property
+    def offset(self):
+        r"""Gets the offset of this ShowInstanceMessagesRequest.
+
+        页数。
+
+        :return: The offset of this ShowInstanceMessagesRequest.
+        :rtype: str
+        """
+        return self._offset
+
+    @offset.setter
+    def offset(self, offset):
+        r"""Sets the offset of this ShowInstanceMessagesRequest.
+
+        页数。
+
+        :param offset: The offset of this ShowInstanceMessagesRequest.
+        :type offset: str
+        """
+        self._offset = offset
+
+    @property
+    def download(self):
+        r"""Gets the download of this ShowInstanceMessagesRequest.
+
+        是否下载。
+
+        :return: The download of this ShowInstanceMessagesRequest.
+        :rtype: bool
+        """
+        return self._download
+
+    @download.setter
+    def download(self, download):
+        r"""Sets the download of this ShowInstanceMessagesRequest.
+
+        是否下载。
+
+        :param download: The download of this ShowInstanceMessagesRequest.
+        :type download: bool
+        """
+        self._download = download
+
+    @property
+    def message_offset(self):
+        r"""Gets the message_offset of this ShowInstanceMessagesRequest.
+
+        消息偏移量。  **查询消息内容时，为必选参数。**  若start_time、end_time参数不为空，该参数无效。
+
+        :return: The message_offset of this ShowInstanceMessagesRequest.
+        :rtype: str
+        """
+        return self._message_offset
+
+    @message_offset.setter
+    def message_offset(self, message_offset):
+        r"""Sets the message_offset of this ShowInstanceMessagesRequest.
+
+        消息偏移量。  **查询消息内容时，为必选参数。**  若start_time、end_time参数不为空，该参数无效。
+
+        :param message_offset: The message_offset of this ShowInstanceMessagesRequest.
+        :type message_offset: str
+        """
+        self._message_offset = message_offset
+
+    @property
+    def partition(self):
+        r"""Gets the partition of this ShowInstanceMessagesRequest.
+
+        分区。  **查询消息内容时，为必选参数。**  若start_time、end_time参数不为空，该参数无效。
+
+        :return: The partition of this ShowInstanceMessagesRequest.
+        :rtype: str
+        """
+        return self._partition
+
+    @partition.setter
+    def partition(self, partition):
+        r"""Sets the partition of this ShowInstanceMessagesRequest.
+
+        分区。  **查询消息内容时，为必选参数。**  若start_time、end_time参数不为空，该参数无效。
+
+        :param partition: The partition of this ShowInstanceMessagesRequest.
+        :type partition: str
+        """
+        self._partition = partition
+
+    @property
+    def keyword(self):
+        r"""Gets the keyword of this ShowInstanceMessagesRequest.
+
+        关键词。 取值范围为0~50。
+
+        :return: The keyword of this ShowInstanceMessagesRequest.
+        :rtype: str
+        """
+        return self._keyword
+
+    @keyword.setter
+    def keyword(self, keyword):
+        r"""Sets the keyword of this ShowInstanceMessagesRequest.
+
+        关键词。 取值范围为0~50。
+
+        :param keyword: The keyword of this ShowInstanceMessagesRequest.
+        :type keyword: str
+        """
+        self._keyword = keyword
+
+    def to_dict(self):
+        result = {}
+
+        for attr, _ in self.openapi_types.items():
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                if attr in self.sensitive_list:
+                    result[attr] = "****"
+                else:
+                    result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        import simplejson as json
+        return json.dumps(sanitize_for_serialization(self), ensure_ascii=False)
+
+    def __repr__(self):
+        """For `print`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, ShowInstanceMessagesRequest):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
