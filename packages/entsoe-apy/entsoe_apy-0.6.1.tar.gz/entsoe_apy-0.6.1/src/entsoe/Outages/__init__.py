51b3e711df15ef2bb348a595dadb2d409332197c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 06:06:26 +0000

    Add package entsoe-apy/0.6.1/entsoe_apy-0.6.1.tar.gz

---
+++ packages/entsoe-apy/entsoe_apy-0.6.1.tar.gz/entsoe_apy-0.6.1/src/entsoe/Outages/__init__.py
@@ -0,0 +1,24 @@
+"""ENTSO-E Outages parameter classes.
+
+This module contains parameter classes for ENTSO-E Outages endpoints,
+providing easy-to-use interfaces for different outage data types based on
+the ENTSO-E Transparency Platform API specification.
+"""
+
+from .specific_params import (
+    AggregatedUnavailabilityOfConsumptionUnits,
+    Fallbacks,
+    UnavailabilityOfGenerationUnits,
+    UnavailabilityOfOffshoreGridInfrastructure,
+    UnavailabilityOfProductionUnits,
+    UnavailabilityOfTransmissionInfrastructure,
+)
+
+__all__ = [
+    "UnavailabilityOfProductionUnits",
+    "UnavailabilityOfGenerationUnits",
+    "AggregatedUnavailabilityOfConsumptionUnits",
+    "UnavailabilityOfTransmissionInfrastructure",
+    "UnavailabilityOfOffshoreGridInfrastructure",
+    "Fallbacks",
+]
