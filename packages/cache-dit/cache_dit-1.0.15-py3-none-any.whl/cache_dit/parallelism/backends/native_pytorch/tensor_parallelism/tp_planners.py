8223c14bbb89b4005eb808aa3cb1d6db881bf061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 15:06:29 +0000

    Add package cache-dit/1.0.15/cache_dit-1.0.15-py3-none-any.whl

---
+++ packages/cache-dit/cache_dit-1.0.15-py3-none-any.whl/cache_dit/parallelism/backends/native_pytorch/tensor_parallelism/tp_planners.py
@@ -0,0 +1,18 @@
+# NOTE: must import all planner classes to register them
+from .tp_plan_flux import FluxTensorParallelismPlanner
+from .tp_plan_hunyuan_dit import HunyuanDiTTensorParallelismPlanner
+from .tp_plan_kandinsky5 import Kandinsky5TensorParallelismPlanner
+from .tp_plan_mochi import MochiTensorParallelismPlanner
+from .tp_plan_qwen_image import QwenImageTensorParallelismPlanner
+from .tp_plan_registers import TensorParallelismPlannerRegister
+from .tp_plan_wan import WanTensorParallelismPlanner
+
+__all__ = [
+    "FluxTensorParallelismPlanner",
+    "HunyuanDiTTensorParallelismPlanner",
+    "Kandinsky5TensorParallelismPlanner",
+    "MochiTensorParallelismPlanner",
+    "QwenImageTensorParallelismPlanner",
+    "TensorParallelismPlannerRegister",
+    "WanTensorParallelismPlanner",
+]
