e455e4982be3a16ae55d4ae3ab75ef79ed09c94a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 03:27:31 +0000

    Add package mzy-py/0.1.3/mzy_py-0.1.3.tar.gz

---
+++ packages/mzy-py/mzy_py-0.1.3.tar.gz/mzy_py-0.1.3/README.md
@@ -0,0 +1,6 @@
+# My Open Source Package
+一个简单的开源Python工具包。
+
+## 安装
+```bash
+pip install mzy-py
---
+++ packages/mzy-py/mzy_py-0.1.3.tar.gz/mzy_py-0.1.3/mzy_py/core.py
@@ -0,0 +1,10 @@
+from typing import List
+
+def mzy() -> str:
+    """Return a confession string."""
+    return "I LOVE WeiJinHe"
+
+
+def print_my_name() -> str:
+    """Return personal intro."""
+    return "My name is Mzy, I am a student in BPU."
