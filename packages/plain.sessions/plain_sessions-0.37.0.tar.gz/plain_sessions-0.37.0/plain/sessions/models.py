bf832fec82ec83b35795dfa5164aba1e7b9344f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 15:06:36 +0000

    Add package plain.sessions/0.37.0/plain_sessions-0.37.0.tar.gz

---
+++ packages/plain.sessions/plain_sessions-0.37.0.tar.gz/plain_sessions-0.37.0/plain/sessions/models.py
@@ -0,0 +1,21 @@
+from plain import models
+
+
+@models.register_model
+class Session(models.Model):
+    session_key = models.CharField(max_length=40)
+    session_data = models.JSONField(default=dict, required=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField(allow_null=True)
+
+    model_options = models.Options(
+        indexes=[
+            models.Index(fields=["expires_at"]),
+        ],
+        constraints=[
+            models.UniqueConstraint(fields=["session_key"], name="unique_session_key")
+        ],
+    )
+
+    def __str__(self) -> str:
+        return self.session_key
