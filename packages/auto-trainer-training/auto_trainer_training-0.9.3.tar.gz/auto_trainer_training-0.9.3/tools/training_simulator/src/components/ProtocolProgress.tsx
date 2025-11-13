59438440260485a0537b5d539ee59709033b487a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 08:06:32 +0000

    Add package auto-trainer-training/0.9.3/auto_trainer_training-0.9.3.tar.gz

---
+++ packages/auto-trainer-training/auto_trainer_training-0.9.3.tar.gz/auto_trainer_training-0.9.3/tools/training_simulator/src/components/ProtocolProgress.tsx
@@ -0,0 +1,23 @@
+import {Card, Group, Stack, Text} from "@mantine/core";
+
+import type {TrainingProtocol} from "../models/trainingProtocol.ts";
+
+export const ProtocolProgress = ({protocol}: {protocol: TrainingProtocol}) => {
+    return (
+        <Card miw={600} mih={240} withBorder>
+            <Card.Section bg="blue.2">
+                <Group p={4} justify="space-between">
+                    <Text fw={500}>Protocol Progress</Text>
+                </Group>
+            </Card.Section>
+            {protocol ? <ProtocolProgressContent protocol={protocol}/> : null}
+        </Card>
+    );
+}
+
+const ProtocolProgressContent = ({protocol}: {protocol: TrainingProtocol}) => {
+    return (
+        <Stack>
+        </Stack>
+    )
+}
