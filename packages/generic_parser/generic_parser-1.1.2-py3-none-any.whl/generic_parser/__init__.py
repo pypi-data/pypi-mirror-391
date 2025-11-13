09afe02ae339325661d4ec12158073dd4bebd7fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 17:07:29 +0000

    Add package generic_parser/1.1.2/generic_parser-1.1.2-py3-none-any.whl

---
+++ packages/generic_parser/generic_parser-1.1.2-py3-none-any.whl/generic_parser/__init__.py
@@ -0,0 +1,13 @@
+# Expose the main functionalities
+from generic_parser.entrypoint_parser import EntryPoint, EntryPointParameters, entrypoint
+from generic_parser.tools import DotDict
+
+__title__ = "generic_parser"
+__description__ = "A parser for arguments and config-files that also allows direct python input."
+__url__ = "https://github.com/pylhc/generic_parser"
+__version__ = "1.1.2"
+__author__ = "pylhc"
+__author_email__ = "pylhc@github.com"
+__license__ = "MIT"
+
+__all__ = ["EntryPoint", "EntryPointParameters", "entrypoint", "DotDict"]
