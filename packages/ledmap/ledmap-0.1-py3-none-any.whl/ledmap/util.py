b6350da8eeeba71ed17fa5ed8d6504a1202dff30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 08:05:28 +0000

    Add package ledmap/0.1/ledmap-0.1-py3-none-any.whl

---
+++ packages/ledmap/ledmap-0.1-py3-none-any.whl/ledmap/util.py
@@ -0,0 +1,9 @@
+import io
+from collections.abc import Callable
+
+
+def get_string(func: Callable[[io.TextIOBase], None]) -> str:
+    """Capture output stream as a string."""
+    with io.StringIO() as f:
+        func(f)
+        return f.getvalue()
