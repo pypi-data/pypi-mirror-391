51ea2eac0cef8d398819e7881c333741bb5acdc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 04:08:49 +0000

    Add package BibliotecaNumericaG2/0.0.6/bibliotecanumericag2-0.0.6-py3-none-any.whl

---
+++ packages/BibliotecaNumericaG2/bibliotecanumericag2-0.0.6-py3-none-any.whl/BibliotecaNumericaG2/raizes/__init__.py
@@ -0,0 +1,5 @@
+from .bissecao import bissecao as metodo_bisseccao
+from .newton_raphson import newton_raphson as metodo_newton_raphson
+from .secante import secante as metodo_secante
+
+__all__ = ["metodo_bisseccao", "metodo_newton_raphson", "metodo_secante"]
