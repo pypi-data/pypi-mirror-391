812b65df0e540b1e0c2c7622c4410cc596d81439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 05:09:48 +0000

    Add package dagster-hex/0.1.9/dagster_hex-0.1.9.tar.gz

---
+++ packages/dagster-hex/dagster_hex-0.1.9.tar.gz/dagster_hex-0.1.9/README.md
@@ -0,0 +1,91 @@
+# Dagster Hex Integration
+
+### Installation
+
+To install the library, use pip alongside your existing Dagster environment.
+
+```bash
+pip install dagster-hex
+```
+
+### Usage
+
+Create a `HexResource` specifying the `api_key`, then, use that resource in our assets.
+
+```python
+import os
+import dagster as dg
+
+from dagster_hex.resources import HexResource
+
+
+@dg.asset
+def custom_hex_asset(
+    context: dg.AssetExecutionContext,
+    hex: HexResource
+) -> dg.MaterializeResult:
+    project_id = "abcdefgh-1234-abcd-1234-abcdefghijkl"
+    context.log.info(f"Running Hex project {project_id}")
+    response = hex.run_project(
+        project_id,
+        inputs={
+            "message": "Hello, World!"
+        }
+    )
+    return dg.MaterializeResult(
+        metadata={
+            "data": response.get("data")
+        }
+    )
+
+
+defs = Definitions(
+    assets=[custom_hex_asset],
+    resources={
+        "hex": HexResource(
+            api_key=dg.EnvVar("HEX_API_KEY")
+        )
+    }
+)
+```
+
+#### Legacy
+
+##### Ops
+
+The `hex_project_op` will call the Hex API to run a project until it completes.
+
+```python
+import os
+import dagster as dg
+
+from dagster_hex.ops import hex_project_op
+from dagster_hex.resources import hex_resource
+
+
+hex_resource = HexResource(
+    api_key=dg.EnvVar("HEX_API_KEY")
+)
+
+run_hex_op = hex_project_op.configured({
+    "name": "run_hex_project_op",
+    "project_id": "abcdefgh-1234-abcd-1234-abcdefghijkl"
+})
+
+@job(resource_defs={"hex": my_resource})
+def hex_job():
+    run_hex_op()
+```
+
+##### Asset Materializations
+
+Ops will return an `AssetMaterialization`  with the following keys:
+
+```
+run_url
+run_status_url
+trace_id
+run_id
+elapsed_time
+notifications
+```
---
+++ packages/dagster-hex/dagster_hex-0.1.9.tar.gz/dagster_hex-0.1.9/dagster_hex/assets.py
@@ -0,0 +1,82 @@
+from packaging import version
+from dagster import (
+    __version__,
+    AssetsDefinition,
+    asset,
+    AssetExecutionContext,
+    AssetMaterialization,
+    MetadataValue,
+    Output,
+)
+from .resources import HexResource
+
+if version.parse(__version__) >= version.parse("1.10.0"):
+    from dagster._annotations import preview  # pyright: ignore[reportAttributeAccessIssue]
+else:
+    from dagster._annotations import experimental as preview  # pyright: ignore[reportAttributeAccessIssue]
+
+
+@preview
+def build_hex_asset(
+    project_id: str,
+    resource: HexResource,
+    inputs: dict | None = None,
+    update_cache: bool = False,
+    notifications: list | None = None,
+    deps: list | None = None,
+    tags: dict | None = None,
+) -> AssetsDefinition:
+    """
+    Returns the AssetDefinition for a Hex Project
+
+    Args:
+        project_id: The Hex project id
+        resource: The HexResource
+        inputs: The inputs for the asset
+        update_cache: Whether to update the cache
+        notifications: The notifications for the asset
+        deps: The dependencies for the asset
+        tags: The tags for the asset
+    Returns:
+        AssetsDefinition: The AssetDefinition for the Hex Project
+    """
+
+    project_name = resource.get_project(project_id)["title"]
+    metadata = resource.get_project_details(project_id)
+
+    @asset(
+        name=f"{project_name}__{project_id.replace('-', '_')}",
+        required_resource_keys={"hex"},
+        deps=deps,
+        metadata=metadata,
+        tags=tags,
+        kinds={"hex"},
+    )
+    def hex_asset(context: AssetExecutionContext):
+        output = context.resources.hex.run_and_poll(
+            project_id=project_id,
+            inputs=inputs,
+            update_cache=update_cache,
+            notifications=notifications,
+        )
+        asset_key = ["hex", project_name, project_id]
+        context.log_event(
+            AssetMaterialization(
+                asset_key,
+                description="Hex Project Details",
+                metadata={
+                    "run_url": MetadataValue.url(output.run_response["runUrl"]),
+                    "run_status_url": MetadataValue.url(
+                        output.run_response["runStatusUrl"]
+                    ),
+                    "trace_id": MetadataValue.text(output.run_response["traceId"]),
+                    "run_id": MetadataValue.text(output.run_response["runId"]),
+                    "elapsed_time": MetadataValue.int(
+                        output.status_response["elapsedTime"]
+                    ),
+                },
+            )
+        )
+        yield Output(output)
+
+    return hex_asset
