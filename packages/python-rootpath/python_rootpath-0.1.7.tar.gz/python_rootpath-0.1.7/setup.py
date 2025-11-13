472d22281b7032ca3490d1953330616ae80cfc5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 05:05:24 +0000

    Add package python-rootpath/0.1.7/python_rootpath-0.1.7.tar.gz

---
+++ packages/python-rootpath/python_rootpath-0.1.7.tar.gz/python_rootpath-0.1.7/setup.py
@@ -0,0 +1,96 @@
+
+# =========================================
+#       IMPORTS
+# --------------------------------------
+
+import os
+import setuptools
+import setupextras
+
+# DISABLED/BUG: this line fails when `pip install palmtree` but works `pip install .`
+# from palmtree import __version__
+
+# =========================================
+#       MAIN
+# --------------------------------------
+
+name = 'python-rootpath'
+version = '0.1.7'
+description = 'Python project/package root path detection.'
+keywords = [
+    'python',
+    'utlity',
+    'common',
+    'root',
+    'rootpath',
+    'root-path',
+    'detect',
+    'autodetect',
+    'auto-detect',
+    'project-root',
+    'project-root-path',
+    'package-root',
+    'package-root-path',
+]
+
+packages = setupextras.get_packages()
+data_files = setupextras.get_data_files(['*.*'], os.path.join('rootpath', 'tests', '__fixtures__'))
+requirements = setupextras.get_requirements()
+readme = setupextras.get_readme()
+
+config = {
+    'name': name,
+    'version': version,
+    'description': (description),
+    'keywords': keywords,
+    'author': 'Jonas Grimfelt',
+    'author_email': 'grimen@gmail.com',
+    'url': 'https://github.com/grimen/{name}'.format(name = name),
+    'download_url': 'https://github.com/grimen/{name}'.format(name = name),
+    'project_urls': {
+        'repository': 'https://github.com/grimen/{name}'.format(name = name),
+        'bugs': 'https://github.com/grimen/{name}/issues'.format(name = name),
+    },
+    'license': 'MIT',
+
+    'long_description': readme,
+    'long_description_content_type': 'text/markdown',
+
+    'classifiers': [
+        'Topic :: Software Development :: Libraries',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+
+    'packages': packages,
+    'package_dir': {
+        'rootpath': 'rootpath',
+    },
+    'package_data': {
+        '': [
+            'MIT-LICENSE',
+            'README.md',
+        ],
+        'rootpath': [
+            '*.*',
+        ],
+    },
+    'data_files': data_files,
+    'include_package_data': True,
+    'zip_safe': True,
+
+    'install_requires': requirements,
+    'setup_requires': [
+        'setuptools_git >= 1.2',
+    ],
+}
+
+setuptools.setup(**config)
+
