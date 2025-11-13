7de75966cb5cd11e1be7fd3344fe2c4c62372208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 05:05:56 +0000

    Add package pydomo/0.3.0.16/pydomo-0.3.0.16.tar.gz

---
+++ packages/pydomo/pydomo-0.3.0.16.tar.gz/pydomo-0.3.0.16/pydomo/Transport.py
@@ -0,0 +1,175 @@
+import requests
+import json
+import base64
+from collections import namedtuple
+from requests.auth import HTTPBasicAuth
+from requests_toolbelt.utils import dump
+from datetime import datetime, timezone
+
+
+class DomoAPITransport:
+    """Essentially a wrapper around the 'requests' library to make
+    it easier to interact with the Domo API.
+
+    OAuth2 authentication is handled automatically, as well as the
+    serialization and deserialization of objects.
+    """
+
+    def __init__(self, client_id, client_secret, api_host, use_https, logger, request_timeout, scope):
+        self.apiHost = self._build_apihost(api_host, use_https)
+        self.clientId = client_id
+        self.clientSecret = client_secret
+        self.logger = logger
+        self.request_timeout = request_timeout
+        self.scope = scope
+        self._renew_access_token()
+
+    @staticmethod
+    def _build_apihost(host, use_https):
+        if use_https:
+            host = 'https://' + host
+        else:
+            host = 'http://' + host
+        return host
+
+    def get(self, url, params):
+        headers = self._headers_default_receive_json()
+        return self.request(url, HTTPMethod.GET, headers, params)
+
+    def get_csv(self, url, params):
+        headers = self._headers_receive_csv()
+        return self.request(url, HTTPMethod.GET, headers, params)
+
+    def post(self, url, body, params):
+        headers = self._headers_send_json()
+        return self.request(url, HTTPMethod.POST, headers, params,
+                            self._obj_to_json(body))
+
+    def put(self, url, body):
+        headers = self._headers_send_json()
+        return self.request(url, HTTPMethod.PUT, headers, {},
+                            self._obj_to_json(body))
+
+    def put_csv(self, url, body):
+        headers = self._headers_send_csv()
+        return self.request(url, HTTPMethod.PUT, headers, {}, body)
+
+    def put_gzip(self, url, body):
+        headers = self._headers_send_gzip()
+        return self.request(url, HTTPMethod.PUT, headers, {}, body)
+
+    def patch(self, url, body):
+        headers = self._headers_send_json()
+        return self.request(url, HTTPMethod.PATCH, headers, {},
+                            self._obj_to_json(body))
+
+    def delete(self, url):
+        headers = self._headers_default_receive_json()
+        return self.request(url, HTTPMethod.DELETE, headers)
+
+    def request(self, url, method, headers, params=None, body=None):
+        url = self.apiHost + url
+        self.logger.debug('{} {} {}'.format(method, url, body))
+        request_args = {'method': method, 'url': url, 'headers': headers,
+                        'params': params, 'data': body, 'stream': True}
+        if self.request_timeout:
+            request_args['timeout'] = self.request_timeout
+
+        # Expiration date should be in UTC
+        if datetime.now(timezone.utc).timestamp() > self.token_expiration:
+            self.logger.debug("Access token is expired")
+            self._renew_access_token()
+            headers['Authorization'] = 'bearer ' + self.access_token
+
+        return requests.request(**request_args)
+
+    def _renew_access_token(self):
+        self.logger.debug("Renewing Access Token")
+        # scope == None means use all scopes from client
+        scope = ' '.join(self.scope) if self.scope else None
+
+        request_args = {
+            'method': HTTPMethod.POST,
+            'url': self.apiHost + '/oauth/token',
+            'data': {'grant_type': 'client_credentials', 'scope': scope},
+            'auth': HTTPBasicAuth(self.clientId, self.clientSecret)
+        }
+        if self.request_timeout:
+            request_args['timeout'] = self.request_timeout
+
+        response = requests.request(**request_args)
+        if response.status_code == requests.codes.OK:
+            self.access_token = response.json()['access_token']
+            self.token_expiration = self._extract_expiration(self.access_token)
+        else:
+            self.logger.debug('Error retrieving access token: ' + self.dump_response(response))
+            raise Exception("Error retrieving a Domo API Access Token: " + response.text)
+
+    def _extract_expiration(self, access_token):
+        expiration_date = 0
+        try:
+            decoded_payload_dict = self._decode_payload(access_token)
+
+            if 'exp' in decoded_payload_dict.keys():
+                expiration_date = decoded_payload_dict['exp']
+                self.logger.debug('Token expiration: {}'
+                                  .format(expiration_date))
+        except Exception as err:
+            # If an Exception is raised, log and continue. expiration_date will
+            # either be 0 or set to the value in the JWT.
+            self.logger.debug('Ran into error parsing token for expiration. '
+                              'Setting expiration date to 0. '
+                              '{}: {}'.format(type(err).__name__, err))
+        return expiration_date
+
+    def _decode_payload(self, access_token):
+        token_parts = access_token.split('.')
+
+        # Padding required for the base64 library
+        payload_bytes = bytes(token_parts[1], 'utf-8') + b'=='
+        decoded_payload_bytes = base64.urlsafe_b64decode(payload_bytes)
+        payload_string = decoded_payload_bytes.decode('utf-8')
+        return json.loads(payload_string)
+
+    def dump_response(self, response):
+        data = dump.dump_all(response)
+        return str(data.decode('utf-8'))
+
+    @staticmethod
+    def _obj_to_json(obj):
+        return json.dumps(obj, default=str)
+
+    def _headers_default_receive_json(self):
+        return {
+            'Authorization': 'bearer ' + self.access_token,
+            'Accept': 'application/json'
+        }
+
+    def _headers_send_json(self):
+        headers = self._headers_default_receive_json()
+        headers['Content-Type'] = 'application/json'
+        return headers
+
+    def _headers_send_csv(self):
+        headers = self._headers_default_receive_json()
+        headers['Content-Type'] = 'text/csv'
+        return headers
+
+    def _headers_send_gzip(self):
+        headers = self._headers_default_receive_json()
+        headers['Content-Type'] = 'text/csv'
+        headers['Content-Encoding'] = 'gzip'
+        return headers
+
+    def _headers_receive_csv(self):
+        headers = self._headers_default_receive_json()
+        headers['Accept'] = 'text/csv'
+        return headers
+
+
+class HTTPMethod:
+    GET = 'GET'
+    POST = 'POST'
+    PUT = 'PUT'
+    PATCH = 'PATCH'
+    DELETE = 'DELETE'
---
+++ packages/pydomo/pydomo-0.3.0.16.tar.gz/pydomo-0.3.0.16/pydomo/utilities/UtilitiesClient.py
@@ -0,0 +1,143 @@
+
+import json
+import math
+import sys
+import json
+from pandas import read_csv
+from pandas import to_datetime
+
+from pydomo.DomoAPIClient import DomoAPIClient
+from pydomo.datasets import DataSetClient
+from pydomo.streams import StreamClient
+
+class UtilitiesClient(DomoAPIClient):
+    def __init__(self, transport, logger):
+        super(UtilitiesClient, self).__init__(transport, logger)
+        self.ds = DataSetClient(self.transport, self.logger)
+        self.stream = StreamClient(self.transport, self.logger)
+        self.transport = transport
+
+    def domo_schema(self, ds_id):
+        this_get = self.ds.get(ds_id)
+        return this_get['schema']['columns']
+
+    def data_schema(self, df):
+        col_types = dict(df.dtypes)
+        output_list = []
+        for key, value in col_types.items():
+            output_list.append({'type': self.type_conversion_text(value), 'name': key})
+        return output_list
+
+    def type_conversion_text(self, dt):
+        """Convert data type strings to standardized type names."""
+
+        dt_str = str(dt)
+
+        if dt_str in {'<M8[ns]', 'datetime64', 'datetime64[ns]', 'datetime64[us]', 'datetime64[ms]', 'datetime64[s]'}:
+            return 'DATETIME'
+        elif dt_str in {'uint8', 'uint16', 'uint32', 'uint64', 'int8', 'int16', 'int32', 'int64', 'Int8', 'Int16', 'Int32', 'Int64'}:
+            return 'LONG'
+        elif dt_str in {'float16', 'float32', 'float64', 'Float32', 'Float64'}:
+            return 'DOUBLE'
+        elif dt_str in {'date', 'datetime.date'}:
+            return 'DATE'
+        elif dt_str in {'decimal', 'decimal.Decimal'}:
+            return 'DECIMAL'
+        else:
+            return 'STRING'
+
+    def convert_domo_type_to_pandas_type(self, domo_type):
+        # type mapping can be adjusted accordingly
+        # there is no Date type in Pandas and Timestamps are not a type in Domo
+        type_mapping = {
+            "LONG": "Int64",
+            "STRING": "string",
+            "DECIMAL": "Float64",
+            "DOUBLE": "Float64",
+            "DATETIME": "datetime64[ns]",
+            "DATE": "datetime64[ns]",
+        }
+
+        return type_mapping.get(domo_type, "object")
+
+    def is_date_type(self, column_type):
+        return column_type in ("DATE", "DATETIME")
+
+    def read_content_to_dataframe(self, content):
+        df = read_csv(content)
+
+        # Convert to dates or datetimes if possible
+        for col in df.columns:
+            if df[col].dtype == "object":
+                try:
+                    df[col] = to_datetime(df[col])
+                except ValueError:
+                    pass
+                except TypeError:
+                    pass
+
+        return df
+
+    def identical(self, c1, c2):
+        cc1 = json.dumps(c1)
+        cc2 = json.dumps(c2)
+        return cc1 == cc2
+
+    def get_stream_id(self, ds_id):
+        url = '/v1/streams/search?q=dataSource.id:{ds_id}'.format(ds_id=ds_id)
+        all_info = self._get(url,'Search to retrieve stream id')
+        return all_info[0]['id']
+
+    def estimate_chunk_rows(self, data, kbytes=10000):
+        sz = sys.getsizeof(data)
+        targetSize = kbytes * 3 # compression factor
+        data_rows = len(data.index)
+        ch_size = data_rows
+        if sz / 1000 > targetSize:
+            ch_size = math.floor(data_rows*(targetSize) / (sz/1000))
+        return(ch_size)
+
+    def stream_upload(self, ds_id, df_up, warn_schema_change=True):
+        domoSchema = self.domo_schema(ds_id)
+        dataSchema = self.data_schema(df_up)
+
+        stream_id = self.get_stream_id(ds_id)
+
+        if self.identical(domoSchema,dataSchema) == False:
+            new_schema = {'schema': {'columns': dataSchema}}
+            url = '/v1/datasets/{ds}'.format(ds=ds_id)
+            change_result = self.transport.put(url,new_schema)
+            if warn_schema_change:
+                print('Schema Updated')
+
+        exec_info = self.stream.create_execution(stream_id)
+        exec_id = exec_info['id']
+
+        chunksz = self.estimate_chunk_rows(df_up)
+        start = 0
+        df_rows = len(df_up.index)
+        end = df_rows
+        if df_rows > chunksz:
+            end = chunksz
+
+        for i in range(math.ceil(df_rows/chunksz)):
+            df_sub = df_up.iloc[start:end]
+            csv = df_sub.to_csv(header=False,index=False)
+            self.stream.upload_part(stream_id, exec_id, i, csv)
+            start = end
+            end = end + chunksz
+            if end > df_rows:
+                end = df_rows
+
+        result = self.stream.commit_execution(stream_id, exec_id)
+
+        return result
+
+    def stream_create(self, up_ds, name, description, updateMethod='REPLACE', keyColumnNames=[]):
+        df_schema = self.data_schema(up_ds)
+        req_body = {'dataSet': {'name': name, 'description': description, 'schema': {'columns': df_schema}}, 'updateMethod': updateMethod}
+        if( updateMethod == 'UPSERT' ):
+            req_body['keyColumnNames'] = keyColumnNames
+        # return req_body
+        st_created = self.transport.post('/v1/streams/', req_body, {})
+        return json.loads(st_created.content.decode('utf-8'))
