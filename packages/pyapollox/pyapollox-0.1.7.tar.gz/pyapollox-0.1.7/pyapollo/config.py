58277b754a0bc85f4991d32d888822182e32b74a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 15:06:28 +0000

    Add package pyapollox/0.1.7/pyapollox-0.1.7.tar.gz

---
+++ packages/pyapollox/pyapollox-0.1.7.tar.gz/pyapollox-0.1.7/pyapollo/config.py
@@ -0,0 +1,68 @@
+import json
+import os
+
+from .client import ApolloClient
+from .exception import InvalidFormatException, ConfigException
+
+
+class ConfigManager(object):
+    
+    def __init__(self, apollo_host: str, app_id: str, namespace, cluster: str = 'default',
+                 secret: str = '', file_cache_dir='/tmp', data_format='properties', timeout=1, keepalive_expiry: float = 60.0):
+        self.cluster_name = cluster
+        self.app_id = app_id
+        self.file_cache_dir = file_cache_dir
+        self.namespace = namespace
+        self.config = {}
+        self.is_hot_reload = False
+        self.data_format = data_format
+        self.client = ApolloClient(apollo_host=apollo_host,
+                                   app_id=app_id,
+                                   namespace=namespace,
+                                   cluster=cluster,
+                                   secret=secret,
+                                   callback=self.receive_notification,
+                                   timeout=timeout,
+                                   keepalive_expiry=keepalive_expiry,
+                                   data_format=data_format)
+    
+    def restore_from_file(self):
+        path = f'{self.file_cache_dir}/{self.app_id}-{self.namespace}.json'
+        if os.path.exists(path):
+            with open(path) as f:
+                self.config = json.loads(f.read())
+    
+    def sync_to_file(self):
+        path = f'{self.file_cache_dir}/{self.app_id}-{self.namespace}.json'
+        with open(path, 'w') as f:
+            f.write(json.dumps(self.config))
+    
+    def receive_notification(self, data):
+        self.config = data
+        self.sync_to_file()
+    
+    def enable_hot_reload(self):
+        if not self.is_hot_reload:
+            self.client.start_long_polling()
+            self.is_hot_reload = True
+    
+    def get_from_json(self, key, default=None):
+        if self.data_format in ('properties', 'json'):
+            if not self.config:
+                self.restore_from_file()
+                if not self.config:
+                    data = self.client.get_config()
+                    if data:
+                        self.config = data
+                        self.sync_to_file()
+                    else:
+                        raise ConfigException('No config found for timeout')
+            return self.config.get(key, default)
+        else:
+            raise InvalidFormatException("only support properties / json")
+    
+    def get_from_yaml(self, key):
+        raise NotImplementedError
+    
+    def get_from_xml(self, key):
+        raise NotImplementedError
