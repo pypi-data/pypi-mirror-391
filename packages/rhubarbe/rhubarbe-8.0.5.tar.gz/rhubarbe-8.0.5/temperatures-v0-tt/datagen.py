8820c616058bf1f9cc2840d5c0bb99229bce799b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 16:06:29 +0000

    Add package rhubarbe/8.0.5/rhubarbe-8.0.5.tar.gz

---
+++ packages/rhubarbe/rhubarbe-8.0.5.tar.gz/rhubarbe-8.0.5/temperatures-v0-tt/datagen.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys, os, logging, urllib, datetime
+
+def fetchtemp():
+	cmd = '/usr/bin/vcgencmd measure_temp'
+	line = os.popen(cmd).readline().strip()
+	output = line.split('=')[1].split("'")[0]#+' C'
+	return output 
+
+format = "%Y-%m-%d,%H:%M:%S" 
+today = datetime.datetime.today()
+s = today.strftime(format)
+output = s+' '+fetchtemp()+'\n'
+
+print(output)
+
+with open('/home/tester/plottemp/tempdata.dat', 'a') as f:
+	f.write(output)
