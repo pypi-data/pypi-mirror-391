1ed31e01b43eff1c5035a1e23f21dfed60160f9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 03:27:31 +0000

    Add package mzy-py/0.1.3/mzy_py-0.1.3-py3-none-any.whl

---
+++ packages/mzy-py/mzy_py-0.1.3-py3-none-any.whl/mzy_py/__init__.py
@@ -0,0 +1,6 @@
+from .core import mzy, print_my_name  # 从core.py导出函数
+from .utils import print_info
+
+__version__ = "0.1.3"  # 版本号（重要，上传时不能重复）
+__author__ = "马子延"
+__all__ = ["mzy", "print_my_name", "print_info"]
