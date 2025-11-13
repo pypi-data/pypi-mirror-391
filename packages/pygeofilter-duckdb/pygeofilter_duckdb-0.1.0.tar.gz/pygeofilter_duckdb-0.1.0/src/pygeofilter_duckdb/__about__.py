8a92535d3eb166eacea37bd616c99300f95d241b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 04:09:06 +0000

    Add package pygeofilter-duckdb/0.1.0/pygeofilter_duckdb-0.1.0.tar.gz

---
+++ packages/pygeofilter-duckdb/pygeofilter_duckdb-0.1.0.tar.gz/pygeofilter_duckdb-0.1.0/src/pygeofilter_duckdb/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2025-present Fabrice Brito <fabrice.brito@terradue.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.1.0"
---
+++ packages/pygeofilter-duckdb/pygeofilter_duckdb-0.1.0.tar.gz/pygeofilter_duckdb-0.1.0/tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2025-present Fabrice Brito <fabrice.brito@terradue.com>
+#
+# SPDX-License-Identifier: MIT
