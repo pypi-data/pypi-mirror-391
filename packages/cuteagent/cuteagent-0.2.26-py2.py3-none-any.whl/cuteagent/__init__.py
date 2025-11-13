09cbb000607d090c6bc9bbf675234dab42596721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 06:05:50 +0000

    Add package cuteagent/0.2.26/cuteagent-0.2.26-py2.py3-none-any.whl

---
+++ packages/cuteagent/cuteagent-0.2.26-py2.py3-none-any.whl/cuteagent/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for CuteAgent."""
+
+__author__ = """Masoud Jb"""
+__email__ = "eng@fintor.com"
+__version__ = "0.2.26"
+
+from .cuteagent import WindowsAgent, VisionAgent, HumanAgent, StationAgent, DocumentAgent
+
+__all__ = ["WindowsAgent", "VisionAgent", "HumanAgent", "StationAgent", "DocumentAgent"] # Optional: to define the public API
---
+++ packages/cuteagent/cuteagent-0.2.26-py2.py3-none-any.whl/cuteagent/common.py
@@ -0,0 +1,13 @@
+"""The common module contains common functions and classes used by the other modules.
+"""
+
+def hello_world():
+    """Prints "Hello World!" to the console.
+    """
+    print("Hello World!")
+
+
+def hello(name: str):
+    """Prints "Hello <name>!" to the console.
+    """
+    print(f"Hello {name}!")
