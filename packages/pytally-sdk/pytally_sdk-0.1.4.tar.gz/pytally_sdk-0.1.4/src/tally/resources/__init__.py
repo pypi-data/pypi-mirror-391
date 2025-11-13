814049fc807d02bfca1562125b6d70da2dd60c51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 14:06:20 +0000

    Add package pytally-sdk/0.1.4/pytally_sdk-0.1.4.tar.gz

---
+++ packages/pytally-sdk/pytally_sdk-0.1.4.tar.gz/pytally_sdk-0.1.4/src/tally/resources/__init__.py
@@ -0,0 +1,11 @@
+"""Resources for the Tally API."""
+
+from tally.resources.organizations import OrganizationsResource
+from tally.resources.users import UsersResource
+from tally.resources.workspaces import WorkspacesResource
+
+__all__ = [
+    "OrganizationsResource",
+    "UsersResource",
+    "WorkspacesResource",
+]
