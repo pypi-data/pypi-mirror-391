fb72614fa8965d919abce7e547e3a52a80f44a3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 07:06:17 +0000

    Add package todoist-digest/0.11.0/todoist_digest-0.11.0.tar.gz

---
+++ packages/todoist-digest/todoist_digest-0.11.0.tar.gz/todoist_digest-0.11.0/todoist_digest/todoist.py
@@ -0,0 +1,53 @@
+"""
+Todoist API extensions, mostly around the sync API, that won't be added to the core API (I tried)
+"""
+
+from functools import lru_cache
+
+
+# https://github.com/iloveitaly/todoist-api-python/commit/ec83531fae94a2ccd0a4bd6b2d1db95d86b129b6
+def todoist_get_sync_resource(api, resource_type):
+    """
+    >>> todoist_get_sync_resource(api, "collaborators")
+    """
+
+    from todoist_api_python._core.endpoints import get_api_url
+    from todoist_api_python._core.http_requests import post
+
+    endpoint = get_api_url("sync")
+    data = {
+        "resource_types": [resource_type],
+        "sync_token": "*",
+    }
+    resource_data = post(api._session, endpoint, api._token, data=data)
+    return resource_data
+
+
+# https://developer.todoist.com/sync/v9#get-item-info
+# https://github.com/iloveitaly/todoist-api-python/commit/ec83531fae94a2ccd0a4bd6b2d1db95d86b129b6
+@lru_cache(maxsize=None)
+def todoist_get_item_info(api, item_id):
+    from todoist_api_python._core.endpoints import get_api_url
+    from todoist_api_python._core.http_requests import get
+
+    endpoint = get_api_url("items/get")
+    data = {
+        "item_id": item_id,
+    }
+    resource_data = get(api._session, endpoint, api._token, params=data)
+    return resource_data
+
+
+def todoist_get_completed_activity(api, task_id):
+    from todoist_api_python._core.endpoints import get_api_url
+    from todoist_api_python._core.http_requests import get
+
+    endpoint = get_api_url("activities")
+    data = {
+        "limit": 100,
+        "event_type": "completed",
+        "object_id": task_id,
+        "object_type": "item",
+    }
+    resource_data = get(api._session, endpoint, api._token, params=data)
+    return resource_data
