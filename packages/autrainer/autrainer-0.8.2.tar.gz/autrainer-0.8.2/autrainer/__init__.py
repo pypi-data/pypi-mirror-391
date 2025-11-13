f0827715802899b37a656e11e7b53af8346e7aa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 06:06:24 +0000

    Add package autrainer/0.8.2/autrainer-0.8.2.tar.gz

---
+++ packages/autrainer/autrainer-0.8.2.tar.gz/autrainer-0.8.2/autrainer/__init__.py
@@ -0,0 +1,11 @@
+from autrainer.core.wrappers import (
+    HydraConvertEnum,
+    instantiate,
+    instantiate_shorthand,
+    main,
+)
+
+
+__version__ = "0.8.2"
+
+__all__ = ["HydraConvertEnum", "instantiate", "instantiate_shorthand", "main"]
