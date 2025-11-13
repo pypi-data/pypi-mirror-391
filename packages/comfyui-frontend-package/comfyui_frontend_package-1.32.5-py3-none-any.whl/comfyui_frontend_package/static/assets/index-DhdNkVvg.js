6a2609318d60dc5ea08e088d2ac973b13ac0c2d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 15:06:26 +0000

    Add package comfyui-frontend-package/1.32.5/comfyui_frontend_package-1.32.5-py3-none-any.whl

---
+++ packages/comfyui-frontend-package/comfyui_frontend_package-1.32.5-py3-none-any.whl/comfyui_frontend_package/static/assets/index-DhdNkVvg.js
@@ -0,0 +1,2 @@
+import{c as o}from"./index-BYfAAqfw.js";const n=o(["not-disabled:bg-component-node-widget-background","not-disabled:text-component-node-foreground","border-none","outline outline-offset-[-1px] outline-component-node-border","rounded-lg","hover:bg-component-node-widget-background-hovered"]);export{n as W};
+//# sourceMappingURL=index-DhdNkVvg.js.map
