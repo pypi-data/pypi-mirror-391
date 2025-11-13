2e214ee8bdea396563805285b3506b77e7a7cb2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 07:06:12 +0000

    Add package fake-useragxxx/0.0.2/fake_useragxxx-0.0.2.tar.gz

---
+++ packages/fake-useragxxx/fake_useragxxx-0.0.2.tar.gz/fake_useragxxx-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, Extension, find_packages
+from Cython.Build import cythonize
+
+# Компилируем основной модуль
+extensions = [
+    Extension(
+        "userFake", 
+        ["userFake.py"],
+    ),
+]
+
+setup(
+    name="fake-useragxxx",
+    version="0.0.2",
+    description="Fake User-Agent Generator with Gray Randomization",
+    author="Anonymous",
+    author_email="m00263277@gmail.com", 
+    license="Apache-2.0",
+    packages=find_packages(include=["fake_useragxxx*"]),
+    ext_modules=cythonize(
+        extensions,
+        compiler_directives={'language_level': 3}
+    ),
+    python_requires=">=3.7",
+    zip_safe=False,
+)
