a2833d2f444a908164485011e9bb3a8716e452a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 07:06:04 +0000

    Add package dandy/1.3.3/dandy-1.3.3-py3-none-any.whl

---
+++ packages/dandy/dandy-1.3.3-py3-none-any.whl/dandy/llm/intelligence/prompts.py
@@ -0,0 +1,66 @@
+from pydantic import ValidationError
+
+from dandy.core.utils import pydantic_validation_error_to_str
+from dandy.llm.prompt.prompt import Prompt
+from dandy.llm.prompt.typing import PromptOrStrOrNone, PromptOrStr
+
+
+def service_system_prompt(
+    role: PromptOrStr,
+    task: PromptOrStrOrNone = None,
+    guidelines: PromptOrStrOrNone = None,
+    system_override_prompt: PromptOrStrOrNone = None,
+) -> Prompt:
+    prompt = Prompt()
+
+    if system_override_prompt:
+        prompt.prompt(system_override_prompt)
+
+        return prompt
+
+    prompt.heading('Role')
+    prompt.line_break()
+    prompt.prompt(role)
+
+    if task:
+        prompt.line_break()
+        prompt.heading('Task')
+        prompt.line_break()
+        prompt.prompt(task)
+
+    if guidelines:
+        prompt.line_break()
+        prompt.heading('Guidelines')
+        prompt.line_break()
+        prompt.prompt(guidelines)
+
+    prompt.line_break()
+    prompt.heading('Constraints')
+    prompt.list(
+        [
+            'Make sure your response is valid JSON reflecting the provided JSON schema.',
+            'Do not use any markdown styling in your response.',
+        ]
+    )
+
+    return prompt
+
+
+def service_system_validation_error_prompt(error: ValidationError) -> Prompt:
+    return (
+        Prompt()
+        .text(
+            'The JSON in the response you provided was not valid based on the JSON schema that was provided.'
+        )
+        .text(
+            'Here is the validation error provided by Pydantic when it tried to parse the JSON:'
+        )
+        .text(f'{pydantic_validation_error_to_str(error)}', triple_quote=True)
+        .text(
+            'Please review your response provide a valid JSON in your next response, based on the previous request.'
+        )
+    )
+
+
+def service_user_prompt(user_prompt: PromptOrStr) -> Prompt:
+    return Prompt().prompt(user_prompt)
---
+++ packages/dandy/dandy-1.3.3-py3-none-any.whl/dandy/llm/request/message.py
@@ -0,0 +1,29 @@
+from typing import List, Any, Literal
+
+from pydantic import BaseModel, Field
+
+RoleLiteralStr = Literal['user', 'assistant', 'system']
+
+
+class RequestMessage(BaseModel):
+    role: RoleLiteralStr
+    content: str | List[Any]
+    images: List[str] |None = None
+
+
+class MessageHistory(BaseModel):
+    messages: List[RequestMessage] = Field(default_factory=list)
+
+    def add_message(
+            self,
+            role: RoleLiteralStr,
+            content: str,
+            images: List[str] | None = None
+    ) -> None:
+        self.messages.append(
+            RequestMessage(
+                role=role,
+                content=content,
+                images=images
+            )
+        )
