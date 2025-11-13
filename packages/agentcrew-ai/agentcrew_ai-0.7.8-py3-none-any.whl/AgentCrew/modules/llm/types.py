7e7c08ff50901581e168389cfc89eb9958c3ca0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 15:06:33 +0000

    Add package agentcrew-ai/0.7.8/agentcrew_ai-0.7.8-py3-none-any.whl

---
+++ packages/agentcrew-ai/agentcrew_ai-0.7.8-py3-none-any.whl/AgentCrew/modules/llm/types.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from typing import List, Literal
+
+
+class Model(BaseModel):
+    """Model metadata class."""
+
+    id: str
+    provider: str
+    name: str
+    description: str
+    capabilities: List[str]
+    default: bool = False
+    max_context_token: int = 128_000
+    input_token_price_1m: float = 0.0
+    output_token_price_1m: float = 0.0
+    endpoint: Literal["completions", "response"] = "completions"
