081c67cfef1687c220b33725cce8f39783db74db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-13 17:07:42 +0000

    Add package huaweicloudsdkasm/3.1.175/huaweicloudsdkasm-3.1.175-py3-none-any.whl

---
+++ packages/huaweicloudsdkasm/huaweicloudsdkasm-3.1.175-py3-none-any.whl/huaweicloudsdkasm/v1/model/list_meshes_request.py
@@ -0,0 +1,136 @@
+# coding: utf-8
+
+from huaweicloudsdkcore.utils.http_utils import sanitize_for_serialization
+
+
+class ListMeshesRequest:
+
+    """
+    Attributes:
+      openapi_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    sensitive_list = []
+
+    openapi_types = {
+        'content_type': 'str',
+        'x_apply_project_id': 'str'
+    }
+
+    attribute_map = {
+        'content_type': 'Content-Type',
+        'x_apply_project_id': 'X-Apply-ProjectID'
+    }
+
+    def __init__(self, content_type=None, x_apply_project_id=None):
+        r"""ListMeshesRequest
+
+        The model defined in huaweicloud sdk
+
+        :param content_type: 消息体的类型（格式）
+        :type content_type: str
+        :param x_apply_project_id: 网格所属ProjectID
+        :type x_apply_project_id: str
+        """
+        
+        
+
+        self._content_type = None
+        self._x_apply_project_id = None
+        self.discriminator = None
+
+        self.content_type = content_type
+        if x_apply_project_id is not None:
+            self.x_apply_project_id = x_apply_project_id
+
+    @property
+    def content_type(self):
+        r"""Gets the content_type of this ListMeshesRequest.
+
+        消息体的类型（格式）
+
+        :return: The content_type of this ListMeshesRequest.
+        :rtype: str
+        """
+        return self._content_type
+
+    @content_type.setter
+    def content_type(self, content_type):
+        r"""Sets the content_type of this ListMeshesRequest.
+
+        消息体的类型（格式）
+
+        :param content_type: The content_type of this ListMeshesRequest.
+        :type content_type: str
+        """
+        self._content_type = content_type
+
+    @property
+    def x_apply_project_id(self):
+        r"""Gets the x_apply_project_id of this ListMeshesRequest.
+
+        网格所属ProjectID
+
+        :return: The x_apply_project_id of this ListMeshesRequest.
+        :rtype: str
+        """
+        return self._x_apply_project_id
+
+    @x_apply_project_id.setter
+    def x_apply_project_id(self, x_apply_project_id):
+        r"""Sets the x_apply_project_id of this ListMeshesRequest.
+
+        网格所属ProjectID
+
+        :param x_apply_project_id: The x_apply_project_id of this ListMeshesRequest.
+        :type x_apply_project_id: str
+        """
+        self._x_apply_project_id = x_apply_project_id
+
+    def to_dict(self):
+        result = {}
+
+        for attr, _ in self.openapi_types.items():
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                if attr in self.sensitive_list:
+                    result[attr] = "****"
+                else:
+                    result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        import simplejson as json
+        return json.dumps(sanitize_for_serialization(self), ensure_ascii=False)
+
+    def __repr__(self):
+        """For `print`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, ListMeshesRequest):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
---
+++ packages/huaweicloudsdkasm/huaweicloudsdkasm-3.1.175-py3-none-any.whl/huaweicloudsdkasm/v1/model/mesh_spec.py
@@ -0,0 +1,189 @@
+# coding: utf-8
+
+from huaweicloudsdkcore.utils.http_utils import sanitize_for_serialization
+
+
+class MeshSpec:
+
+    """
+    Attributes:
+      openapi_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    sensitive_list = []
+
+    openapi_types = {
+        'type': 'str',
+        'version': 'str',
+        'extend_params': 'MeshExtendParams',
+        'tags': 'list[MeshTags]'
+    }
+
+    attribute_map = {
+        'type': 'type',
+        'version': 'version',
+        'extend_params': 'extendParams',
+        'tags': 'tags'
+    }
+
+    def __init__(self, type=None, version=None, extend_params=None, tags=None):
+        r"""MeshSpec
+
+        The model defined in huaweicloud sdk
+
+        :param type: 网格类型： InCluster: 集群内控制平面形态，基础版网格取值为InCluster
+        :type type: str
+        :param version: 网格版本
+        :type version: str
+        :param extend_params: 
+        :type extend_params: :class:`huaweicloudsdkasm.v1.MeshExtendParams`
+        :param tags: 网格资源标签
+        :type tags: list[:class:`huaweicloudsdkasm.v1.MeshTags`]
+        """
+        
+        
+
+        self._type = None
+        self._version = None
+        self._extend_params = None
+        self._tags = None
+        self.discriminator = None
+
+        self.type = type
+        self.version = version
+        if extend_params is not None:
+            self.extend_params = extend_params
+        if tags is not None:
+            self.tags = tags
+
+    @property
+    def type(self):
+        r"""Gets the type of this MeshSpec.
+
+        网格类型： InCluster: 集群内控制平面形态，基础版网格取值为InCluster
+
+        :return: The type of this MeshSpec.
+        :rtype: str
+        """
+        return self._type
+
+    @type.setter
+    def type(self, type):
+        r"""Sets the type of this MeshSpec.
+
+        网格类型： InCluster: 集群内控制平面形态，基础版网格取值为InCluster
+
+        :param type: The type of this MeshSpec.
+        :type type: str
+        """
+        self._type = type
+
+    @property
+    def version(self):
+        r"""Gets the version of this MeshSpec.
+
+        网格版本
+
+        :return: The version of this MeshSpec.
+        :rtype: str
+        """
+        return self._version
+
+    @version.setter
+    def version(self, version):
+        r"""Sets the version of this MeshSpec.
+
+        网格版本
+
+        :param version: The version of this MeshSpec.
+        :type version: str
+        """
+        self._version = version
+
+    @property
+    def extend_params(self):
+        r"""Gets the extend_params of this MeshSpec.
+
+        :return: The extend_params of this MeshSpec.
+        :rtype: :class:`huaweicloudsdkasm.v1.MeshExtendParams`
+        """
+        return self._extend_params
+
+    @extend_params.setter
+    def extend_params(self, extend_params):
+        r"""Sets the extend_params of this MeshSpec.
+
+        :param extend_params: The extend_params of this MeshSpec.
+        :type extend_params: :class:`huaweicloudsdkasm.v1.MeshExtendParams`
+        """
+        self._extend_params = extend_params
+
+    @property
+    def tags(self):
+        r"""Gets the tags of this MeshSpec.
+
+        网格资源标签
+
+        :return: The tags of this MeshSpec.
+        :rtype: list[:class:`huaweicloudsdkasm.v1.MeshTags`]
+        """
+        return self._tags
+
+    @tags.setter
+    def tags(self, tags):
+        r"""Sets the tags of this MeshSpec.
+
+        网格资源标签
+
+        :param tags: The tags of this MeshSpec.
+        :type tags: list[:class:`huaweicloudsdkasm.v1.MeshTags`]
+        """
+        self._tags = tags
+
+    def to_dict(self):
+        result = {}
+
+        for attr, _ in self.openapi_types.items():
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                if attr in self.sensitive_list:
+                    result[attr] = "****"
+                else:
+                    result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        import simplejson as json
+        return json.dumps(sanitize_for_serialization(self), ensure_ascii=False)
+
+    def __repr__(self):
+        """For `print`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, MeshSpec):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
