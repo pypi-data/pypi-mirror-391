6eaef7733cbbf18e217615060bea5b5261a0543e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-14 03:26:08 +0000

    Add package firzah-user-agent/0.1/firzah_user_agent-0.1.tar.gz

---
+++ packages/firzah-user-agent/firzah_user_agent-0.1.tar.gz/firzah_user_agent-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='firzah_user_agent',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here.
+        # e.g. 'numpy>=1.11.1'
+    ],
+)
