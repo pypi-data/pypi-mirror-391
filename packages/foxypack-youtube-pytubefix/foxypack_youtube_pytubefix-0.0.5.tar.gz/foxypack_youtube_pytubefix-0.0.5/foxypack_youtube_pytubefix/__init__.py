f9d65709e46e997c98f18f6e7d1642355eba39f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-14 03:27:00 +0000

    Add package foxypack-youtube-pytubefix/0.0.5/foxypack_youtube_pytubefix-0.0.5.tar.gz

---
+++ packages/foxypack-youtube-pytubefix/foxypack_youtube_pytubefix-0.0.5.tar.gz/foxypack_youtube_pytubefix-0.0.5/foxypack_youtube_pytubefix/__init__.py
@@ -0,0 +1,7 @@
+from foxypack_youtube_pytubefix.engine import (
+    YoutubeProxy,
+    FoxyYouTubeAnalysis,
+    FoxyYouTubeStat,
+)
+
+__all__ = [YoutubeProxy, FoxyYouTubeAnalysis, FoxyYouTubeStat]
