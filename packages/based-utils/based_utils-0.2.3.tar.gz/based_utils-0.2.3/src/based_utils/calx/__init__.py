9b382febe694f78ed5517098800f6136d0f51129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-14 03:26:53 +0000

    Add package based-utils/0.2.3/based_utils-0.2.3.tar.gz

---
+++ packages/based-utils/based_utils-0.2.3.tar.gz/based_utils-0.2.3/src/based_utils/calx/__init__.py
@@ -0,0 +1,3 @@
+from .interpol import Bounds, CyclicBounds, trim
+
+__all__ = ["Bounds", "CyclicBounds", "trim"]
---
+++ packages/based-utils/based_utils-0.2.3.tar.gz/based_utils-0.2.3/src/based_utils/cli/__init__.py
@@ -0,0 +1,3 @@
+from .clox import human_readable_duration, timed, timed_awaitable
+
+__all__ = ["human_readable_duration", "timed", "timed_awaitable"]
