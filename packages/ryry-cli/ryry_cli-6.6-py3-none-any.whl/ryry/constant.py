dfc0ae18c54eabed4cf3d7d78260c7c35c042952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-14 03:26:26 +0000

    Add package ryry-cli/6.6/ryry_cli-6.6-py3-none-any.whl

---
+++ packages/ryry-cli/ryry_cli-6.6-py3-none-any.whl/ryry/constant.py
@@ -0,0 +1,10 @@
+#!!!!! do not change this file !!!!!
+app_version="6.6"
+app_bulld_anchor="Noh_2025-11-13 18:52:07.414384"
+app_name="ryry-cli"
+import sys, os
+if getattr(sys, 'frozen', False):
+    base_path = sys._MEIPASS
+else:
+    base_path = os.path.dirname(os.path.abspath(__file__))
+
