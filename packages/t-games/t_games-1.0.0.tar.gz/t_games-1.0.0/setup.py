ec1c5891ca73601b3b6f07ac86a91aeb5af26633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-14 03:26:18 +0000

    Add package t-games/1.0.0/t_games-1.0.0.tar.gz

---
+++ packages/t-games/t_games-1.0.0.tar.gz/t_games-1.0.0/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="t-games",
+    version="1.0.0",
+    author="pavansai-tanguturi",
+    author_email="pavansaitanguturi@gmail.com",
+    description="A collection of classic games playable in your terminal",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pavansai-tanguturi/t-games",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: End Users/Desktop",
+        "Topic :: Games/Entertainment",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "python-chess>=1.9.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "games=games.main:main",
+            "tictactoe=games.tictactoe:main",
+            "chess-game=games.chess:main",
+            "sudoku-game=games.sudoku:main",
+            "adventure-game=games.adventure:main",
+        ],
+    },
+    keywords="games terminal console multiplayer chess sudoku tictactoe battleship adventure",
+    project_urls={
+        "Bug Reports": "https://github.com/pavansai-tanguturi/t-games/issues",
+        "Source": "https://github.com/pavansai-tanguturi/t-games",
+    },
+)
