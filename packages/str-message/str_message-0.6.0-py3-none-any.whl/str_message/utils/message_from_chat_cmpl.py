2441d4681b550e9e99bc3c34d515772e95654b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-14 03:26:14 +0000

    Add package str-message/0.6.0/str_message-0.6.0-py3-none-any.whl

---
+++ packages/str-message/str_message-0.6.0-py3-none-any.whl/str_message/utils/message_from_chat_cmpl.py
@@ -0,0 +1,31 @@
+import typing
+
+from openai.types.chat.chat_completion import ChatCompletion, Choice
+from openai.types.chat.chat_completion_message import ChatCompletionMessage
+
+from str_message import (
+    AssistantMessage,
+    ReasoningMessage,
+    ToolCallMessage,
+)
+
+
+def message_from_chat_cmpl(
+    data: ChatCompletion,
+) -> AssistantMessage | ReasoningMessage | ToolCallMessage:
+    from str_message.utils.message_from_chat_cmpl_message import (
+        message_from_chat_cmpl_message,
+    )
+
+    might_choice: typing.Optional[Choice] = next(
+        (choice for choice in data.choices),
+        None,
+    )
+
+    if might_choice is None:
+        raise ValueError("No choice found in ChatCompletion")
+
+    choice: Choice = might_choice
+    message: ChatCompletionMessage = choice.message
+
+    return message_from_chat_cmpl_message(message, msg_id=data.id)
