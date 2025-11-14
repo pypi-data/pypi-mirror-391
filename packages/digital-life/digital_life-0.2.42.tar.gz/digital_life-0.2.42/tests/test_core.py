d8573866b04a4a0ccb6466424e6d3d5783cd43a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-14 03:26:45 +0000

    Add package digital-life/0.2.42/digital_life-0.2.42.tar.gz

---
+++ packages/digital-life/digital_life-0.2.42.tar.gz/digital_life-0.2.42/tests/test_core.py
@@ -0,0 +1,9 @@
+from pro_craft_infer.core import AsyncIntel
+from pro_craft_infer.core import atest_by_use_case
+from digital_life.core.memorycard import MemoryCardManager
+from digital_life.core.user import UserInfo
+import os
+
+import pytest
+
+
