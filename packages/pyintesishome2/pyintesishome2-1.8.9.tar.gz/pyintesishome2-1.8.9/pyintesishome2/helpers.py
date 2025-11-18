8f28cfee3b3bb5825d7acb0d2278bb73fa71dede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 11:08:51 +0000

    Add package pyintesishome2/1.8.9/pyintesishome2-1.8.9.tar.gz

---
+++ packages/pyintesishome2/pyintesishome2-1.8.9.tar.gz/pyintesishome2-1.8.9/pyintesishome2/helpers.py
@@ -0,0 +1,14 @@
+""" Helpers for pyintesishome2 """
+
+
+def twos_complement_16bit(val):
+    """Compute Two's Complement, to represent negative temperatures"""
+    if (val & (1 << 15)) != 0:
+        val = val - (1 << 16)
+    return val
+
+
+def uint32(value):
+    """uint32"""
+    result = int(value) & 0xFFFF
+    return result
---
+++ packages/pyintesishome2/pyintesishome2-1.8.9.tar.gz/pyintesishome2-1.8.9/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# read the contents of your README file
+from pathlib import Path
+
+from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="pyintesishome2",
+    version="1.8.9",
+    description="A python3 library for running asynchronus communications with IntesisHome Smart AC Controllers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dolkensp/pyIntesisHome",
+    author="James Nimmo",
+    author_email="james@nimmo.net.nz",
+    license="MIT",
+    install_requires=["aiohttp>=3.7.4,<4"],
+    packages=["pyintesishome2"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator",
+    ],
+)
