bd1500422eda91818b2e7e5a27128c76ceef00fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 03:26:53 +0000

    Add package odoo-addon-sale_subscription_disable_tokenization/18.0.1.0.0/odoo_addon_sale_subscription_disable_tokenization-18.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo-addon-sale_subscription_disable_tokenization/odoo_addon_sale_subscription_disable_tokenization-18.0.1.0.0-py3-none-any.whl/odoo/addons/sale_subscription_disable_tokenization/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Sale Subscription Disable Tokenization",
+    "summary": """
+        Do not force payment tokenization for order with subscriptions.
+    """,
+    "author": "Mint System GmbH",
+    "website": "https://github.com/Mint-system/",
+    "category": "Sale",
+    "version": "18.0.1.0.0",
+    "license": "OPL-1",
+    "depends": ["sale_subscription"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
