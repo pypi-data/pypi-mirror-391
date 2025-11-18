5118d21f593e792420b0e5ee311e8617ff854e8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 09:07:38 +0000

    Add package azure-appconfiguration-provider/2.3.1/azure_appconfiguration_provider-2.3.1.tar.gz

---
+++ packages/azure-appconfiguration-provider/azure_appconfiguration_provider-2.3.1.tar.gz/azure_appconfiguration_provider-2.3.1/azure/appconfiguration/provider/_constants.py
@@ -0,0 +1,29 @@
+# ------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# -------------------------------------------------------------------------
+
+FEATURE_MANAGEMENT_KEY = "feature_management"
+FEATURE_FLAG_KEY = "feature_flags"
+FEATURE_FLAG_PREFIX = ".appconfig.featureflag/"
+
+NULL_CHAR = "\0"
+
+REQUEST_TRACING_DISABLED_ENVIRONMENT_VARIABLE = "AZURE_APP_CONFIGURATION_TRACING_DISABLED"
+AzureFunctionEnvironmentVariable = "FUNCTIONS_EXTENSION_VERSION"
+AzureWebAppEnvironmentVariable = "WEBSITE_SITE_NAME"
+ContainerAppEnvironmentVariable = "CONTAINER_APP_NAME"
+KubernetesEnvironmentVariable = "KUBERNETES_PORT"
+ServiceFabricEnvironmentVariable = "Fabric_NodeName"  # cspell:disable-line
+
+TELEMETRY_KEY = "telemetry"
+METADATA_KEY = "metadata"
+
+ALLOCATION_ID_KEY = "AllocationId"
+ETAG_KEY = "ETag"
+FEATURE_FLAG_REFERENCE_KEY = "FeatureFlagReference"
+
+# Mime profiles
+APP_CONFIG_AI_MIME_PROFILE = "https://azconfig.io/mime-profiles/ai/"
+APP_CONFIG_AICC_MIME_PROFILE = "https://azconfig.io/mime-profiles/ai/chat-completion"
