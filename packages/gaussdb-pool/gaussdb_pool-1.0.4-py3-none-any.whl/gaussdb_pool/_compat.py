aab51d04b8f9f41d95b660c0a71c1e8fb38e72af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 12:06:41 +0000

    Add package gaussdb-pool/1.0.4/gaussdb_pool-1.0.4-py3-none-any.whl

---
+++ packages/gaussdb-pool/gaussdb_pool-1.0.4-py3-none-any.whl/gaussdb_pool/_compat.py
@@ -0,0 +1,43 @@
+"""
+compatibility functions for different Python versions
+"""
+
+# Copyright (C) 2021 The Psycopg Team
+
+from __future__ import annotations
+
+import sys
+
+if sys.version_info >= (3, 10):
+    from typing import TypeAlias
+else:
+    from typing_extensions import TypeAlias
+
+if sys.version_info >= (3, 11):
+    from typing import Self
+else:
+    from typing_extensions import Self
+
+if sys.version_info >= (3, 13):
+    from typing import TypeVar
+else:
+    from typing_extensions import TypeVar
+
+import gaussdb.errors as e
+
+__all__ = [
+    "Self",
+    "TypeAlias",
+    "TypeVar",
+]
+
+# Workaround for gaussdb < 3.0.8.
+# Timeout on NullPool connection mignt not work correctly.
+try:
+    ConnectionTimeout: type[e.OperationalError] = e.ConnectionTimeout
+except AttributeError:
+
+    class DummyConnectionTimeout(e.OperationalError):
+        pass
+
+    ConnectionTimeout = DummyConnectionTimeout
