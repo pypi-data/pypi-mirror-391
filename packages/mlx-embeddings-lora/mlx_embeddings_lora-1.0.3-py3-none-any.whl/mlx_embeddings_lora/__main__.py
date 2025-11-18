f24b272d94d0b04a7f80832538ff6341005ab281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 04:09:33 +0000

    Add package mlx-embeddings-lora/1.0.3/mlx_embeddings_lora-1.0.3-py3-none-any.whl

---
+++ packages/mlx-embeddings-lora/mlx_embeddings_lora-1.0.3-py3-none-any.whl/mlx_embeddings_lora/__main__.py
@@ -0,0 +1,20 @@
+import importlib
+import sys
+
+if __name__ == "__main__":
+    subcommands = ["train"]
+    
+    if len(sys.argv) < 2:
+        raise ValueError(f"CLI requires a subcommand in {subcommands}")
+    
+    subcommand = sys.argv[1]
+    
+    if subcommand in subcommands:
+        submodule = importlib.import_module(f"mlx_embeddings_lora.{subcommand}")
+        sys.argv = [sys.argv[0]] + sys.argv[2:]
+        submodule.main()
+    elif subcommand == "--version":
+        from mlx_embeddings_lora import __version__
+        print(__version__)
+    else:
+        raise ValueError(f"CLI requires a subcommand in {subcommands}")
