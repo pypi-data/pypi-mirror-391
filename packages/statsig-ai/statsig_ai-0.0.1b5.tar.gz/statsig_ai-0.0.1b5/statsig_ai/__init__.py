ec7205d34a29383d0b9ad4c36e2a9c8a02993fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 09:06:19 +0000

    Add package statsig-ai/0.0.1b5/statsig_ai-0.0.1b5.tar.gz

---
+++ packages/statsig-ai/statsig_ai-0.0.1b5.tar.gz/statsig_ai-0.0.1b5/statsig_ai/__init__.py
@@ -0,0 +1,28 @@
+from statsig_python_core import Statsig, StatsigOptions, StatsigUser
+from .prompt import PromptEvaluationOptions, Prompt
+from .ai_eval_grade_data import AIEvalGradeData
+from .prompt_version import PromptVersion
+from .statsig_ai import StatsigAI
+from .statsig_ai_base import StatsigCreateConfig, StatsigAttachConfig
+from .evals import Eval, EvalScorerArgs, EvalDataRecord, EvalHook
+
+__all__ = [
+    "StatsigAI",
+    "StatsigCreateConfig",
+    "StatsigAttachConfig",
+    # Serving
+    "Prompt",
+    "PromptVersion",
+    "PromptEvaluationOptions",
+    # Logging
+    "AIEvalGradeData",
+    # Re-export from statsig-python-core for convenience
+    "Statsig",
+    "StatsigOptions",
+    "StatsigUser",
+    # Evals
+    "Eval",
+    "EvalScorerArgs",
+    "EvalDataRecord",
+    "EvalHook",
+]
