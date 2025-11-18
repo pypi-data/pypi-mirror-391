25b28e97d23ff449de0da725aaaaef57fdd5edde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 09:08:28 +0000

    Add package plain.support/0.23.0/plain_support-0.23.0.tar.gz

---
+++ packages/plain.support/plain_support-0.23.0.tar.gz/plain_support-0.23.0/plain/support/models.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from plain import models
+from plain.models import types
+from plain.runtime import SettingsReference
+
+
+@models.register_model
+class SupportFormEntry(models.Model):
+    user = types.ForeignKey(
+        SettingsReference("AUTH_USER_MODEL"),
+        on_delete=models.SET_NULL,
+        related_name="support_form_entries",
+        allow_null=True,
+        required=False,
+    )
+    name: str = types.CharField(max_length=255)
+    email: str = types.EmailField()
+    message: str = types.TextField()
+    created_at: datetime = types.DateTimeField(auto_now_add=True)
+    form_slug: str = types.CharField(max_length=255)
+    # referrer? source? session?
+    # extra_data
+
+    model_options = models.Options(
+        ordering=["-created_at"],
+        indexes=[
+            models.Index(fields=["created_at"]),
+        ],
+    )
