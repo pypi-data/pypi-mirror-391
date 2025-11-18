c3a75265fb4fb5d081f292575cb95279718c40e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 09:07:59 +0000

    Add package dpqc/0.0.1/dpqc-0.0.1-py3-none-any.whl

---
+++ packages/dpqc/dpqc-0.0.1-py3-none-any.whl/dpqc/__init__.py
@@ -0,0 +1,5 @@
+﻿"""PQC Utils Package"""
+
+__version__ = "0.0.1"
+from .classes import MLKEM512, MLKEM768, MLKEM1024, MLDSA44, MLDSA65, MLDSA87, Falcon512, Falcon1024
+__all__ = ["MLKEM512", "MLKEM768", "MLKEM1024", "MLDSA44", "MLDSA65", "MLDSA87", "Falcon512", "Falcon1024"]
