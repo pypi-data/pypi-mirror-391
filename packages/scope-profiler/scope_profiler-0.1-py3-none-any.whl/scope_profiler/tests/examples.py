dc03a00ff98701819241aec2bc25538905c776d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 13:15:49 +0000

    Add package scope-profiler/0.1/scope_profiler-0.1-py3-none-any.whl

---
+++ packages/scope-profiler/scope_profiler-0.1-py3-none-any.whl/scope_profiler/tests/examples.py
@@ -0,0 +1,40 @@
+from scope_profiler.profiling import ProfileManager
+
+
+def loop(
+    label,
+    num_loops: int = 100,
+):
+    s = 0
+    for i in range(num_loops):
+        with ProfileManager.profile_region(region_name=label):
+            s += 1
+
+
+if __name__ == "__main__":
+    from scope_profiler.profiling import (
+        ProfilingConfig,
+    )
+
+    config = ProfilingConfig(
+        use_likwid=False,
+        time_trace=True,
+        flush_to_disk=True,
+    )
+
+    ProfileManager.reset()
+    num_loops = 10
+
+    loop(
+        label="loop1",
+        num_loops=num_loops,
+    )
+
+    loop(
+        label="loop2",
+        num_loops=num_loops * 2,
+    )
+
+    ProfileManager.print_summary()
+
+    ProfileManager.finalize()
