b2796cd9d63829525fed7953f5b71e8e609f12e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 09:06:39 +0000

    Add package devguide/0.1.4/devguide-0.1.4.tar.gz

---
+++ packages/devguide/devguide-0.1.4.tar.gz/devguide-0.1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="devguide",
+    version="0.1.4",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "scikit-learn==1.4.0",
+        "stop-words",
+        "rich"
+    ],
+    entry_points={
+        "console_scripts": [
+            "devguide=devguide.cli:main"
+        ]
+    }
+)
