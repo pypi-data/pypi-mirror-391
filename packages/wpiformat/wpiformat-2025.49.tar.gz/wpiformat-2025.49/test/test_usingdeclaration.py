90e2d5ca5ff137ac62e452a438479da087c328d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 11:07:23 +0000

    Add package wpiformat/2025.49/wpiformat-2025.49.tar.gz

---
+++ packages/wpiformat/wpiformat-2025.49.tar.gz/wpiformat-2025.49/test/test_usingdeclaration.py
@@ -0,0 +1,70 @@
+import os
+
+from wpiformat.usingdeclaration import UsingDeclaration
+
+from .test_tasktest import *
+
+
+def test_usingdeclaration():
+    test = TaskTest(UsingDeclaration())
+
+    # Before class block
+    test.add_input(
+        "./Test.h",
+        "using std::chrono;"
+        + os.linesep
+        + "class Test {"
+        + os.linesep
+        + "}"
+        + os.linesep,
+    )
+    test.add_output("./Test.h: 1: 'using std::chrono;' in global namespace\n", False)
+
+    # Inside enum block
+    test.add_input(
+        "./Test.h",
+        "enum Test {"
+        + os.linesep
+        + "  using std::chrono;"
+        + os.linesep
+        + "}"
+        + os.linesep,
+    )
+    test.add_output("", True)
+
+    # After { block
+    test.add_input(
+        "./Test.h",
+        "{" + os.linesep + "}" + os.linesep + "using std::chrono;" + os.linesep,
+    )
+    test.add_output("./Test.h: 3: 'using std::chrono;' in global namespace\n", False)
+
+    # Before class block with NOLINT
+    test.add_input(
+        "./Test.h",
+        "using std::chrono;  // NOLINT"
+        + os.linesep
+        + "class Test {"
+        + os.linesep
+        + "}"
+        + os.linesep,
+    )
+    test.add_output("", True)
+
+    # "using" in comment without trailing semicolon
+    test.add_input(
+        "./Test.h",
+        "// using"
+        + os.linesep
+        + "void func() {"
+        + os.linesep
+        + "  using A = int;"
+        + os.linesep
+        + "  using B = int;"
+        + os.linesep
+        + "}"
+        + os.linesep,
+    )
+    test.add_output("", True)
+
+    test.run(OutputType.STDOUT)
