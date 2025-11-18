891373b9648f88b45a68848131fcd073861d4a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 11:08:41 +0000

    Add package pyqpanda-alg/2.0.0/pyqpanda_alg-2.0.0-cp313-cp313-macosx_13_0_arm64.whl

---
+++ packages/pyqpanda-alg/pyqpanda_alg-2.0.0-cp313-cp313-macosx_13_0_arm64.whl/pyqpanda_alg/QCmp/__init__.py
@@ -0,0 +1,5 @@
+
+from .QCmp import int_comparator,interpolation_comparator,qubit_comparator,_zero_control
+from .QCmp import qft_comparator,qft_qubit_comparator
+
+__all__ = ["int_comparator","interpolation_comparator","qubit_comparator","_zero_control","qft_comparator","qft_qubit_comparator"]
---
+++ packages/pyqpanda-alg/pyqpanda_alg-2.0.0-cp313-cp313-macosx_13_0_arm64.whl/pyqpanda_alg/extensions/__init__.py
@@ -0,0 +1,7 @@
+# from . import _EXTENSIONS
+
+# __all__ = [_EXTENSIONS]
+
+from ._EXTENSIONS import QAdder
+from ._EXTENSIONS import QAdderIgnoreCarry
+from ._EXTENSIONS import amplitude_encode
