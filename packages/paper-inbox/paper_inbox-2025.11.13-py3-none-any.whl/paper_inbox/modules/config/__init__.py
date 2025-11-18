885d389279c2972c678ecf5054aca996afe704fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 08:07:35 +0000

    Add package paper-inbox/2025.11.13/paper_inbox-2025.11.13-py3-none-any.whl

---
+++ packages/paper-inbox/paper_inbox-2025.11.13-py3-none-any.whl/paper_inbox/modules/config/__init__.py
@@ -0,0 +1,39 @@
+from . import getters, setters, validators, file, paths
+
+from paper_inbox.modules.config.getters import (
+    telegram_bot_token,
+    telegram_chat_id,
+    send_telegram_notifications,
+    printer_timeout_seconds,
+    printer_poll_interval_seconds,
+    network_retry_max_retries,
+    network_retry_delay,
+    trusted_ssids,
+    email_account,
+    email_senders,
+    email_server_url,
+    print_only_first_page,
+    skip_printing_irl,
+    force_grayscale,
+    cups_path,
+    libreoffice_path,
+)
+
+__all__ = [
+    'telegram_bot_token',
+    'telegram_chat_id',
+    'send_telegram_notifications',
+    'printer_timeout_seconds',
+    'printer_poll_interval_seconds',
+    'network_retry_max_retries',
+    'network_retry_delay',
+    'trusted_ssids',
+    'email_account',
+    'email_senders',
+    'email_server_url',
+    'print_only_first_page',
+    'skip_printing_irl',
+    'force_grayscale',
+    'cups_path',
+    'libreoffice_path',
+]
