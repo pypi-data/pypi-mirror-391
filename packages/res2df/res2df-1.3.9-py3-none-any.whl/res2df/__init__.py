2654e8c5268687db7ac35919b23f560b8c73c8b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 12:06:40 +0000

    Add package res2df/1.3.9/res2df-1.3.9-py3-none-any.whl

---
+++ packages/res2df/res2df-1.3.9-py3-none-any.whl/res2df/__init__.py
@@ -0,0 +1,29 @@
+import importlib
+
+from .__version__ import __version__ as __version__
+from .res2csvlogger import getLogger_res2csv as getLogger_res2csv
+from .resdatafiles import ResdataFiles as ResdataFiles
+
+SUBMODULES: list[str] = [
+    "compdat",
+    "equil",
+    "faults",
+    "fipreports",
+    "grid",
+    "gruptree",
+    "nnc",
+    "pillars",
+    "pvt",
+    "rft",
+    "satfunc",
+    "summary",
+    "trans",
+    "vfp",
+    "wellcompletiondata",
+    "wellconnstatus",
+    "wcon",
+]
+
+
+for submodule in [*SUBMODULES, "res2csv", "csv2res"]:
+    importlib.import_module("res2df." + submodule)
