aba7048e46ec266b6f5694db26ab278680c99421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 05:06:57 +0000

    Add package aiiware-cli/0.9.0/aiiware_cli-0.9.0-py3-none-any.whl

---
+++ packages/aiiware-cli/aiiware_cli-0.9.0-py3-none-any.whl/aii/data/storage/migrations/__init__.py
@@ -0,0 +1,28 @@
+# Copyright 2025-present aiiware.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Database migrations for Aii CLI storage"""
+
+# Import migration definitions
+from .migrations_001_initial_schema import migration as migration_001_initial_schema
+from .migrations_002_enhance_executions import (
+    migration as migration_002_enhance_executions,
+)
+from .migrations_003_add_client_type import migration as migration_003_add_client_type
+
+__all__ = [
+    "migration_001_initial_schema",
+    "migration_002_enhance_executions",
+    "migration_003_add_client_type",
+]
