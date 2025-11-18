5d1fbe811cf2fa5743768488a54618a0fbfe92e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 11:07:14 +0000

    Add package wink-sdk-extranet-property-register/0.0.57/wink_sdk_extranet_property_register-0.0.57.tar.gz

---
+++ packages/wink-sdk-extranet-property-register/wink_sdk_extranet_property_register-0.0.57.tar.gz/wink_sdk_extranet_property_register-0.0.57/wink_sdk_extranet_property_register/api/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+
+# import apis into api package
+from wink_sdk_extranet_property_register.api.leads_api import LeadsApi
+from wink_sdk_extranet_property_register.api.registration_api import RegistrationApi
+
