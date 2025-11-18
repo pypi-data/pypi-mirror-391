c1a4b26d851acbb19c0f38b8eccc7d67cf0a911e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 03:27:35 +0000

    Add package nsj-integracao-api-entidades/1.0.0a25.post45/nsj_integracao_api_entidades-1.0.0a25.post45-py3-none-any.whl

---
+++ packages/nsj-integracao-api-entidades/nsj_integracao_api_entidades-1.0.0a25.post45-py3-none-any.whl/nsj_integracao_api_entidades/dto/ns_df_fretes.py
@@ -0,0 +1,62 @@
+
+import datetime
+import uuid
+
+from nsj_rest_lib.decorator.dto import DTO
+from nsj_rest_lib.descriptor.dto_field import DTOField
+from nsj_rest_lib.descriptor.dto_field_validators import DTOFieldValidators
+from nsj_rest_lib.dto.dto_base import DTOBase
+
+
+
+# Configuracoes execucao
+from nsj_integracao_api_entidades.config import (tenant_is_partition_data)
+
+@DTO()
+class DfFreteDTO(DTOBase):
+    # Atributos da entidade
+    id: uuid.UUID = DTOField(
+      pk=True,
+      entity_field='df_frete',
+      resume=True,
+      not_null=True,
+      strip=True,
+      min=36,
+      max=36,
+      validator=DTOFieldValidators().validate_uuid,)
+    tenant: int = DTOField(
+      partition_data=tenant_is_partition_data,
+      resume=True,
+      not_null=True,)
+    id_docfis: uuid.UUID = DTOField(
+      not_null=True,
+      strip=True,
+      min=36,
+      max=36,
+      validator=DTOFieldValidators().validate_uuid,)
+    ibgeocorrenciafatorgeradoricms: str = DTOField()
+    id_transportadora: uuid.UUID = DTOField(
+      strip=True,
+      min=36,
+      max=36,
+      validator=DTOFieldValidators().validate_uuid,)
+    modalidade: int = DTOField(
+      not_null=True,)
+    valorservico: float = DTOField()
+    valorbcretencaoicms: float = DTOField()
+    parcelaicmsretido: float = DTOField()
+    valoricmsretido: float = DTOField()
+    cfopservicotransporte: str = DTOField()
+    placaveiculo: str = DTOField()
+    ufveiculo: str = DTOField()
+    rntcveiculo: str = DTOField()
+    vagao: str = DTOField()
+    balsa: str = DTOField()
+    lastupdate: datetime.datetime = DTOField()
+    id_veiculo: uuid.UUID = DTOField(
+      strip=True,
+      min=36,
+      max=36,
+      validator=DTOFieldValidators().validate_uuid,)
+    tiporateio: int = DTOField()
+
---
+++ packages/nsj-integracao-api-entidades/nsj_integracao_api_entidades-1.0.0a25.post45-py3-none-any.whl/nsj_integracao_api_entidades/dto/workflow_escopo.py
@@ -0,0 +1,30 @@
+
+import datetime
+import uuid
+
+from nsj_rest_lib.decorator.dto import DTO
+from nsj_rest_lib.descriptor.dto_field import DTOField
+from nsj_rest_lib.descriptor.dto_field_validators import DTOFieldValidators
+from nsj_rest_lib.dto.dto_base import DTOBase
+
+
+
+# Configuracoes execucao
+from nsj_integracao_api_entidades.config import (tenant_is_partition_data)
+
+@DTO()
+class EscopoDTO(DTOBase):
+    # Atributos da entidade
+    id: int = DTOField(
+      pk=True,
+      entity_field='escopoworkflow',
+      resume=True,
+      not_null=True,)
+    tenant: int = DTOField(
+      partition_data=tenant_is_partition_data,
+      resume=True,
+      not_null=True,)
+    nome: str = DTOField()
+    codigo: str = DTOField()
+    lastupdate: datetime.datetime = DTOField()
+
