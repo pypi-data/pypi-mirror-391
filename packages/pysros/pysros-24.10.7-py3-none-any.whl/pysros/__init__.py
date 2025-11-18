0a2acbcf0460d4b7b5ed6be7b4ba287c8a723d8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 06:08:46 +0000

    Add package pysros/24.10.7/pysros-24.10.7-py3-none-any.whl

---
+++ packages/pysros/pysros-24.10.7-py3-none-any.whl/pysros/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2021-2025 Nokia
+
+__all__ = ("management", "exceptions", "wrappers", "pprint", )
+
+__doc__ = """Library for management of Nokia SR OS nodes."""
+
+__version__ = "24.10.7"
