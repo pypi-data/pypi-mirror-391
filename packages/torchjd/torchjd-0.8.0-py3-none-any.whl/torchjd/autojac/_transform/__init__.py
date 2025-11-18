3c4aed149f7cafa57fda9d00d11dc80cf64708f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 05:07:11 +0000

    Add package torchjd/0.8.0/torchjd-0.8.0-py3-none-any.whl

---
+++ packages/torchjd/torchjd-0.8.0-py3-none-any.whl/torchjd/autojac/_transform/__init__.py
@@ -0,0 +1,10 @@
+from ._accumulate import Accumulate
+from ._aggregate import Aggregate
+from ._base import Composition, Conjunction, RequirementError, Transform
+from ._diagonalize import Diagonalize
+from ._grad import Grad
+from ._init import Init
+from ._jac import Jac
+from ._ordered_set import OrderedSet
+from ._select import Select
+from ._stack import Stack
