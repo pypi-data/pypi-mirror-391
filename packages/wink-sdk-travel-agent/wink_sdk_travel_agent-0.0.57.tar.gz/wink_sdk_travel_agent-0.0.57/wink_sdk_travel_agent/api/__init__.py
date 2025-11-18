0c7b10d90d6d7124299b12210918360f9a447284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 11:07:15 +0000

    Add package wink-sdk-travel-agent/0.0.57/wink_sdk_travel_agent-0.0.57.tar.gz

---
+++ packages/wink-sdk-travel-agent/wink_sdk_travel_agent-0.0.57.tar.gz/wink_sdk_travel_agent-0.0.57/wink_sdk_travel_agent/api/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+
+# import apis into api package
+from wink_sdk_travel_agent.api.analytics_api import AnalyticsApi
+from wink_sdk_travel_agent.api.travel_agent_api import TravelAgentApi
+
