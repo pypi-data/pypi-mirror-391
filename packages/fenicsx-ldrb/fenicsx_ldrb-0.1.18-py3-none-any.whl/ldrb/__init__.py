4a35044faf94f2e183f41a6f19e3f54c5e6068b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 03:26:35 +0000

    Add package fenicsx-ldrb/0.1.18/fenicsx_ldrb-0.1.18-py3-none-any.whl

---
+++ packages/fenicsx-ldrb/fenicsx_ldrb-0.1.18-py3-none-any.whl/ldrb/__init__.py
@@ -0,0 +1,25 @@
+from importlib.metadata import metadata
+
+from . import aha, calculus, io, ldrb, utils
+from .ldrb import dolfinx_ldrb, project_gradients, scalar_laplacians
+from .utils import space_from_string
+
+meta = metadata("fenicsx-ldrb")
+__version__ = meta["Version"]
+__author__ = meta["Author-email"]
+__license__ = meta["License"]
+__email__ = meta["Author-email"]
+__program_name__ = meta["Name"]
+
+
+__all__ = [
+    "ldrb",
+    "dolfinx_ldrb",
+    "scalar_laplacians",
+    "project_gradients",
+    "utils",
+    "space_from_string",
+    "calculus",
+    "io",
+    "aha",
+]
