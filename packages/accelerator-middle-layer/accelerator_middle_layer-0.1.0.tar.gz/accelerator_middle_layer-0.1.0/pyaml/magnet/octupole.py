890178fdb6edf525145f2d7865d37dbb8b037683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 11:08:13 +0000

    Add package accelerator-middle-layer/0.1.0/accelerator_middle_layer-0.1.0.tar.gz

---
+++ packages/accelerator-middle-layer/accelerator_middle_layer-0.1.0.tar.gz/accelerator_middle_layer-0.1.0/pyaml/magnet/octupole.py
@@ -0,0 +1,18 @@
+from .magnet import Magnet,MagnetConfigModel
+from ..lattice.polynom_info import PolynomInfo
+
+# Define the main class name for this module
+PYAMLCLASS = "Octupole"
+
+class ConfigModel(MagnetConfigModel):...
+
+class Octupole(Magnet):    
+    """Octupole class"""
+    polynom = PolynomInfo('PolynomB',3)
+
+    def __init__(self, cfg: ConfigModel):
+        super().__init__(
+            cfg.name,
+            cfg.model if hasattr(cfg, "model") else None,
+        )
+        self._cfg = cfg
