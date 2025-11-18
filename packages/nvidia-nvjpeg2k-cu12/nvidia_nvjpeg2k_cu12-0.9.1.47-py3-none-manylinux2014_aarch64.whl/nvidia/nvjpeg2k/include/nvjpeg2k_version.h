8cc57a9897078e0524f27d4b0b7e065f5652406b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 05:07:10 +0000

    Add package nvidia-nvjpeg2k-cu12/0.9.1.47/nvidia_nvjpeg2k_cu12-0.9.1.47-py3-none-manylinux2014_aarch64.whl

---
+++ packages/nvidia-nvjpeg2k-cu12/nvidia_nvjpeg2k_cu12-0.9.1.47-py3-none-manylinux2014_aarch64.whl/nvidia/nvjpeg2k/include/nvjpeg2k_version.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2020 - 2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ *
+ * NVIDIA CORPORATION and its licensors retain all intellectual property
+ * and proprietary rights in and to this software, related documentation
+ * and any modifications thereto.  Any use, reproduction, disclosure or
+ * distribution of this software and related documentation without an express
+ * license agreement from NVIDIA CORPORATION is strictly prohibited.
+ */
+
+
+#ifndef NVJPEG2K_VERSION_H__
+#define NVJPEG2K_VERSION_H__
+
+
+
+#define NVJPEG2K_VER_MAJOR 0
+#define NVJPEG2K_VER_MINOR 9
+#define NVJPEG2K_VER_PATCH 1
+#define NVJPEG2K_VER_BUILD 47
+
+
+
+#endif // NVJPEG2K_VERSION
