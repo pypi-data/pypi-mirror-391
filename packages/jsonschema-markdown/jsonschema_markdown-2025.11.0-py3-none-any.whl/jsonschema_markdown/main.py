e0f0f167e9439a8b970db45326b58a13fc02df39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 13:15:52 +0000

    Add package jsonschema-markdown/2025.11.0/jsonschema_markdown-2025.11.0-py3-none-any.whl

---
+++ packages/jsonschema-markdown/jsonschema_markdown-2025.11.0-py3-none-any.whl/jsonschema_markdown/main.py
@@ -0,0 +1,93 @@
+import json
+
+import click
+
+import jsonschema_markdown
+
+
+@click.command()
+@click.argument("filename", type=click.File("r"))
+@click.option(
+    "-t",
+    "--title",
+    type=str,
+    help="Do not use the title from the schema, use this title instead.",
+)
+@click.option(
+    "--footer/--no-footer",
+    is_flag=True,
+    default=True,
+    show_default=True,
+    help="Add a footer with a link to the project.",
+)
+@click.option(
+    "--empty-columns/--no-empty-columns",
+    is_flag=True,
+    default=True,
+    show_default=True,
+    help="Remove empty columns from the output, useful when deprecated or examples are not used.",
+)
+@click.option(
+    "--resolve/--no-resolve",
+    is_flag=True,
+    default=False,
+    show_default=True,
+    help="[Experimental] Resolve $ref pointers.",
+)
+@click.option(
+    "--debug/--no-debug",
+    is_flag=True,
+    default=False,
+    show_default=True,
+    help="Enable debug output.",
+)
+@click.option(
+    "--examples-format",
+    type=click.Choice(["text", "yaml", "json"], case_sensitive=False),
+    default="text",
+    show_default=True,
+    help="Format of the examples in the output.",
+)
+@click.option(
+    "--sort-yaml-keys/--no-sort-yaml-keys",
+    is_flag=True,
+    default=False,
+    show_default=True,
+    help="Sort keys in YAML examples. Only applies when --examples-format is yaml.",
+)
+@click.version_option(package_name="jsonschema_markdown")
+def cli(
+    filename,
+    title,
+    footer,
+    empty_columns,
+    resolve,
+    debug,
+    examples_format,
+    sort_yaml_keys,
+):
+    """
+    Load FILENAME and output a markdown version.
+
+    Use '-' as FILENAME to read from stdin.
+    """
+
+    file_contents = json.loads(filename.read())
+
+    kwargs = {
+        "footer": footer,
+        "replace_refs": resolve,
+        "debug": debug,
+        "hide_empty_columns": not empty_columns,
+        "examples_format": examples_format,
+        "sort_yaml_keys": sort_yaml_keys,
+    }
+
+    if title:
+        kwargs["title"] = title
+
+    # Convert the file contents to markdown
+    markdown = jsonschema_markdown.generate(file_contents, **kwargs)
+
+    # Output the markdown
+    click.echo(markdown, nl=False)
