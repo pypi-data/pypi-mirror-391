f75d6679dfde221baafcfaab489a87323f6a4bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 05:07:33 +0000

    Add package pwq-qr-gen-4/4.0.3/pwq_qr_gen_4-4.0.3.tar.gz

---
+++ packages/pwq-qr-gen-4/pwq_qr_gen_4-4.0.3.tar.gz/pwq_qr_gen_4-4.0.3/pwq_qr_gen_4/__init__.py
@@ -0,0 +1,24 @@
+"""
+PWQ_QR_Gen_4 – A fully customizable QR Code Generator.
+Developed by PawlexCode95.
+"""
+
+from .PWQ_QR_Gen_4 import (
+    PWQ_QR_GEN_4,
+    PWQ_QR_Segmentation,
+    BitOverflowError,
+    ECIEncodingError,
+    PWQAIUnprovidedError,
+    DataTooLongError,
+)
+
+__all__ = [
+    "PWQ_QR_GEN_4",
+    "PWQ_QR_Segmentation",
+    "BitOverflowError",
+    "ECIEncodingError",
+    "PWQAIUnprovidedError",
+    "DataTooLongError",
+]
+
+__version__ = "4.0.3"
---
+++ packages/pwq-qr-gen-4/pwq_qr_gen_4-4.0.3.tar.gz/pwq_qr_gen_4-4.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pwq_qr_gen_4",
+    version="4.0.3",
+    author="pawlexcode95",
+    description="A fully customizable QR code generator with support for colors, logos, and links.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    license_files=["LICENSE"],
+    python_requires=">=3.11",
+    packages=find_packages(where="."),
+    install_requires=[
+        "pillow",
+    ],
+    url="https://github.com/pawlexcode95/PWQ-QR-Gen-4-A-Fully-Customizable-QR-Code-Generator",
+    project_urls={
+        "Homepage": "https://github.com/pawlexcode95/PWQ-QR-Gen-4-A-Fully-Customizable-QR-Code-Generator",
+        "Script": "https://github.com/pawlexcode95/PWQ-QR-Gen-4-A-Fully-Customizable-QR-Code-Generator/blob/main/PWQ_QR_Gen_4.py",
+        "Issues": "https://github.com/pawlexcode95/PWQ-QR-Gen-4-A-Fully-Customizable-QR-Code-Generator/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
