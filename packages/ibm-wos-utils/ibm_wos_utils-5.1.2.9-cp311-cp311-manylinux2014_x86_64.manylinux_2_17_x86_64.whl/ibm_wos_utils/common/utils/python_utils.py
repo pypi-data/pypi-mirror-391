2ea7d9bd4271be964cd26b7d0a27eac5678368ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 03:27:34 +0000

    Add package ibm-wos-utils/5.1.2.9/ibm_wos_utils-5.1.2.9-cp311-cp311-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/ibm-wos-utils/ibm_wos_utils-5.1.2.9-cp311-cp311-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/ibm_wos_utils/common/utils/python_utils.py
@@ -0,0 +1,22 @@
+# ----------------------------------------------------------------------------------------------------
+# IBM Confidential
+# OCO Source Materials
+# 5900-A3Q, 5737-H76
+# Copyright IBM Corp. 2023
+# The source code for this program is not published or other-wise divested of its trade
+# secrets, irrespective of what has been deposited with the U.S.Copyright Office.
+# ----------------------------------------------------------------------------------------------------
+
+def strtobool(val: str):
+    """Convert a string representation of truth to true (1) or false (0).
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return 1
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return 0
+    else:
+        raise ValueError("invalid truth value %r" % (val,))
