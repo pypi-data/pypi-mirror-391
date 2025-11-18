9b54d69933de47544833498e4a9d579a4601e7e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 03:30:06 +0000

    Add package torrra/1.3.1/torrra-1.3.1-py3-none-any.whl

---
+++ packages/torrra/torrra-1.3.1-py3-none-any.whl/torrra/_types.py
@@ -0,0 +1,40 @@
+from dataclasses import asdict, dataclass
+from typing import Any, Literal, TypedDict
+
+
+# TORRENT TYPES
+class TorrentDict(TypedDict):
+    title: str
+    size: float
+    seeders: int
+    leechers: int
+    source: str
+    magnet_uri: str | None
+
+
+@dataclass
+class Torrent:
+    title: str
+    size: float
+    seeders: int
+    leechers: int
+    source: str
+    magnet_uri: str | None
+
+    @classmethod
+    def from_dict(cls, d: TorrentDict) -> "Torrent":
+        return cls(**d)
+
+    def to_dict(self) -> dict[str, Any]:
+        return asdict(self)
+
+
+# INDEXER TYPES
+IndexerName = Literal["jackett", "prowlarr"]
+
+
+@dataclass
+class Indexer:
+    name: IndexerName
+    url: str
+    api_key: str
