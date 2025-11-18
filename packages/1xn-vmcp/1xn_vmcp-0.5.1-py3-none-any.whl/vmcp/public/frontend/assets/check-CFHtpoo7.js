5f02cff40ec1c5f3f9c00d3b7ae8f1c2ce122a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 04:09:03 +0000

    Add package 1xn-vmcp/0.5.1/1xn_vmcp-0.5.1-py3-none-any.whl

---
+++ packages/1xn-vmcp/1xn_vmcp-0.5.1-py3-none-any.whl/vmcp/public/frontend/assets/check-CFHtpoo7.js
@@ -0,0 +1,2 @@
+import{c}from"./index-pMK5HuNj.js";const e=[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]],t=c("check",e);export{t as C};
+//# sourceMappingURL=check-CFHtpoo7.js.map
---
+++ packages/1xn-vmcp/1xn_vmcp-0.5.1-py3-none-any.whl/vmcp/public/frontend/assets/clock-BQJPWFP7.js
@@ -0,0 +1,2 @@
+import{c}from"./index-pMK5HuNj.js";const o=[["path",{d:"M12 6v6l4 2",key:"mmk7yg"}],["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}]],r=c("clock",o);export{r as C};
+//# sourceMappingURL=clock-BQJPWFP7.js.map
