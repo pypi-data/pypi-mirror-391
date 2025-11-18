0a8abe6b39ca5e0ebbb8327e5cfd08f4d2ac9205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 03:29:16 +0000

    Add package safe-eth-py/7.16.0/safe_eth_py-7.16.0.tar.gz

---
+++ packages/safe-eth-py/safe_eth_py-7.16.0.tar.gz/safe_eth_py-7.16.0/safe_eth/eth/tests/account_abstraction/test_user_operation.py
@@ -0,0 +1,37 @@
+from unittest import TestCase
+
+from safe_eth.util.util import to_0x_hex_str
+
+from ...account_abstraction import UserOperation
+from ...account_abstraction.user_operation import UserOperationV07
+from ..mocks.mock_bundler import (
+    safe_4337_chain_id_mock,
+    safe_4337_user_operation_hash_mock,
+    user_operation_mock,
+    user_operation_v07_hash,
+    user_operation_v07_mock,
+)
+
+
+class TestUserOperation(TestCase):
+    def test_calculate_user_operation_hash_V06(self):
+        user_operation_hash = safe_4337_user_operation_hash_mock
+        user_operation = UserOperation.from_bundler_response(
+            to_0x_hex_str(user_operation_hash), user_operation_mock["result"]
+        )
+        self.assertIsInstance(user_operation, UserOperation)
+        self.assertEqual(
+            user_operation.calculate_user_operation_hash(safe_4337_chain_id_mock),
+            user_operation_hash,
+        )
+
+    def test_calculate_user_operation_hash_V07(self):
+        user_operation_hash = user_operation_v07_hash
+        user_operation = UserOperation.from_bundler_response(
+            to_0x_hex_str(user_operation_hash), user_operation_v07_mock["result"]
+        )
+        self.assertIsInstance(user_operation, UserOperationV07)
+        self.assertEqual(
+            user_operation.calculate_user_operation_hash(safe_4337_chain_id_mock),
+            user_operation_hash,
+        )
