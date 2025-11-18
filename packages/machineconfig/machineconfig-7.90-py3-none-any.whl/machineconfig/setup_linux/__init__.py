04f48ceec872160bfa00be9ba2a17fbe0ad82f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 09:08:25 +0000

    Add package machineconfig/7.90/machineconfig-7.90-py3-none-any.whl

---
+++ packages/machineconfig/machineconfig-7.90-py3-none-any.whl/machineconfig/setup_linux/__init__.py
@@ -0,0 +1,10 @@
+
+from pathlib import Path
+
+INTERACTIVE = Path(__file__).parent.joinpath("web_shortcuts/interactive.sh")
+UV = Path(__file__).parent.joinpath("uv.sh")
+
+APPS_DESKTOP = Path(__file__).parent.joinpath("apps_desktop.sh")
+APPS_GUI = Path(__file__).parent.joinpath("apps_gui.sh")
+
+SSH_SERVER = Path(__file__).parent.joinpath("ssh/openssh_server.sh")
