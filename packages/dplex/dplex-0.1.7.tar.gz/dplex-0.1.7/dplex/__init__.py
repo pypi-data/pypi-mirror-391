5a0b41ae5512f176634886c221fa9cb772b36fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 12:07:00 +0000

    Add package dplex/0.1.7/dplex-0.1.7.tar.gz

---
+++ packages/dplex/dplex-0.1.7.tar.gz/dplex-0.1.7/dplex/__init__.py
@@ -0,0 +1,54 @@
+"""
+dplex - Enterprise-grade data layer framework for Python
+"""
+
+__version__ = "0.1.0"
+
+__all__ = [
+    # Filters
+    "StringFilter",
+    "IntFilter",
+    "FloatFilter",
+    "DecimalFilter",
+    "BooleanFilter",
+    "DateFilter",
+    "DateTimeFilter",
+    "TimeFilter",
+    "TimestampFilter",
+    "EnumFilter",
+    "UUIDFilter",
+    # Core classes
+    "DPRepo",
+    "DPService",
+    "DPFilters",
+    # Sort
+    "Sort",
+    "Order",
+    "NullsPlacement",
+]
+
+
+from dplex.internal.sort import NullsPlacement, Order, Sort
+
+
+from dplex.internal.filters import (
+    StringFilter,
+    IntFilter,
+    FloatFilter,
+    DecimalFilter,
+    BooleanFilter,
+    DateFilter,
+    DateTimeFilter,
+    TimeFilter,
+    TimestampFilter,
+    EnumFilter,
+    UUIDFilter,
+)
+
+
+from dplex.dp_filters import DPFilters
+
+from dplex.dp_repo import DPRepo
+
+
+from dplex.dp_service import DPService
