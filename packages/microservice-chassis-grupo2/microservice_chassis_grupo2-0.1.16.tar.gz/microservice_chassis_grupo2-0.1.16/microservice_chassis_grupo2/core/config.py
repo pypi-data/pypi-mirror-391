8038a3074589ce210cc03f78fe68ead31820eda2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 08:07:04 +0000

    Add package microservice-chassis-grupo2/0.1.16/microservice_chassis_grupo2-0.1.16.tar.gz

---
+++ packages/microservice-chassis-grupo2/microservice_chassis_grupo2-0.1.16.tar.gz/microservice_chassis_grupo2-0.1.16/microservice_chassis_grupo2/core/config.py
@@ -0,0 +1,11 @@
+import os
+
+class Settings():
+    ALGORITHM: str = "RS256"
+    RABBITMQ_HOST = f"amqp://{os.getenv("RABBITMQ_USER", "guest")}:{os.getenv("RABBITMQ_PASSWORD", "guest")}@{os.getenv("RABBITMQ_HOST", "localhost")}/"
+    EXCHANGE_NAME = "broker"
+    EXCHANGE_NAME_COMMAND = "command"
+    EXCHANGE_NAME_SAGA = "saga"
+    EXCHANGE_NAME_LOGS = "logs"
+
+settings = Settings()
