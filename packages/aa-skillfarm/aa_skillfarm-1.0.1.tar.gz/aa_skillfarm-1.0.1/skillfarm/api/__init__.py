296ab67ce274a37de58e9b5dc1d6a62a0f629a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 08:07:53 +0000

    Add package aa-skillfarm/1.0.1/aa_skillfarm-1.0.1.tar.gz

---
+++ packages/aa-skillfarm/aa_skillfarm-1.0.1.tar.gz/aa_skillfarm-1.0.1/skillfarm/api/__init__.py
@@ -0,0 +1,26 @@
+# Third Party
+from ninja import NinjaAPI
+from ninja.security import django_auth
+
+# Django
+from django.conf import settings
+
+# AA Skillfarm
+from skillfarm.api import skillfarm
+
+api = NinjaAPI(
+    title="AA Skillfarm API",
+    version="0.5.0",
+    urls_namespace="skillfarm:api",
+    auth=django_auth,
+    csrf=True,
+    openapi_url=settings.DEBUG and "/openapi.json" or "",
+)
+
+
+def setup(ninja_api):
+    skillfarm.SkillFarmApiEndpoints(ninja_api)
+
+
+# Initialize API endpoints
+setup(api)
