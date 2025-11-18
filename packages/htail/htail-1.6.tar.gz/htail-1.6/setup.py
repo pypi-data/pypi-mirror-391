2812f8422665e5f07d50be325d388cb145206b16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 06:14:49 +0000

    Add package htail/1.6/htail-1.6.tar.gz

---
+++ packages/htail/htail-1.6.tar.gz/htail-1.6/setup.py
@@ -0,0 +1,52 @@
+# htail - tail over HTTP
+# Copyright (C) 2013  Vincent Pelletier <plr.vincent@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+from setuptools import setup
+from os.path import join, dirname
+import sys
+
+description = open(join(dirname(__file__), 'README.rst')).read()
+setup(
+    name='htail',
+    version='1.6',
+    author='Vincent Pelletier',
+    author_email='plr.vincent@gmail.com',
+    description=next(x for x in description.splitlines() if x.strip()),
+    long_description=description,
+    long_description_content_type='text/x-rst',
+    url='http://github.com/vpelletier/htail',
+    license='GPL-2.0-or-later',
+    platforms=['any'],
+    classifiers=[
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Information Technology',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Utilities',
+    ],
+    py_modules=['htail'],
+    entry_points = {
+        'console_scripts': [
+            'htail=htail:main',
+        ],
+    },
+    zip_safe=True,
+)
