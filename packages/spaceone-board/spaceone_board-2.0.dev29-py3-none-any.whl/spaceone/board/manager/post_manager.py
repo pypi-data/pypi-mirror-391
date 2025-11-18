9764125638b47abc7676fc41f56437c8f38f70a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 11:07:09 +0000

    Add package spaceone-board/2.0.dev29/spaceone_board-2.0.dev29-py3-none-any.whl

---
+++ packages/spaceone-board/spaceone_board-2.0.dev29-py3-none-any.whl/spaceone/board/manager/post_manager.py
@@ -0,0 +1,65 @@
+import logging
+from typing import Union
+
+from spaceone.core.manager import BaseManager
+from spaceone.board.model.post_model import Post
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class PostManager(BaseManager):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.post_model: Post = self.locator.get_model(Post)
+
+    def create_post(self, params: dict) -> Post:
+        def _rollback(vo: Post):
+            _LOGGER.info(f"[create_post._rollback] " f"Delete post : {vo.post_id}")
+            vo.delete()
+
+        post_vo: Post = self.post_model.create(params)
+        self.transaction.add_rollback(_rollback, post_vo)
+
+        return post_vo
+
+    def update_post(self, params: dict) -> Post:
+        post_vo: Post = self.get_post(params["board_id"], params["post_id"])
+        return self.update_post_by_vo(params, post_vo)
+
+    def update_post_by_vo(self, params: dict, post_vo: Post) -> Post:
+        def _rollback(old_data: dict):
+            _LOGGER.info(
+                f"[update_post_by_vo._rollback] Revert Data : " f'{old_data["post_id"]}'
+            )
+            post_vo.update(old_data)
+
+        self.transaction.add_rollback(_rollback, post_vo.to_dict())
+
+        return post_vo.update(params)
+
+    def get_post(self, post_id: str, domain_id: str = None, workspace_id: list = None) -> Post:
+        conditions = {"post_id": post_id}
+
+        if domain_id:
+            conditions["domain_id"] = domain_id
+
+        if workspace_id:
+            conditions["workspaces"] = workspace_id
+
+        return self.post_model.get(**conditions)
+
+    def list_boards(self, query: Union[dict, None] = None):
+        if query is None:
+            query = {}
+        return self.post_model.query(**query)
+
+    def stat_boards(self, query: dict) -> dict:
+        return self.post_model.stat(**query)
+
+    @staticmethod
+    def increase_view_count(post_vo: Post) -> None:
+        post_vo.increment("view_count")
+
+    @staticmethod
+    def delete_post_vo(post_vo: Post) -> None:
+        post_vo.delete()
---
+++ packages/spaceone-board/spaceone_board-2.0.dev29-py3-none-any.whl/spaceone/board/model/post_model.py
@@ -0,0 +1,60 @@
+from tokenize import String
+from mongoengine import *
+
+from spaceone.core.model.mongo_model import MongoModel
+
+
+class Post(MongoModel):
+    board_type = StringField(max_length=40)
+    post_id = StringField(max_length=40, generate_id="post", unique=True)
+    category = StringField(null=True, default=None)
+    title = StringField(max_length=255)
+    contents = StringField()
+    contents_type = StringField(max_length=40, default="markdown")
+    options = DictField(default={})
+    view_count = IntField(default=0)
+    writer = StringField()
+    files = ListField(StringField(), default=[])
+    resource_group = StringField(max_length=40, choices=("SYSTEM", "DOMAIN", "WORKSPACE"))
+    domain_id = StringField(max_length=40)
+    workspaces = ListField(StringField(), default=None)
+    user_id = StringField(max_length=40)
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+
+    meta = {
+        "updatable_fields": [
+            "category",
+            "title",
+            "contents",
+            "contents_type",
+            "options",
+            "writer",
+            "files",
+            "workspaces",
+        ],
+        "minimal_fields": [
+            "board_type",
+            "post_id",
+            "category",
+            "title",
+            "resource_group",
+            "domain_id",
+            "workspaces",
+        ],
+        "ordering": ["-created_at"],
+        "change_query_keys": {
+            "is_pinned": "options.is_pinned",
+            "is_popup": "options.is_popup",
+            "workspace_id": "workspaces",
+        },
+        "indexes": [
+            "board_type",
+            "category",
+            "writer",
+            "resource_group",
+            "domain_id",
+            "workspaces",
+            "user_id",
+        ],
+    }
