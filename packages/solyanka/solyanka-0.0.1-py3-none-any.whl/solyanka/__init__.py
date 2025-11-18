7e8c2fb13f8ab8c15a7c16c7eb9abd0e55aabb4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 08:08:04 +0000

    Add package solyanka/0.0.1/solyanka-0.0.1-py3-none-any.whl

---
+++ packages/solyanka/solyanka-0.0.1-py3-none-any.whl/solyanka/__init__.py
@@ -0,0 +1,6 @@
+"""Solyanka – lightweight helpers for statement generation tooling."""
+
+from .transaction_patterns.service import EEA_COUNTRIES, Pattern, PatternsService
+
+__all__ = ["EEA_COUNTRIES", "Pattern", "PatternsService"]
+__version__ = "0.1.0"
