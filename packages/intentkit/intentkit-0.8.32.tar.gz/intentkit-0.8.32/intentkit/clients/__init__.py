9c9a1125451b72036708abc8576f76c2623dc441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 11:07:27 +0000

    Add package intentkit/0.8.32/intentkit-0.8.32.tar.gz

---
+++ packages/intentkit/intentkit-0.8.32.tar.gz/intentkit-0.8.32/intentkit/clients/__init__.py
@@ -0,0 +1,17 @@
+from intentkit.clients.cdp import get_cdp_client, get_evm_account, get_wallet_provider
+from intentkit.clients.twitter import (
+    TwitterClient,
+    TwitterClientConfig,
+    get_twitter_client,
+)
+from intentkit.clients.web3 import get_web3_client
+
+__all__ = [
+    "TwitterClient",
+    "TwitterClientConfig",
+    "get_twitter_client",
+    "get_evm_account",
+    "get_cdp_client",
+    "get_wallet_provider",
+    "get_web3_client",
+]
