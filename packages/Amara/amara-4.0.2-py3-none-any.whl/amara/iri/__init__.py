371034f4e1b032eaf41dd4212a4e91d28366945b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 11:07:09 +0000

    Add package Amara/4.0.2/amara-4.0.2-py3-none-any.whl

---
+++ packages/Amara/amara-4.0.2-py3-none-any.whl/amara/iri/__init__.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2023-present Oori Data <info@oori.dev>
+#
+# SPDX-License-Identifier: Apache-2.0
+# amara
+
+# ruff: noqa: F401,F403
+
+# Expose main public API
+from amara.iri.iri import *
+from amara.iri.irihelper import *
