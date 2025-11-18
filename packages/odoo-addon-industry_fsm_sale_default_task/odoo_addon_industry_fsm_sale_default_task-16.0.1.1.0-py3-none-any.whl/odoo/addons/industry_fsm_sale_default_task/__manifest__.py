7c50dfd16b11d29c2e4658e904b72de987031f61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 12:07:06 +0000

    Add package odoo-addon-industry_fsm_sale_default_task/16.0.1.1.0/odoo_addon_industry_fsm_sale_default_task-16.0.1.1.0-py3-none-any.whl

---
+++ packages/odoo-addon-industry_fsm_sale_default_task/odoo_addon_industry_fsm_sale_default_task-16.0.1.1.0-py3-none-any.whl/odoo/addons/industry_fsm_sale_default_task/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Industry FSM Sale Default Task",
+    "summary": """
+        Additional sale order lines will be linked with field service project task.
+    """,
+    "author": "Mint System GmbH",
+    "website": "https://www.mint-system.ch/",
+    "category": "Services",
+    "version": "16.0.1.1.0",
+    "license": "OPL-1",
+    "depends": ["industry_fsm_sale"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
