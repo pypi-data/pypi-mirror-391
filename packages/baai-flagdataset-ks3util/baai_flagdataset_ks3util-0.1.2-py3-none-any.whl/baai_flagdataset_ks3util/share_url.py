83325e4974cca045cfba68e766bae99ddc43349d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 12:06:35 +0000

    Add package baai-flagdataset-ks3util/0.1.2/baai_flagdataset_ks3util-0.1.2-py3-none-any.whl

---
+++ packages/baai-flagdataset-ks3util/baai_flagdataset_ks3util-0.1.2-py3-none-any.whl/baai_flagdataset_ks3util/share_url.py
@@ -0,0 +1,44 @@
+import subprocess
+
+from .baai_prepare import ks3util
+
+
+def _get_share_url(output: str):
+    lines = output.split("\n")
+    for line in lines:
+        if line.startswith("http"):
+            return line
+    return None
+
+
+def get_share_url(dir_path, ak=None, sk=None):
+    import random
+
+
+    # 生产6位随机数
+    access_code = f"{random.randint(100000, 999999)}"
+
+    cmd_args = [
+        ks3util(),
+        "share-create",
+        "--access-code",
+        access_code,
+        "--valid-period",
+        "30d",
+        dir_path,
+    ]
+
+    if ak and sk:
+        cmd_args.append("-i")
+        cmd_args.append(ak)
+        cmd_args.append("-k")
+        cmd_args.append(sk)
+
+    result = subprocess.run(
+        cmd_args,
+        capture_output=True,
+        text=True,
+        encoding="utf-8"
+    )
+    return _get_share_url(result.stdout), access_code
+
