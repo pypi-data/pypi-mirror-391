23a354efdc45babb0462dba18b5c0c37d8b4de47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 09:08:45 +0000

    Add package age-mcp-server/0.2.32/age_mcp_server-0.2.32.tar.gz

---
+++ packages/age-mcp-server/age_mcp_server-0.2.32.tar.gz/age_mcp_server-0.2.32/age-mcp-server.rb
@@ -0,0 +1,30 @@
+class AgeMcpServer < Formula
+  include Language::Python::Virtualenv
+
+  desc "Apache AGE MCP Server"
+  homepage "https://github.com/rioriost/homebrew-age-mcp-server/"
+  url "https://files.pythonhosted.org/packages/a5/56/6de669302bec9bbd7bb91fd7cbf89bd4a03ab526dbc44dcd87cc9e4b2d0e/age_mcp_server-0.2.31.tar.gz"
+  sha256 "853374f1f73bfbc3df7078119776003b30344cc11422ac1e0fa827756569861b"
+  license "MIT"
+
+  depends_on "python@3.13"
+
+  resource "agefreighter" do
+    url "https://files.pythonhosted.org/packages/df/3d/5f701f1135cef1ced759c319d6f5c0454c379e5e427d4067e20e03017be9/agefreighter-1.0.20.tar.gz"
+    sha256 "1a7994b684fe3e91727e632c1883751646bb157b02ff3047f6f73e68912cafd5"
+  end
+
+  resource "ply" do
+    url "https://files.pythonhosted.org/packages/e5/69/882ee5c9d017149285cab114ebeab373308ef0f874fcdac9beb90e0ac4da/ply-3.11.tar.gz"
+    sha256 "00c7c1aaa88358b9c765b6d3000c6eec0ba42abca5351b095321aef446081da3"
+  end
+
+  def install
+    virtualenv_install_with_resources
+    system libexec/"bin/python", "-m", "pip", "install", "psycopg[binary,pool]", "mcp"
+  end
+
+  test do
+    system "#{bin}/age-mcp-server", "--help"
+  end
+end
