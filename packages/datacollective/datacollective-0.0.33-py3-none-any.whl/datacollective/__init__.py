a0efb1358ec66ad18b8cfce7fa9423bbda1cfd55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 04:08:35 +0000

    Add package datacollective/0.0.33/datacollective-0.0.33-py3-none-any.whl

---
+++ packages/datacollective/datacollective-0.0.33-py3-none-any.whl/datacollective/__init__.py
@@ -0,0 +1,13 @@
+"""
+Mozilla Data Collective Python Client Library
+
+Usage:
+    from datacollective import DataCollective
+    client = DataCollective()
+"""
+
+from .client import DataCollective
+
+__all__ = ["DataCollective"]
+
+__version__ = "0.0.33"
