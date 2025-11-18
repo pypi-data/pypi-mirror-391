7eae3f4aa7487b25a6ac9e411c659f71ebee65a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 08:08:15 +0000

    Add package perpetual-context/2.0.2/perpetual_context-2.0.2.tar.gz

---
+++ packages/perpetual-context/perpetual_context-2.0.2.tar.gz/perpetual_context-2.0.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'perpetual_context',
+    version = '2.0.2',
+    author = 'OPENSAPI',
+    packages=find_packages(),
+    install_requires=[
+        'count-tokens',
+        'requests',
+        'numpy',
+        'openpyxl',
+        'pandas',
+        'statistics',
+        'certifi',
+        'tabulate',
+        'PyPDF2',
+        'PyMuPDF',
+        'python-docx',
+        'python-pptx',
+        'beautifulsoup4',
+        'youtube-search-python',
+        'youtube-transcript-api',
+        'pillow',
+        'easyocr',
+        'torch',
+        'torchvision',
+        'webcolors',
+        'scikit-learn',
+        'pydub',
+       ' SpeechRecognition'
+    ],
+    url = 'https://github.com/sapiens-technology/PerpetualContext',
+    license = 'Proprietary Software'
+)
