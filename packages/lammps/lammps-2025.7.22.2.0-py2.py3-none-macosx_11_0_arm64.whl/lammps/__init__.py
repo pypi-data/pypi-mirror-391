64887d98dc159444db6ef63e5c1ba5682a663a6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 12:07:09 +0000

    Add package lammps/2025.7.22.2.0/lammps-2025.7.22.2.0-py2.py3-none-macosx_11_0_arm64.whl

---
+++ packages/lammps/lammps-2025.7.22.2.0-py2.py3-none-macosx_11_0_arm64.whl/lammps/__init__.py
@@ -0,0 +1,15 @@
+"""
+LAMMPS module global members:
+.. data:: __version__
+   Numerical representation of the LAMMPS version this
+   module was taken from.  Has the same format as the
+   result of :py:func:`lammps.version`.
+"""
+import lammps.executable
+
+from .constants import *                # lgtm [py/polluting-import]
+from .core import *                     # lgtm [py/polluting-import]
+from .data import *                     # lgtm [py/polluting-import]
+from .pylammps import *                 # lgtm [py/polluting-import]
+
+__version__ = 20250722
