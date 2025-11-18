5c575d8a10ebea0b9a550250ee29e7c3a4a96a32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 09:07:06 +0000

    Add package elfhub/0.0.1/elfhub-0.0.1.tar.gz

---
+++ packages/elfhub/elfhub-0.0.1.tar.gz/elfhub-0.0.1/README.md
@@ -0,0 +1,1 @@
+# elfhub-python
---
+++ packages/elfhub/elfhub-0.0.1.tar.gz/elfhub-0.0.1/src/elfhub/__init__.py
@@ -0,0 +1,6 @@
+# Set the version of your package
+__version__ = "0.0.1"
+
+# Expose the function for easy import
+# e.g., from my_awesome_package import say_hello
+from .base import greet
