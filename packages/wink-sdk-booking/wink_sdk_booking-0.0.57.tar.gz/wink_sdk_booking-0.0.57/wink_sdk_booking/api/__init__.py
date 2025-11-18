9981f568a1886d5a144ebc6b8926cb9ea1a13a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 11:07:11 +0000

    Add package wink-sdk-booking/0.0.57/wink_sdk_booking-0.0.57.tar.gz

---
+++ packages/wink-sdk-booking/wink_sdk_booking-0.0.57.tar.gz/wink_sdk_booking-0.0.57/wink_sdk_booking/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from wink_sdk_booking.api.checkout_api import CheckoutApi
+from wink_sdk_booking.api.consumer_booking_api import ConsumerBookingApi
+from wink_sdk_booking.api.review_api import ReviewApi
+from wink_sdk_booking.api.shopping_cart_api import ShoppingCartApi
+
