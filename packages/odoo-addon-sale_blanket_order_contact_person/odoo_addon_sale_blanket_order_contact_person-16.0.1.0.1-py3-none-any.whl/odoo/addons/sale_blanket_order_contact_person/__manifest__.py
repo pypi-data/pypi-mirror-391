f10432643dc63fdfce2a142ca814471b81780841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 12:07:06 +0000

    Add package odoo-addon-sale_blanket_order_contact_person/16.0.1.0.1/odoo_addon_sale_blanket_order_contact_person-16.0.1.0.1-py3-none-any.whl

---
+++ packages/odoo-addon-sale_blanket_order_contact_person/odoo_addon_sale_blanket_order_contact_person-16.0.1.0.1-py3-none-any.whl/odoo/addons/sale_blanket_order_contact_person/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Sale Blanket Order Contact Person",
+    "summary": """
+        Set contact person on blanket order.
+    """,
+    "author": "Mint System GmbH",
+    "website": "https://www.mint-system.ch/",
+    "category": "Sale",
+    "version": "16.0.1.0.1",
+    "license": "AGPL-3",
+    "depends": ["sale_blanket_order", "sale_order_sale_partner"],
+    "data": ["views/sale_blanket_order.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
