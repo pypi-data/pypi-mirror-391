1f8f10adf1550a131041d7305d0e2613f26b9b13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 04:08:29 +0000

    Add package json_2_postgres/0.2.6/json_2_postgres-0.2.6-py3-none-any.whl

---
+++ packages/json_2_postgres/json_2_postgres-0.2.6-py3-none-any.whl/json_2_postgres/__init__.py
@@ -0,0 +1,9 @@
+from .table_to_dict import export_table_to_dict, import_table_from_dict
+from .table_to_json import dict_as_json, json_to_table
+
+__all__ = [
+    "export_table_to_dict",
+    "import_table_from_dict",
+    "dict_as_json",
+    "json_to_table",
+]
