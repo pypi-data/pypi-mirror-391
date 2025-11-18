139ca631d3ad1bf99c7da0fbfe7da20431deeba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 08:06:42 +0000

    Add package pyanglianwater/2.2.0/pyanglianwater-2.2.0.tar.gz

---
+++ packages/pyanglianwater/pyanglianwater-2.2.0.tar.gz/pyanglianwater-2.2.0/pyanglianwater/enum.py
@@ -0,0 +1,9 @@
+"""Anglian Water enums."""
+
+from enum import StrEnum
+
+class UsagesReadGranularity(StrEnum):
+    """Usages Read Granularity."""
+    MONTHLY = "30"
+    DAILY = "20"
+    HOURLY = "10"
