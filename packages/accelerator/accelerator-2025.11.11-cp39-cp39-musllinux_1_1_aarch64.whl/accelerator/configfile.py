8508522e4c4fd4f73fb7444617de71da110fcc61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 03:28:27 +0000

    Add package accelerator/2025.11.11/accelerator-2025.11.11-cp39-cp39-musllinux_1_1_aarch64.whl

---
+++ packages/accelerator/accelerator-2025.11.11-cp39-cp39-musllinux_1_1_aarch64.whl/accelerator/configfile.py
@@ -0,0 +1,263 @@
+# -*- coding: utf-8 -*-
+############################################################################
+#                                                                          #
+# Copyright (c) 2017 eBay Inc.                                             #
+# Modifications copyright (c) 2019-2021 Anders Berkeman                    #
+# Modifications copyright (c) 2019-2024 Carl Drougge                       #
+#                                                                          #
+# Licensed under the Apache License, Version 2.0 (the "License");          #
+# you may not use this file except in compliance with the License.         #
+# You may obtain a copy of the License at                                  #
+#                                                                          #
+#  http://www.apache.org/licenses/LICENSE-2.0                              #
+#                                                                          #
+# Unless required by applicable law or agreed to in writing, software      #
+# distributed under the License is distributed on an "AS IS" BASIS,        #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+# See the License for the specific language governing permissions and      #
+# limitations under the License.                                           #
+#                                                                          #
+############################################################################
+
+from __future__ import print_function
+from __future__ import division
+
+import re
+import os
+import shlex
+
+from accelerator.compat import url_quote_more, open
+
+from accelerator.extras import DotDict
+
+
+_re_var = re.compile(r'(?<!\\)\$\{([^\}=]*)(?:=([^\}]*))?\}')
+def interpolate(s):
+	"""Replace ${FOO=BAR} with os.environ.get('FOO', 'BAR')
+	(just ${FOO} is of course also supported, but not $FOO)"""
+	return _re_var.subn(lambda m: os.environ.get(m.group(1), m.group(2)), s)[0]
+
+
+class HostPortTuple(tuple):
+	__slots__ = ()
+	def __str__(self):
+		return "http://%s:%d" % self
+
+def resolve_listen(listen):
+	if listen.startswith('http://'):
+		tmp = listen[7:].rstrip('/')
+		if '/' not in tmp and ':' in tmp:
+			listen = tmp
+	if '/' not in listen and ':' in listen:
+		hostname, port = listen.rsplit(':', 1)
+		listen = HostPortTuple((hostname or 'localhost', int(port),))
+		url = 'http://%s:%d' % listen
+	else:
+		url = None
+	return listen, url
+
+def fixup_listen(project_directory, listen):
+	listen, url = listen
+	if not isinstance(listen, tuple):
+		socket = os.path.join(project_directory, listen)
+		url = 'unixhttp://' + url_quote_more(os.path.realpath(socket))
+	return listen, url
+
+
+def load_config(filename):
+	from accelerator.error import UserError
+
+	multivalued = {'workdirs', 'method packages', 'interpreters'}
+	required = {'slices', 'workdirs', 'method packages'}
+	known = {'target workdir', 'listen', 'urd', 'board listen', 'result directory', 'input directory', 'project directory'} | required | multivalued
+	cfg = {key: [] for key in multivalued}
+
+	def fixpath(fn, realpath=True):
+		# convert relative path to absolute wrt location of config file
+		p = os.path.join(project_directory, fn)
+		if realpath:
+			p = os.path.realpath(p)
+		else:
+			p = os.path.normpath(p)
+		return p
+
+	class _E(Exception):
+		__slots__ = ()
+	def parse_package(val):
+		if len(val) == 2:
+			if val[1] != 'auto-discover':
+				raise _E('Either no option or "auto-discover" for package %r.' % (val[0],))
+			else:
+				return (val[0], True)
+		return (val[0], False)
+	def parse_workdir(val):
+		return val[0], fixpath(val[1])
+	def parse_interpreter(val):
+		return val[0], fixpath(val[1], False)
+	def check_interpreter(val):
+		if val[0] == 'DEFAULT':
+			raise _E("Don't override DEFAULT interpreter")
+		if not os.path.isfile(val[1]):
+			raise _E('%r does not exist' % (val,))
+	def check_workdirs(val):
+		name, path = val
+		if val in cfg['workdirs']:
+			# The exact same thing was repeated.
+			# This is allowed, because you want to be able to have something like
+			#    ${USER} /workdirs/${USER}
+			#    foo     /workdirs/foo
+			#    bar     /workdirs/bar
+			# in your config, and have it still work for the foo and bar users.
+			return
+		if name in (v[0] for v in cfg['workdirs']):
+			raise _E('Workdir %s redefined' % (name,))
+		if path in (v[1] for v in cfg['workdirs']):
+			raise _E('Workdir path %r re-used' % (path,))
+		if project_directory == path or project_directory.startswith(path + '/'):
+			raise _E('project directory (%r) is under workdir %s (%r)' % (project_directory, name, path))
+
+	def resolve_urd(val):
+		orig_val = val
+		is_local = (val[0] == 'local')
+		if val[0] in ('local', 'remote'):
+			if len(val) == 1:
+				val = ['.socket.dir/urd']
+			else:
+				val = [val[1]]
+		if len(val) != 1:
+			raise _E("urd takes 1 or 2 values (expected %s, got %r)" % (' '.join(parsers['urd'][0]), orig_val))
+		return is_local, resolve_listen(val[0])
+
+	parsers = {
+		'slices': (['count'], int),
+		'workdirs': (['name', 'path'], parse_workdir),
+		'interpreters': (['name', 'path'], parse_interpreter),
+		'listen': (['path or [host]:port'], resolve_listen),
+		'urd': (['["local" or "remote"] and/or', '[path or [host]:port]'], resolve_urd), # special cased
+		'board listen': (['path or [host]:port'], resolve_listen),
+		'input directory': (['path'], fixpath),
+		'result directory': (['path'], fixpath),
+		'method packages': (['package', '[auto-discover]'], parse_package),
+	}
+	checkers = dict(
+		interpreter=check_interpreter,
+		workdirs=check_workdirs,
+	)
+
+	with open(filename, 'r', encoding='utf-8') as fh:
+		lines = list(enumerate(fh, 1))
+	def parse(handle):
+		key = None
+		for n, line in lines:
+			lineno[0] = n
+			line_stripped = line.strip()
+			if not line_stripped or line_stripped[0] == '#':
+				continue
+			if line == line.lstrip():
+				if ':' not in line:
+					raise _E('Expected a ":"')
+				key, val = line.split(':', 1)
+				if key not in known:
+					raise _E('Unknown key %r' % (key,))
+			else:
+				if not key:
+					raise _E('First line indented')
+				val = line
+			val = shlex.split(interpolate(val), posix=True, comments=True)
+			if val:
+				handle(key, val)
+	def just_project_directory(key, val):
+		if key == 'project directory':
+			if len(val) != 1:
+				raise _E("%s takes a single value path (maybe you meant to quote it?)" % (key,))
+			project_directory[0] = val[0]
+	def everything(key, val):
+		if key in parsers:
+			args, p = parsers[key]
+			if key in ('urd', 'method packages'):
+				want_count = (1, 2)
+				want_count_str = "1 or 2"
+			else:
+				want_count = [len(args)]
+				want_count_str = str(want_count[0])
+			if len(val) not in want_count:
+				if len(args) == 1:
+					raise _E("%s takes a single value %s (maybe you meant to quote it?)" % (key, args[0]))
+				else:
+					raise _E("%s takes %s values (expected %s, got %r)" % (key, want_count_str, ' '.join(args), val))
+			if len(args) == 1:
+				val = val[0]
+			val = p(val)
+		elif len(val) == 1:
+			val = val[0]
+		else:
+			raise _E("%s takes a single value (maybe you meant to quote it?)" % (key,))
+		if key in checkers:
+			checkers[key](val)
+		if key in multivalued:
+			cfg[key].append(val)
+		else:
+			if key in cfg:
+				raise _E("%r doesn't take multiple values" % (key,))
+			cfg[key] = val
+
+	try:
+		project_directory = [os.path.dirname(filename)]
+		lineno = [None]
+		parse(just_project_directory)
+		lineno = [None]
+		project_directory = os.path.realpath(project_directory[0])
+		parse(everything)
+		lineno = [None]
+
+		missing = set()
+		for req in required:
+			if not cfg[req]:
+				missing.add(req)
+		if missing:
+			raise _E('Missing required keys %r' % (missing,))
+
+		# Reformat result a bit so the new format doesn't require code changes all over the place.
+		rename = {
+			'target workdir': 'target_workdir',
+			'method packages': 'method_directories',
+			'input directory': 'input_directory',
+			'result directory': 'result_directory',
+			'project directory': 'project_directory',
+			'board listen': 'board_listen',
+		}
+		res = DotDict({rename.get(k, k): v for k, v in cfg.items()})
+		if 'listen' not in res:
+			res.listen = '.socket.dir/server', None
+		if 'target_workdir' not in res:
+			res.target_workdir = res.workdirs[0][0]
+		if 'project_directory' not in res:
+			res.project_directory = os.path.dirname(filename)
+		res.project_directory = os.path.realpath(res.project_directory)
+		if 'input_directory' not in res:
+			res.input_directory = fixpath('')
+		if 'result_directory' not in res:
+			res.result_directory = fixpath('')
+		res.workdirs = dict(res.workdirs)
+		if res.target_workdir not in res.workdirs:
+			raise _E('target workdir %r not in defined workdirs %r' % (res.target_workdir, set(res.workdirs),))
+		res.interpreters = dict(res.interpreters)
+		for exe in res.interpreters.values():
+			assert os.path.exists(exe), 'Executable %r does not exist.' % (exe,)
+		res.listen, res.url = fixup_listen(res.project_directory, res.listen)
+		if res.get('urd'):
+			res.urd_local, listen = res.urd
+			res.urd_listen, res.urd = fixup_listen(res.project_directory, listen)
+		else:
+			res.urd_local, res.urd_listen, res.urd = False, None, None
+		res.board_listen, _ = fixup_listen(res.project_directory, res.get('board_listen', ('.socket.dir/board', None)))
+		res.method_directories = dict(res.method_directories)
+	except _E as e:
+		if lineno[0] is None:
+			prefix = 'Error in %s:\n' % (filename,)
+		else:
+			prefix = 'Error on line %d of %s:\n' % (lineno[0], filename,)
+		raise UserError(prefix + e.args[0])
+
+	res.config_filename = os.path.realpath(filename)
+	return res
---
+++ packages/accelerator/accelerator-2025.11.11-cp39-cp39-musllinux_1_1_aarch64.whl/accelerator/deptree.py
@@ -0,0 +1,223 @@
+# -*- coding: utf-8 -*-
+############################################################################
+#                                                                          #
+# Copyright (c) 2017 eBay Inc.                                             #
+# Modifications copyright (c) 2019-2024 Carl Drougge                       #
+# Modifications copyright (c) 2023 Pablo Correa Gómez                      #
+#                                                                          #
+# Licensed under the Apache License, Version 2.0 (the "License");          #
+# you may not use this file except in compliance with the License.         #
+# You may obtain a copy of the License at                                  #
+#                                                                          #
+#  http://www.apache.org/licenses/LICENSE-2.0                              #
+#                                                                          #
+# Unless required by applicable law or agreed to in writing, software      #
+# distributed under the License is distributed on an "AS IS" BASIS,        #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+# See the License for the specific language governing permissions and      #
+# limitations under the License.                                           #
+#                                                                          #
+############################################################################
+
+from __future__ import print_function
+from __future__ import division
+
+from traceback import print_exc
+from collections import OrderedDict
+from datetime import datetime, date, time, timedelta
+from pathlib import Path, PosixPath, PurePath, PurePosixPath
+import sys
+
+from accelerator.compat import iteritems, itervalues, first_value, str_types, int_types, num_types, unicode
+
+from accelerator.extras import OptionEnum, OptionEnumValue, _OptionString, OptionDefault, RequiredOption, typing_conv
+from accelerator.job import JobWithFile
+
+class OptionException(Exception):
+	__slots__ = ()
+	pass
+
+_date_types = (datetime, date, time, timedelta)
+
+class DepTree:
+
+	def __init__(self, methods, setup):
+		self.methods = methods
+		m = self.method = setup.method
+		self.params = {
+			'options': setup.options,
+			'datasets': setup.datasets,
+			'jobs': setup.jobs,
+		}
+		self.item = {
+			'method': m,
+			'params': {m: self.params},
+			'make': False,
+			'link': False,
+			'uid': 0,
+		}
+		self._fix_options(False)
+		self._fix_jobids('jobs')
+		self._fix_jobids('datasets')
+
+	def get_reqlist(self):
+		params = {k: dict(v) for k, v in iteritems(self.methods.params[self.method].defaults)}
+		for k, v in iteritems(self.params):
+			params[k].update(v)
+		return [(self.method, 0, self.methods.params2optset({self.method: params}))]
+
+	def fill_in_default_options(self):
+		self._fix_options(True)
+
+	def _fix_jobids(self, key):
+		method = self.method
+		data = self.params[key]
+		method_wants = self.methods.params[method][key]
+		res = {}
+		for jobid_name in method_wants:
+			if isinstance(jobid_name, str_types):
+				value = data.get(jobid_name)
+				assert value is None or isinstance(value, str), 'Input %s on %s not a string as required' % (jobid_name, method,)
+			elif isinstance(jobid_name, list):
+				if len(jobid_name) != 1 or not isinstance(jobid_name[0], str_types):
+					raise OptionException('Bad %s item on %s: %s' % (key, method, repr(jobid_name),))
+				jobid_name = jobid_name[0]
+				value = data.get(jobid_name)
+				if value:
+					if isinstance(value, str_types):
+						value = [e.strip() for e in value.split(',')]
+				else:
+					value = []
+				assert isinstance(value, list), 'Input %s on %s not a list or string as required' % (jobid_name, method,)
+			else:
+				raise OptionException('%s item of unknown type %s on %s: %s' % (key, type(jobid_name), method, repr(jobid_name),))
+			res[jobid_name] = value
+		self.params[key] = res
+		spill = set(data) - set(res)
+		if spill:
+			raise OptionException('Unknown %s on %s: %s' % (key, method, ', '.join(sorted(spill)),))
+
+	def _fix_options(self, fill_in):
+		method = self.method
+		options = self.methods.params[method].options
+		res_options = {}
+		def typefuzz(t):
+			if issubclass(t, str_types):
+				return str_types
+			if issubclass(t, int_types):
+				return int_types
+			return t
+		def convert(default_v, v):
+			if isinstance(default_v, RequiredOption):
+				if v is None and not default_v.none_ok:
+					raise OptionException('Option %s on method %s requires a non-None value (%r)' % (k, method, default_v.value,))
+				default_v = default_v.value
+			if default_v is None or v is None:
+				if isinstance(default_v, _OptionString):
+					raise OptionException('Option %s on method %s requires a non-empty string value' % (k, method,))
+				if hasattr(default_v, '_valid') and v not in default_v._valid:
+					raise OptionException('Option %s on method %s requires a value in %s' % (k, method, default_v._valid,))
+				if isinstance(default_v, OptionDefault):
+					v = default_v.default
+				return v
+			if isinstance(default_v, OptionDefault):
+				default_v = default_v.value
+			if isinstance(default_v, dict) and isinstance(v, dict):
+				if default_v:
+					sample_v = first_value(default_v)
+					for chk_v in itervalues(default_v):
+						assert isinstance(chk_v, type(sample_v))
+					assert isinstance(v, OrderedDict)
+					return OrderedDict((k, convert(sample_v, v)) for k, v in iteritems(v))
+				else:
+					return v
+			if isinstance(default_v, (list, set, tuple,)) and isinstance(v, str_types + (list, set, tuple,)):
+				if isinstance(v, str_types):
+					v = (e.strip() for e in v.split(','))
+				if default_v:
+					sample_v = first_value(default_v)
+					for chk_v in default_v:
+						assert isinstance(chk_v, type(sample_v))
+					v = (convert(sample_v, e) for e in v)
+				return type(default_v)(v)
+			if isinstance(default_v, (OptionEnum, OptionEnumValue,)):
+				if not (v or None) in default_v._valid:
+					ok = False
+					for cand_prefix in default_v._prefixes:
+						if v.startswith(cand_prefix):
+							ok = True
+							break
+					if not ok:
+						raise OptionException('%r not a permitted value for option %s on method %s (%s)' % (v, k, method, default_v._valid))
+				return v or None
+			if isinstance(default_v, str_types + num_types) and isinstance(v, str_types + num_types):
+				if isinstance(default_v, _OptionString):
+					v = str(v)
+					if not v:
+						raise OptionException('Option %s on method %s requires a non-empty string value' % (k, method,))
+					return v
+				if isinstance(default_v, unicode) and isinstance(v, bytes):
+					return v.decode('utf-8')
+				return type(default_v)(v)
+			if (isinstance(default_v, type) and isinstance(v, typefuzz(default_v))) or isinstance(v, typefuzz(type(default_v))):
+				return v
+			if isinstance(default_v, bool) and isinstance(v, (str, int)):
+				lv = str(v).lower()
+				if lv in ('true', '1', 't', 'yes', 'on',):
+					return True
+				if lv in ('false', '0', 'f', 'no', 'off', '',):
+					return False
+			if isinstance(default_v, _date_types):
+				default_v = type(default_v)
+			if isinstance(default_v, PosixPath) or default_v is PosixPath:
+				default_v = Path
+			if isinstance(default_v, PurePosixPath) or default_v is PurePosixPath:
+				default_v = PurePath
+			if default_v in _date_types + (Path, PurePath,):
+				try:
+					return typing_conv[default_v.__name__](v)
+				except Exception:
+					raise OptionException('Failed to convert option %s %r to %s on method %s' % (k, v, default_v, method,))
+			if isinstance(v, str_types) and not v:
+				return type(default_v)()
+			if isinstance(default_v, JobWithFile) or default_v is JobWithFile:
+				defaults = ('', '', False, None,)
+				if default_v is JobWithFile:
+					default_v = defaults
+				if not isinstance(v, (list, tuple,)) or not (2 <= len(v) <= 4):
+					raise OptionException('Option %s (%r) on method %s is not %s compatible' % (k, v, method, type(default_v)))
+				v = tuple(v) + defaults[len(v):] # so all of default_v gets convert()ed.
+				v = [convert(dv, vv) for dv, vv in zip(default_v, v)]
+				return JobWithFile(*v)
+			if type(default_v) != type:
+				default_v = type(default_v)
+			raise OptionException('Failed to convert option %s of %s to %s on method %s' % (k, type(v), default_v, method,))
+		for k, v in iteritems(self.params['options']):
+			if k in options:
+				try:
+					res_options[k] = convert(options[k], v)
+				except OptionException:
+					raise
+				except Exception:
+					print_exc(file=sys.stderr)
+					raise OptionException('Failed to convert option %s on method %s' % (k, method,))
+			else:
+				raise OptionException('Unknown option %s on method %s' % (k, method,))
+		if fill_in:
+			missing = set(options) - set(res_options)
+			missing_required = missing & self.methods.params[method].required
+			if missing_required:
+				raise OptionException('Missing required options {%s} on method %s' % (', '.join(sorted(missing_required)), method,))
+			defaults = self.methods.params[method].defaults
+			res_options.update({k: defaults.options[k] for k in missing})
+		self.params['options'] = res_options
+
+	def set_link(self, uid, job):
+		self.item['link'] = job.id
+		self.item['total_time'] = job.total
+
+	def propagate_make(self):
+		self.item['make'] = not self.item['link']
+
+	def get_sorted_joblist(self):
+		return [self.item]
