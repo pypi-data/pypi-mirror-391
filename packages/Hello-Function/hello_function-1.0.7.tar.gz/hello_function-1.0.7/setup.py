892400a0a4a461274fc92fb99e07c497658da21b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 13:15:40 +0000

    Add package Hello-Function/1.0.7/hello_function-1.0.7.tar.gz

---
+++ packages/Hello-Function/hello_function-1.0.7.tar.gz/hello_function-1.0.7/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Hello_Function",  
+    version="1.0.7",
+    author="Saqib Raheem(ws)",
+    author_email="wsssaqib99@gmail.com",
+    description="A simple library that says hello.",
+    packages=['hello_function'],  
+)
