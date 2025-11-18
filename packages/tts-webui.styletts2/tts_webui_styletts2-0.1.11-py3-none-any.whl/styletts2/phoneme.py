abddf6776fbae532f23d5ae22e8a5b8c304436b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 05:06:43 +0000

    Add package tts-webui.styletts2/0.1.11/tts_webui_styletts2-0.1.11-py3-none-any.whl

---
+++ packages/tts-webui.styletts2/tts_webui_styletts2-0.1.11-py3-none-any.whl/styletts2/phoneme.py
@@ -0,0 +1,34 @@
+from gruut import sentences
+from collections.abc import Iterable
+
+
+class PhonemeConverter:
+    def phonemize(self, text):
+        pass
+
+
+class GruutPhonemizer(PhonemeConverter):
+    def phonemize(self, text, lang='en-us'):
+        phonemized = []
+        for sent in sentences(text, lang=lang):
+            for word in sent:
+                if isinstance(word.phonemes, Iterable):
+                    phonemized.append(''.join(word.phonemes))
+                elif isinstance(word.phonemes, str):
+                    phonemized.append(word.phonemes)
+        phonemized_text = ' '.join(phonemized)
+        return phonemized_text
+
+
+# class YourPhonemizer(Phonemizer):
+#     def phonemize(self, text):
+#         ...
+
+
+class PhonemeConverterFactory:
+    @staticmethod
+    def load_phoneme_converter(name: str, **kwargs):
+        if name == 'gruut':
+            return GruutPhonemizer()
+        else:
+            raise ValueError("Invalid phoneme converter.")
