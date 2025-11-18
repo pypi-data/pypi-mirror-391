adedfa00020784ee11794541d7ae5d17d3cd60d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 03:26:51 +0000

    Add package winipedia-utils/0.7.15/winipedia_utils-0.7.15.tar.gz

---
+++ packages/winipedia-utils/winipedia_utils-0.7.15.tar.gz/winipedia_utils-0.7.15/winipedia_utils/dev/projects/create_root.py
@@ -0,0 +1,20 @@
+"""Utilities for working with Python projects."""
+
+from winipedia_utils.dev.configs.base.base import ConfigFile
+from winipedia_utils.dev.configs.pyproject import PyprojectConfigFile
+from winipedia_utils.dev.testing.convention import TESTS_PACKAGE_NAME
+from winipedia_utils.utils.modules.package import (
+    make_init_modules_for_package,
+)
+
+
+def create_project_root() -> None:
+    """Create the project root."""
+    src_package_name = PyprojectConfigFile.get_package_name()
+    ConfigFile.init_config_files()
+    make_init_modules_for_package(src_package_name)
+    make_init_modules_for_package(TESTS_PACKAGE_NAME)
+
+
+if __name__ == "__main__":
+    create_project_root()
