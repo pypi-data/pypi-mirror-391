34b467c0d3df7b1a2df1bce4f29f40c1c3bc7b5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 09:08:28 +0000

    Add package plain.sessions/0.38.0/plain_sessions-0.38.0-py3-none-any.whl

---
+++ packages/plain.sessions/plain_sessions-0.38.0-py3-none-any.whl/plain/sessions/models.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from plain import models
+from plain.models import types
+
+
+@models.register_model
+class Session(models.Model):
+    session_key: str = types.CharField(max_length=40)
+    session_data: dict = types.JSONField(default=dict, required=False)
+    created_at: datetime = types.DateTimeField(auto_now_add=True)
+    expires_at: datetime | None = types.DateTimeField(allow_null=True)
+
+    model_options = models.Options(
+        indexes=[
+            models.Index(fields=["expires_at"]),
+        ],
+        constraints=[
+            models.UniqueConstraint(fields=["session_key"], name="unique_session_key")
+        ],
+    )
+
+    def __str__(self) -> str:
+        return self.session_key
