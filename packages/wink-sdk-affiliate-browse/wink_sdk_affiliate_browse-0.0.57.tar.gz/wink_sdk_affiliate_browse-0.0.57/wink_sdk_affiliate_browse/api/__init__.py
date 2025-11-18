02944272322312ede3fb900b427915bf83c2fc99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 11:07:10 +0000

    Add package wink-sdk-affiliate-browse/0.0.57/wink_sdk_affiliate_browse-0.0.57.tar.gz

---
+++ packages/wink-sdk-affiliate-browse/wink_sdk_affiliate_browse-0.0.57.tar.gz/wink_sdk_affiliate_browse-0.0.57/wink_sdk_affiliate_browse/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from wink_sdk_affiliate_browse.api.browse_api import BrowseApi
+from wink_sdk_affiliate_browse.api.curated_list_api import CuratedListApi
+from wink_sdk_affiliate_browse.api.dynamic_list_api import DynamicListApi
+
