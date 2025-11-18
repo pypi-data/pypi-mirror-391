f57c6c7e2e575aeaaa733a56db08157cd93f8aa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 11:07:03 +0000

    Add package fiuai-sdk-python/0.5.0/fiuai_sdk_python-0.5.0.tar.gz

---
+++ packages/fiuai-sdk-python/fiuai_sdk_python-0.5.0.tar.gz/fiuai_sdk_python-0.5.0/src/fiuai_sdk_python/const.py
@@ -0,0 +1,9 @@
+# -- coding: utf-8 --
+# Project: fiuaiclient
+# Created Date: 2025 07 We
+# Author: liming
+# Email: lmlala@aliyun.com
+# Copyright (c) 2025 FiuAI
+
+
+V2_GET_LIST_URI = "/document"
