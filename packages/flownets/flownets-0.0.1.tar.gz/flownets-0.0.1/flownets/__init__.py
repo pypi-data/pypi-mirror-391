8bed6bc3f4c13e3c92ff1ea3cd4edd093cbd0d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 12:06:15 +0000

    Add package flownets/0.0.1/flownets-0.0.1.tar.gz

---
+++ packages/flownets/flownets-0.0.1.tar.gz/flownets-0.0.1/flownets/__init__.py
@@ -0,0 +1,10 @@
+from .model2D import HybridUNetCFM2D
+from .model3D import HybridUNetCFM3D
+
+from .version import __version__
+
+__all__ = [
+  'HybridUNetCFM2D',
+  'HybridUNetCFM3D',
+  '__version__'
+  ]
